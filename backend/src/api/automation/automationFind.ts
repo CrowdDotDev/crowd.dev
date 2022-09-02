bd0a157d4933f033017bfa8e5f41bcbf0ecc3c0d
Uros Marolt <uros@marolt.me>
2022-09-02 12:23:23 +0200

    List and Find automation endpoints

--- backend/src/api/automation/automationFind.ts
+++ backend/src/api/automation/automationFind.ts
@@ -1,18 +1,16 @@
 import PermissionChecker from '../../services/user/permissionChecker'
 import Permissions from '../../security/permissions'
 import AutomationService from '../../services/automationService'
-import track from '../../segment/track'
 import ApiResponseHandler from '../apiResponseHandler'
 
 /**
- * PUT /tenant/{tenantId}/automation/{automationId}
- * @summary Update an existing automation
+ * GET /tenant/{tenantId}/automation/{automationId}
+ * @summary Get an existing automation data
  * @tag Automations
  * @security Bearer
- * @description Updates an existing automation in the tenant.
+ * @description Get an existing automation data in the tenant.
  * @pathParam {string} tenantId - Your workspace/tenant ID
  * @pathParam {string} automationId - Automation ID that you want to update
- * @bodyContent {AutomationUpdateInput} application/json
  * @response 200 - Ok
  * @responseContent {Automation} 200.application/json
  * @responseExample {Automation} 200.application/json.Automation
@@ -21,10 +19,8 @@ import ApiResponseHandler from '../apiResponseHandler'
  */
 export default async (req, res) => {
   try {
-    new PermissionChecker(req).validateHas(Permissions.values.automationUpdate)
-    const payload = await new AutomationService(req).update(req.params.automationId, req.body)
-
-    track('Automation Updated', { ...payload }, { ...req })
+    new PermissionChecker(req).validateHas(Permissions.values.automationDestroy)
+    const payload = await new AutomationService(req).findById(req.params.automationId)
 
     await ApiResponseHandler.success(req, res, payload)
   } catch (error) {
---
+++ backend/src/api/automation/automationList.ts
@@ -0,0 +1,41 @@
+import PermissionChecker from '../../services/user/permissionChecker'
+import Permissions from '../../security/permissions'
+import AutomationService from '../../services/automationService'
+import ApiResponseHandler from '../apiResponseHandler'
+import {
+  AutomationCriteria,
+  AutomationState,
+  AutomationTrigger,
+  AutomationType,
+} from '../../types/automationTypes'
+
+/**
+ * GET /tenant/{tenantId}/automation
+ * @summary Get all automation data for tenant
+ * @tag Automations
+ * @security Bearer
+ * @description Get all existing automation data for tenant.
+ * @pathParam {string} tenantId - Your workspace/tenant ID
+ * @response 200 - Ok
+ * @responseContent {AutomationList} 200.application/json
+ * @responseExample {AutomationList} 200.application/json.AutomationList
+ * @response 401 - Unauthorized
+ * @response 429 - Too many requests
+ */
+export default async (req, res) => {
+  try {
+    new PermissionChecker(req).validateHas(Permissions.values.automationDestroy)
+
+    const criteria: AutomationCriteria = {
+      type: req.query.type ? (req.query.type as AutomationType) : undefined,
+      trigger: req.query.trigger ? (req.query.trigger as AutomationTrigger) : undefined,
+      state: req.query.state ? (req.query.state as AutomationState) : undefined,
+    }
+
+    const payload = await new AutomationService(req).list(criteria)
+
+    await ApiResponseHandler.success(req, res, payload)
+  } catch (error) {
+    await ApiResponseHandler.error(req, res, error)
+  }
+}
