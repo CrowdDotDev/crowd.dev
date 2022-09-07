3e2f26c1a6c677b9d126b8a4f01747e7cf5479a4
anil <epipav@gmail.com>
2022-09-07 12:00:34 +0300

    github worker now checks if repo is available before iterating

--- backend/src/serverless/integrations/workers/githubWorker.ts
+++ backend/src/serverless/integrations/workers/githubWorker.ts
@@ -3,6 +3,9 @@ import IntegrationRepository from '../../../database/repositories/integrationRep
 import GithubIterator from '../iterators/githubIterator'
 import { PlatformType } from '../../../utils/platforms'
 import { IntegrationsMessage } from '../types/messageTypes'
+import StargazersQuery from '../usecases/github/graphql/stargazers'
+import BaseIterator from '../iterators/baseIterator'
+import { IRepositoryOptions } from '../../../database/repositories/IRepositoryOptions'
 
 async function githubWorker(body: IntegrationsMessage) {
   try {
@@ -13,19 +16,60 @@ async function githubWorker(body: IntegrationsMessage) {
 
     const integration = await IntegrationRepository.findByPlatform(PlatformType.GITHUB, userContext)
 
-    const githubIterator = new GithubIterator(
-      tenant,
-      integration.settings.repos,
-      integration.token,
-      state,
-      onboarding,
-    )
+    if (integration.settings.repos.length > 0) {
+      const githubIterator = new GithubIterator(
+        tenant,
+        await getAvailableRepos(integration, userContext),
+        integration.token,
+        state,
+        onboarding,
+      )
 
-    return await githubIterator.iterate()
+      return await githubIterator.iterate()
+    }
+
+    return BaseIterator.success
   } catch (err) {
     console.log('Error in github worker, ', err)
     throw err
   }
+}
+
+/**
+ * Gets the repos available with given token.
+ * Repositories can become unavailable if it's deleted after
+ * making a github integration.
+ * If a repo is not available, it will be removed from integration.settings
+ * @param integration
+ * @param userContext
+ * @returns
+ */
+async function getAvailableRepos(integration, userContext: IRepositoryOptions) {
+  const availableRepos = []
+  let hasUnavailableRepos = false
+  for (const repo of integration.settings.repos) {
+    try {
+      // we don't need to get default 100 item per page, just 1 is enough to check if repo is available
+      const stargazersQuery = new StargazersQuery(repo, integration.token, 1)
+      await stargazersQuery.getSinglePage('')
+      availableRepos.push(repo)
+    } catch (e) {
+      console.log(`Repo ${repo.name} will not be parsed. It is not available with the github token`)
+      hasUnavailableRepos = true
+    }
+  }
+
+  integration.settings.repos = availableRepos
+
+  if (hasUnavailableRepos) {
+    await IntegrationRepository.update(
+      integration.id,
+      { settings: integration.settings },
+      userContext,
+    )
+  }
+
+  return availableRepos
 }
 
 export default githubWorker
