9acc6b57efec9976a8c5c477c52ae7650ae0d579
Joan Reyero <joan@crowd.dev>
2022-09-20 11:29:05 +0200

    Flask eagleEye

---
+++ premium/eagle-eye/server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from crowd.eagle_eye.search import search_main
+import logging
+from flask import request
+
+app = Flask(__name__)
+
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03d %(levelname)s [%(filename)s:%(lineno)s] %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+@app.route("/search", methods=['POST'])
+def search():
+    try:
+        body = request.get_json()
+        queries = body.get('queries', [])
+        ndays = body.get('nDays', 10)
+        exclude = body.get('filters', [])
+        return search_main(queries, ndays, exclude)
+    except Exception as e:
+        print(e)
+        return str(e)
