c6c6cb89546f9670ff8bf8c4f44100adc8c3c77b
Mário Balça <mario.balca@gmail.com>
2022-09-29 18:19:02 +0100

    Weekly emails new conversations filter should use first activity time (#64)

--- backend/src/cubejs/metrics/newConversations.ts
+++ backend/src/cubejs/metrics/newConversations.ts
@@ -4,7 +4,7 @@ import CubeMeasures from '../../services/cubejs/cubeMeasures'
 
 /**
  * Gets `new conversations` count for a given date range.
- * Conversations are new when conversation.createdAt is in between given date range.
+ * Conversations are new when conversation.firstActivityTime is in between given date range.
  * @param cjs cubejs service instance
  * @param startDate
  * @param endDate
@@ -18,7 +18,7 @@ export default async (cjs: CubeJsService, startDate: moment.Moment, endDate: mom
           measures: [CubeMeasures.CONVERSATION_COUNT],
           timeDimensions: [
             {
-              dimension: CubeDimensions.CONVERSATION_CREATED_AT,
+              dimension: CubeDimensions.CONVERSATION_FIRST_ACTIVITY_TIME,
               dateRange: [startDate.toISOString(), endDate.toISOString()],
             },
           ],
