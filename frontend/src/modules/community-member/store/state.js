25cf5ec46d4140ad0d13d0681f207f1286ae03b4
Mário Balça <mario.balca@gmail.com>
2022-09-29 16:42:51 +0100

    Start integrating with new API

--- frontend/src/modules/community-member/store/state.js
+++ frontend/src/modules/community-member/store/state.js
@@ -1,5 +1,28 @@
 export default {
   records: {},
+  views: {
+    all: {
+      id: 'all',
+      name: 'All members',
+      columns: [],
+      filters: [],
+      sorter: {}
+    },
+    active: {
+      id: 'active',
+      name: 'Most active',
+      columns: [],
+      filters: [],
+      sorter: {}
+    },
+    recent: {
+      id: 'recent',
+      name: 'Recent',
+      columns: [],
+      filters: [],
+      sorter: {}
+    }
+  },
   list: {
     ids: [],
     loading: false,
