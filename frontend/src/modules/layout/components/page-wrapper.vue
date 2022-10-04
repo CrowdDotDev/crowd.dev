b0c4aa9451d1f74ef2e98dccc2480e26c054e571
Mário Balça <mario.balca@gmail.com>
2022-10-04 16:07:32 +0100

    Fix page-wrapper.vue

--- frontend/src/modules/layout/components/page-wrapper.vue
+++ frontend/src/modules/layout/components/page-wrapper.vue
@@ -1,7 +1,7 @@
 <!-- Grid layout for medium and large screens -->
 <template>
   <div
-    class="gap-x-4 mx-6 grid md:grid-cols-6 lg:container lg:grid-cols-12"
+    class="gap-x-4 px-6 grid md:grid-cols-6 lg:container lg:grid-cols-12 lg:px-8"
   >
     <div class="col-start-1 col-end-13"><slot></slot></div>
   </div>
