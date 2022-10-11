b07fd5cb4d9b9d02de26be6de1c0044c4821e470
anil <epipav@gmail.com>
2022-10-11 11:57:18 +0300

    sentiment start 2

--- backend/src/database/initializers/entities/2022-10-11-api-v1-activity-sentiment.ts
+++ backend/src/database/initializers/entities/2022-10-11-api-v1-activity-sentiment.ts
@@ -1,21 +1,24 @@
+import { QueryTypes } from 'sequelize'
 import TenantService from '../../../services/tenantService'
+import SequelizeRepository from '../../repositories/sequelizeRepository'
+
 
 /**
  * Since requests to aws activity sentiment api creates a bottleneck,
  * We'll be generating the sentiment for this month's activities only.
  */
 export default async () => {
-    let tenants = (await TenantService._findAndCountAllForEveryUser({ filter: {} })).rows
-    tenants = [tenants[0]]
+  let tenants = (await TenantService._findAndCountAllForEveryUser({ filter: {} })).rows
+  tenants = [tenants[0]]
+  
+  const options = await SequelizeRepository.getDefaultIRepositoryOptions()
 
-    const activityCountQuery = `select count(*) from activities a`
+  const activityCountQuery = `select count(*) from activities a
+                              where a."timestamp"  between '2022-09-01' and now() `
 
-    const activityCount = (
-      await options.database.sequelize.query(activityCountQuery, {
-        type: QueryTypes.SELECT,
-      })
-    )[0].count
-
-
-
+  const activityCount = (
+    await options.database.sequelize.query(activityCountQuery, {
+      type: QueryTypes.SELECT,
+    })
+  )[0].count
 }
