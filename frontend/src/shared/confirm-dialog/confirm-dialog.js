6610bd4a6c50b73ac8883a0c66d856a518cb12b0
Joana Maia <joana@crowd.dev>
2022-10-14 06:22:19 +0100

    Add Edit member

--- frontend/src/shared/confirm-dialog/confirm-dialog.js
+++ frontend/src/shared/confirm-dialog/confirm-dialog.js
@@ -1,31 +1,27 @@
 import { ElMessageBox } from 'element-plus'
 import { h } from 'vue'
 
-// TODO: Make dialog dynamic on the icon
-export default (
-  {
-    title,
-    message,
-    showCancelButton,
-    showClose,
-    customClass,
-    cancelButtonText,
-    cancelButtonClass,
-    confirmButtonText,
-    confirmButtonClass
-  } = {
-    title: 'Unsaved changes',
-    message:
-      'Are you sure you want to leave this page? Unsaved changes will be lost',
-    showCancelButton: true,
-    showClose: false,
-    customClass: 'confirm-dialog',
-    cancelButtonText: 'Stay on this page',
-    cancelButtonClass: 'btn btn--md btn--bordered',
-    confirmButtonText: 'Discard',
-    confirmButtonClass: 'btn btn--md btn--primary'
+export default ({
+  type = 'warning',
+  title = 'Unsaved changes',
+  message = 'Are you sure you want to leave this page? Unsaved changes will be lost',
+  showCancelButton = true,
+  showClose = false,
+  customClass = 'confirm-dialog',
+  cancelButtonText = 'Stay on this page',
+  cancelButtonClass = 'btn btn--md btn--bordered',
+  confirmButtonText = 'Discard',
+  confirmButtonClass = 'btn btn--md btn--primary'
+}) => {
+  let iconClass = 'ri-error-warning-line'
+  let iconColorClass = 'text-yellow-600'
+  let iconBgColorClass = 'bg-yellow-100'
+
+  if (type === 'error') {
+    iconColorClass = 'text-red-600'
+    iconBgColorClass = 'bg-red-100'
   }
-) => {
+
   const MessageWithWarnignIcon = h(
     'div', // type
     {
@@ -35,15 +31,13 @@ export default (
       h(
         'span', // type
         {
-          class:
-            'rounded-full bg-yellow-100 w-10 h-10 flex items-center justify-center absolute custom-icon'
+          class: `rounded-full ${iconBgColorClass} w-10 h-10 flex items-center justify-center absolute custom-icon`
         }, // props
         [
           h(
             'i', // type
             {
-              class:
-                'ri-error-warning-line text-lg text-yellow-600 leading-none'
+              class: `${iconClass} text-lg ${iconColorClass} leading-none`
             }, // props
             []
           )
