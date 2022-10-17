2a3907f856f252a2ab78cdfee607892eb15ae452
anil <epipav@gmail.com>
2022-10-17 17:08:10 +0300

    formatting

--- backend/src/services/sampleDataService.ts
+++ backend/src/services/sampleDataService.ts
@@ -84,10 +84,10 @@ export default class SampleDataService {
 
     const tenant = await tenantService.findById(this.options.currentTenant.id)
 
-    if (tenant.hasSampleData){
+    if (tenant.hasSampleData) {
       const memberService = new MemberService(this.options)
       const memberAttributeSettingsService = new MemberAttributeSettingsService(this.options)
-  
+
       const memberIds = await (
         await memberService.findAndCountAll({
           advancedFilter: { sample: true },
@@ -96,10 +96,10 @@ export default class SampleDataService {
         acc.push(item.id)
         return acc
       }, [])
-  
+
       // deleting sample members should cascade to their activities as well
       await memberService.destroyBulk(memberIds)
-  
+
       // delete attribute settings for attributes.sample.crowd as well
       const sampleAttributeSettings = (
         await memberAttributeSettingsService.findAndCountAll({
@@ -107,15 +107,12 @@ export default class SampleDataService {
         })
       ).rows[0]
       await memberAttributeSettingsService.destroyAll([sampleAttributeSettings.id])
-  
+
       await tenantService.update(this.options.currentTenant.id, {
         hasSampleData: false,
       })
-  
     }
 
     console.log(`Sample data for tenant ${this.options.currentTenant.id} deleted succesfully.`)
-
-    
   }
 }
