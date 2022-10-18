93087850c525bd02985585106f942fca60968381
Joana Maia <joana@crowd.dev>
2022-10-18 11:25:15 +0100

    Fix url on attributes and avatar initial letters

--- frontend/src/shared/avatar/avatar.vue
+++ frontend/src/shared/avatar/avatar.vue
@@ -77,7 +77,11 @@ export default {
       return `avatar--${this.size}`
     },
     computedInitials() {
-      const names = this.entity.displayName.split(' ')
+      const names = this.entity.displayName
+        .replace(/\s+/g, ' ')
+        .trim()
+        .split(' ')
+
       return names.length > 1
         ? names[0][0] + names[1][0]
         : names[0][0]
