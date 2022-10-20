4e755f379df73fda150ef54ce2d445a33a09ebf3
Gašper Grom <gasper.grom@gmail.com>
2022-10-20 12:40:00 +0200

    [Frontend] Dashboard (#77)
    Co-authored-by: Uroš Marolt <uros@marolt.me>

---
+++ frontend/src/modules/dashboard/components/shared/dashboard-tab.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="text-xs leading-5 border-b-2 transition p-2.5 text-center cursor-pointer"
+    :class="[active ? 'font-medium border-brand-500' : '']"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppDashboardTab',
+  props: {
+    active: {
+      required: false,
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
