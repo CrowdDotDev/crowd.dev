c56abcfbc9dc65efa4353b1a978a133d60f260e8
Mário Balça <mario.balca@gmail.com>
2022-10-25 23:24:53 +0100

    Add error pages

--- frontend/src/modules/layout/layout-routes.js
+++ frontend/src/modules/layout/layout-routes.js
@@ -1,21 +1,37 @@
-import Error403Page from '@/modules/layout/components/error-403-page.vue'
-import Error404Page from '@/modules/layout/components/error-404-page.vue'
-import Error500Page from '@/modules/layout/components/error-500-page.vue'
+import ErrorPage from '@/modules/layout/components/error-page.vue'
 
 export default [
   {
     name: 'error403',
     path: '/403',
-    component: Error403Page
+    component: ErrorPage,
+    props: {
+      code: 403,
+      title: 'Restricted access',
+      subtitle:
+        'Sorry, you don’t have permissions to access this page.'
+    }
   },
   {
     name: 'error404',
     path: '/404',
-    component: Error404Page
+    component: ErrorPage,
+    props: {
+      code: 404,
+      title: 'Page not found',
+      subtitle:
+        'Sorry, the page you are looking for doesn’t exist or was removed.'
+    }
   },
   {
     name: 'error500',
     path: '/500',
-    component: Error500Page
+    component: ErrorPage,
+    props: {
+      code: 500,
+      title: 'Oops, something went wrong',
+      subtitle:
+        'Please try to reload the page. If the problem remains, reach out to us.'
+    }
   }
 ]
