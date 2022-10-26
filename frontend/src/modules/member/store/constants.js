e17d844cc1f18d4e5878d770d8658a06c62b80b7
Joan Reyero <joan@crowd.dev>
2022-10-26 17:59:55 +0100

    Added views and fixed team member (#120)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/modules/member/store/constants.js
+++ frontend/src/modules/member/store/constants.js
@@ -65,6 +65,86 @@ export const INITIAL_VIEW_ACTIVE_FILTER = {
     }
   }
 }
+
+export const INITIAL_VIEW_SLIPPING_AWAY_FILTER = {
+  operator: 'and',
+  attributes: {
+    score: {
+      name: 'score',
+      label: 'Engagement Level',
+      custom: false,
+      props: {
+        options: [
+          {
+            value: [0, 1],
+            label: 'Silent'
+          },
+          {
+            value: [2, 3],
+            label: 'Quiet'
+          },
+          {
+            value: [4, 6],
+            label: 'Engaged'
+          },
+          {
+            value: [7, 8],
+            label: 'Fan'
+          },
+          {
+            value: [9, 10],
+            label: 'Ultra'
+          }
+        ],
+        multiple: true
+      },
+      defaultValue: [
+        {
+          value: [7, 8],
+          label: 'Fan',
+          selected: false
+        },
+        {
+          value: [9, 10],
+          label: 'Ultra',
+          selected: false
+        }
+      ],
+      value: [
+        {
+          value: [7, 8],
+          label: 'Fan',
+          selected: false
+        },
+        {
+          value: [9, 10],
+          label: 'Ultra',
+          selected: false
+        }
+      ],
+      defaultOperator: null,
+      operator: null,
+      type: 'select-multi',
+      expanded: false
+    },
+    lastActive: {
+      name: 'lastActive',
+      label: 'Last activity',
+      custom: false,
+      props: {},
+      defaultValue: moment()
+        .subtract(14, 'days')
+        .format('YYYY-MM-DD'),
+      value: moment()
+        .subtract(14, 'days')
+        .format('YYYY-MM-DD'),
+      defaultOperator: 'lt',
+      operator: 'lt',
+      type: 'date',
+      expanded: false
+    }
+  }
+}
 export const INITIAL_VIEW_RECENT_FILTER = {
   operator: 'and',
   attributes: {
@@ -82,6 +162,40 @@ export const INITIAL_VIEW_RECENT_FILTER = {
       defaultOperator: 'gt',
       operator: 'gt',
       type: 'date',
+      expanded: false
+    },
+    lastActive: {
+      name: 'lastActive',
+      label: 'Last activity',
+      custom: false,
+      props: {},
+      defaultValue: moment()
+        .subtract(30, 'days')
+        .format('YYYY-MM-DD'),
+      value: moment()
+        .subtract(30, 'days')
+        .format('YYYY-MM-DD'),
+      defaultOperator: 'gt',
+      operator: 'gt',
+      type: 'date',
+      expanded: false
+    }
+  }
+}
+
+export const INITIAL_VIEW_TEAM_MEMBERS_FILTER = {
+  operator: 'and',
+  attributes: {
+    isTeamMember: {
+      name: 'isTeamMember',
+      label: 'Team member',
+      custom: false,
+      props: {},
+      defaultValue: true,
+      value: true,
+      defaultOperator: 'eq',
+      operator: 'eq',
+      type: 'boolean',
       expanded: false
     }
   }
