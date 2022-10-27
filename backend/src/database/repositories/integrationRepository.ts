853ff7add47b7536660f32a30a9d51964e5bbfbb
Joan Reyero <joan@crowd.dev>
2022-10-27 19:44:14 +0100

    Hard-delete integrations

--- backend/src/database/repositories/integrationRepository.ts
+++ backend/src/database/repositories/integrationRepository.ts
@@ -111,6 +111,7 @@ class IntegrationRepository {
 
     await record.destroy({
       transaction,
+      force: true,
     })
 
     await this._createAuditLog(AuditLogRepository.DELETE, record, record, options)
