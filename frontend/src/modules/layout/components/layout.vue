e073f1845d69923995864ce3861d535d71c3d006
Mário Balça <mario.balca@gmail.com>
2022-10-31 17:08:54 +0000

    Add alert when tenant was created in <= 10 minutes

--- frontend/src/modules/layout/components/layout.vue
+++ frontend/src/modules/layout/components/layout.vue
@@ -3,41 +3,58 @@
     <app-menu></app-menu>
     <el-container :style="elMainStyle">
       <el-main class="relative">
-        <banner
-          v-if="currentTenant.hasSampleData"
-          variant="alert"
-        >
-          <div
-            class="flex items-center justify-center grow text-sm"
+        <div :class="computedBannerWrapperClass">
+          <banner
+            v-if="shouldShowSampleDataAlert"
+            variant="alert"
           >
-            This workspace is using sample data, before
-            adding real data please
-            <el-button
-              class="btn btn--sm btn--primary ml-4"
-              :loading="loading"
-              @click="handleDeleteSampleDataClick"
+            <div
+              class="flex items-center justify-center grow text-sm"
             >
-              Delete Sample Data
-            </el-button>
-          </div>
-        </banner>
-        <banner
-          v-if="shouldShowIntegrationsAlert"
-          variant="alert"
-        >
-          <div
-            class="flex items-center justify-center grow text-sm"
+              This workspace is using sample data, before
+              adding real data please
+              <el-button
+                class="btn btn--sm btn--primary ml-4"
+                :loading="loading"
+                @click="handleDeleteSampleDataClick"
+              >
+                Delete Sample Data
+              </el-button>
+            </div>
+          </banner>
+          <banner
+            v-if="shouldShowIntegrationsAlert"
+            variant="alert"
           >
-            Currently you have integrations with
-            connectivity issues
-            <router-link
-              :to="{ name: 'integration' }"
-              class="btn btn--sm btn--primary ml-4"
+            <div
+              class="flex items-center justify-center grow text-sm"
             >
-              Go to Integrations
-            </router-link>
-          </div>
-        </banner>
+              Currently you have integrations with
+              connectivity issues
+              <router-link
+                :to="{ name: 'integration' }"
+                class="btn btn--sm btn--primary ml-4"
+              >
+                Go to Integrations
+              </router-link>
+            </div>
+          </banner>
+          <banner
+            v-if="shouldShowTenantCreatingAlert"
+            variant="info"
+          >
+            <div
+              class="flex items-center justify-center grow text-sm"
+            >
+              <div
+                v-loading="true"
+                class="w-4 h-4 mr-2"
+              ></div>
+              Finishing your workspace setup, data might
+              take a few minutes until is completely loaded.
+            </div>
+          </banner>
+        </div>
         <router-view></router-view>
       </el-main>
     </el-container>
@@ -51,6 +68,7 @@ import Banner from '@/shared/banner/banner.vue'
 import identify from '@/shared/segment/identify'
 import { i18n } from '@/i18n'
 import ConfirmDialog from '@/shared/confirm-dialog/confirm-dialog.js'
+import moment from 'moment'
 
 export default {
   name: 'AppLayout',
@@ -80,6 +98,25 @@ export default {
         this.integrationsWithErrors.length > 0 &&
         this.$route.name !== 'integration'
       )
+    },
+    shouldShowSampleDataAlert() {
+      return this.currentTenant.hasSampleData
+    },
+    shouldShowTenantCreatingAlert() {
+      return (
+        moment().diff(
+          moment(this.currentTenant.createdAt),
+          'minutes'
+        ) <= 10
+      )
+    },
+    computedBannerWrapperClass() {
+      return {
+        'pt-16':
+          this.shouldShowSampleDataAlert ||
+          this.shouldShowIntegrationsAlert ||
+          this.shouldShowTenantCreatingAlert
+      }
     },
     elMainStyle() {
       if (this.isMobile && !this.collapsed) {
