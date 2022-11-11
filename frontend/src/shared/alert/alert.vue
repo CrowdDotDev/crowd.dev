0abefa87689dadc857a56aa1650ff0ade765e3d7
Mário Balça <mario.balca@gmail.com>
2022-11-11 12:24:08 +0000

    Tweak alerts

--- frontend/src/shared/alert/alert.vue
+++ frontend/src/shared/alert/alert.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="rounded-lg mt-4 p-4 border"
+    class="rounded-lg mt-4 px-4 p-2.5 border relative"
     :class="alertClass"
   >
     <div class="flex items-start">
       <i
-        class="mr-4 ri-xl flex items-center pt-1"
+        class="mr-2.5 flex items-center h-3.5 pt-1"
         :class="iconClass"
       ></i>
-      <div class="text-sm">
-        <div class="font-semibold mb-1">
-          <slot name="title"> Action Required </slot>
+      <div class="text-xs flex-grow">
+        <div class="font-semibold inline-flex pr-1">
+          <slot name="title"> Action Required:</slot>
         </div>
         <slot name="body"> This is the body </slot>
       </div>
@@ -35,15 +35,15 @@ export default {
     alertClass() {
       switch (this.type) {
         case 'warning':
-          return 'bg-yellow-50 border-yellow-500'
+          return 'bg-yellow-50 border-yellow-100'
         default:
-          return 'border-blue-500 bg-blue-50'
+          return 'bg-blue-50 border-blue-100'
       }
     },
     iconClass() {
       switch (this.type) {
         case 'warning':
-          return 'text-yellow-500 ri-error-warning-fill'
+          return 'text-yellow-500 ri-alert-fill'
         default:
           return 'text-blue-500 ri-information-fill'
       }
