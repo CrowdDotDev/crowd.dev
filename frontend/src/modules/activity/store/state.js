c27204e96779e20911fe7b65a97dd6d202d01b10
Joan Reyero <joan@crowd.dev>
2022-11-15 12:19:46 +0100

    Updated filter

--- frontend/src/modules/activity/store/state.js
+++ frontend/src/modules/activity/store/state.js
@@ -1,4 +1,7 @@
-import { INITIAL_PAGE_SIZE, TRENDING_CONVERSATIONS_FILTER } from './constants'
+import {
+  INITIAL_PAGE_SIZE,
+  TRENDING_CONVERSATIONS_FILTER
+} from './constants'
 
 export default () => {
   return {
@@ -37,14 +40,10 @@ export default () => {
         id: 'conversations',
         type: 'conversations',
         label: 'Conversations',
-        initialFilter: {
-          operator: 'and',
-          attributes: TRENDING_CONVERSATIONS_FILTER
-        },
-        filter: {
-          operator: 'and',
-          attributes: TRENDING_CONVERSATIONS_FILTER
-        },
+        initialFilter: TRENDING_CONVERSATIONS_FILTER,
+        filter: JSON.parse(
+          JSON.stringify(TRENDING_CONVERSATIONS_FILTER)
+        ),
         pagination: {
           currentPage: 1,
           pageSize: INITIAL_PAGE_SIZE
