47e31ea324400751d1da113e133e96d2c9bb6f1b
anilb <epipav@gmail.com>
2022-11-21 19:10:43 +0100

    redis middleware removed ping check

--- backend/src/middlewares/redisMiddleware.ts
+++ backend/src/middlewares/redisMiddleware.ts
@@ -1,19 +1,8 @@
-import { getServiceLogger } from '../utils/logging'
 import { RedisClient } from '../utils/redis'
-
-const log = getServiceLogger()
 
 export function redisMiddleware(redis: RedisClient) {
   return async (req, res, next) => {
-    try {
-      if ((await redis.ping()) !== 'PONG') {
-        throw new Error(`Can't ping redis.`)
-      }
-      req.redis = redis
-    } catch (error) {
-      log.error(error, 'Error connecting to redis!')
-    } finally {
-      next()
-    }
+    req.redis = redis
+    next()
   }
 }
