746e18ea6a96ecdd7582cf27e4e666d8e5fb980b
Joana Maia <joana@crowd.dev>
2022-11-21 13:00:34 +0100

    New filters for organizations module

--- frontend/src/modules/organization/organization-model.js
+++ frontend/src/modules/organization/organization-model.js
@@ -3,6 +3,8 @@ import IdField from '@/shared/fields/id-field'
 import { GenericModel } from '@/shared/model/generic-model'
 import DateTimeField from '@/shared/fields/date-time-field'
 import StringField from '@/shared/fields/string-field'
+import OrganizationMemberCountField from '@/modules/organization/organization-member-count-field'
+import OrganizationEmployeesField from '@/modules/organization/organization-employees-field'
 
 function label(name) {
   return i18n(`entities.member.fields.${name}`)
@@ -20,6 +22,18 @@ const fields = {
   updatedAt: new DateTimeField(
     'updatedAt',
     label('updatedAt')
+  ),
+  members: new OrganizationMemberCountField(
+    'memberCount',
+    '# of members',
+    { filterable: true }
+  ),
+  employees: new OrganizationEmployeesField(
+    'employees',
+    '# of employees',
+    {
+      filterable: true
+    }
   )
 }
 
