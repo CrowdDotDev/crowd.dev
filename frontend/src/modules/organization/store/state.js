129a95ab3ad45656671d8d89f7543fc0f552b862
Mário Balça <mario.balca@gmail.com>
2022-11-21 17:39:35 +0000

    Fix organization/state.js

--- frontend/src/modules/organization/store/state.js
+++ frontend/src/modules/organization/store/state.js
@@ -3,16 +3,39 @@ import { INITIAL_PAGE_SIZE } from './constants'
 export default () => {
   return {
     records: {},
+    views: {
+      all: {
+        id: 'all',
+        label: 'All organizations',
+        columns: [],
+        filter: {
+          operator: 'and',
+          attributes: {}
+        },
+        initialFilter: {
+          operator: 'and',
+          attributes: {}
+        },
+        pagination: {
+          currentPage: 1,
+          pageSize: INITIAL_PAGE_SIZE
+        },
+        initialSorter: {
+          prop: 'name',
+          order: 'descending'
+        },
+        sorter: {
+          prop: 'name',
+          order: 'descending'
+        },
+        active: true
+      }
+    },
     list: {
       ids: [],
-      loading: false
+      loading: false,
+      table: false
     },
-    count: 0,
-    filter: {},
-    pagination: {
-      currentPage: 1,
-      pageSize: INITIAL_PAGE_SIZE
-    },
-    sorter: {}
+    count: 0
   }
 }
