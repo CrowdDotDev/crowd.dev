1fd3d35bea4ac77e129bca2ef1e5e67333ba1865
Mário Balça <mario.balca@gmail.com>
2022-11-23 20:20:29 +0000

    Add export related files

--- frontend/src/modules/organization/store/actions.js
+++ frontend/src/modules/organization/store/actions.js
@@ -1,6 +1,45 @@
 import { OrganizationService } from '@/modules/organization/organization-service'
 import sharedActions from '@/shared/store/actions'
+import organizationListExporterFields from '@/modules/organization/organization-list-exporter-fields'
+import { MemberService } from '@/modules/member/member-service'
+import Exporter from '@/shared/exporter/exporter'
+import Errors from '@/shared/error/errors'
 
 export default {
-  ...sharedActions('organization', OrganizationService)
+  ...sharedActions('organization', OrganizationService),
+
+  async doExport({ commit, state, getters }) {
+    try {
+      if (
+        !organizationListExporterFields ||
+        !organizationListExporterFields.length
+      ) {
+        throw new Error(
+          'organizationListExporterFields is required'
+        )
+      }
+
+      commit('EXPORT_STARTED')
+
+      const filter = state.filter
+
+      const response = await MemberService.list(
+        filter,
+        getters.orderBy,
+        null,
+        null
+      )
+
+      new Exporter(
+        organizationListExporterFields,
+        'member'
+      ).transformAndExportAsExcelFile(response.rows)
+
+      commit('EXPORT_SUCCESS')
+    } catch (error) {
+      Errors.handle(error)
+
+      commit('EXPORT_ERROR')
+    }
+  }
 }
