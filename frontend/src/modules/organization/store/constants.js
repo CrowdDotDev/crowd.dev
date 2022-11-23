4b9b710323e5c5fd71c8f9fa1841ca5e9cf09c9c
Mário Balça <mario.balca@gmail.com>
2022-11-23 13:25:28 +0000

    Add default views

--- frontend/src/modules/organization/store/constants.js
+++ frontend/src/modules/organization/store/constants.js
@@ -1,1 +1,37 @@
+import { formatDate } from '@/utils/date'
+
 export const INITIAL_PAGE_SIZE = 20
+
+export const INITIAL_VIEW_NEW_AND_ACTIVE_FILTER = {
+  operator: 'and',
+  attributes: {
+    activityCount: {
+      name: 'activityCount',
+      label: '# of activities',
+      custom: false,
+      props: {},
+      defaultValue: 1000,
+      value: 1000,
+      defaultOperator: 'gt',
+      operator: 'gt',
+      type: 'number',
+      expanded: false
+    },
+    activeSince: {
+      name: 'activeSince',
+      label: 'Active since',
+      custom: false,
+      props: {},
+      defaultValue: formatDate({
+        subtractDays: 30
+      }),
+      value: formatDate({
+        subtractDays: 30
+      }),
+      defaultOperator: 'lt',
+      operator: 'lt',
+      type: 'date',
+      expanded: false
+    }
+  }
+}
--- frontend/src/modules/organization/store/state.js
+++ frontend/src/modules/organization/store/state.js
@@ -1,4 +1,5 @@
 import { INITIAL_PAGE_SIZE } from './constants'
+import { INITIAL_VIEW_NEW_AND_ACTIVE_FILTER } from './constants'
 
 export default () => {
   return {
@@ -29,6 +30,101 @@ export default () => {
           order: 'descending'
         },
         active: true
+      },
+      'new-and-active': {
+        id: 'new-and-active',
+        label: 'New and active',
+        initialFilter: INITIAL_VIEW_NEW_AND_ACTIVE_FILTER,
+        filter: JSON.parse(
+          JSON.stringify(INITIAL_VIEW_NEW_AND_ACTIVE_FILTER)
+        ),
+        pagination: {
+          currentPage: 1,
+          pageSize: INITIAL_PAGE_SIZE
+        },
+        initialSorter: {
+          prop: 'firstActivity',
+          order: 'descending'
+        },
+        sorter: {
+          prop: 'firstActivity',
+          order: 'descending'
+        },
+        active: false
+      },
+      'most-members': {
+        id: 'most-members',
+        label: 'Most members',
+        filter: {
+          operator: 'and',
+          attributes: {}
+        },
+        initialFilter: {
+          operator: 'and',
+          attributes: {}
+        },
+        pagination: {
+          currentPage: 1,
+          pageSize: INITIAL_PAGE_SIZE
+        },
+        initialSorter: {
+          prop: 'memberCount',
+          order: 'descending'
+        },
+        sorter: {
+          prop: 'memberCount',
+          order: 'descending'
+        }
+      },
+      'enterprise-size': {
+        id: 'enterprise-size',
+        label: 'Enterprise size',
+        filter: {
+          operator: 'and',
+          attributes: {
+            employeesRange: {
+              name: 'activityCount',
+              label: '# of activities',
+              custom: false,
+              props: {},
+              defaultValue: [1001, 5000],
+              value: [1001, 5000],
+              defaultOperator: 'between',
+              operator: 'between',
+              type: 'number',
+              expanded: false
+            }
+          }
+        },
+        initialFilter: {
+          operator: 'and',
+          attributes: {
+            employeesRange: {
+              name: 'activityCount',
+              label: '# of activities',
+              custom: false,
+              props: {},
+              defaultValue: [1001, 5000],
+              value: [1001, 5000],
+              defaultOperator: 'between',
+              operator: 'between',
+              type: 'number',
+              expanded: false
+            }
+          }
+        },
+        pagination: {
+          currentPage: 1,
+          pageSize: INITIAL_PAGE_SIZE
+        },
+        initialSorter: {
+          prop: 'memberCount',
+          order: 'descending'
+        },
+        sorter: {
+          prop: 'memberCount',
+          order: 'descending'
+        }
       }
     },
     list: {
