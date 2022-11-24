20971d1de1711754154f1206ef5d3390def3d575
Joana Maia <joana@crowd.dev>
2022-11-24 23:51:21 +0100

    Add export feedback messages to organizations

--- frontend/src/modules/organization/store/actions.js
+++ frontend/src/modules/organization/store/actions.js
@@ -3,6 +3,7 @@ import sharedActions from '@/shared/store/actions'
 import organizationListExporterFields from '@/modules/organization/organization-list-exporter-fields'
 import Exporter from '@/shared/exporter/exporter'
 import Errors from '@/shared/error/errors'
+import Message from '@/shared/message/message'
 
 export default {
   ...sharedActions('organization', OrganizationService),
@@ -33,10 +34,16 @@ export default {
       ).transformAndExportAsExcelFile(response.rows)
 
       commit('EXPORT_SUCCESS')
+
+      Message.success('Organizations exported successfully')
     } catch (error) {
       Errors.handle(error)
 
       commit('EXPORT_ERROR')
+
+      Message.error(
+        'There was an error exporting organizations'
+      )
     }
   }
 }
