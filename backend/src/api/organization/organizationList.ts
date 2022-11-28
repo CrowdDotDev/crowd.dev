4f88ae28d53f0a87e5c9bb18c4baebe82b1e48cf
Joana Maia <joana@crowd.dev>
2022-11-28 16:20:25 +0000

    Add event tracking to organizations

--- backend/src/api/organization/organizationList.ts
+++ backend/src/api/organization/organizationList.ts
@@ -1,4 +1,5 @@
 import Permissions from '../../security/permissions'
+import track from '../../segment/track'
 import OrganizationService from '../../services/organizationService'
 import PermissionChecker from '../../services/user/permissionChecker'
 
@@ -6,6 +7,10 @@ export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.organizationRead)
 
   const payload = await new OrganizationService(req).findAndCountAll(req.query)
+
+  if (req.query.filter) {
+    track('Organizations Filtered', { filter: req.query.filter }, { ...req })
+  }
 
   await req.responseHandler.success(req, res, payload)
 }
