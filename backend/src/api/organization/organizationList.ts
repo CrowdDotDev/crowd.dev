af13b0c2bb9d9c4b6f4ae3bfd1dc751213ed5f53
Joana Maia <joana@crowd.dev>
2022-11-29 11:50:46 +0000

    Refactor events in organizations and members

--- backend/src/api/organization/organizationList.ts
+++ backend/src/api/organization/organizationList.ts
@@ -1,5 +1,4 @@
 import Permissions from '../../security/permissions'
-import track from '../../segment/track'
 import OrganizationService from '../../services/organizationService'
 import PermissionChecker from '../../services/user/permissionChecker'
 
@@ -7,10 +6,6 @@ export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.organizationRead)
 
   const payload = await new OrganizationService(req).findAndCountAll(req.query)
-
-  if (req.query.filter) {
-    track('Organizations Filtered', { filter: req.query.filter }, { ...req })
-  }
 
   await req.responseHandler.success(req, res, payload)
 }
