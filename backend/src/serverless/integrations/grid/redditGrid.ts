743492bc1278854381eb1d87719d91cd5b23b07e
Joan Reyero <joan@crowd.dev>
2022-12-05 16:51:15 +0100

    NA

---
+++ backend/src/serverless/integrations/grid/redditGrid.ts
@@ -0,0 +1,13 @@
+import { gridEntry } from './grid'
+
+export class RedditGrid {
+  static post: gridEntry = {
+    score: 10,
+    isKeyAction: true,
+  }
+
+  static comment: gridEntry = {
+    score: 6,
+    isKeyAction: true,
+  }
+}
