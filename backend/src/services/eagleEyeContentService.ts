2a45b0619ca95e78575872396ca733e3713185a8
Joan Reyero <joan@crowd.dev>
2022-12-06 14:35:32 +0100

    Format

--- backend/src/services/eagleEyeContentService.ts
+++ backend/src/services/eagleEyeContentService.ts
@@ -101,14 +101,14 @@ export default class EagleEyeContentService extends LoggingBase {
 
     if (API_CONFIG.premiumApiUrl) {
       const response = await request
-          .post(`${API_CONFIG.premiumApiUrl}/search`)
-          .send({ queries: keywords, nDays, filters })
+        .post(`${API_CONFIG.premiumApiUrl}/search`)
+        .send({ queries: keywords, nDays, filters })
       try {
         const fromEagleEye: EagleEyeSearchOutput = JSON.parse(response.text)
         await this.bulkUpsert(fromEagleEye)
         return fromEagleEye
       } catch (error) {
-        this.log.error({error: response.text}, 'error when calling eagle eye server!')
+        this.log.error({ error: response.text }, 'error when calling eagle eye server!')
         throw new Error400('en', 'errors.wrongEagleEyeSearch.message')
       }
     }
@@ -120,12 +120,12 @@ export default class EagleEyeContentService extends LoggingBase {
 
     if (API_CONFIG.premiumApiUrl) {
       const response = await request
-          .post(`${API_CONFIG.premiumApiUrl}/keyword-match`)
-          .send({ exactKeywords: keywords, nDays, platform })
+        .post(`${API_CONFIG.premiumApiUrl}/keyword-match`)
+        .send({ exactKeywords: keywords, nDays, platform })
       try {
         return JSON.parse(response.text)
       } catch (error) {
-        this.log.error({error: response.error}, 'error while calling eagle eye server!')
+        this.log.error({ error: response.error }, 'error while calling eagle eye server!')
         throw new Error400('en', 'errors.wrongEagleEyeSearch.message')
       }
     } else {
