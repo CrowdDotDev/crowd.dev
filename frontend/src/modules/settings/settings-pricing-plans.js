35d241f944826d44b48549127514df36a841eb07
joanagmaia <joanamaiaportugal@gmail.com>
2022-12-14 16:40:26 +0000

    Plan page (#350)

---
+++ frontend/src/modules/settings/settings-pricing-plans.js
@@ -0,0 +1,84 @@
+import Plans from '@/security/plans'
+
+const crowdHostedPlans = Plans.values
+const communityPlans = Plans.communityValues
+
+export const plans = {
+  crowdHosted: [
+    {
+      key: crowdHostedPlans.essential,
+      title: 'Essential',
+      description: 'Understand & manage your community',
+      price: 'Free',
+      features: [
+        'Unlimited seats',
+        'Unlimited community members & activities',
+        'Community management',
+        'Community intelligence',
+        'Full API access & native integrations with GitHub, Discord, Slack, Twitter, DEV, and Hacker News',
+        '2 active workflows & CSV exports per month',
+        'Community & email support'
+      ]
+    },
+    {
+      key: crowdHostedPlans.growth,
+      title: 'Growth',
+      description:
+        'Unlock community-led growth for your organization',
+      price: '$150/month',
+      featuresNote: 'Everything in Essential, plus:',
+      features: [
+        'Community growth',
+        'Organization-level insights',
+        '10 active workflows & CSV exports per month',
+        'Slack connect support'
+      ],
+      sale: '🐦 Early bird offer: 25% off (billed annually)'
+    },
+    {
+      key: crowdHostedPlans.enterprise,
+      title: 'Custom',
+      description: 'Tailored to your exact requirements',
+      price: 'from $850/month',
+      featuresNote: 'Everything in Growth, plus:',
+      features: [
+        'Powerful enrichment for members & organizations',
+        'Activity categorization & topic analysis',
+        'Integrations with CRMs & CDPs',
+        'Unlimited active workflows & CSV exports per month',
+        'Dedicated community expert'
+      ]
+    }
+  ],
+  community: [
+    {
+      key: communityPlans.community,
+      title: 'Community',
+      description:
+        "Keep ownership of your data and host crowd.dev's community version for free on your own premises",
+      price: 'Free',
+      features: [
+        'Unlimited seats',
+        'Unlimited community members & activities',
+        'Community management',
+        'Community intelligence',
+        'Integrations with GitHub, Discord, Slack, Twitter, DEV, Hacker News',
+        'Community support'
+      ]
+    },
+    {
+      key: communityPlans.custom,
+      title: 'Custom',
+      description:
+        "Get access to crowd.dev's premium features and support, and host the platform on your own premises",
+      price: 'On request',
+      featuresNote: 'Everything in Community, plus:',
+      features: [
+        'Community growth',
+        'Organization-level insights',
+        'Custom integrations',
+        'Enterprise-grade support'
+      ]
+    }
+  ]
+}
