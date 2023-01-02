64b4a636ede0157b75f53a8b2820996a3b6779fa
Joana Maia <joana@crowd.dev>
2023-01-02 12:03:11 +0000

    feature/widget-title

---
+++ frontend/src/modules/widget/components/v2/shared/widget-title.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex gap-2">
+    <div
+      class="font-medium text-gray-900"
+      :class="`${textSize}`"
+    >
+      {{ title }}
+    </div>
+    <el-tooltip
+      v-if="description"
+      popper-class="w-48"
+      effect="dark"
+      :content="description"
+      placement="top"
+    >
+      <i class="ri-question-line text-base text-gray-400" />
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppWidgetTitle'
+}
+</script>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: null
+  },
+  textSize: {
+    type: String,
+    default: 'text-base'
+  }
+})
+</script>
