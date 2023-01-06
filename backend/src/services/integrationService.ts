bc9d92ab0c4e132c80cae182b033b8c02ed35028
Joan Reyero <joan@crowd.dev>
2023-01-06 10:19:59 +0100

    Fixed empty state

--- backend/src/services/integrationService.ts
+++ backend/src/services/integrationService.ts
@@ -344,8 +344,6 @@ export default class IntegrationService {
       ),
     )
 
-    console.log('DONE')
-
     return integration
   }
 
@@ -445,8 +443,9 @@ export default class IntegrationService {
    */
   async twitterCallback(integrationData) {
     const { profileId, token, refreshToken } = integrationData
-    const hashtags = integrationData.hashtags || []
-    console.log( hashtags.split(','))
+    console.log('integrationData', integrationData)
+    const hashtags = !integrationData.hashtags || integrationData.hashtags === '' ? [] : integrationData.hashtags
+    console.log( hashtags)
     const integration = await this.createOrUpdate({
       platform: PlatformType.TWITTER,
       integrationIdentifier: profileId,
