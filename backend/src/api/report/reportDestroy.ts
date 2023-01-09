d414258cab2a5fd4bcf00421261c83945fee90da
Joana Maia <joana@crowd.dev>
2023-01-09 15:19:27 +0000

    Update widget states

--- backend/src/api/report/reportDestroy.ts
+++ backend/src/api/report/reportDestroy.ts
@@ -1,11 +1,14 @@
 import Permissions from '../../security/permissions'
 import ReportService from '../../services/reportService'
 import PermissionChecker from '../../services/user/permissionChecker'
+import track from '../../segment/track'
 
 export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.reportDestroy)
 
   await new ReportService(req).destroyAll(req.query.ids)
+
+  track('Report Deleted')
 
   const payload = true
 
--- frontend/src/modules/widget/components/v2/shared/widget-loading.vue
+++ frontend/src/modules/widget/components/v2/shared/widget-loading.vue
@@ -2,13 +2,46 @@
   <div
     class="min-h-88 py-12 flex items-center justify-center"
   >
-    <div class="text-center">
+    <!-- Chart and Table widgets -->
+    <div v-if="type !== 'kpi'" class="text-center">
       <i
-        class="ri-bar-chart-fill animate-pulse text-gray-200 text-5xl"
+        class="animate-pulse text-gray-200 text-5xl"
+        :class="icon"
       ></i>
       <div class="text-gray-600 text-sm font-semibold">
         Loading your data...
       </div>
     </div>
+
+    <!-- KPI widgets -->
+    <div v-else class="w-full">
+      <app-loading
+        class="mb-4"
+        height="20px"
+        width="10%"
+        radius="2px"
+      />
+      <app-loading height="12px" radius="2px" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+import AppLoading from '@/shared/loading/loading-placeholder.vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'chart'
+  }
+})
+
+const icon = computed(() => {
+  if (props.type === 'chart') {
+    return 'ri-bar-chart-fill'
+  }
+
+  return 'ri-list-unordered'
+})
+</script>
