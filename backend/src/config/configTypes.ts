ebe58b76259c618cd54325bc11d9473eab5590c9
Uroš Marolt <uros@crowd.dev>
2023-01-10 17:24:14 +0100

    removed unused configuration

--- backend/src/config/configTypes.ts
+++ backend/src/config/configTypes.ts
@@ -27,7 +27,6 @@ export interface AwsCredentials {
 export interface SQSConfiguration {
   host?: string
   port?: number
-  apiQueue: string
   nodejsWorkerQueue: string
   nodejsWorkerDelayableQueue: string
   pythonWorkerQueue: string
