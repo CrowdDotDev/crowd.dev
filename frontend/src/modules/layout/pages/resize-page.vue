9527d6102a762f495ad2a5e323193158815e45c4
joanagmaia <joanamaiaportugal@gmail.com>
2023-01-11 09:59:11 +0000

    Add placeholder for small screens (#414)

---
+++ frontend/src/modules/layout/pages/resize-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid grid-cols-12 gap-4 mx-6 h-screen">
+    <div class="col-span-8 col-start-3 h-fit self-center">
+      <i
+        class="ri-aspect-ratio-line text-gray-200 text-8xl mb-8"
+      />
+      <h2 class="text-gray-900 mb-2">Resize window</h2>
+      <div class="text-gray-600 text-base">
+        The platform is not ready for lower resolutions,
+        please resize your window in order to display the
+        page contents.
+      </div>
+    </div>
+  </div>
+</template>
