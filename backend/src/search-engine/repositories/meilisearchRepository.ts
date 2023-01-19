155e8afc5d3a06e2387f1d1174e222daa9c82db8
anilb <epipav@gmail.com>
2023-01-19 11:54:15 +0100

    formatting

--- backend/src/search-engine/repositories/meilisearchRepository.ts
+++ backend/src/search-engine/repositories/meilisearchRepository.ts
@@ -3,10 +3,7 @@
  */
 export default class MeilisearchRepository {
   static isDocumentNotFoundError(error) {
-    if (
-      error.code === 'document_not_found' ||
-      error.code === 'index_not_found'
-    ) {
+    if (error.code === 'document_not_found' || error.code === 'index_not_found') {
       return true
     }
     return false
