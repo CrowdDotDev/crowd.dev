9ca7a75408f63502e0af9d6c294b4beec1f56ccb
Mário Balça <mario.balca@gmail.com>
2023-01-23 15:43:04 +0000

    Add script to add isBot member custom attribute to all tenants

---
+++ backend/src/database/initializers/entities/2023-01-23-add-isBot-to-member-attributes.ts
@@ -0,0 +1,25 @@
+import TenantService from '../../../services/tenantService'
+import getUserContext from '../../utils/getUserContext'
+import MemberAttributeSettingsService from "../../../services/memberAttributeSettingsService";
+import { DefaultMemberAttributes } from "../../attributes/member/default";
+
+
+export default async () => {
+  const tenants = await TenantService._findAndCountAllForEveryUser({})
+  const isBotAttributes = DefaultMemberAttributes.find((a) => a.name === 'isBot')
+
+  // for each tenant
+  for (const tenant of tenants.rows) {
+    const userContext = await getUserContext(tenant.id)
+    const memberAttributeSettingsService = new MemberAttributeSettingsService(userContext)
+
+    console.log(`Creating isBot member attribute for tenant ${tenant.id}`)
+    await memberAttributeSettingsService.create({
+      name: isBotAttributes.name,
+      label: isBotAttributes.label,
+      type: isBotAttributes.type,
+      canDelete: isBotAttributes.canDelete,
+      show: isBotAttributes.show
+    })
+  }
+}
