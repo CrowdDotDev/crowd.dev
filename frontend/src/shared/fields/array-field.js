59c9cf21dd87250afbc0e2243e16f9e0e4ba8e14
Mário Balça <mario.balca@gmail.com>
2023-01-26 12:42:49 +0000

    Fix a few more issues

---
+++ frontend/src/shared/fields/array-field.js
@@ -0,0 +1,25 @@
+import GenericField from '@/shared/fields/generic-field'
+import { i18n } from '@/i18n'
+import * as yup from 'yup'
+
+export default class ArrayField extends GenericField {
+  constructor(name, label, config = {}) {
+    super(name, label)
+    this.required = config.required
+  }
+
+  forFormRules() {
+    return {
+      type: 'array',
+      required: Boolean(this.required),
+      message: i18n('validation.mixed.required').replace(
+        '${path}',
+        this.label
+      )
+    }
+  }
+
+  forFormCast() {
+    return yup.array().nullable(true).label(this.label)
+  }
+}
