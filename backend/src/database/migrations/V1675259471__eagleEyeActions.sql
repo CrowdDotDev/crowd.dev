ffc51d3dcb3eb59a0e300e58d40435e14b8cc9ca
anil <epipav@gmail.com>
2023-02-02 10:33:10 +0100

    schema updates

--- backend/src/database/migrations/V1675259471__eagleEyeActions.sql
+++ backend/src/database/migrations/V1675259471__eagleEyeActions.sql
@@ -11,22 +11,22 @@ CREATE TABLE public."eageEyeContents" (
 
 ALTER TABLE public."eageEyeContents" ADD CONSTRAINT "eageEyeContents_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES public.tenants(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-CREATE TYPE "eagleEyeActions_action_type" AS ENUM ('thumbs-up', 'thumbs-down', 'bookmark');
+CREATE TYPE public."eagleEyeActions_action_type" AS ENUM ('thumbs-up', 'thumbs-down', 'bookmark');
 
 CREATE TABLE public."eageEyeActions" (
     "id" uuid NOT NULL,
     "platform" text NOT NULL,
-    "action" public."eagleEyeActions_actions_type" NOT NULL,
+    "action" public."eagleEyeActions_action_type" NOT NULL,
     "timestamp" timestamptz NOT NULL,
     "url" text NOT NULL,
     "contentId" uuid NOT NULL,
     "tenantId" uuid NOT NULL,
-    "actionBy" uuid NOT NULL,
+    "actionById" uuid NOT NULL,
 	"createdAt" timestamptz NOT NULL,
 	"updatedAt" timestamptz NOT NULL,
 	CONSTRAINT "eageEyeActions_pkey" PRIMARY KEY ("id")
 );
 
 ALTER TABLE public."eageEyeActions" ADD CONSTRAINT "eageEyeActions_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES public.tenants(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE public."eageEyeActions" ADD CONSTRAINT "eageEyeActions_actionBy_fkey" FOREIGN KEY ("actionBy") REFERENCES public.users(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE public."eageEyeActions" ADD CONSTRAINT "eageEyeActions_actionBy_fkey" FOREIGN KEY ("actionById") REFERENCES public.users(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 ALTER TABLE public."eageEyeActions" ADD CONSTRAINT "eageEyeActions_contentId_fkey" FOREIGN KEY ("contentId") REFERENCES public."eageEyeContents"(id) ON DELETE NO ACTION ON UPDATE NO ACTION;
--- backend/src/database/models/eagleEyeContent.ts
+++ backend/src/database/models/eagleEyeContent.ts
@@ -6,120 +6,14 @@ const eagleEyeContentModel = {
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
   },
-  post: {
+  content: {
     type: DataTypes.JSONB,
     allowNull: false
-  },
-  action: {
-    type: DataTypes.TEXT,
-    validate: {
-      isIn: [['thumbs-up', 'thumbs-down', 'bookmark']],
-    },
-    defaultValue: null,
-  },
-  timestamp: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  post: {
-    type: DataTypes.JSONB,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  username: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  text: {
-    type: DataTypes.TEXT,
-  },
-
-  platform: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  keywords: {
-    type: DataTypes.ARRAY(DataTypes.TEXT),
-    default: [],
-  },
-  exactKeywords: {
-    type: DataTypes.ARRAY(DataTypes.TEXT),
-    default: [],
-  },
-  similarityScore: {
-    type: DataTypes.FLOAT,
-  },
-  userAttributes: {
-    type: DataTypes.JSONB,
-  },
-  postAttributes: {
-    type: DataTypes.JSONB,
-    default: {},
-  },
-  importHash: {
-    type: DataTypes.STRING(255),
-    allowNull: true,
-    validate: {
-      len: [0, 255],
-    },
   },
 }
 
 export default (sequelize) => {
   const eagleEyeContent = sequelize.define('eagleEyeContent', eagleEyeContentModel, {
-    indexes: [
-      {
-        unique: true,
-        fields: ['importHash', 'tenantId'],
-        where: {
-          deletedAt: null,
-        },
-      },
-      {
-        fields: ['platform', 'tenantId', 'timestamp'],
-        where: {
-          deletedAt: null,
-        },
-      },
-      {
-        fields: ['status', 'tenantId', 'timestamp'],
-        where: {
-          deletedAt: null,
-        },
-      },
-      {
-        fields: ['tenantId', 'timestamp'],
-        where: {
-          deletedAt: null,
-        },
-      },
-    ],
     timestamps: true,
     paranoid: true,
   })
