ff078092be247de141fc5920c4f407863f903343
anilb <epipav@gmail.com>
2023-02-05 20:37:09 +0100

    transactions for upsert

--- backend/src/services/eagleEyeContentService.ts
+++ backend/src/services/eagleEyeContentService.ts
@@ -3,6 +3,7 @@ import EagleEyeContentRepository from '../database/repositories/eagleEyeContentR
 import { LoggingBase } from './loggingBase'
 import { EagleEyeContent, EagleEyeAction } from '../types/eagleEyeTypes'
 import { PageData, QueryData } from '../types/common'
+import SequelizeRepository from '../database/repositories/sequelizeRepository'
 
 export interface EagleEyeContentUpsertData extends EagleEyeAction {
   content: EagleEyeContent
@@ -23,22 +24,37 @@ export default class EagleEyeContentService extends LoggingBase {
    * @returns Created EagleEyeContent record.
    */
   async upsert(data: EagleEyeContent): Promise<EagleEyeContent | null> {
-    if (!data.url) {
-      throw new Error(`Can't upsert without url`)
+
+    const transaction = await SequelizeRepository.createTransaction(this.options)
+
+    try {
+
+      if (!data.url) {
+        throw new Error(`Can't upsert without url`)
+      }
+
+      // find by url
+      const existing = await EagleEyeContentRepository.findByUrl(data.url, this.options)
+
+      let record
+
+      if (existing) {
+        record = await EagleEyeContentRepository.update(existing.id, data, this.options)
+      } else {
+        record = await EagleEyeContentRepository.create(data, this.options)
+      }
+
+      await SequelizeRepository.commitTransaction(transaction)
+
+      return record
+    } catch (error) {
+      await SequelizeRepository.rollbackTransaction(transaction)
+
+      SequelizeRepository.handleUniqueFieldError(error, this.options.language, 'EagleEyeContent')
+
+      throw error
     }
 
-    // find by url
-    const existing = await EagleEyeContentRepository.findByUrl(data.url, this.options)
-
-    let record
-
-    if (existing) {
-      record = await EagleEyeContentRepository.update(existing.id, data, this.options)
-    } else {
-      record = await EagleEyeContentRepository.create(data, this.options)
-    }
-
-    return record
   }
 
   async findById(id: string): Promise<EagleEyeContent> {
