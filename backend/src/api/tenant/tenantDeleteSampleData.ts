b1a89b04be718a0b4e6a1a951f8209211aac4573
Joan Reyero <joan@crowd.dev>
2023-02-08 16:36:10 +0100

    Fixed permissions for sample data

--- backend/src/api/tenant/tenantDeleteSampleData.ts
+++ backend/src/api/tenant/tenantDeleteSampleData.ts
@@ -2,11 +2,15 @@ import Error403 from '../../errors/Error403'
 import { i18n } from '../../i18n'
 import track from '../../segment/track'
 import SampleDataService from '../../services/sampleDataService'
+import Permissions from '../../security/permissions'
+import PermissionChecker from '../../services/user/permissionChecker'
 
 export default async (req, res) => {
   if (!req.currentUser || !req.currentUser.id) {
     throw new Error403(req.language)
   }
+
+  new PermissionChecker(req).validateHas(Permissions.values.memberDestroy)
 
   await new SampleDataService(req).deleteSampleData()
 
