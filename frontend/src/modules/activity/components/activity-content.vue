e7286e617cf5c5d05a2c4a93c4d00ae4b349cfa0
Gasper Grom <gasper.grom@gmail.com>
2023-02-23 15:55:50 +0100

    Fix code issues

--- frontend/src/modules/activity/components/activity-content.vue
+++ frontend/src/modules/activity/components/activity-content.vue
@@ -43,7 +43,7 @@
             activity.type === 'reaction_added' &&
             displayBody
           "
-          v-html="renderEmoji(activity.body)"
+          v-html="contentRenderEmojis(`:${activity.body}:`)"
         />
         <span
           v-else-if="displayBody"
@@ -157,16 +157,13 @@ export default {
     }
   },
   methods: {
-    renderEmoji(message) {
-      return joypixels.toImage(`:${message}:`)
-    },
     contentRenderEmojis(content) {
-      const render = joypixels.toImage(content)
-      return render
+      return joypixels
+        .toImage(content)
         .trim()
         .replaceAll(
           /(?<!"):[a-z_-]+:/g,
-          '<abbr title="Unable to detect emoji">⚫</abbr>'
+          '<abbr class="no-underline" title="Unable to detect emoji">⚪</abbr>'
         )
     }
   }
