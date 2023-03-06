30992fd376210757bbbfc183d624cd54da37dacf
Uros Marolt <uros@crowd.dev>
2023-03-06 16:38:49 +0100

    Slack commands admin tool (#613)

---
+++ backend/src/api/slack/index.ts
@@ -0,0 +1,8 @@
+import { SLACK_CONFIG } from '../../config/index'
+import { safeWrap } from '../../middlewares/errorMiddleware'
+
+export default (app) => {
+  if (SLACK_CONFIG.appId && SLACK_CONFIG.appToken && SLACK_CONFIG.teamId) {
+    app.post('/slack/commands', safeWrap(require('./command').default))
+  }
+}
