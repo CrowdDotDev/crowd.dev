1e93ede2fd75042a05de8fc629dd8c5a21930a0e
Joan Reyero <joan@crowd.dev>
2023-03-06 12:12:02 +0100

    New log

--- frontend/src/modules/layout/components/layout.vue
+++ frontend/src/modules/layout/components/layout.vue
@@ -252,7 +252,7 @@ export default {
     identify(this.currentUser)
     this.initPendo()
     // formbricks pmf
-    console.log('formbricks 23', config.formbricks)
+    console.log('formbricks', config.formbricks)
     if (
       config.formbricks.url &&
       config.formbricks.pmfFormId
