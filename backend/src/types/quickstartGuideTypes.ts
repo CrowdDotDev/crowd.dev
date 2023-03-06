9478a88f27143d59bd8c8dad65241c9617a6cce3
Gasper Grom <gasper.grom@gmail.com>
2023-03-06 15:50:13 +0100

    Fix video links

--- backend/src/types/quickstartGuideTypes.ts
+++ backend/src/types/quickstartGuideTypes.ts
@@ -27,7 +27,7 @@ export interface QuickstartGuideSettings {
 const connectIntegrationGuide: QuickstartGuide = {
   title: 'Connect your first 2 integrations',
   body: 'Connect with our built-in integrations to start syncing data from your digital channels.',
-  videoLink: 'https://www.crowd.dev/quickstart-video-integrations',
+  videoLink: 'https://www.loom.com/share/578ea6ef431c48e0b338cf975d3b80bc',
   buttonLink: '/integrations',
   buttonText: 'Connect integrations',
   completed: false,
@@ -36,7 +36,7 @@ const connectIntegrationGuide: QuickstartGuide = {
 const enrichMemberGuide: QuickstartGuide = {
   title: 'Enrich a member',
   body: 'Get more insights about members by enriching them with attributes such as emails, seniority, OSS contributions and much more.',
-  videoLink: 'https://www.crowd.dev/quickstart-video-enrichment',
+  videoLink: 'https://www.loom.com/share/8fdbdd2c0d4c4ab59ae845248b1db04f',
   buttonLink: '/members',
   buttonText: 'Try enrichment',
   completed: false,
@@ -45,7 +45,7 @@ const enrichMemberGuide: QuickstartGuide = {
 const viewReportGuide: QuickstartGuide = {
   title: 'Look into a report',
   body: 'Check our specially crafted default reports and dig into the inner workings of your community.',
-  videoLink: 'https://www.crowd.dev/quickstart-video-report',
+  videoLink: 'https://www.loom.com/share/545e7dfc692540d09115ee32653640ca',
   buttonLink: '/reports',
   buttonText: 'Explore reports',
   completed: false,
@@ -54,7 +54,7 @@ const viewReportGuide: QuickstartGuide = {
 const setEagleEyeGuide: QuickstartGuide = {
   title: 'Discover content in your niche',
   body: 'Discover and engage with relevant content across various community platforms in order to gain developers’ mindshare and increase your community awareness.',
-  videoLink: 'https://www.crowd.dev/quickstart-video-eagleeye',
+  videoLink: 'https://www.loom.com/share/7900b1c0ea0b4a33a2cf85d3b175b1b7',
   buttonLink: '/eagle-eye',
   buttonText: 'Explore Eagle Eye',
   completed: false,
@@ -63,7 +63,7 @@ const setEagleEyeGuide: QuickstartGuide = {
 const inviteColleaguesGuide: QuickstartGuide = {
   title: 'Invite your colleagues',
   body: 'Invite colleagues to your crowd.dev workspace by giving full access or read-only permissions.',
-  videoLink: 'https://www.crowd.dev/quickstart-video-invitecolleague',
+  videoLink: 'https://www.loom.com/share/545e7dfc692540d09115ee32653640ca',
   buttonLink: '/settings',
   buttonText: 'Invite colleagues',
   completed: false,
