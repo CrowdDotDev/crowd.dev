c38dc3588e7620fcc0e7d9d67115968958b4001f
anilb <epipav@gmail.com>
2023-03-06 18:20:50 +0100

    isOrganization as a default member attribute (#605)

--- backend/src/services/__tests__/tenantService.test.ts
+++ backend/src/services/__tests__/tenantService.test.ts
@@ -189,6 +189,7 @@ describe('TenantService tests', () => {
       expect(defaultAttributes.rows.map((i) => i.name).sort()).toEqual([
         MemberAttributeName.BIO,
         MemberAttributeName.IS_BOT,
+        MemberAttributeName.IS_ORGANIZATION,
         MemberAttributeName.IS_TEAM_MEMBER,
         MemberAttributeName.JOB_TITLE,
         MemberAttributeName.LOCATION,
