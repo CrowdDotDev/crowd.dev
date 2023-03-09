e3d1d5cc787349bf8ddbfd641fbb42061e19bce1
Joana Maia <joana@crowd.dev>
2023-03-09 16:33:21 +0000

    Fix other missing computed properties

--- frontend/src/premium/eagle-eye/pages/eagle-eye-page-wrapper.vue
+++ frontend/src/premium/eagle-eye/pages/eagle-eye-page-wrapper.vue
@@ -3,16 +3,19 @@
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue'
+import { computed, defineAsyncComponent } from 'vue'
 import AppPageLoader from '@/shared/loading/page-loader.vue'
 import { FeatureFlag } from '@/featureFlag'
 import { mapGetters } from '@/shared/vuex/vuex.helpers'
 
 const { currentUser, currentTenant } = mapGetters('auth')
 
-const eagleEyeSettings = currentUser?.value.tenants.find(
-  (tu) => tu.tenantId === currentTenant?.value.id
-).settings?.eagleEye
+const eagleEyeSettings = computed(
+  () =>
+    currentUser?.value.tenants.find(
+      (tu) => tu.tenantId === currentTenant?.value.id
+    ).settings?.eagleEye
+)
 
 const AppEagleEyePage = defineAsyncComponent({
   loader: () => {
@@ -21,7 +24,7 @@ const AppEagleEyePage = defineAsyncComponent({
     )
 
     if (isFeatureEnabled) {
-      const isOnboarded = eagleEyeSettings?.onboarded
+      const isOnboarded = eagleEyeSettings.value?.onboarded
 
       if (isOnboarded) {
         return import(
