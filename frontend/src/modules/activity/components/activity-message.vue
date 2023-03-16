ebda98ef2338f9a95b0904c83f2add36f2757fba
Gasper Grom <gasper.grom@gmail.com>
2023-03-16 16:01:27 +0100

    Fix activity message

--- frontend/src/modules/activity/components/activity-message.vue
+++ frontend/src/modules/activity/components/activity-message.vue
@@ -1,80 +1,40 @@
 <template>
-  <div>
-    <div>{{ activity.type }}</div>
-    <div
-      class="activity-message"
-      v-html="$sanitize(activity.display.default)"
-    ></div>
-    <div class="flex">
-      <component
-        :is="platformConfig.activityMessage"
-        v-if="platformConfig.activityMessage"
-        :activity="activity"
-        :channel-only="channelOnly"
-        :short="short"
-      ></component>
-      <!-- other -->
-      <template v-else>
-        <app-i18n
-          v-if="!channelOnly"
-          :code="computedMessage"
-          :args="computedArgs"
-          :fallback="'entities.activity.fallback'"
-          :class="{ truncate: short }"
-        ></app-i18n>
-      </template>
-    </div>
-  </div>
+  <div
+    class="activity-message"
+    v-html="$sanitize(activityMessage)"
+  ></div>
 </template>
 
 <script>
-import AppI18n from '@/shared/i18n/i18n'
-import { computedArgs } from '@/modules/activity/activity.helpers'
-import { CrowdIntegrations } from '@/integrations/integrations-config'
 export default {
-  name: 'AppActivityMessage',
-  components: {
-    AppI18n
-  },
-  props: {
-    activity: {
-      type: Object,
-      required: true
-    },
-    short: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    channelOnly: {
-      type: Boolean,
-      required: false,
-      default: false
-    }
-  },
-  computed: {
-    platformConfig() {
-      return CrowdIntegrations.getConfig(
-        this.activity.platform
-      )
-    },
-    computedMessage() {
-      return `entities.activity.${this.activity.platform}.${this.activity.type}`
-    },
-    computedArgs() {
-      return computedArgs(this.activity)
-    }
-  },
-  methods: {
-    getRepositoryName(repositoryUrl) {
-      const splittedUrl = repositoryUrl.split('/')
-      if (splittedUrl.length > 0) {
-        return splittedUrl[splittedUrl.length - 1]
-      }
-      return ''
-    }
-  }
+  name: 'AppActivityMessage'
 }
+</script>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  activity: {
+    type: Object,
+    required: true
+  },
+  type: {
+    type: String,
+    required: false,
+    default: 'default'
+  }
+})
+
+const activityMessage = computed(() => {
+  if (
+    props.activity?.display &&
+    props.type in props.activity.display
+  ) {
+    return props.activity.display[props.type]
+  }
+  return ''
+})
 </script>
 
 <style lang="scss">
