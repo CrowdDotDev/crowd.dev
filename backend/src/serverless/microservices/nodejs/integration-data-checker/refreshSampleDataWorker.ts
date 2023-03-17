d9cbd0f6ef67a7e4ea796ed7d8a824e97a8d2199
Joan Reyero <joan@crowd.dev>
2023-03-17 11:10:02 +0100

    Added comments

--- backend/src/serverless/microservices/nodejs/integration-data-checker/refreshSampleDataWorker.ts
+++ backend/src/serverless/microservices/nodejs/integration-data-checker/refreshSampleDataWorker.ts
@@ -3,33 +3,35 @@ import { API_CONFIG, SAMPLE_DATA_CONFIG } from '../../../../config'
 import getUserContext from '../../../../database/utils/getUserContext'
 
 async function refreshSampleDataWorker(): Promise<void> {
-  // if (API_CONFIG.edition === 'crowd-hosted') {
-  const tenantId = SAMPLE_DATA_CONFIG.tenantId
-  const userContext = await getUserContext(SAMPLE_DATA_CONFIG.tenantId)
-  const updateDays = 1
+  // This is only needed for hosted edition
+  if (API_CONFIG.edition === 'crowd-hosted') {
+    const tenantId = SAMPLE_DATA_CONFIG.tenantId
+    const userContext = await getUserContext(SAMPLE_DATA_CONFIG.tenantId)
+    const updateDays = 1 // Every day we need to refresh
 
-  const tables = [
-    { name: 'activities', columns: ['createdAt', 'timestamp'] },
-    { name: 'members', columns: ['joinedAt', 'createdAt'] },
-    { name: 'notes', columns: ['createdAt'] },
-    { name: 'conversations', columns: ['createdAt'] },
-    { name: 'organizations', columns: ['createdAt'] },
-    { name: 'tags', columns: ['createdAt'] },
-  ]
+    // These are all the tables that have columns that need to be updated
+    const tables = [
+      { name: 'activities', columns: ['createdAt', 'timestamp'] },
+      { name: 'members', columns: ['joinedAt', 'createdAt'] },
+      { name: 'notes', columns: ['createdAt'] },
+      { name: 'conversations', columns: ['createdAt'] },
+      { name: 'organizations', columns: ['createdAt'] },
+      { name: 'tags', columns: ['createdAt'] },
+    ]
 
-  for (const table of tables) {
-    for (const column of table.columns) {
-      const query = `
+    // We are using a direct query because it is very specific functionality.
+    // There is no point creating repository methods.
+    for (const table of tables) {
+      for (const column of table.columns) {
+        const query = `
         UPDATE ${table.name}
         SET "${column}" = "${column}" + INTERVAL '${updateDays} days'
         WHERE "tenantId" = '${tenantId}'; 
       `
-
-      console.log(query)
-      await userContext.database.sequelize.query(query, { type: QueryTypes.UPDATE })
+        await userContext.database.sequelize.query(query, { type: QueryTypes.UPDATE })
+      }
     }
   }
-  // }
 }
 
 export { refreshSampleDataWorker }
