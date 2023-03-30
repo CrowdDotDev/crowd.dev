1b8d2b3fa70d1683359a8215b48e9c49b330b2c1
Gašper Grom <gasper.grom@gmail.com>
2023-03-30 11:51:13 +0200

    Temporarly disable other guards (#701)

--- frontend/src/middleware/auth/index.js
+++ frontend/src/middleware/auth/index.js
@@ -1,15 +1,16 @@
 import AuthGuard from '@/middleware/auth/auth-guard';
-import UnauthGuard from '@/middleware/auth/unauth-guard';
-import EmailAlreadyVerifiedGuard from '@/middleware/auth/email-already-verified-guard';
-import PermissionGuard from '@/middleware/auth/permission-guard';
-import NotEmptyTenant from '@/middleware/auth/not-empty-tenant-guard';
-import NotEmptyPermissionsGuard from '@/middleware/auth/not-empty-permissions-guard';
+// import UnauthGuard from '@/middleware/auth/unauth-guard';
+// import EmailAlreadyVerifiedGuard from '@/middleware/auth/email-already-verified-guard';
+// import PermissionGuard from '@/middleware/auth/permission-guard';
+// import NotEmptyTenant from '@/middleware/auth/not-empty-tenant-guard';
+// import NotEmptyPermissionsGuard from '@/middleware/auth/not-empty-permissions-guard';
 
+/* Temporarly disabling guards, only AuthGuard has been working before and other caused too much issues when enabled */
 export default [
   AuthGuard,
-  UnauthGuard,
-  EmailAlreadyVerifiedGuard,
-  PermissionGuard,
-  NotEmptyTenant,
-  NotEmptyPermissionsGuard,
+  // UnauthGuard,
+  // EmailAlreadyVerifiedGuard,
+  // PermissionGuard,
+  // NotEmptyTenant,
+  // NotEmptyPermissionsGuard,
 ];
