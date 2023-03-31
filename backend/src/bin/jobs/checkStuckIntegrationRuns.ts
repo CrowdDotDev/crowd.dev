07f6cbe342b9576bbb57881a76e28522c448d147
Uroš Marolt <uros@crowd.dev>
2023-03-31 16:00:53 +0200

    added comments

--- backend/src/bin/jobs/checkStuckIntegrationRuns.ts
+++ backend/src/bin/jobs/checkStuckIntegrationRuns.ts
@@ -48,6 +48,11 @@ const job: CrowdJob = {
 
         const now = moment()
         for (const run of runsToCheck) {
+          // let's first check if the integration run itself is older than 3 hours
+          // we are updating updatedAt at the end of the integration run when we process all streams or if it's stopped by rate limit/delays
+          // so it should be a good indicator if the integration run is stuck
+          // but because some integrations are really long running it must not be the only one
+
           const integrationLastUpdatedAt = moment(run.updatedAt)
           const diff = now.diff(integrationLastUpdatedAt, 'hours')
 
@@ -55,6 +60,9 @@ const job: CrowdJob = {
             const logger = createChildLogger('fixer', log, { runId: run.id })
             log.warn({ runId: run.id }, 'Investigating possible stuck integration run!')
 
+            // first lets check if the we have any integration streams that are in state processing
+            // and if we have let's see when were they moved to that state based on updatedAt column
+            // if they are older than 3 hours we will reset them to pending state and start integration back up
             const processingStreams = await streamsRepo.findByRunId(
               run.id,
               IntegrationStreamState.PROCESSING,
@@ -80,6 +88,7 @@ const job: CrowdJob = {
               }
             }
 
+            // if there were no processing streams lets check if we have pending streams that are older than 3 hours
             if (!stuck) {
               const pendingStreams = await streamsRepo.findByRunId(
                 run.id,
@@ -97,6 +106,7 @@ const job: CrowdJob = {
               }
             }
 
+            // and the last check is to see whether we have any errored streams that are older than 3 hours and haven't been retried enough times
             if (!stuck) {
               const errorStreams = await streamsRepo.findByRunId(
                 run.id,
@@ -123,6 +133,8 @@ const job: CrowdJob = {
               }
             }
 
+            // this check tries to see whether the integration run is actually finished but it's in a wrong state
+            // by checking all streams and determining whether they are in a final state
             if (!stuck) {
               // check if there are any streams that are not in a final state
               const allStreams = await streamsRepo.findByRunId(run.id)
