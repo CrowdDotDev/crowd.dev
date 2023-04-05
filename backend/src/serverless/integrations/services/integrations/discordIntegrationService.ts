8d20eacaff2982463a78067d3725987eb84c7cc3
Joan Reyero <joan@crowd.dev>
2023-04-05 13:51:27 +0200

    Changed integration to always put channel as channel and keep the child channel in attributes.childchannel

--- backend/src/serverless/integrations/services/integrations/discordIntegrationService.ts
+++ backend/src/serverless/integrations/services/integrations/discordIntegrationService.ts
@@ -525,9 +525,9 @@ export class DiscordIntegrationService extends IntegrationServiceBase {
             ? DiscordIntegrationService.replaceMentions(record.content, record.mentions)
             : '',
           url: `https://discordapp.com/channels/${channel.guild_id}/${channel.id}/${record.id}`,
-          channel: channel.name,
+          channel: parentChannel,
           attributes: {
-            parentChannel,
+            childChannel: channel.name,
             thread: isThread,
             reactions: record.reactions ? record.reactions : [],
             attachments: record.attachments ? record.attachments : [],
