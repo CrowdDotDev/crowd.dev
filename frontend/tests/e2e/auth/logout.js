78927ac2c500f6e1b9c50108fb425ec04c2dd186
Gašper Grom <gasper.grom@gmail.com>
2023-04-11 16:01:29 +0200

    Auth & onboarding test improvement (#728)

--- frontend/tests/e2e/auth/logout.js
+++ frontend/tests/e2e/auth/logout.js
@@ -1,16 +1,18 @@
 export default () => {
   beforeEach(() => {
-    cy.get('#accountDropdown').as('accountDropdown')
-  })
+    cy.get('#accountDropdown').as('accountDropdown');
+  });
 
   it('Logs out successfully', () => {
+    cy.wait(500);
+
+    cy.get('@accountDropdown').click();
+    cy.wait(200);
+    cy.get('#logout').as('logout');
+    cy.get('@logout').click();
+
     cy.wait(500)
 
-    cy.get('@accountDropdown').click()
-    cy.wait(100)
-    cy.get('#logout').as('logout')
-    cy.get('@logout').click()
-
-    cy.url().should('include', '/auth/signin')
-  })
-}
+    cy.url().should('include', '/auth/signin');
+  });
+};
