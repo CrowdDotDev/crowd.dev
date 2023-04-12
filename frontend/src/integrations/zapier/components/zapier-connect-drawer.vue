3fdbb296cef07d76ee64032a8a8aaecb1ad0d62e
garrrikkotua <kotuaigor@gmail.com>
2023-04-12 13:41:55 +0300

    linting

--- frontend/src/integrations/zapier/components/zapier-connect-drawer.vue
+++ frontend/src/integrations/zapier/components/zapier-connect-drawer.vue
@@ -12,22 +12,22 @@
   >
     <template #content>
       <div class="flex flex-row items-center justify-between w-full">
-        <p class="font-bold text-base">Set up crowd.dev in Zapier</p>
+        <p class="font-bold text-base">
+          Set up crowd.dev in Zapier
+        </p>
         <a
           href="https://docs.crowd.dev/docs/zapier-integration"
           target="_blank"
           rel="noopener noreferrer"
           class="btn btn--bordered btn--md"
-          ><i class="ri-external-link-line mr-2" />Read our docs</a
-        >
+        ><i class="ri-external-link-line mr-2" />Read our docs</a>
       </div>
       <div class="container flex flex-row w-full items-start py-10">
         <ul class="list-none space-y-4">
           <li class="flex items-start">
             <span
               class="flex-none w-8 h-8 flex justify-center items-center rounded-full bg-gray-300 text-gray-900 font-bold text-sm"
-              >1</span
-            >
+            >1</span>
             <p class="ml-4 text-gray-900 text-xs">
               Navigate to Settings >
               <a
@@ -35,8 +35,7 @@
                 target="_blank"
                 rel="noopener noreferrer"
                 class="underline underline-offset-4"
-                >API Keys</a
-              >
+              >API Keys</a>
               and get your workspace
               <span class="font-medium">Tenant ID</span> and
               <span class="font-medium">Auth token</span>
@@ -45,8 +44,7 @@
           <li class="flex items-start">
             <span
               class="flex-none w-8 h-8 flex justify-center items-center rounded-full bg-gray-300 text-gray-900 font-bold text-sm"
-              >2</span
-            >
+            >2</span>
             <p class="ml-4 text-gray-900 text-xs">
               Go to
               <a
@@ -54,16 +52,14 @@
                 target="_blank"
                 rel="noopener noreferrer"
                 class="underline underline-offset-4"
-                >Zapier</a
-              >
+              >Zapier</a>
               and connect your crowd.dev account using your workspace API Keys
             </p>
           </li>
           <li class="flex items-center">
             <span
               class="flex-none w-8 h-8 flex justify-center items-center rounded-full bg-gray-300 text-gray-900 font-bold text-sm"
-              >3</span
-            >
+            >3</span>
             <p class="ml-4 text-gray-900 text-xs">
               Configure your Zaps using our
               <a
@@ -71,8 +67,7 @@
                 target="_blank"
                 rel="noopener noreferrer"
                 class="underline underline-offset-4"
-                >supported triggers and actions</a
-              >
+              >supported triggers and actions</a>
             </p>
           </li>
         </ul>
@@ -87,8 +82,8 @@
 </template>
 
 <script setup>
-import { computed, defineEmits, defineProps } from "vue";
-import { CrowdIntegrations } from "@/integrations/integrations-config";
+import { computed, defineEmits, defineProps } from 'vue';
+import { CrowdIntegrations } from '@/integrations/integrations-config';
 
 const props = defineProps({
   modelValue: {
@@ -97,22 +92,22 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(['update:modelValue']);
 
-const logoUrl = CrowdIntegrations.getConfig("zapier").image;
+const logoUrl = CrowdIntegrations.getConfig('zapier').image;
 
 const isVisible = computed({
   get() {
     return props.modelValue;
   },
   set(value) {
-    return emit("update:modelValue", value);
+    return emit('update:modelValue', value);
   },
 });
 </script>
 
 <script>
 export default {
-  name: "AppZapierConnectDrawer",
+  name: 'AppZapierConnectDrawer',
 };
 </script>
