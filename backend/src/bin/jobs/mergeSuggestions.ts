9517a008ac4f17d79f68e18f2ea43c543df7c1ed
Joan Reyero <joan@crowd.dev>
2023-04-22 11:56:59 +0100

    Back to 1h

--- backend/src/bin/jobs/mergeSuggestions.ts
+++ backend/src/bin/jobs/mergeSuggestions.ts
@@ -8,9 +8,9 @@ import { timeout } from '../../utils/timing'
 const job: CrowdJob = {
   name: 'Merge suggestions',
   // every hour
-  // cronTime: '0 * * * *',
+  cronTime: '0 * * * *',
   // every minute
-  cronTime: '* * * * *',
+  // cronTime: '* * * * *',
   onTrigger: async () => {
     const tenants = await TenantService._findAndCountAllForEveryUser({})
     for (const tenant of tenants.rows) {
