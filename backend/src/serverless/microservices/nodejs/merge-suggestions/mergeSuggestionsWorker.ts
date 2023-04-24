3821d3935c035dc8f92b23299f4d33e1a806cd0a
Joan Reyero <joan@crowd.dev>
2023-04-24 12:01:30 +0100

    Format

--- backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
+++ backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
@@ -1,10 +1,7 @@
 import getUserContext from '../../../../database/utils/getUserContext'
 import MemberService from '../../../../services/memberService'
 import { IRepositoryOptions } from '../../../../database/repositories/IRepositoryOptions'
-import { createServiceChildLogger } from '../../../../utils/logging'
 import { IMemberMergeAllSuggestions } from '../../../../database/repositories/types/memberTypes'
-
-const log = createServiceChildLogger('mergeSuggestionsWorker')
 
 async function mergeSuggestionsWorker(tenantId): Promise<void> {
   const userContext: IRepositoryOptions = await getUserContext(tenantId)
