bf836a7d9fc4f4b7e8a888bdc0bde1dcbad11d61
Uroš Marolt <uros@crowd.dev>
2023-04-24 17:49:53 +0200

    optimized query

--- backend/src/database/migrations/U1681803038__member-multiple-platform-usernames.sql
+++ backend/src/database/migrations/U1681803038__member-multiple-platform-usernames.sql
@@ -1,39 +1,40 @@
 drop materialized view "memberActivityAggregatesMVs";
 
 create materialized view "memberActivityAggregatesMVs" as
-with identities as (select "memberId",
-                           array_agg(distinct platform)                as identities,
-                           jsonb_object_agg(platform, latest_username) as username,
-                           jsonb_object_agg(platform, usernames)       as "newUsername"
+with identities as (select mi."memberId",
+                           array_agg(distinct mi.platform)                   as identities,
+                           jsonb_object_agg(mi.platform, mi.latest_username) as username,
+                           jsonb_object_agg(mi.platform, mi.usernames)       as "newUsername"
                     from (select "memberId",
                                  platform,
-                                 first_value(username)
-                                 over (partition by "memberId", platform order by "createdAt" desc) as latest_username,
-                                 jsonb_agg(username) over (partition by "memberId", platform)       as usernames
-                          from "memberIdentities") ranked
-                    group by "memberId")
+                                 max(username) filter (where is_latest) as latest_username,
+                                 array_agg(username)                    as usernames
+                          from (select "memberId",
+                                       platform,
+                                       username,
+                                       "createdAt",
+                                       row_number() over (partition by "memberId", platform order by "createdAt" desc) =
+                                       1 as is_latest
+                                from "memberIdentities") sub
+                          group by "memberId", platform) mi
+                    group by mi."memberId")
 select m.id,
-       max(a."timestamp")                                                   as "lastActive",
+       max(a."timestamp")                                                                        as "lastActive",
        i.identities,
        i.username,
        i."newUsername",
-       count(a.id)                                                          as "activityCount",
-       array_agg(
-       distinct (concat(a.platform, ':', a.type))
-           ) filter (
-           where
-           a.platform is not null
-           )                                                                as "activityTypes",
-       array_agg(distinct a.platform) filter (where a.platform is not null) as "activeOn",
-       count(distinct a.timestamp::date)                                    as "activeDaysCount",
+       count(a.id)                                                                               as "activityCount",
+       array_agg(distinct concat(a.platform, ':', a.type)) filter (where a.platform is not null) as "activityTypes",
+       array_agg(distinct a.platform) filter (where a.platform is not null)                      as "activeOn",
+       count(distinct a."timestamp"::date)                                                       as "activeDaysCount",
        round(avg(
                      case
-                         when (a.sentiment ->> 'sentiment'::text) is not null
-                             then (a.sentiment ->> 'sentiment'::text)::double precision
+                         when (a.sentiment ->> 'sentiment') is not null
+                             then (a.sentiment ->> 'sentiment')::double precision
                          else null::double precision
-                         end)::numeric, 2)                                  as "averageSentiment"
+                         end)::numeric, 2)                                                       as "averageSentiment"
 from members m
-         inner join identities i on m.id = i."memberId"
+         join identities i on m.id = i."memberId"
          left join activities a on m.id = a."memberId" and a."deletedAt" is null
 group by m.id, i.identities, i.username, i."newUsername";
 
