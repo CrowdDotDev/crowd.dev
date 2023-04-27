5fe6519bb103cf395113a64df013a23e919ebe0b
Joan Reyero <joan@crowd.dev>
2023-04-27 11:23:24 +0100

    More time

--- backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
+++ backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
@@ -12,19 +12,23 @@ const log = createServiceChildLogger('mergeSuggestionsWorker')
 async function mergeSuggestionsWorker(tenantId): Promise<void> {
   const userContext: IRepositoryOptions = await getUserContext(tenantId)
   const memberService = new MemberService(userContext)
+  const hours = 24 * 30 * 12 // 1 year
   // Splitting these because in the near future we will be treating them differently
   const byUsername: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.USERNAME,
+    hours,
   )
   log.info(`Merge suggestions: Found ${byUsername.length} merge suggestions by username`)
   await memberService.addToMerge(byUsername)
   const byEmail: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.EMAIL,
+    hours,
   )
   log.info(`Merge suggestions: Found ${byEmail.length} merge suggestions by email`)
   await memberService.addToMerge(byEmail)
   const bySimilarity: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.SIMILARITY,
+    hours,
   )
   log.info(`Merge suggestions: Found ${bySimilarity.length} merge suggestions by similarity`)
   await memberService.addToMerge(bySimilarity)
