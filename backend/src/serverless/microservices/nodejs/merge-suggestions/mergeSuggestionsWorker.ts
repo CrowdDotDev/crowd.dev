627064fe924607fb81d93a2f07b262dae32fa2cb
Joan Reyero <joan@crowd.dev>
2023-04-27 11:15:17 +0100

    Ready to re-trigger

--- backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
+++ backend/src/serverless/microservices/nodejs/merge-suggestions/mergeSuggestionsWorker.ts
@@ -1,10 +1,13 @@
 import getUserContext from '../../../../database/utils/getUserContext'
 import MemberService from '../../../../services/memberService'
 import { IRepositoryOptions } from '../../../../database/repositories/IRepositoryOptions'
+import { createServiceChildLogger } from '../../../../utils/logging'
 import {
   IMemberMergeSuggestionsType,
   IMemberMergeSuggestion,
 } from '../../../../database/repositories/types/memberTypes'
+
+const log = createServiceChildLogger('mergeSuggestionsWorker')
 
 async function mergeSuggestionsWorker(tenantId): Promise<void> {
   const userContext: IRepositoryOptions = await getUserContext(tenantId)
@@ -13,14 +16,17 @@ async function mergeSuggestionsWorker(tenantId): Promise<void> {
   const byUsername: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.USERNAME,
   )
+  log.info(`Merge suggestions: Found ${byUsername.length} merge suggestions by username`)
   await memberService.addToMerge(byUsername)
   const byEmail: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.EMAIL,
   )
+  log.info(`Merge suggestions: Found ${byEmail.length} merge suggestions by email`)
   await memberService.addToMerge(byEmail)
   const bySimilarity: IMemberMergeSuggestion[] = await memberService.getMergeSuggestions(
     IMemberMergeSuggestionsType.SIMILARITY,
   )
+  log.info(`Merge suggestions: Found ${bySimilarity.length} merge suggestions by similarity`)
   await memberService.addToMerge(bySimilarity)
 }
 
