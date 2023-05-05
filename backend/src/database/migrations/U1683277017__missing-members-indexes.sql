dfb7636a190cb4edeae90017445394761e9424a9
Uroš Marolt <uros@crowd.dev>
2023-05-05 11:17:04 +0200

    added missing indexes to improve members query

---
+++ backend/src/database/migrations/U1683277017__missing-members-indexes.sql
@@ -0,0 +1,17 @@
+drop index "ix_members_displayName";
+
+drop index "ix_members_reach_total";
+
+drop index "ix_members_score";
+
+drop index "ix_members_numberOfOpenSourceContributions";
+
+drop index "ix_members_lastEnriched";
+
+drop index "ix_members_updatedAt";
+
+drop index "ix_members_attributes_isOrganization";
+
+drop index "ix_members_attributes_isTeamMember";
+
+drop index "ix_members_attributes_isBot";
