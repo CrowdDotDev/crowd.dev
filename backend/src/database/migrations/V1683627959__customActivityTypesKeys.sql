d6e3500c8d444a273b63fcea641b0c666c9f62e5
Joana Maia <joana@crowd.dev>
2023-05-09 14:54:22 +0100

    Update script

--- backend/src/database/migrations/V1683627959__customActivityTypesKeys.sql
+++ backend/src/database/migrations/V1683627959__customActivityTypesKeys.sql
@@ -1,13 +1,14 @@
-UPDATE
-    "settings"
-SET
-    "customActivityTypes" = jsonb_set(
-        "customActivityTypes",
-        '{other}',
-        (
-            SELECT
-                jsonb_object_agg(lower(key), value)
-            FROM
-                jsonb_each("customActivityTypes" -> 'other') AS entries(key, value)
-        )
-    )
+UPDATE settings
+SET "customActivityTypes" = jsonb_set("customActivityTypes", '{other}', t."newKey")
+FROM (
+    SELECT
+        id,
+        jsonb_object_agg(lower(key), value) as "newKey"
+    FROM
+        settings,
+        jsonb_each("customActivityTypes" -> 'other') AS otherObjects(key, value)
+    GROUP BY id
+) t
+WHERE
+    settings.id = t.id
+    AND "customActivityTypes" :: text <> '{}' :: text;
