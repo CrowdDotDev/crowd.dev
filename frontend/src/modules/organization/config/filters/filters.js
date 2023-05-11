d39faf06d3b37bc17db07393f22cf11361437924
Gasper Grom <gasper.grom@gmail.com>
2023-05-11 07:51:50 +0200

    Prepare organization structure

---
+++ frontend/src/modules/organization/config/filters/filters.js
@@ -0,0 +1,13 @@
+import noOfActivities from './noOfActivities'
+import noOfMembers from './noOfMembers'
+import activeOn from './activeOn'
+import joinedDate from './joinedDate'
+import lastActivityDate from './lastActivityDate'
+
+export const memberFilters = {
+  noOfActivities,
+  noOfMembers,
+  activeOn,
+  joinedDate,
+  lastActivityDate
+}
