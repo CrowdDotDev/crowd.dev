52a6613b21024f0842142d593b4d7f637a920c17
Misha Savelyev <mikhail@crowd.dev>
2023-05-29 11:47:25 +0200

    Reformat `memberActivityAggregatesMVs` migration

--- backend/src/database/migrations/R__memberActivityAggregatesMVs.sql
+++ backend/src/database/migrations/R__memberActivityAggregatesMVs.sql
@@ -1,40 +1,50 @@
-drop materialized view "memberActivityAggregatesMVs";
+DROP MATERIALIZED VIEW "memberActivityAggregatesMVs";
 
-create materialized view "memberActivityAggregatesMVs" as
-with identities as (select mi."memberId",
-                           array_agg(distinct mi.platform)             as identities,
-                           jsonb_object_agg(mi.platform, mi.usernames) as username
-                    from (select "memberId",
-                                 platform,
-                                 array_agg(username) as usernames
-                          from (select "memberId",
-                                       platform,
-                                       username,
-                                       "createdAt",
-                                       row_number() over (partition by "memberId", platform order by "createdAt" desc) =
-                                       1 as is_latest
-                                from "memberIdentities") sub
-                          where is_latest
-                          group by "memberId", platform) mi
-                    group by mi."memberId")
-select m.id,
-       max(a."timestamp")                                                                        as "lastActive",
-       i.identities,
-       i.username,
-       count(a.id)                                                                               as "activityCount",
-       array_agg(distinct concat(a.platform, ':', a.type)) filter (where a.platform is not null) as "activityTypes",
-       array_agg(distinct a.platform) filter (where a.platform is not null)                      as "activeOn",
-       count(distinct a."timestamp"::date)                                                       as "activeDaysCount",
-       round(avg(
-                 case
-                     when (a.sentiment ->> 'sentiment') is not null
-                         then (a.sentiment ->> 'sentiment')::double precision
-                     else null::double precision
-                 end)::numeric, 2)                                                       as "averageSentiment"
-from members m
-join identities i on m.id = i."memberId"
-left join activities a on m.id = a."memberId" and a."deletedAt" is null
-group by m.id, i.identities, i.username;
+CREATE MATERIALIZED VIEW "memberActivityAggregatesMVs" AS
+WITH
+    identities AS (
+        SELECT
+            mi."memberId",
+            ARRAY_AGG(DISTINCT mi.platform) AS identities,
+            JSONB_OBJECT_AGG(mi.platform, mi.usernames) AS username
+        FROM (
+            SELECT
+                "memberId",
+                platform,
+                ARRAY_AGG(username) AS usernames
+            FROM (
+                SELECT
+                    "memberId",
+                    platform,
+                    username,
+                    "createdAt",
+                    ROW_NUMBER() OVER (PARTITION BY "memberId", platform ORDER BY "createdAt" DESC) =
+                    1 AS is_latest
+                FROM "memberIdentities"
+            ) sub
+            WHERE is_latest
+            GROUP BY "memberId", platform
+        ) mi
+        GROUP BY mi."memberId"
+    )
+SELECT
+    m.id,
+    MAX(a."timestamp") AS "lastActive",
+    i.identities,
+    i.username,
+    COUNT(a.id) AS "activityCount",
+    ARRAY_AGG(DISTINCT CONCAT(a.platform, ':', a.type)) FILTER (WHERE a.platform IS NOT NULL) AS "activityTypes",
+    ARRAY_AGG(DISTINCT a.platform) FILTER (WHERE a.platform IS NOT NULL) AS "activeOn",
+    COUNT(DISTINCT a."timestamp"::DATE) AS "activeDaysCount",
+    ROUND(AVG(
+              CASE
+                  WHEN (a.sentiment ->> 'sentiment') IS NOT NULL
+                      THEN (a.sentiment ->> 'sentiment')::DOUBLE PRECISION
+                  ELSE NULL::DOUBLE PRECISION
+              END)::NUMERIC, 2) AS "averageSentiment"
+FROM members m
+JOIN identities i ON m.id = i."memberId"
+LEFT JOIN activities a ON m.id = a."memberId" AND a."deletedAt" IS NULL
+GROUP BY m.id, i.identities, i.username;
 
-create unique index ix_memberactivityaggregatesmvs_memberid
-    on "memberActivityAggregatesMVs" (id);
+CREATE UNIQUE INDEX ix_memberactivityaggregatesmvs_memberid ON "memberActivityAggregatesMVs" (id);
