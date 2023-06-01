dad032530975c950b5a9bafdfbd2511239d5308d
Joana Maia <joana@crowd.dev>
2023-06-01 20:26:03 +0100

    Remove images from project group selection

--- frontend/src/modules/lf/layout/components/lf-menu-project-group-selection.vue
+++ frontend/src/modules/lf/layout/components/lf-menu-project-group-selection.vue
@@ -7,16 +7,6 @@
     popper-class="lf-project-group-selection-dropdown"
     fit-input-width
   >
-    <template
-      v-if="selectedProjectGroup && isUrl(selectedProjectGroup.url)"
-      #prefix
-    >
-      <img
-        class="max-w-5 max-h-5"
-        :src="selectedProjectGroup.url"
-        alt="Project group logo"
-      />
-    </template>
     <el-option
       v-for="projectGroup of list"
       :key="projectGroup.id"
@@ -25,12 +15,6 @@
       class="!pr-8 !pl-2 !h-14"
     >
       <div class="flex gap-2 items-start truncate">
-        <img
-          v-if="isUrl(projectGroup.url)"
-          class="max-w-5 max-h-5"
-          :src="projectGroup.url"
-          alt="Project group logo"
-        />
         <div class="block truncate mr-2">
           <div class="h-5 leading-5 text-xs text-gray-900 truncate">
             {{ projectGroup.name }}
@@ -49,7 +33,6 @@ import { storeToRefs } from 'pinia';
 import { computed, onMounted } from 'vue';
 import { useLfSegmentsStore } from '@/modules/lf/segments/store';
 import pluralize from 'pluralize';
-import isUrl from '@/utils/isUrl';
 
 const lsSegmentsStore = useLfSegmentsStore();
 const { projectGroups, selectedProjectGroup } = storeToRefs(lsSegmentsStore);
