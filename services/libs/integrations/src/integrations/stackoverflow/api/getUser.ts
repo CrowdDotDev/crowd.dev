27dd0049f958615faae122f8ec7ba129640ce6b1
garrrikkotua <kotuaigor@gmail.com>
2023-06-01 17:09:11 +0300

    first working version

--- services/libs/integrations/src/integrations/stackoverflow/api/getUser.ts
+++ services/libs/integrations/src/integrations/stackoverflow/api/getUser.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 import { timeout } from '@crowd/common'
 import { RateLimitError } from '@crowd/types'
 import { getNangoToken } from '../../nango'
-import { IProcessStreamContext } from 'src/types'
+import { IProcessStreamContext } from '@/types'
 import {
   StackOverflowPlatformSettings,
   StackOverflowUser,
