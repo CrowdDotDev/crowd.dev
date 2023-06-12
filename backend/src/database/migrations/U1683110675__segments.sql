4a6976de07f69aab7774b65a2aaf6a3e3a015563
Misha Savelyev <mikhail@crowd.dev>
2023-06-12 16:04:04 +0200

    Re-arrange segments migrations
    The 1st migration won't apply because it wants to make `segmentId` part
    of primary key. But it's not possible until we fill the field with data.
    We only fill it with data in the 2nd migration.
    
    Since we remove the field in the 3rd migration, lets just not creat it
    in the first place

--- backend/src/database/migrations/U1683110675__segments.sql
+++ backend/src/database/migrations/U1683110675__segments.sql
@@ -1,5 +1,5 @@
 
-drop table "segments" 
+drop table "segments"
 ALTER TABLE "activities" DROP COLUMN "segmentId";
 
 ALTER TABLE "integrations" DROP COLUMN "segmentId";
@@ -13,20 +13,6 @@ ALTER TABLE "tasks" DROP COLUMN "segmentId";
 ALTER TABLE "reports" DROP COLUMN "segmentId";
 
 ALTER TABLE "widgets" DROP COLUMN "segmentId";
-
-ALTER TABLE "memberToMerge" DROP CONSTRAINT "memberToMerge_pkey";
-
-ALTER TABLE public."memberToMerge" ADD CONSTRAINT "memberToMerge_pkey" PRIMARY KEY ("memberId", "toMergeId");
-
-ALTER TABLE "memberToMerge" DROP COLUMN "segmentId";
-
-
-ALTER TABLE "memberNoMerge" DROP CONSTRAINT "memberNoMerge_pkey";
-
-ALTER TABLE public."memberNoMerge" ADD CONSTRAINT "memberNoMerge_pkey" PRIMARY KEY ("memberId", "noMergeId");
-
-ALTER TABLE "memberNoMerge" DROP COLUMN "segmentId";
-
 
 DROP TABLE public."memberSegments";
 
--- backend/src/database/migrations/V1683110675__segments.sql
+++ backend/src/database/migrations/V1683110675__segments.sql
@@ -37,19 +37,6 @@ ALTER TABLE "reports" ADD COLUMN "segmentId" uuid;
 
 ALTER TABLE "widgets" ADD COLUMN "segmentId" uuid;
 
-ALTER TABLE "memberToMerge" ADD COLUMN "segmentId" uuid;
-
-ALTER TABLE "memberToMerge" DROP CONSTRAINT "memberToMerge_pkey";
-
-ALTER TABLE public."memberToMerge" ADD CONSTRAINT "memberToMerge_pkey" PRIMARY KEY ("memberId", "toMergeId", "segmentId");
-
-
-ALTER TABLE "memberNoMerge" ADD COLUMN "segmentId" uuid;
-
-ALTER TABLE "memberNoMerge" DROP CONSTRAINT "memberNoMerge_pkey";
-
-ALTER TABLE public."memberNoMerge" ADD CONSTRAINT "memberNoMerge_pkey" PRIMARY KEY ("memberId", "noMergeId", "segmentId");
-
 
 CREATE TABLE public."memberSegments" (
     "memberId" uuid NOT NULL,
