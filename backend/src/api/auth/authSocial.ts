ce82a79b839837d37c9e385e91eefc111529860c
Yeganathan S <hilariousengineer@protonmail.com>
2023-06-14 20:25:30 +0530

    feat: support for OAuth with GitHub

--- backend/src/api/auth/authSocial.ts
+++ backend/src/api/auth/authSocial.ts
@@ -1,6 +1,6 @@
 import passport from 'passport'
 import { getServiceChildLogger } from '@crowd/logging'
-import { API_CONFIG, GOOGLE_CONFIG } from '../../conf'
+import { API_CONFIG, GITHUB_CONFIG, GOOGLE_CONFIG } from '../../conf'
 import AuthService from '../../services/auth/authService'
 
 const log = getServiceChildLogger('AuthSocial')
@@ -42,6 +42,26 @@ export default (app, routes) => {
 
     routes.get('/auth/social/google/callback', (req, res) => {
       passport.authenticate('google', (err, jwtToken) => {
+        handleCallback(res, err, jwtToken)
+      })(req, res)
+    })
+  }
+
+  if (GITHUB_CONFIG.clientId) {
+    routes.get(
+      '/auth/social/github',
+      passport.authenticate('github', {
+        scope: ['user:email'],
+        session: false,
+      }),
+      () => {
+        // The request will be redirected for authentication, so this
+        // function will not be called.
+      },
+    )
+
+    routes.get('/auth/social/github/callback', (req, res) => {
+      passport.authenticate('github', (err, jwtToken) => {
         handleCallback(res, err, jwtToken)
       })(req, res)
     })
