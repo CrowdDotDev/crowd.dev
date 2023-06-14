84cb0a97222c7db28e4bd30b3e6062ef354cff37
Joan Reyero <joan@crowd.dev>
2023-06-14 14:51:04 +0100

    Fix channel

--- backend/src/serverless/integrations/services/integrations/githubIntegrationService.ts
+++ backend/src/serverless/integrations/services/integrations/githubIntegrationService.ts
@@ -1321,7 +1321,7 @@ export class GithubIntegrationService extends IntegrationServiceBase {
           tenant: context.integration.tenantId,
           username: author.user.login,
           platform: PlatformType.GITHUB,
-          channel: repo.name,
+          channel: repo.url,
           url: `https://github.com/${repo.owner}/${repo.name}.git`,
           body: record.commit.message,
           type: 'authored-commit',
