26a8977e07af02e1a9dccdb6639f923f651a7cf0
Gasper Grom <gasper.grom@gmail.com>
2023-06-14 12:59:22 +0200

    Fix errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,32 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(
-        new URL('./node_modules', import.meta.url),
-      ),
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+export default defineConfig(({mode}) => {
+  return {
+    define: {
+      'import.meta.env': process.env
     },
-  },
+    envPrefix: 'VUE_APP',
+    plugins: [vue(), Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+      dts: 'components.d.ts',
+    }),],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(
+          new URL('./node_modules', import.meta.url),
+        ),
+      },
+    },
+  }
 });
