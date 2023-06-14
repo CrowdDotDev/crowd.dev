d63d4b806362f42806fa22962a0870d9ea744ac4
Misha Savelyev <mikhail@crowd.dev>
2023-06-14 11:19:18 +0200

    Fix lint

--- backend/src/database/utils/segmentTestUtils.ts
+++ backend/src/database/utils/segmentTestUtils.ts
@@ -3,8 +3,6 @@ import SegmentRepository from '../repositories/segmentRepository'
 export async function populateSegments(options) {
   const repository = new SegmentRepository(options)
   options.currentSegments = await Promise.all(
-    options.currentSegments.map(async (segment) => {
-      return repository.findById(segment.id)
-    }),
+    options.currentSegments.map(async (segment) => repository.findById(segment.id)),
   )
 }
