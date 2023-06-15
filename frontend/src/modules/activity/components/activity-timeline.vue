38bbdd68715d3caa168b8f16c686f8feddf16b81
Joana Maia <joana@crowd.dev>
2023-06-15 15:01:18 +0100

    Add member and display object to activity's parent

--- frontend/src/modules/activity/components/activity-timeline.vue
+++ frontend/src/modules/activity/components/activity-timeline.vue
@@ -73,23 +73,55 @@
               with-link
               class="bl"
             />
-            <div class="flex gap-4 justify-between items-center h-9 -mt-1">
+            <div
+              class="flex gap-4 justify-between min-h-9 -mt-1"
+              :class="{
+                'items-center': !isMemberEntity,
+                'items-start': isMemberEntity,
+              }"
+            >
               <app-activity-header
                 :activity="activity"
                 class="flex flex-wrap items-center"
+                :class="{
+                  'mt-1.5': isMemberEntity,
+                }"
               />
 
-              <app-activity-dropdown
-                v-if="showAffiliations"
-                :show-affiliations="true"
-                :activity="activity"
-                :organizations="entity.organizations"
-                @on-update="fetchActivities({ reset: true })"
-              />
+              <div class="flex items-center flex-nowrap">
+                <a
+                  v-if="
+                    activity.conversationId && isMemberEntity
+                  "
+                  class="text-xs font-medium flex items-center mr-4 cursor-pointer"
+                  target="_blank"
+                  @click="
+                    conversationId = activity.conversationId
+                  "
+                >
+                  <i
+                    class="ri-lg ri-arrow-right-up-line mr-1"
+                  />
+                  <span class="block whitespace-nowrap">Open conversation</span>
+                </a>
+                <app-activity-dropdown
+                  v-if="showAffiliations"
+                  :show-affiliations="true"
+                  :activity="activity"
+                  :organizations="entity.organizations"
+                  @on-update="fetchActivities({ reset: true })"
+                />
+              </div>
             </div>
+
+            <app-lf-activity-parent
+              v-if="activity.parent && isMemberEntity"
+              :parent="activity.parent"
+            />
+
             <app-activity-content
               v-if="activity.title || activity.body"
-              class="text-sm bg-gray-50 rounded-lg p-4 mt-5"
+              class="text-sm bg-gray-50 rounded-lg p-4 mt-3"
               :activity="activity"
               :show-more="true"
             >
@@ -152,6 +184,12 @@
       </div>
     </div>
   </div>
+
+  <app-conversation-drawer
+    :expand="conversationId != null"
+    :conversation-id="conversationId"
+    @close="conversationId = null"
+  />
 </template>
 
 <script setup>
@@ -176,6 +214,8 @@ import AppActivityLink from '@/modules/activity/components/activity-link.vue';
 import AuthCurrentTenant from '@/modules/auth/auth-current-tenant';
 import AppActivityContentFooter from '@/modules/activity/components/activity-content-footer.vue';
 import AppActivityDropdown from '@/modules/activity/components/activity-dropdown.vue';
+import AppLfActivityParent from '@/modules/lf/activity/components/lf-activity-parent.vue';
+import AppConversationDrawer from '@/modules/conversation/components/conversation-drawer.vue';
 
 const SearchIcon = h(
   'i', // type
@@ -199,6 +239,8 @@ const props = defineProps({
   },
 });
 
+const conversationId = ref(null);
+
 const activeIntegrations = computed(() => {
   const activeIntegrationList = store.getters['integration/activeList'];
   return Object.keys(activeIntegrationList).map((i) => ({
@@ -216,6 +258,16 @@ const offset = ref(0);
 const noMore = ref(false);
 
 let filter = {};
+
+const isMemberEntity = computed(() => props.entityType === 'member');
+
+const segments = computed(() => props.entity.segments?.map((s) => {
+  if (typeof s === 'string') {
+    return s;
+  }
+
+  return s.id;
+}) || []);
 
 const fetchActivities = async ({ reset } = { reset: false }) => {
   const filterToApply = {
@@ -288,13 +340,7 @@ const fetchActivities = async ({ reset } = { reset: false }) => {
       orderBy: 'timestamp_DESC',
       limit: limit.value,
       offset: offset.value,
-      segments: props.entity.segments?.map((s) => {
-        if (typeof s === 'string') {
-          return s;
-        }
-
-        return s.id;
-      }) || [],
+      segments: segments.value,
     },
     {
       headers: {
