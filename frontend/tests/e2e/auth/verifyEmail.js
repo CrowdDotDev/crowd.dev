91991091f1d149469a6ab1925d5f020b7e4f1c05
Gasper Grom <gasper.grom@gmail.com>
2023-06-16 12:08:19 +0200

    Fix e2e tests

--- frontend/tests/e2e/auth/verifyEmail.js
+++ frontend/tests/e2e/auth/verifyEmail.js
@@ -1,11 +1,16 @@
 export default (inject) => () => {
   let verifyUrl;
   it('Opens verify link', () => {
+    cy.server();
+    cy.route('PUT', '/api/auth/verify-email').as('apiAuthVerifyEmail');
+    cy.route('GET', '/api/auth/me').as('apiAuthMe');
     cy.mailosaurGetMessage(inject.serverId, {
       sentTo: inject.email,
     }).then((email) => {
       verifyUrl = email.text.links[0].href;
       cy.visit(verifyUrl);
+      cy.wait('@apiAuthVerifyEmail');
+      cy.wait('@apiAuthMe');
     });
   });
 
