6639f1cf4c4e2bff1171a3224d6b86bad09fadc9
Misha Savelyev <mikhail@crowd.dev>
2023-06-19 10:25:23 +0200

    Remove unused code related to activity channels

--- backend/src/services/settingsService.ts
+++ backend/src/services/settingsService.ts
@@ -9,37 +9,6 @@ class SettingsService {
     return SettingsRepository.findOrCreateDefault(DEFAULT_SETTINGS, options)
   }
 
-  /**
-   * update activity channels after checking for duplicates with platform key
-   */
-  static async updateActivityChannels(data, options) {
-    if (!data.channel) {
-      throw new Error400(
-        options.language,
-        'settings.activityChannels.errors.typeRequiredWhenCreating',
-      )
-    }
-
-    const activityChannels = SettingsRepository.getActivityChannels(options)
-
-    if (activityChannels[data.platform]) {
-      const channelList = activityChannels[data.platform]
-      if (!channelList.includes(data.channel)) {
-        const updatedChannelList = [...channelList, data.channel]
-        activityChannels[data.platform] = updatedChannelList
-      }
-    } else {
-      activityChannels[data.platform] = [data.channel]
-    }
-    const updated = await SettingsRepository.save(
-      {
-        activityChannels,
-      },
-      options,
-    )
-    return updated.activityChannels
-  }
-
   static async save(data, options) {
     const transaction = await SequelizeRepository.createTransaction(options)
 
