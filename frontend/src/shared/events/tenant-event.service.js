8f77000a0afdf5ef6810bd623bcf326f8ccb9a63
Gasper Grom <gasper.grom@gmail.com>
2023-06-20 16:22:46 +0200

    Rename event tracking service

--- frontend/src/shared/events/tenant-event.service.js
+++ frontend/src/shared/events/tenant-event.service.js
@@ -1,8 +1,8 @@
 import authAxios from '@/shared/axios/auth-axios';
 import AuthCurrentTenant from '@/modules/auth/auth-current-tenant';
 
-export class EventTrackingService {
-  static async track(data) {
+export class TenantEventService {
+  static async event(data) {
     const tenantId = AuthCurrentTenant.get();
 
     const response = await authAxios.post(
