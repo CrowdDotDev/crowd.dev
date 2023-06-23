157f1d808c6c4bb6baf2ab89a7f4d93c0edf9932
Uroš Marolt <uros@crowd.dev>
2023-06-23 11:15:02 +0200

    fixed migration because of locks

--- backend/src/database/migrations/V1686900115__sync-timestamp.sql
+++ backend/src/database/migrations/V1686900115__sync-timestamp.sql
@@ -1,5 +1,13 @@
+-- Add the column with NOT NULL and a default value
 alter table members
-    add column "searchSyncedAt" timestamptz null;
+    add column "searchSyncedAt" timestamptz not null default now();
 
 alter table activities
-    add column "searchSyncedAt" timestamptz null;
+    add column "searchSyncedAt" timestamptz not null default now();
+
+-- Then, make the column nullable
+alter table members
+    alter column "searchSyncedAt" drop not null;
+
+alter table activities
+    alter column "searchSyncedAt" drop not null;
