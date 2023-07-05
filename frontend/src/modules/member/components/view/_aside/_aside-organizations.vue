b566bd147264a9136a88cc60671d4c17b58e3a33
Joana Maia <joana@crowd.dev>
2023-07-05 14:52:55 +0100

    Update member view page and list

---
+++ frontend/src/modules/member/components/view/_aside/_aside-organizations.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between">
+      <div class="font-medium text-black">
+        Organizations
+      </div>
+      <router-link
+        :to="{
+          name: 'memberEdit',
+          params: {
+            id: member.id,
+          },
+        }"
+        :class="{
+          'pointer-events-none cursor-not-allowed':
+            isEditLockedForSampleData,
+        }"
+      >
+        <el-button
+          class="btn btn-link btn-link--primary"
+          :disabled="isEditLockedForSampleData"
+        >
+          <i class="ri-pencil-line" /><span>Edit</span>
+        </el-button>
+      </router-link>
+    </div>
+
+    <div
+      v-if="member.organizations.length"
+      class="flex flex-col gap-4 mt-6"
+    >
+      <router-link
+        v-for="organization of member.organizations"
+        :key="organization.id"
+        :to="{
+          name: 'organizationView',
+          params: {
+            id: organization.id,
+          },
+        }"
+        class="group hover:cursor-pointer"
+      >
+        <div
+          class="flex justify-start items-center gap-3"
+        >
+          <div
+            class="w-8 h-8 rounded-md border border-gray-200 p-1.5 flex items-center justify-center overflow-hidden"
+            :class="{
+              'bg-white': organization.logo,
+              'bg-gray-50': !organization.logo,
+            }"
+          >
+            <img
+              v-if="organization.logo"
+              :src="organization.logo"
+              :alt="`${organization.displayName} logo`"
+            />
+            <i
+              v-else
+              class="ri-community-line text-lg text-gray-300"
+            />
+          </div>
+          <div
+            class="text-xs text-gray-900 group-hover:text-brand-500 transition"
+          >
+            {{ organization.displayName }}
+          </div>
+        </div>
+      </router-link>
+    </div>
+    <div v-else class="text-gray-400 mt-6 text-sm">
+      No organizations
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MemberPermissions } from '@/modules/member/member-permissions';
+import { mapGetters } from '@/shared/vuex/vuex.helpers';
+import { computed } from 'vue';
+
+defineProps({
+  member: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+const { currentTenant, currentUser } = mapGetters('auth');
+
+const isEditLockedForSampleData = computed(() => new MemberPermissions(
+  currentTenant.value,
+  currentUser.value,
+).editLockedForSampleData);
+</script>
