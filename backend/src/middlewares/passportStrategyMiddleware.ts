43c220256f025155f02eba98a53515f8ec563f0e
Yeganathan S <hilariousengineer@protonmail.com>
2023-07-07 17:53:48 +0530

    fix import and prettify the code

--- backend/src/middlewares/passportStrategyMiddleware.ts
+++ backend/src/middlewares/passportStrategyMiddleware.ts
@@ -1,9 +1,9 @@
 import { getServiceLogger } from '@crowd/logging'
 import passport from 'passport'
-import { GOOGLE_CONFIG, SLACK_CONFIG, GITHUB_CONFIG} from '../conf'
+import { GOOGLE_CONFIG, SLACK_CONFIG, GITHUB_CONFIG } from '../conf'
 import { getGoogleStrategy } from '../services/auth/passportStrategies/googleStrategy'
 import { getSlackStrategy } from '../services/auth/passportStrategies/slackStrategy'
-import { getGithubStrategy } from '@/services/auth/passportStrategies/githubStrategy'
+import { getGithubStrategy } from '../services/auth/passportStrategies/githubStrategy'
 
 const log = getServiceLogger()
 
