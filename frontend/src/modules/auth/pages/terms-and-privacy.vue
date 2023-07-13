55220321d65be9ed0609a9f5486f8db097d4dcc5
Gasper Grom <gasper.grom@gmail.com>
2023-07-13 13:45:06 +0200

    Remove form elements

--- frontend/src/modules/auth/pages/terms-and-privacy.vue
+++ frontend/src/modules/auth/pages/terms-and-privacy.vue
@@ -4,44 +4,34 @@
       Terms of service & privacy policy
     </h3>
     <div class="pt-10">
-      <el-form
-        ref="form"
-        :model="model"
-        class="form mb-2"
-        label-position="left"
-        label-width="0px"
-        @submit.prevent="doSubmit"
+      <el-checkbox
+        id="remember-me"
+        v-model="model[fields.acceptedTermsAndPrivacy.name]"
       >
-        <el-checkbox
-          id="remember-me"
-          v-model="model[fields.acceptedTermsAndPrivacy.name]"
-        >
-          <span class="text-sm text-gray-900">
-            I hereby accept the <a href="https://www.crowd.dev/terms-of-use" target="_blank" rel="noopener noreferrer">terms of service</a>
-            and <a href="https://www.crowd.dev/privacy-policy" target="_blank" rel="noopener noreferrer">privacy policy</a>.
-          </span>
-        </el-checkbox>
-        <div v-if="acceptTerms && !model[fields.acceptedTermsAndPrivacy.name]" class="flex items-center mt-1">
-          <i
-            class="h-4 flex items-center ri-error-warning-line text-base text-red-500"
-          />
-          <span
-            class="pl-1 text-2xs text-red-500 leading-4.5"
-          >You have to accept terms of service and privacy policy before continuing</span>
-        </div>
+        <span class="text-sm text-gray-900">
+          I hereby accept the <a href="https://www.crowd.dev/terms-of-use" target="_blank" rel="noopener noreferrer">terms of service</a>
+          and <a href="https://www.crowd.dev/privacy-policy" target="_blank" rel="noopener noreferrer">privacy policy</a>.
+        </span>
+      </el-checkbox>
+      <div v-if="acceptTerms && !model[fields.acceptedTermsAndPrivacy.name]" class="flex items-center mt-1">
+        <i
+          class="h-4 flex items-center ri-error-warning-line text-base text-red-500"
+        />
+        <span
+          class="pl-1 text-2xs text-red-500 leading-4.5"
+        >You have to accept terms of service and privacy policy before continuing</span>
+      </div>
 
-        <el-form-item class="pt-4 mb-0">
-          <el-button
-            id="submit"
-            :loading="loading"
-            :disabled="!model[fields.acceptedTermsAndPrivacy.name]"
-            native-type="submit"
-            class="w-full btn btn--primary btn--lg"
-          >
-            Continue
-          </el-button>
-        </el-form-item>
-      </el-form>
+      <el-button
+        id="submit"
+        :loading="loading"
+        :disabled="!model[fields.acceptedTermsAndPrivacy.name]"
+        native-type="submit"
+        class="w-full btn btn--primary btn--lg"
+        @click="doSubmit"
+      >
+        Continue
+      </el-button>
     </div>
   </div>
 </template>
@@ -85,7 +75,7 @@ export default {
           })
           .finally(() => {
             this.loading = false;
-          })
+          });
       } else {
         this.acceptTerms = true;
       }
