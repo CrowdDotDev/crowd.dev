43917110d34aa5cf7508344b128120db528d60e0
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2023-07-18 18:10:25 +0300

    Fix token retrieval in Discord integration (#1134)

--- services/libs/integrations/src/integrations/discord/types.ts
+++ services/libs/integrations/src/integrations/discord/types.ts
@@ -36,7 +36,6 @@ export interface IDiscordAPIData {
 
 export interface DiscordRootStreamData {
   guildId: string
-  token: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   channels: any[]
 }
