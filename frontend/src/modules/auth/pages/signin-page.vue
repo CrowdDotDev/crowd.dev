ee0bb2384f58699a243dcecab2e56ad6b0da70ad
Joana Maia <joana@crowd.dev>
2023-07-19 20:37:15 +0100

    Refactor authentication pages style

--- frontend/src/modules/auth/pages/signin-page.vue
+++ frontend/src/modules/auth/pages/signin-page.vue
@@ -1,21 +1,31 @@
 <template>
-  <div>
-    <h3 class="text-2xl leading-12 font-semibold mb-1">
-      Signing you in
-    </h3>
+  <div class="flex flex-col items-center gap-16">
+    <div class="flex gap-4 items-center">
+      <img src="/images/lf/logo_lfx_cm.svg" alt="lf logo" class="h-5" />
+      <el-divider direction="vertical" class="h-5 !m-0" />
+      <div class="text-xl text-brand-500">
+        Community Management
+      </div>
+    </div>
+    <div class="flex gap-4 items-center">
+      <div
+        v-loading="true"
+        class="app-page-spinner h-10 w-10 !relative !min-h-10"
+      />
+      <div class="font-semibold text-lg">
+        Signing you in...
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { Auth0Service } from '@/shared/services/auth0.service';
 
-Auth0Service.loginWithRedirect();
-
 export default {
   name: 'AppSigninPage',
-  data() {
-    return {
-    };
+  created() {
+    Auth0Service.loginWithRedirect();
   },
 };
 </script>
