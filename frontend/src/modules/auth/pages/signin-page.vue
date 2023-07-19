46397ad66c28404579acae6aa074826b828ba667
Misha Savelyev <mikhail@crowd.dev>
2023-07-19 13:37:54 +0200

    Trigger login with redirect when opening signin page

--- frontend/src/modules/auth/pages/signin-page.vue
+++ frontend/src/modules/auth/pages/signin-page.vue
@@ -1,34 +1,21 @@
 <template>
   <div>
     <h3 class="text-2xl leading-12 font-semibold mb-1">
-      👋 Welcome back
+      Signing you in
     </h3>
-    <div class="pt-10">
-      <a
-        id="googleLogin"
-        class="btn btn--secondary btn--lg w-full"
-        @click="signin()"
-      >
-        <span class="pl-3 text-gray-600">Sign in</span>
-      </a>
-    </div>
   </div>
 </template>
 
 <script>
 import { Auth0Service } from '@/shared/services/auth0.service';
 
+Auth0Service.loginWithRedirect();
+
 export default {
   name: 'AppSigninPage',
   data() {
     return {
     };
-  },
-
-  methods: {
-    signin() {
-      Auth0Service.loginWithRedirect();
-    },
   },
 };
 </script>
