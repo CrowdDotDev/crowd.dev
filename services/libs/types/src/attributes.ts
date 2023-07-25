73ab6185da4ed0effb5ab3af57cff072fd416f8c
anilb <epipav@gmail.com>
2023-07-25 11:12:10 +0200

    prod ci for integration sync worker, more linting

--- services/libs/types/src/attributes.ts
+++ services/libs/types/src/attributes.ts
@@ -1,4 +1,3 @@
-import { PlatformType } from './enums/platforms'
 export interface IAttributes {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: Record<string, any>
