7b13f365c97b8bfb031ecb2010ddf6e0af71462e
Yeganathan S <hilariousengineer@protonmail.com>
2023-07-28 02:36:00 +0530

    rewrite in vue composition api

--- frontend/src/modules/member/components/member-edit-attributes.vue
+++ frontend/src/modules/member/components/member-edit-attributes.vue
@@ -2,7 +2,7 @@
   <app-dialog v-if="computedVisible" v-model="computedVisible" title="Edit attributes">
     <template #content>
       <div class="px-6 pb-6">
-        <el-form ref="form" :model="formInline" class="attributes-form">
+        <el-form :model="form" class="attributes-form">
           <div class="rounded-md bg-yellow-50 border border-yellow-100 flex items-center gap-2 py-3 px-4 mt-2">
             <i class="ri-alert-fill text-yellow-500 text-base " />
             <span class="text-xs leading-5 text-gray-900">Some changes may overwrite current attributes from the
@@ -10,7 +10,7 @@
           </div>
           <!-- <h2 class="text-lg font-semibold mb-4">Attributes</h2> -->
 
-          <div class="mt-6 mb-8 flex flex-col gap-4">
+          <!-- <div class="mt-6 mb-8 flex flex-col gap-4">
             <h6 class="text-xs text-gray-400">
               DEFAULT ATTRIBUTES
             </h6>
@@ -35,60 +35,76 @@
               <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
             </div>
 
-          </div>
-          <div class="mt-4 flex flex-col gap-8">
+          </div> -->
+          <div class="mt-6 mb-10 flex flex-col gap-8">
             <h6 class="text-xs text-gray-400">
               CUSTOM ATTRIBUTES
             </h6>
 
-            <div class="flex">
+            <div v-for="(attribute, index) in customAttributes" :key="index" class="flex">
               <div class="flex flex-col flex-shrink-0 w-1/3">
-                <span class="text-xs font-medium text-black">Organizations</span>
-                <p class="text-2xs">
-                  {attribute type}
+                <span class="text-xs font-medium text-gray-900">{{ attribute.label }}</span>
+                <p class="text-2xs text-gray-500">
+                  {{ attributesTypes[attribute.type] }}
                 </p>
               </div>
-              <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
-            </div>
 
-            <div class="flex">
-              <div class="flex flex-col flex-shrink-0 w-1/3">
-                <span class="text-xs font-medium text-black">Website</span>
-                <p class="text-2xs">
-                  URL
-                </p>
-              </div>
-              <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
-            </div>
+              <el-date-picker
+                v-if="attribute.type === 'date'"
+                v-model="model[attribute.name]"
+                :prefix-icon="CalendarIcon"
+                :clearable="false"
+                class="custom-date-picker"
+                popper-class="date-picker-popper"
+                type="date"
+                value-format="YYYY-MM-DD"
+                format="YYYY-MM-DD"
+                placeholder="YYYY-MM-DD"
+              />
+              <!-- <app-autocomplete-many-input
+                v-else-if="attribute.type === 'multiSelect'"
+                v-model="model[attribute.name]"
+                :fetch-fn="
+                  () => fetchCustomAttribute(attribute.id)
+                "
+                :create-fn="
+                  (value) =>
+                    updateCustomAttribute(attribute, value)
+                "
+                placeholder="Select an option or create one"
+                input-class="w-full multi-select-field"
+                :create-if-not-found="true"
+                :collapse-tags="true"
+                :parse-model="true"
+                :are-options-in-memory="true"
+              /> -->
+              <el-select
+                v-else-if="attribute.type === 'boolean'"
+                v-model="model[attribute.name]"
+                class="w-full"
+                clearable
+                placeholder="Select option"
+              >
+                <el-option
+                  key="true"
+                  label="True"
+                  :value="true"
+                  @mouseleave="onSelectMouseLeave"
+                />
+                <el-option
+                  key="false"
+                  label="False"
+                  :value="false"
+                  @mouseleave="onSelectMouseLeave"
+                />
+              </el-select>
 
-            <div class="flex">
-              <div class="flex flex-col flex-shrink-0 w-1/3">
-                <span class="text-xs font-medium text-black">is Hireable</span>
-                <p class="text-2xs">
-                    Boolean
-                </p>
-              </div>
-              <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
-            </div>
-
-            <div class="flex">
-              <div class="flex flex-col flex-shrink-0 w-1/3">
-                <span class="text-xs font-medium text-black">{attribute}</span>
-                <p class="text-2xs">
-                  {attribute type}
-                </p>
-              </div>
-              <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
-            </div>
-
-            <div class="flex">
-              <div class="flex flex-col flex-shrink-0 w-1/3">
-                <span class="text-xs font-medium text-black">{attribute}</span>
-                <p class="text-2xs">
-                  {attribute type}
-                </p>
-              </div>
-              <el-input v-model="computedVisible" type="text" class="flex-grow ml-4" />
+              <el-input
+                v-else
+                v-model="model[attribute.name]"
+                :type="attribute.type"
+                clearable
+              />
             </div>
           </div>
         </el-form>
@@ -106,73 +122,99 @@
   </app-dialog>
 </template>
 
-<script>
+<script setup>
 import { mapActions } from 'vuex';
 import { storeToRefs } from 'pinia';
-import { reactive } from 'vue';
+import {
+  reactive, ref, computed, h,
+} from 'vue';
 import { MemberModel } from '@/modules/member/member-model';
 import AppDialog from '@/shared/dialog/dialog.vue';
-import AppTagAutocompleteInput from '@/modules/tag/components/tag-autocomplete-input.vue';
 import { FormSchema } from '@/shared/form/form-schema';
 import { useMemberStore } from '@/modules/member/store/pinia';
-import AppFormItem from '@/shared/form/form-item.vue';
+
+const CalendarIcon = h(
+  'i', // type
+  {
+    class:
+      'ri-calendar-line text-base leading-none text-gray-400',
+  }, // props
+  [],
+);
 
 const memberStore = useMemberStore();
 const { selectedMembers } = storeToRefs(memberStore);
 
 const { fields } = MemberModel;
+const formSchema = new FormSchema([fields.attributes]);
 
-const formInline = reactive({
-  user: '',
-  region: '',
-  date: '',
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-export default {
-  name: 'AppEditAttributesPopover',
-  components: { AppDialog, AppFormItem },
+const emits = defineEmits(['reload', 'update:modelValue']);
 
-  props: {
-    modelValue: {
-      type: Boolean,
-      default: () => false,
-    },
-  },
-  emits: ['reload', 'update:modelValue'],
-
-  data() {
-    return {
-      loading: false,
-    };
-  },
-
-  computed: {
-    fields() {
-      return fields;
-    },
-    computedVisible: {
-      get() {
-        return this.modelValue;
-      },
-      set() {
-        this.$emit('update:modelValue', false);
-      },
-    },
-  },
-
-  methods: {
-    async handleSubmit() {
-      this.loading = true;
-      // do something
-    },
-
-    handleCancel() {
-      this.editTagsModel = [];
-      this.editTagsInCommon = [];
-      this.computedVisible = false;
-    },
-  },
+const attributesTypes = {
+  string: 'Text',
+  number: 'Number',
+  email: 'E-mail',
+  url: 'URL',
+  date: 'Date',
+  boolean: 'Boolean',
+  multiSelect: 'Multi-select',
 };
-</script>
 
-<style scoped></style>
+const loading = ref(false);
+
+const model = ref({
+  yearsOfExperience: '',
+  country: '',
+  isHireable: '',
+  expertise: '',
+  seniorityLevel: '',
+})
+
+const customAttributes = [
+  {
+    label: 'Years of Experience', name: 'yearsOfExperience', type: 'number',
+  },
+  {
+    label: 'Country', name: 'country', type: 'string',
+  },
+  {
+    label: 'is Hireable', name: 'isHireable', type: 'boolean',
+  },
+  {
+    label: 'Expertise', name: 'expertise', type: 'multiSelect',
+  },
+  {
+    label: 'Skills', name: 'skills', type: 'multiSelect',
+  },
+  {
+    label: 'Seniority Level', name: 'seniorityLevel', type: 'string',
+  },
+];
+
+const handleSubmit = async () => {
+  this.loading = true;
+  // do something
+  console.log("helo", this.model);
+};
+
+const handleCancel = () => {
+  computeVisible.value = false;
+};
+
+const computedVisible = computed({
+  get() {
+    return props.modelValue;
+  },
+  set() {
+    emits['update:modelValue'](false);
+  },
+});
+
+</script>
