6f2027fa615837a1defb03fd0c3da1a0bb9999f9
anil <epipav@gmail.com>
2023-07-31 12:22:12 +0200

    linting lib packages

--- services/libs/integrations/src/types.ts
+++ services/libs/integrations/src/types.ts
@@ -1,10 +1,4 @@
-import {
-  IMemberAttribute,
-  IActivityData,
-  IntegrationResultType,
-  IMemberIdentity,
-  Entity,
-} from '@crowd/types'
+import { IMemberAttribute, IActivityData, IntegrationResultType, Entity } from '@crowd/types'
 import { Logger } from '@crowd/logging'
 import { ICache, IIntegration, IIntegrationStream, IRateLimiter } from '@crowd/types'
 
