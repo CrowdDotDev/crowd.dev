946fc25db7ec732d6f81960313447d840151afb6
anil <epipav@gmail.com>
2023-08-01 13:09:09 +0200

    get mappable fields also returns readonly information

--- backend/src/api/integration/helpers/hubspotConnect.ts
+++ backend/src/api/integration/helpers/hubspotConnect.ts
@@ -3,7 +3,7 @@ import IntegrationService from '../../../services/integrationService'
 import PermissionChecker from '../../../services/user/permissionChecker'
 
 export default async (req, res) => {
-  new PermissionChecker(req).validateHas(Permissions.values.tenantEdit)
+  new PermissionChecker(req).validateHas(Permissions.values.integrationEdit)
   const payload = await new IntegrationService(req).hubspotConnect()
   await req.responseHandler.success(req, res, payload)
 }
--- services/libs/integrations/src/integrations/premium/hubspot/types.ts
+++ services/libs/integrations/src/integrations/premium/hubspot/types.ts
@@ -83,6 +83,41 @@ export interface IHubspotProperty {
   modificationMetadata: IHubspotPropertyModificationMetadata
 }
 
+export interface IHubspotListMetadata {
+  size: number
+  lastSizeChangeAt: number
+  processing: string
+  lastProcessingStateChangeAt: number
+  error: string
+  listReferencesCount: number
+  parentFolderId: number
+}
+
+export interface IHubspotList {
+  portalId: number
+  listId: number
+  createdAt: number
+  updatedAt: number
+  name: string
+  listType: string
+  authorId: number
+  parentId: number
+  filters: string[]
+  metaData: IHubspotListMetadata
+  archived: boolean
+  teamIds: number[]
+  ilsFilterBranch: string
+  readOnly: boolean
+  internal: boolean
+  limitExempt: boolean
+  dynamic: boolean
+}
+
+export interface ITypeInfo {
+  hubspotType: HubspotPropertyType
+  readonly: boolean
+}
+
 export interface IHubspotObject {
   id: string
   properties: unknown
