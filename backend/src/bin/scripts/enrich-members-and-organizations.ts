5833b7e3b37dec6d985c0e38e549992f8c4d153b
Yeganathan S <hilariousengineer@protonmail.com>
2023-08-09 18:22:04 +0530

    Prettify the code

--- backend/src/bin/scripts/enrich-members-and-organizations.ts
+++ backend/src/bin/scripts/enrich-members-and-organizations.ts
@@ -146,7 +146,7 @@ if (parameters.help || (!parameters.tenant && (!parameters.organization || !para
               tenantId,
               // Since there is no pagination implemented for the organizations enrichment,
               // we set a limit of 20,000 to ensure all organizations are included when enriched in bulk.
-              maxEnrichLimit: 20000, 
+              maxEnrichLimit: 20000,
             } as NodeWorkerMessageBase
 
             await sendNodeWorkerMessage(tenantId, payload)
