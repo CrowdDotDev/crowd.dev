780116a85ff17945926d2454bb8ed901be849ef2
Misha Savelyev <mikhail@crowd.dev>
2023-08-22 16:28:02 +0000

    Don't actually update segments with activity channels unnecessarily

--- services/apps/data_sink_worker/src/service/settings.repo.ts
+++ services/apps/data_sink_worker/src/service/settings.repo.ts
@@ -85,7 +85,18 @@ export default class SettingsRepository extends RepositoryBase<SettingsRepositor
                     end
       where "tenantId" = $(tenantId)
         and id = $(segmentId)
-
+        and case
+              -- If platform exists, and channel does not exist, add it
+              when "activityChannels" ? $(platform)
+                  and not ($(channel) = any (select jsonb_array_elements_text("activityChannels" -> $(platform)))) then
+                  1
+              -- If platform does not exist, create it
+              when not ("activityChannels" ? $(platform)) or "activityChannels" is null then
+                  1
+              -- Else, do nothing
+              else
+                  0
+            end = 1
       `,
       {
         tenantId,
@@ -94,7 +105,5 @@ export default class SettingsRepository extends RepositoryBase<SettingsRepositor
         channel,
       },
     )
-
-    this.checkUpdateRowCount(result.rowCount, 1)
   }
 }
