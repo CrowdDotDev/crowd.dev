ec4ea572908ef5449668510962be426a9d0a4edc
Gasper Grom <gasper.grom@gmail.com>
2023-08-22 17:30:39 +0200

    Fix identify check

--- frontend/src/shared/monitoring/identify.js
+++ frontend/src/shared/monitoring/identify.js
@@ -2,6 +2,9 @@ import LogRocket from 'logrocket';
 import config from '@/config';
 
 export default function identify(user) {
+  if (!user) {
+    return;
+  }
   window.analytics.identify(user.id, {
     name: user.fullName,
     firstName: user.firstName,
