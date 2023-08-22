24321f67d8fd08feedd290a4842e244e6b385427
Uroš Marolt <uros@crowd.dev>
2023-08-22 12:44:21 +0200

    bugfix

--- services/libs/common/src/byteLength.ts
+++ services/libs/common/src/byteLength.ts
@@ -1,4 +1,12 @@
 export const trimUtf8ToMaxByteLength = (utf8Str: string, maxByteLength: number): string => {
+  if (utf8Str === null) {
+    return null
+  }
+
+  if (utf8Str === undefined) {
+    return undefined
+  }
+
   if (Buffer.byteLength(utf8Str, 'utf8') > maxByteLength) {
     // this will get us close but some characters could be multibyte encoded so we might need to trim a bit more
     utf8Str = utf8Str.slice(0, maxByteLength)
