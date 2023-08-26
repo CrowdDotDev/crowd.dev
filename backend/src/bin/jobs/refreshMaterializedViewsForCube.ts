8bb6095c642ef2ff87fa8732d9445bbe9125d3b6
Misha Savelyev <mikhail@crowd.dev>
2023-08-26 10:38:22 +0000

    Create cronjob for refreshing cubejs materlialized views

---
+++ backend/src/bin/jobs/refreshMaterializedViewsForCube.ts
@@ -0,0 +1,39 @@
+import cronGenerator from 'cron-time-generator'
+import { Logger, logExecutionTimeV2 } from '@crowd/logging'
+import SequelizeRepository from '../../database/repositories/sequelizeRepository'
+import { CrowdJob } from '../../types/jobTypes'
+
+let processing = false
+
+const job: CrowdJob = {
+  name: 'Refresh Materialized View For Cube',
+  cronTime: cronGenerator.every(10).minutes(),
+  onTrigger: async (log: Logger) => {
+    if (!processing) {
+      processing = true
+    } else {
+      return
+    }
+    const dbOptions = await SequelizeRepository.getDefaultIRepositoryOptions()
+
+    const materializedViews = [
+      'mv_members_cube',
+      'mv_activities_cube',
+      'mv_organizations_cube',
+      'mv_segments_cube',
+    ]
+
+    for (const view of materializedViews) {
+      await logExecutionTimeV2(
+        () =>
+          dbOptions.database.sequelize.query(`REFRESH MATERIALIZED VIEW CONCURRENTLY "${view}"`),
+        log,
+        `Refresh Materialized View ${view}`,
+      )
+    }
+
+    processing = false
+  },
+}
+
+export default job
