ee27b4653d4635bd5a43d68b89776d1e411c8cc0
garrrikkotua <kotuaigor@gmail.com>
2023-08-29 15:55:55 +0300

    change webhooks logic

--- backend/src/api/webhooks/groupsio.ts
+++ backend/src/api/webhooks/groupsio.ts
@@ -1,7 +1,5 @@
-import { PlatformType } from '@crowd/types'
 import IntegrationRepository from '../../database/repositories/integrationRepository'
 import SequelizeRepository from '../../database/repositories/sequelizeRepository'
-import TenantRepository from '../../database/repositories/tenantRepository'
 import IncomingWebhookRepository from '../../database/repositories/incomingWebhookRepository'
 import { WebhookType } from '../../types/webhooks'
 import { getIntegrationStreamWorkerEmitter } from '@/serverless/utils/serviceSQS'
@@ -14,13 +12,19 @@ export default async (req, res) => {
   // TODO: Validate signature
   // TODO: tied to webhook to integration by group name / group id
 
+  if (!data?.group?.name) {
+    await req.responseHandler.error(req, res, {
+      message: 'No group name found in Groups.io Webhook payload, skipping!',
+    })
+    return
+  }
+
   const options = await SequelizeRepository.getDefaultIRepositoryOptions()
-  const tenant = await TenantRepository.findById(req.params.tenantId, options)
-  const optionsWithTenant = await SequelizeRepository.getDefaultIRepositoryOptions(null, tenant)
-  const integration = (await IntegrationRepository.findByPlatform(
-    PlatformType.GROUPSIO,
-    optionsWithTenant,
-  )) as any
+  const integration = await IntegrationRepository.findGroupsioIntegrationByGrounName(
+    data.group.name,
+    options,
+  )
+
 
   if (integration) {
     req.log.info({ integrationId: integration.id }, 'Incoming Groups.io Webhook!')
@@ -48,7 +52,7 @@ export default async (req, res) => {
 
     await req.responseHandler.success(req, res, {}, 204)
   } else {
-    req.log.error({ tenant }, 'No integration found for incoming Groups.io Webhook!')
+    req.log.error({ event }, 'No integration found for incoming Groups.io Webhook!')
     await req.responseHandler.success(req, res, {}, 200)
   }
 }
--- backend/src/database/repositories/integrationRepository.ts
+++ backend/src/database/repositories/integrationRepository.ts
@@ -1,6 +1,6 @@
 import lodash from 'lodash'
 import Sequelize, { QueryTypes } from 'sequelize'
-import { IntegrationRunState } from '@crowd/types'
+import { IntegrationRunState , PlatformType } from '@crowd/types'
 import SequelizeRepository from './sequelizeRepository'
 import AuditLogRepository from './auditLogRepository'
 import SequelizeFilterUtils from '../utils/sequelizeFilterUtils'
@@ -569,6 +569,37 @@ class IntegrationRepository {
 
     return output
   }
+
+  static async findGroupsioIntegrationByGrounName(groupName: string, options: IRepositoryOptions) {
+
+    // I need a a query to find integration of type PlatformType.GROUPSIO by groupName
+    // integration table has settings column which is a jsonb column
+    // inside it where is a groupNames fiels which is an array of strings
+    const transaction = SequelizeRepository.getTransaction(options)
+
+    const currentTenant = SequelizeRepository.getCurrentTenant(options)
+
+    const record = await options.database.integration.findOne({
+      where: {
+        platform: PlatformType.GROUPSIO,
+        tenantId: currentTenant.id,
+        settings: {
+          groups: {
+            [Op.contains]: [groupName]
+          }
+        }
+      },
+      transaction,
+    })
+
+    if (!record) {
+      return null
+    }
+
+    return this._populateRelations(record)
+
+  }
+
 }
 
 export default IntegrationRepository
