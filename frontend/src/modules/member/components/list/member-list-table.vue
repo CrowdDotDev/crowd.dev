6b775a6718c71519d4cb38c7e9a461d1396f332d
Yeganathan S <hilariousengineer@protonmail.com>
2023-08-31 19:33:53 +0530

    Fix sorting by displayName for members and organizations

--- frontend/src/modules/member/components/list/member-list-table.vue
+++ frontend/src/modules/member/components/list/member-list-table.vue
@@ -102,6 +102,7 @@
                 width="250"
                 fixed
                 class="-my-2"
+                sortable="custom"
               >
                 <template #default="scope">
                   <router-link
