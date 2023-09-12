fe5b3f082a70b8e396b3f5afb92d2d874e4b0063
Gašper Grom <gasper.grom@gmail.com>
2023-09-12 10:58:16 +0200

    Force typescript for org enrich config (#1467)

--- frontend/src/modules/organization/config/enrichment/grossDeparturesByMonth.ts
+++ frontend/src/modules/organization/config/enrichment/grossDeparturesByMonth.ts
@@ -1,16 +1,19 @@
-import { attributesTypes } from '@/modules/organization/types/Attributes';
+import { AttributeType } from '@/modules/organization/types/Attributes';
 import OrganizationAttributesJSONRenderer from '@/modules/organization/components/organization-attributes-json-renderer.vue';
 import { formatDate } from '@/utils/date';
+import { OrganizationEnrichmentConfig } from '@/modules/organization/config/enrichment/index';
 
-export default {
+const grossDeparturesByMonth: OrganizationEnrichmentConfig = {
   name: 'grossDeparturesByMonth',
   label: 'Gross Departures by Month',
-  type: attributesTypes.json,
+  type: AttributeType.JSON,
   showInForm: true,
   showInAttributes: true,
   component: OrganizationAttributesJSONRenderer,
   keyParser: (key) => formatDate({
     timestamp: key,
     format: 'MMMM YYYY',
-  }),
+  } as any),
 };
+
+export default grossDeparturesByMonth;
