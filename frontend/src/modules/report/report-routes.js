70831edce88ee16495e33529a82c588138155e14
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-09-14 02:04:21 +0300

    chore: change templates to defaults

--- frontend/src/modules/report/report-routes.js
+++ frontend/src/modules/report/report-routes.js
@@ -29,7 +29,7 @@ export default [
       },
       {
         name: 'reportTemplate',
-        path: '/reports/template/:id',
+        path: '/reports/defaults/:id',
         component: ReportTemplatePage,
         meta: {
           auth: true,
