f8d9d9f4dae81afb0fb8d1b75c9b3faf0314bddd
Uroš Marolt <uros@crowd.dev>
2023-09-22 13:48:31 +0200

    added tenant and integration processing parameters

--- services/apps/integration_stream_worker/src/bin/process-all-streams.ts
+++ services/apps/integration_stream_worker/src/bin/process-all-streams.ts
@@ -11,11 +11,47 @@ import {
   getSqsClient,
 } from '@crowd/sqs'
 import { IntegrationStreamState } from '@crowd/types'
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
 
 const BATCH_SIZE = 100
 const MAX_CONCURRENT = 3
 
 const log = getServiceLogger()
+
+const options = [
+  {
+    name: 'tenant',
+    alias: 't',
+    type: String,
+    description: 'The unique ID of tenant.',
+  },
+  {
+    name: 'integration',
+    alias: 'i',
+    type: String,
+    description: 'The unique ID of the integration.',
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Print this usage guide.',
+  },
+]
+const sections = [
+  {
+    header: 'Update tenant plan',
+    content: 'Updates tenant plan.',
+  },
+  {
+    header: 'Options',
+    optionList: options,
+  },
+]
+
+const usage = commandLineUsage(sections)
+const parameters = commandLineArgs(options)
 
 async function processStream(
   streamId: string,
@@ -26,6 +62,11 @@ async function processStream(
   } catch (err) {
     return false
   }
+}
+
+if (parameters.help || !(parameters.tenant && parameters.integration)) {
+  console.log(usage)
+  process.exit(1)
 }
 
 setImmediate(async () => {
@@ -52,14 +93,31 @@ setImmediate(async () => {
     log,
   )
 
+  const conditions = [
+    `state in ('${IntegrationStreamState.ERROR}', '${IntegrationStreamState.PENDING}')`,
+  ]
+
+  const params: Record<string, unknown> = {}
+
+  if (parameters.tenant) {
+    conditions.push(`"tenantId" = $(tenantId)`)
+    params.tenantId = parameters.tenant
+  }
+
+  if (parameters.integration) {
+    conditions.push(`"integrationId" = $(integrationId)`)
+    params.integrationId = parameters.integration
+  }
+
   let results = await dbConnection.any(
     `
     select id
     from integration.streams
-    where state in ('${IntegrationStreamState.ERROR}', '${IntegrationStreamState.PENDING}')
+    where ${conditions.join(' and ')}
     order by id
     limit ${BATCH_SIZE};
     `,
+    params,
   )
 
   let current = 0
@@ -91,14 +149,14 @@ setImmediate(async () => {
       `
       select id
       from integration.streams
-      where state in ('${IntegrationStreamState.ERROR}', '${IntegrationStreamState.PENDING}')
+      where ${conditions.join(' and ')}
       and id > $(lastId)
       order by id
       limit ${BATCH_SIZE};
       `,
-      {
+      Object.assign({}, params, {
         lastId: results[results.length - 1].id,
-      },
+      }),
     )
   }
 
