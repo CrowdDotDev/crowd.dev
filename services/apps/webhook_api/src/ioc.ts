e28e87fc7862dd144907244131e6fef681e2af67
Uroš Marolt <uros@crowd.dev>
2023-09-26 13:55:04 +0200

    wip

--- services/apps/webhook_api/src/ioc.ts
+++ services/apps/webhook_api/src/ioc.ts
@@ -1,28 +1,15 @@
-import 'reflect-metadata'
-
 import { DATABASE_IOC_MODULE } from '@crowd/database'
 import { LOGGING_IOC, LOGGING_IOC_MODULE, Logger } from '@crowd/logging'
 import { Emitters, SQS_IOC_MODULE } from '@crowd/sqs'
-import { Container } from 'inversify'
 import { DB_CONFIG, SQS_CONFIG } from './conf'
-
-export const IOC = new Container({
-  skipBaseClassChecks: true,
-  autoBindInjectable: true,
-})
-
-export const childIocContainer = (): Container => {
-  const child = new Container()
-  child.parent = IOC
-
-  return child
-}
+import { IOC } from '@crowd/ioc'
 
 export const APP_IOC_MODULE = async (maxConcurrentProcessing: number): Promise<void> => {
-  IOC.load(LOGGING_IOC_MODULE())
-  const log = IOC.get<Logger>(LOGGING_IOC.logger)
+  const ioc = IOC()
+  ioc.load(LOGGING_IOC_MODULE())
+  const log = ioc.get<Logger>(LOGGING_IOC.logger)
   log.info('Loading IOC container...')
 
-  IOC.load(await DATABASE_IOC_MODULE(IOC, DB_CONFIG(), maxConcurrentProcessing))
-  IOC.load(await SQS_IOC_MODULE(IOC, SQS_CONFIG(), Emitters.INTEGRATION_STREAM_WORKER))
+  ioc.load(await DATABASE_IOC_MODULE(ioc, DB_CONFIG(), maxConcurrentProcessing))
+  ioc.load(await SQS_IOC_MODULE(ioc, SQS_CONFIG(), Emitters.INTEGRATION_STREAM_WORKER))
 }
---
+++ services/libs/ioc/src/index.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'
+
+import { IS_TEST_ENV } from '@crowd/common'
+import { Container } from 'inversify'
+
+let instance: Container | undefined
+
+export const IOC = (): Container => {
+  if (instance) {
+    return instance
+  }
+
+  instance = new Container({
+    skipBaseClassChecks: true,
+    autoBindInjectable: true,
+  })
+}
+
+export const childIocContainer = (): Container => {
+  const child = new Container()
+  child.parent = IOC()
+
+  return child
+}
+
+export const setIocInstance = (ioc: Container): void => {
+  if (IS_TEST_ENV) {
+    instance = ioc
+  } else {
+    throw new Error('IOC instance can only be set in test environment')
+  }
+}
