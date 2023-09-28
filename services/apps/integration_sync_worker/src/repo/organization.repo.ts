ae38aa926f006c7e42960af4dbf23b9f48811e4d
anil <epipav@gmail.com>
2023-09-28 18:08:56 +0200

    update org.findById in integration sync worker

--- services/apps/integration_sync_worker/src/repo/organization.repo.ts
+++ services/apps/integration_sync_worker/src/repo/organization.repo.ts
@@ -84,80 +84,89 @@ export class OrganizationRepository extends RepositoryBase<OrganizationRepositor
     return results
   }
 
-  public async findOrganization(organizationId: string, tenantId: string): Promise<IOrganization> {
+  public async findOrganization(
+    organizationId: string,
+    tenantId: string,
+    segmentId: string,
+  ): Promise<IOrganization> {
+    const segmentsSubQuery = `
+    with input_segment as (select id,
+                                  slug,
+                                  "parentSlug",
+                                  "grandparentSlug"
+                          from segments
+                          where id = $(segmentId)
+                            and "tenantId" = $(tenantId)),
+                          segment_level as (select case
+                                      when "parentSlug" is not null and "grandparentSlug" is not null
+                                          then 'child'
+                                      when "parentSlug" is not null and "grandparentSlug" is null
+                                          then 'parent'
+                                      when "parentSlug" is null and "grandparentSlug" is null
+                                          then 'grandparent'
+                                      end as level,
+                                  id,
+                                  slug,
+                                  "parentSlug",
+                                  "grandparentSlug"
+                          from input_segment)
+                          select s.id
+                          from segments s
+                          join
+                          segment_level sl
+                          on
+                          (sl.level = 'child' and s.id = sl.id) or
+                          (sl.level = 'parent' and s."parentSlug" = sl.slug and s."grandparentSlug" is not null) or
+                          (sl.level = 'grandparent' and s."grandparentSlug" = sl.slug)`
+
     return await this.db().oneOrNone(
       `
-      WITH
-      activity_counts AS (
-          SELECT mo."organizationId", COUNT(a.id) AS "activityCount"
-          FROM "memberOrganizations" mo
-          LEFT JOIN activities a ON a."memberId" = mo."memberId"
-          WHERE mo."organizationId" = $(organizationId)
-            AND mo."deletedAt" IS NULL
-          GROUP BY mo."organizationId"
-      ),
-      member_counts AS (
-          SELECT "organizationId", COUNT(DISTINCT "memberId") AS "memberCount"
-          FROM "memberOrganizations"
-          WHERE "organizationId" = $(organizationId)
-            AND "deletedAt" IS NULL
-          GROUP BY "organizationId"
-      ),
-      active_on AS (
-          SELECT mo."organizationId", ARRAY_AGG(DISTINCT platform) AS "activeOn"
-          FROM "memberOrganizations" mo
-          JOIN activities a ON a."memberId" = mo."memberId"
-          WHERE mo."organizationId" = $(organizationId)
-            AND mo."deletedAt" IS NULL
-          GROUP BY mo."organizationId"
-      ),
-      identities AS (
-          SELECT "organizationId", ARRAY_AGG(DISTINCT platform) AS "identities"
-          FROM "memberOrganizations" mo
-          JOIN "memberIdentities" mi ON mi."memberId" = mo."memberId"
-          WHERE mo."organizationId" = $(organizationId)
-            AND mo."deletedAt" IS NULL
-          GROUP BY "organizationId"
-      ),
-      last_active AS (
-          SELECT mo."organizationId", MAX(timestamp) AS "lastActive", MIN(timestamp) AS "joinedAt"
-          FROM "memberOrganizations" mo
-          JOIN activities a ON a."memberId" = mo."memberId"
-          WHERE mo."organizationId" = $(organizationId)
-            AND mo."deletedAt" IS NULL
-          GROUP BY mo."organizationId"
-      ),
-      segments AS (
-          SELECT "organizationId", ARRAY_AGG("segmentId") AS "segments"
-          FROM "organizationSegments"
-          WHERE "organizationId" = $(organizationId)
-          GROUP BY "organizationId"
+      with leaf_segment_ids as (${segmentsSubQuery}),
+      member_data as (select a."organizationId",
+          count(distinct a."memberId")                                                        as "memberCount",
+          count(distinct a.id)                                                        as "activityCount",
+          case
+              when array_agg(distinct a.platform) = array [null] then array []::text[]
+              else array_agg(distinct a.platform) end                                 as "activeOn",
+          max(a.timestamp)                                                            as "lastActive",
+          min(a.timestamp) filter ( where a.timestamp <> '1970-01-01T00:00:00.000Z' ) as "joinedAt"
+      from leaf_segment_ids ls
+            join activities a
+                      on a."segmentId" = ls.id and a."organizationId" = $(id) and
+                        a."deletedAt" is null
+            join members m on a."memberId" = m.id and m."deletedAt" is null
+            join "memberOrganizations" mo on m.id = mo."memberId" and mo."organizationId" = $(id) and mo."dateEnd" is null
+      group by a."organizationId"),
+      organization_segments as (select "organizationId", array_agg("segmentId") as "segments"
+            from "organizationSegments"
+            where "organizationId" = $(id)
+            group by "organizationId"),
+      identities as (
+        SELECT oi."organizationId", jsonb_agg(oi) AS "identities"
+        FROM "organizationIdentities" oi
+        WHERE oi."organizationId" = $(id)
+        GROUP BY "organizationId"
       )
-  SELECT
-      o.*,
-      nullif(o."employeeChurnRate" -> '12_month', 'null')::decimal as "employeeChurnRate12Month",
-      nullif(o."employeeGrowthRate" -> '12_month', 'null')::decimal as "employeeGrowthRate12Month",
-      nullif(o."revenueRange" -> 'min', 'null')::integer as "revenueRangeMin",
-      nullif(o."revenueRange" -> 'max', 'null')::integer as "revenueRangeMax",
-      COALESCE(ac."activityCount", 0)::INTEGER AS "activityCount",
-      COALESCE(mc."memberCount", 0)::INTEGER AS "memberCount",
-      COALESCE(ao."activeOn", '{}') AS "activeOn",
-      COALESCE(id."identities", '{}') AS "identities",
-      COALESCE(s."segments", '{}') AS "segments",
-      a."lastActive",
-      a."joinedAt"
-  FROM organizations o
-  LEFT JOIN activity_counts ac ON ac."organizationId" = o.id
-  LEFT JOIN member_counts mc ON mc."organizationId" = o.id
-  LEFT JOIN active_on ao ON ao."organizationId" = o.id
-  LEFT JOIN identities id ON id."organizationId" = o.id
-  LEFT JOIN last_active a ON a."organizationId" = o.id
-  LEFT JOIN segments s ON s."organizationId" = o.id
-  WHERE o.id = $(organizationId)
-    AND o."tenantId" = $(tenantId);`,
+      select 
+        o.*,
+        coalesce(md."activityCount", 0)::integer as "activityCount",
+        coalesce(md."memberCount", 0)::integer   as "memberCount",
+        coalesce(md."activeOn", '{}')            as "activeOn",
+        coalesce(i.identities, '{}')            as identities,
+        coalesce(os.segments, '{}')              as segments,
+        md."lastActive",
+        md."joinedAt"
+      from organizations o
+      left join member_data md on md."organizationId" = o.id
+      left join organization_segments os on os."organizationId" = o.id
+      left join identities i on i."organizationId" = o.id
+      where o.id = $(id)
+      and o."tenantId" = $(tenantId);
+  `,
       {
-        organizationId,
+        id: organizationId,
         tenantId,
+        segmentId,
       },
     )
   }
