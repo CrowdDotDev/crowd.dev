846569098c0a116163a427753c8691d1d4abdd95
RaginiAngarka <raginia@proximabiz.com>
2023-09-29 19:40:51 +0530

    Added logo for 'tnc' platform, added color for distribution bar in Reports
    Signed-off-by: RaginiAngarka <raginia@proximabiz.com>

--- frontend/src/integrations/custom/cvent/config.js
+++ frontend/src/integrations/custom/cvent/config.js
@@ -2,5 +2,6 @@ export default {
   name: 'cvent',
   image: '/images/integrations/custom/cvent.png',
   enabled: true,
+  chartColor: '#199AE0',
   url: () => null,
 };
--- frontend/src/integrations/custom/lfx/config.js
+++ frontend/src/integrations/custom/lfx/config.js
@@ -2,5 +2,6 @@ export default {
   name: 'lfx',
   image: '/images/integrations/custom/lfx.png',
   enabled: true,
+  chartColor: '#003764',
   url: () => null,
 };
