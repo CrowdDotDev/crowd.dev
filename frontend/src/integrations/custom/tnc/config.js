c3a37590670b45969c09b1aef2db192feb463f1a
Ragini Angarkar <raginia@proximabiz.com>
2023-10-02 13:41:43 +0530

    Added logo for 'tnc' platform, added color for distribution bar in Re… (#1582)
    Signed-off-by: RaginiAngarka <raginia@proximabiz.com>

---
+++ frontend/src/integrations/custom/tnc/config.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'tnc',
+  image: '/images/integrations/custom/tnc.png',
+  enabled: true,
+  chartColor: '#9FD0CB',
+  url: () => null,
+};
--- frontend/src/integrations/integrations-config.js
+++ frontend/src/integrations/integrations-config.js
@@ -18,6 +18,7 @@ import n8n from './n8n';
 import lfx from './custom/lfx';
 import groupsio from './groupsio';
 import cvent from './custom/cvent';
+import tnc from './custom/tnc';
 
 class IntegrationsConfig {
   get integrations() {
@@ -98,6 +99,7 @@ class IntegrationsConfig {
     return {
       lfx,
       cvent,
+      tnc,
     };
   }
 }
