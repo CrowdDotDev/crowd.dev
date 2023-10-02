9a774b7247ec2ac16b53cb3eb0edb3ba4299c98a
anilb <epipav@gmail.com>
2023-10-02 13:49:15 +0200

    tracking string update

--- backend/src/api/organization/organizationNotMerge.ts
+++ backend/src/api/organization/organizationNotMerge.ts
@@ -10,7 +10,7 @@ export default async (req, res) => {
     req.body.organizationToNotMerge,
   )
 
-  track('Ignore merge members', {}, { ...req })
+  track('Ignore merge organizations', {}, { ...req })
 
   await req.responseHandler.success(req, res, { status: 200 })
 }
