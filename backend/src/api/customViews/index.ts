023b862d56475cb2522925a677f0c90c0a6f6d29
Gasper Grom <gasper.grom@gmail.com>
2023-10-06 15:39:52 +0700

    Fix require for custom view list

--- backend/src/api/customViews/index.ts
+++ backend/src/api/customViews/index.ts
@@ -4,5 +4,5 @@ export default (app) => {
   app.post(`/tenant/:tenantId/customview`, safeWrap(require('./customViewCreate').default))
   app.put(`/tenant/:tenantId/customview/:id`, safeWrap(require('./customViewUpdate').default))
   app.delete(`/tenant/:tenantId/customview`, safeWrap(require('./customViewDestroy').default))
-  app.get(`/tenant/:tenantId/customview`, safeWrap(require('./customViewList').default))
+  app.get(`/tenant/:tenantId/customview`, safeWrap(require('./customViewQuery').default))
 }
