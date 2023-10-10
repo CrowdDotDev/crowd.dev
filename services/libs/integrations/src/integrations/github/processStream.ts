c754f50758202bbb01cfd12475588c4c7bf37c11
garrrikkotua <kotuaigor@gmail.com>
2023-10-10 19:33:36 +0300

    increase limits to 10 requests

--- services/libs/integrations/src/integrations/github/processStream.ts
+++ services/libs/integrations/src/integrations/github/processStream.ts
@@ -71,7 +71,7 @@ export function getConcurrentRequestLimiter(
 ): IConcurrentRequestLimiter {
   if (concurrentRequestLimiter === undefined) {
     concurrentRequestLimiter = ctx.getConcurrentRequestLimiter(
-      2, // max 2 concurrent requests
+      10, // max 10 concurrent requests
       'github-concurrent-request-limiter',
     )
   }
