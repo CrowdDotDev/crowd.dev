a2df94d19fa5a84f86d1eec1802e889a375041eb
Gasper Grom <gasper.grom@gmail.com>
2023-10-10 15:48:32 +0700

    Fix build issues

--- frontend/src/modules/onboard/config/steps/index.ts
+++
@@ -1,9 +0,0 @@
-import workspaceDetails from './workspace-details';
-import syncData from './sync-data';
-import inviteColleagues from './invite-colleagues';
-
-export default {
-  workspaceDetails,
-  syncData,
-  inviteColleagues,
-};
