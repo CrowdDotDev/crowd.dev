d2f23b86011e7669b919596b54321938904ad1a7
anilb <epipav@gmail.com>
2023-10-10 15:48:09 +0200

    sort direction fixed for org merge suggestions

--- backend/src/database/repositories/organizationRepository.ts
+++ backend/src/database/repositories/organizationRepository.ts
@@ -1404,7 +1404,7 @@ class OrganizationRepository {
         ORDER BY Greatest(Hashtext(Concat(org.id, otm."toMergeId")), Hashtext(Concat(otm."toMergeId", org.id))), org.id
       ) AS "organizationsToMerge" 
     ORDER BY 
-      "organizationsToMerge"."similarity", "organizationsToMerge".id DESC 
+      "organizationsToMerge"."similarity" DESC, "organizationsToMerge".id 
     LIMIT :limit OFFSET :offset
     `,
       {
