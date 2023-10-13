e81453e443562bef8ab7fc20dae679a412ee5b9b
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2023-10-13 18:29:34 +0300

    Increase limit from 5 to 7 for GitHub request limiter (#1677)

--- services/libs/integrations/src/integrations/github/processStream.ts
+++ services/libs/integrations/src/integrations/github/processStream.ts
@@ -71,7 +71,7 @@ export function getConcurrentRequestLimiter(
 ): IConcurrentRequestLimiter {
   if (concurrentRequestLimiter === undefined) {
     concurrentRequestLimiter = ctx.getConcurrentRequestLimiter(
-      5, // max 10 concurrent requests
+      7, // max 10 concurrent requests
       'github-concurrent-request-limiter',
     )
   }
