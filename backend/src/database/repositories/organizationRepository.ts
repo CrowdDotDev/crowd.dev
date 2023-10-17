9bffcd350d5b034f5e09402b1705ec850c3c708f
anilb <epipav@gmail.com>
2023-10-17 11:49:23 +0200

    Pagination and total_count improvements in organization merge suggestions (#1701)

--- backend/src/database/repositories/organizationRepository.ts
+++ backend/src/database/repositories/organizationRepository.ts
@@ -1518,29 +1518,49 @@ class OrganizationRepository {
     const segmentIds = SequelizeRepository.getSegmentIds(options)
 
     const orgs = await options.database.sequelize.query(
-      `SELECT 
-      "organizationsToMerge".id, 
-      "organizationsToMerge"."toMergeId",
-      "organizationsToMerge"."total_count",
-      "organizationsToMerge"."similarity"
-    FROM 
-    (
-      SELECT DISTINCT ON (Greatest(Hashtext(Concat(org.id, otm."toMergeId")), Hashtext(Concat(otm."toMergeId", org.id)))) 
-          org.id, 
-          otm."toMergeId", 
-          org."createdAt", 
-          COUNT(*) OVER() AS total_count,
+      `WITH
+      cte AS (
+        SELECT
+          Greatest(Hashtext(Concat(org.id, otm."toMergeId")), Hashtext(Concat(otm."toMergeId", org.id))) as hash,
+          org.id,
+          otm."toMergeId",
+          org."createdAt",
           otm."similarity"
         FROM organizations org
-        INNER JOIN "organizationToMerge" otm ON org.id = otm."organizationId"
+        JOIN "organizationToMerge" otm ON org.id = otm."organizationId"
         JOIN "organizationSegments" os ON os."organizationId" = org.id
+        JOIN "organizationSegments" to_merge_segments on to_merge_segments."organizationId" = otm."toMergeId"
         WHERE org."tenantId" = :tenantId
           AND os."segmentId" IN (:segmentIds)
-        ORDER BY Greatest(Hashtext(Concat(org.id, otm."toMergeId")), Hashtext(Concat(otm."toMergeId", org.id))), org.id
-      ) AS "organizationsToMerge" 
-    ORDER BY 
-      "organizationsToMerge"."similarity" DESC, "organizationsToMerge".id 
-    LIMIT :limit OFFSET :offset
+          AND to_merge_segments."segmentId" IN (:segmentIds)
+      ),
+      
+      count_cte AS (
+        SELECT COUNT(DISTINCT hash) AS total_count
+        FROM cte
+      ),
+      
+      final_select AS (
+        SELECT DISTINCT ON (hash)
+          id,
+          "toMergeId",
+          "createdAt",
+          "similarity"
+        FROM cte
+        ORDER BY hash, id
+      )
+      
+      SELECT
+        "organizationsToMerge".id,
+        "organizationsToMerge"."toMergeId",
+        count_cte."total_count",
+        "organizationsToMerge"."similarity"
+      FROM
+        final_select AS "organizationsToMerge",
+        count_cte
+      ORDER BY
+        "organizationsToMerge"."similarity" DESC, "organizationsToMerge".id
+      LIMIT :limit OFFSET :offset
     `,
       {
         replacements: {
