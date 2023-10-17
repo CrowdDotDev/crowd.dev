953406c31048d30dc7aff0d2b8e7c04675418384
anilb <epipav@gmail.com>
2023-10-17 16:19:22 +0200

    formatting

--- backend/src/database/repositories/organizationRepository.ts
+++ backend/src/database/repositories/organizationRepository.ts
@@ -1139,8 +1139,9 @@ class OrganizationRepository {
     let updatedRowsCount = 0
 
     do {
-
-      options.log.info(`[Move Activities] - Moving ${batchSize} activities from ${fromOrganizationId} to ${toOrganizationId}.`)
+      options.log.info(
+        `[Move Activities] - Moving ${batchSize} activities from ${fromOrganizationId} to ${toOrganizationId}.`,
+      )
 
       const query = `
         UPDATE "activities" 
