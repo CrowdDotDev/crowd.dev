fe418f77bac92f2be90669ba42491899135d62e0
garrrikkotua <kotuaigor@gmail.com>
2023-10-18 19:36:34 +0300

    fix discord error handling

--- services/libs/integrations/src/integrations/discord/api/getMessages.ts
+++ services/libs/integrations/src/integrations/discord/api/getMessages.ts
@@ -2,26 +2,28 @@ import axios from 'axios'
 import { DiscordApiMessage, DiscordParsedReponse, DiscordGetMessagesInput } from '../types'
 import { IProcessStreamContext } from '../../../types'
 import { getRateLimiter } from './handleRateLimit'
+import { handleDiscordError } from './errorHandler'
 
 async function getMessages(
   input: DiscordGetMessagesInput,
   ctx: IProcessStreamContext,
-  showError = true,
 ): Promise<DiscordParsedReponse> {
   const rateLimiter = getRateLimiter(ctx)
-  try {
-    let url = `https://discord.com/api/v10/channels/${input.channelId}/messages?limit=${input.perPage}`
-    if (input.page !== undefined && input.page !== '') {
-      url += `&before=${input.page}`
-    }
-    const config = {
-      method: 'get',
-      url,
-      headers: {
-        Authorization: input.token,
-      },
-    }
 
+  let url = `https://discord.com/api/v10/channels/${input.channelId}/messages?limit=${input.perPage}`
+  if (input.page !== undefined && input.page !== '') {
+    url += `&before=${input.page}`
+  }
+
+  const config = {
+    method: 'get',
+    url,
+    headers: {
+      Authorization: input.token,
+    },
+  }
+
+  try {
     await rateLimiter.checkRateLimit('getMessages')
     await rateLimiter.incrementRateLimit()
     const response = await axios(config)
@@ -37,27 +39,10 @@ async function getMessages(
       timeUntilReset,
     }
   } catch (err) {
-    if (err.response.status === 429) {
-      ctx.log.warn(
-        `Rate limit exceeded in Get Messages. Wait value in header is ${err.response.headers['x-ratelimit-reset-after']}`,
-      )
-      return {
-        records: [],
-        nextPage: input.page,
-        limit: 0,
-        timeUntilReset: err.response.headers['x-ratelimit-reset-after'],
-      }
+    const newErr = handleDiscordError(err, config, { input }, ctx)
+    if (newErr) {
+      throw newErr
     }
-    if (!showError) {
-      return {
-        records: [],
-        nextPage: '',
-        limit: 0,
-        timeUntilReset: 0,
-      }
-    }
-    ctx.log.error({ err, input }, 'Error while getting messages from Discord')
-    throw err
   }
 }
 
