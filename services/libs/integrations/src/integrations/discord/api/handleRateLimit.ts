90187df548fe54b9cc03517e7f8cd0a4caaab44e
garrrikkotua <kotuaigor@gmail.com>
2023-10-19 18:34:41 +0300

    update configuration for disord integration

--- services/libs/integrations/src/integrations/discord/api/handleRateLimit.ts
+++ services/libs/integrations/src/integrations/discord/api/handleRateLimit.ts
@@ -1,6 +1,6 @@
 import { IProcessStreamContext } from '../../../types'
 
-const DISCORD_RATE_LIMIT = 10000
+const DISCORD_RATE_LIMIT = 100000
 const DISCORD_RATE_LIMIT_TIME = 100 // 100 seconds
 const REDIS_KEY = 'discord-ratelimits-requests-count'
 
