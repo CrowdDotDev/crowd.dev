3118e41a0581c2541793ddcf50818c805defc239
Gašper Grom <gasper.grom@gmail.com>
2023-10-20 15:14:50 +0200

    Disable bot settings if team member filtering (#1743)

--- frontend/src/modules/member/config/saved-views/settings/bot/MemberBotSetting.vue
+++ frontend/src/modules/member/config/saved-views/settings/bot/MemberBotSetting.vue
@@ -3,7 +3,7 @@
     <p class="text-xs font-semibold leading-5 mb-3">
       Bot contacts visibility
     </p>
-    <el-radio-group v-model="value">
+    <el-radio-group v-model="value" :disabled="props.settings?.teamMember === 'filter'">
       <el-radio :label="IncludeEnum.EXCLUDE" class="!h-5">
         <span class="text-xs">Exclude bot contacts</span>
       </el-radio>
@@ -16,22 +16,37 @@
 
 <script setup lang="ts">
 import { IncludeEnum } from '@/modules/member/config/saved-views/settings/common/types/IncludeEnum';
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 const props = defineProps<{
-  modelValue: IncludeEnum,
+  modelValue: IncludeEnum | '',
+  settings: Record<string, string>,
 }>();
 
-const emit = defineEmits<{(e: 'update:modelValue', value: IncludeEnum): any }>();
+const emit = defineEmits<{(e: 'update:modelValue', value: IncludeEnum | ''): any }>();
 
-const value = computed<IncludeEnum>({
+const value = computed<IncludeEnum | ''>({
   get() {
     return props.modelValue;
   },
-  set(val: IncludeEnum) {
+  set(val: IncludeEnum | '') {
     emit('update:modelValue', val);
   },
 });
+
+const previousSettings = ref<Record<string, string> | null>(null);
+
+watch(() => props.settings, (current) => {
+  if (current?.teamMember === 'filter') {
+    if (!previousSettings.value || previousSettings.value.teamMember !== 'filter') {
+      value.value = '';
+    }
+  }
+  if (previousSettings.value?.teamMember === 'filter' && current?.teamMember !== 'filter') {
+    value.value = IncludeEnum.EXCLUDE;
+  }
+  previousSettings.value = { ...current };
+}, { deep: true, immediate: true });
 </script>
 
 <script lang="ts">
