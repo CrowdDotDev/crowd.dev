f55ff409dd612b2a78d84d1e8c7c54365b1310f1
Loïc Saint-Roch <loic@nunchi.studio>
2023-10-26 10:28:53 +0200

    Put new email workflow behind feature flags

--- services/apps/emails_worker/src/activities/getNextEmails.ts
+++ services/apps/emails_worker/src/activities/getNextEmails.ts
@@ -1,3 +1,6 @@
+import { FeatureFlag } from '@crowd/types'
+import { isFeatureEnabled } from '@crowd/feature-flags'
+
 import { svc } from '../main'
 import { UserTenant } from '../types'
 
@@ -7,9 +10,9 @@ settings to send emails to. This relies on some settings, such as when the next
 email should send at.
 */
 export async function getNextEmails(): Promise<UserTenant[]> {
-  let users: UserTenant[] = []
+  let rows: UserTenant[] = []
   try {
-    users = await svc.postgres.reader.connection().query(
+    rows = await svc.postgres.reader.connection().query(
       `SELECT "userId", "tenantId", "settings" FROM "tenantUsers"
         WHERE (settings -> 'eagleEye' -> 'emailDigestActive')::BOOLEAN IS TRUE
         AND (settings -> 'eagleEye' ->> 'onboarded')::BOOLEAN IS TRUE
@@ -18,6 +21,24 @@ export async function getNextEmails(): Promise<UserTenant[]> {
   } catch (err) {
     throw new Error(err)
   }
+
+  // Filter rows to only return tenants with this feature flag enabled.
+  const users: UserTenant[] = []
+  rows.forEach((row) => {
+    if (
+      isFeatureEnabled(
+        FeatureFlag.TEMPORAL_EMAILS,
+        async () => {
+          return {
+            tenantId: row.tenantId,
+          }
+        },
+        svc.unleash,
+      )
+    ) {
+      users.push(row)
+    }
+  })
 
   return users
 }
