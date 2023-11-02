317ba32b39502000d9de4729104bb0f3b22648a0
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2023-11-02 17:21:17 +0300

    Migrate Discourse integration to the new framework (#1778)

--- services/libs/integrations/src/integrations/discourse/api/getUser.ts
+++ services/libs/integrations/src/integrations/discourse/api/getUser.ts
@@ -1,19 +1,26 @@
 import axios, { AxiosRequestConfig } from 'axios'
-import { Logger } from '@crowd/logging'
 import { RateLimitError } from '@crowd/types'
-import type { DiscourseConnectionParams } from '../../types/discourseTypes'
-import { DiscourseUserResponse, DisourseUserByUsernameInput } from '../../types/discourseTypes'
+import type {
+  DiscourseConnectionParams,
+  DiscourseUserResponse,
+  DisourseUserByUsernameInput,
+} from '../types'
+import { IProcessStreamContext } from '../../../types'
 
+// this methods returns ids of posts in a topic
+// then we need to parse each topic individually (can be batched)
 export const getDiscourseUserByUsername = async (
   params: DiscourseConnectionParams,
   input: DisourseUserByUsernameInput,
-  logger: Logger,
+  ctx: IProcessStreamContext,
 ): Promise<DiscourseUserResponse | undefined> => {
-  logger.info({
+  ctx.log.info({
     message: 'Fetching user by username from Discourse',
     params,
     input,
   })
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const config: AxiosRequestConfig<any> = {
     method: 'get',
     url: `${params.forumHostname}/u/${encodeURIComponent(input.username)}.json`,
@@ -35,7 +42,7 @@ export const getDiscourseUserByUsername = async (
     if (err?.response?.status === 404) {
       return undefined
     }
-    logger.error({ err, params, input }, 'Error while fetching user by username from Discourse ')
+    ctx.log.error({ err, params, input }, 'Error while fetching user by username from Discourse ')
     throw err
   }
 }
