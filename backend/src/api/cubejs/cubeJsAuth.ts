0b96becc8700d62564884b7475394f6344b1ce8a
Joan Reyero <joan@crowd.dev>
2023-11-06 12:33:48 +0100

    Permissions (#1816)

--- backend/src/api/cubejs/cubeJsAuth.ts
+++ backend/src/api/cubejs/cubeJsAuth.ts
@@ -1,7 +1,10 @@
 import CubeJsService from '../../services/cubejs/cubeJsService'
 import SequelizeRepository from '../../database/repositories/sequelizeRepository'
+import PermissionChecker from '../../services/user/permissionChecker'
+import Permissions from '../../security/permissions'
 
 export default async (req, res) => {
+  new PermissionChecker(req).validateHas(Permissions.values.memberRead)
   const segments = SequelizeRepository.getSegmentIds(req)
   const payload = await CubeJsService.generateJwtToken(req.params.tenantId, segments)
   await req.responseHandler.success(req, res, payload)
