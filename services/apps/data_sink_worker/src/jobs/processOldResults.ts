901086284894578664b0cca6c04dc6cfdb7898c7
garrrikkotua <kotuaigor@gmail.com>
2023-11-07 16:50:27 +0300

    more concurrent version of processOldResults

--- services/apps/data_sink_worker/src/jobs/processOldResults.ts
+++ services/apps/data_sink_worker/src/jobs/processOldResults.ts
@@ -6,6 +6,7 @@ import { Logger } from '@crowd/logging'
 import { RedisClient, processWithLock } from '@crowd/redis'
 import { NodejsWorkerEmitter, SearchSyncWorkerEmitter } from '@crowd/sqs'
 import { Client as TemporalClient } from '@crowd/temporal'
+import { timeout } from '@crowd/common'
 
 const MAX_CONCURRENT_PROMISES = 10
 const MAX_RESULTS_TO_LOAD = 100
@@ -31,6 +32,7 @@ export const processOldResultsJob = async (
     log,
   )
 
+  let current = 0
   const loadNextBatch = async (): Promise<string[]> => {
     return await processWithLock(redis, 'process-old-results', 5 * 60, 3 * 60, async () => {
       const resultIds = await repo.getOldResultsToProcess(MAX_RESULTS_TO_LOAD)
@@ -39,42 +41,41 @@ export const processOldResultsJob = async (
     })
   }
 
-  // load 100 oldest results and try process them
   let resultsToProcess = await loadNextBatch()
 
   let successCount = 0
   let errorCount = 0
 
-  while (resultsToProcess.length > 0) {
-    log.info(`Detected ${resultsToProcess.length} old results rows to process!`)
+  for (let i = 0; i < resultsToProcess.length; i++) {
+    const resultId = resultsToProcess[i]
 
-    const promises = []
-    for (let i = 0; i < MAX_CONCURRENT_PROMISES && i < resultsToProcess.length; i++) {
-      const resultId = resultsToProcess[i]
-      promises.push(
-        service
-          .processResult(resultId)
-          .then((result) => {
-            if (result) {
-              successCount++
-            } else {
-              errorCount++
-            }
-          })
-          .catch((err) => {
-            log.error(err, 'Failed to process result!')
-            errorCount++
-          }),
-      )
+    while (current == MAX_CONCURRENT_PROMISES) {
+      await timeout(1000)
     }
 
-    await Promise.all(promises)
+    log.info(`Processing result ${i + 1}/${resultsToProcess.length}`)
+    current += 1
+    service
+      .processResult(resultId)
+      .then(() => {
+        current--
+        successCount++
+        log.info(`Processed result ${i + 1}/${resultsToProcess.length}`)
+      })
+      .catch((err) => {
+        current--
+        errorCount++
+        log.error(err, `Error processing result ${i + 1}/${resultsToProcess.length}!`)
+      })
+  }
 
-    log.info(`Processed ${successCount} old results successfully and ${errorCount} with errors.`)
+  while (current > 0) {
+    await timeout(1000)
+  }
 
-    resultsToProcess = resultsToProcess.slice(MAX_CONCURRENT_PROMISES)
-    if (resultsToProcess.length === 0) {
-      resultsToProcess = await loadNextBatch()
-    }
+  log.info(`Processed ${successCount} old results successfully and ${errorCount} with errors.`)
+
+  if (resultsToProcess.length === 0) {
+    resultsToProcess = await loadNextBatch()
   }
 }
