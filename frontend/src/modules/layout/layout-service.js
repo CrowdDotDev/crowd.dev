0cda346b0f864ef4151a2c605068a51fa46e635f
Joana Maia <joana@crowd.dev>
2023-11-13 11:28:47 +0000

    test direct url

--- frontend/src/modules/layout/layout-service.js
+++ frontend/src/modules/layout/layout-service.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 export class LayoutService {
   static async getSystemStatus() {
     const response = await Axios.get(
-      '/system-status',
+      'https://api.openstatus.dev/public/status/crowddev',
     );
 
     return response.data;
