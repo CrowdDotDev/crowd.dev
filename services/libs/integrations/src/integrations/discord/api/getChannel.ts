d44144d471b6c3a6b1cc8c9bbd72aaef46427a33
garrrikkotua <kotuaigor@gmail.com>
2023-11-20 19:20:02 +0300

    add retry wrapper to all discord endpoints

--- services/libs/integrations/src/integrations/discord/api/getChannel.ts
+++ services/libs/integrations/src/integrations/discord/api/getChannel.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 import { handleDiscordError } from './errorHandler'
 import { DiscordApiChannel } from '../types'
 import { IProcessStreamContext } from '../../../types'
+import { retryWrapper } from './handleRateLimit'
 
 export const getChannel = async (
   channelId: string,
@@ -16,13 +17,15 @@ export const getChannel = async (
     },
   }
 
-  try {
-    const response = await axios(config)
-    return response.data
-  } catch (err) {
-    const newErr = handleDiscordError(err, config, { channelId }, ctx)
-    if (newErr) {
-      throw newErr
+  return await retryWrapper(3, async () => {
+    try {
+      const response = await axios(config)
+      return response.data
+    } catch (err) {
+      const newErr = handleDiscordError(err, config, { channelId }, ctx)
+      if (newErr) {
+        throw newErr
+      }
     }
-  }
+  })
 }
