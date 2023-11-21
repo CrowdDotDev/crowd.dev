bb8ba1969e5953e42f37498d63aa2bc977ef57b9
Joana Maia <joana@crowd.dev>
2023-11-21 15:14:15 +0000

    Update copy in auth page

--- frontend/src/modules/auth/components/layout/auth-layout-side.vue
+++ frontend/src/modules/auth/components/layout/auth-layout-side.vue
@@ -10,26 +10,27 @@
       </div>
       <div class="flex flex-col gap-4">
         <h4>
-          Find revenue opportunities in your open source data
+          Find high-intent accounts in your developer data
         </h4>
         <p class="text-sm leading-6">
-          Centralize community, product, and customer data to understand which companies are engaging with your open source project.
+          Combine data from GitHub, LinkedIn, your product, and many other sources to understand which companies
+          engage with your developer product. Sync insights with your CRM to prioritize the right accounts and build pipeline.
         </p>
       </div>
       <div class="grid grid-rows-2 gap-8">
         <div class="grid grid-cols-3 gap-4 col-span-1">
           <div class="flex items-center">
             <img
-              src="/images/auth/meilisearch.png"
-              alt="meilisearch logo"
-              class="h-4"
+              src="/images/auth/thelinuxfoundation.png"
+              alt="thelinuxfoundation logo"
+              class="h-7"
             />
           </div>
           <div class="flex items-center justify-end">
             <img
-              src="/images/auth/thelinuxfoundation.png"
-              alt="thelinuxfoundation logo"
-              class="h-7"
+              src="/images/auth/openbb.png"
+              alt="openbb logo"
+              class="h-6"
             />
           </div>
           <div class="flex items-center justify-end">
@@ -43,8 +44,8 @@
         <div class="grid grid-cols-3 gap-4">
           <div class="flex items-center">
             <img
-              src="/images/auth/dragonfly.png"
-              alt="dragonfly logo"
+              src="/images/auth/drant.png"
+              alt="drant logo"
               class="h-7"
             />
           </div>
@@ -68,10 +69,8 @@
     <div>
       <div class="bg-brand-500 rounded-lg p-6">
         <p class="text-white text-sm font-medium pb-6">
-          “Building and engaging community is my top
-          priority as a founder of an open-source startup.
-          Crowd.dev is basically the operating system for
-          that.”
+          “crowd.dev uncovers companies that already use our product but haven't
+          engaged with our Go-To-Market team yet. This helped us to 4x the opportunities in our sales pipeline.”
         </p>
         <div class="flex items-center">
           <div class="pr-4">
@@ -88,7 +87,7 @@
               Johannes Hötter
             </h6>
             <p class="text-white text-xs leading-5">
-              Co-Founder & CEO, kern.ai
+              Co-Founder & CEO, Kern AI
             </p>
           </div>
         </div>
