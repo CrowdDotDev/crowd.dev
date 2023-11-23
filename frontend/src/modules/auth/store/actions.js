0ba15206995a778942331381107c04193a042505
Gašper Grom <gasper.grom@gmail.com>
2023-11-23 18:41:42 +0700

    Fix slack automation flow (#1876)

--- frontend/src/modules/auth/store/actions.js
+++ frontend/src/modules/auth/store/actions.js
@@ -27,13 +27,18 @@ export default {
             localStorage.removeItem('userDateTime');
           }
         }
-        const currentUserLocally = AuthService.fetchMeLocally();
         connectSocket(token);
-        if (currentUserLocally) {
-          commit('AUTH_INIT_SUCCESS', { currentUser: currentUserLocally });
+        const { pathname } = window.location;
+        if (!['/automations'].includes(pathname)) {
+          const currentUserLocally = AuthService.fetchMeLocally();
 
-          return currentUserLocally;
+          if (currentUserLocally) {
+            commit('AUTH_INIT_SUCCESS', { currentUser: currentUserLocally });
+
+            return currentUserLocally;
+          }
         }
+
         const currentUser = await AuthService.fetchMe();
         commit('AUTH_INIT_SUCCESS', { currentUser });
         return currentUser;
