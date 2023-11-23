3ac65fcea526fc2dff04e00590afb82af2fdb00b
garrrikkotua <kotuaigor@gmail.com>
2023-11-23 19:27:41 +0300

    disable process old data job

--- services/apps/integration_data_worker/src/main.ts
+++ services/apps/integration_data_worker/src/main.ts
@@ -5,13 +5,13 @@ import { getRedisClient } from '@crowd/redis'
 import { getDbConnection } from '@crowd/database'
 import { DataSinkWorkerEmitter, IntegrationStreamWorkerEmitter, getSqsClient } from '@crowd/sqs'
 import { WorkerQueueReceiver } from './queue'
-import { processOldDataJob } from './jobs/processOldData'
+// import { processOldDataJob } from './jobs/processOldData'
 
 const tracer = getServiceTracer()
 const log = getServiceLogger()
 
 const MAX_CONCURRENT_PROCESSING = 3
-const PROCESSING_INTERVAL_MINUTES = 5
+// const PROCESSING_INTERVAL_MINUTES = 5
 
 setImmediate(async () => {
   log.info('Starting integration data worker...')
@@ -39,25 +39,25 @@ setImmediate(async () => {
     await streamWorkerEmitter.init()
     await dataSinkWorkerEmitter.init()
 
-    let processing = false
-    setInterval(async () => {
-      try {
-        if (!processing) {
-          processing = true
-          await processOldDataJob(
-            dbConnection,
-            redisClient,
-            streamWorkerEmitter,
-            dataSinkWorkerEmitter,
-            log,
-          )
-        }
-      } catch (err) {
-        log.error(err, 'Failed to process old data!')
-      } finally {
-        processing = false
-      }
-    }, PROCESSING_INTERVAL_MINUTES * 60 * 1000)
+    // let processing = false
+    // setInterval(async () => {
+    //   try {
+    //     if (!processing) {
+    //       processing = true
+    //       await processOldDataJob(
+    //         dbConnection,
+    //         redisClient,
+    //         streamWorkerEmitter,
+    //         dataSinkWorkerEmitter,
+    //         log,
+    //       )
+    //     }
+    //   } catch (err) {
+    //     log.error(err, 'Failed to process old data!')
+    //   } finally {
+    //     processing = false
+    //   }
+    // }, PROCESSING_INTERVAL_MINUTES * 60 * 1000)
     await queue.start()
   } catch (err) {
     log.error({ err }, 'Failed to start queues!')
