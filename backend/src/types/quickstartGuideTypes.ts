410ffed6014babb4af73c6ff1e4eff6a83e45db5
Gasper Grom <gasper.grom@gmail.com>
2023-11-23 09:27:11 +0700

    Fasten guides loading

--- backend/src/types/quickstartGuideTypes.ts
+++ backend/src/types/quickstartGuideTypes.ts
@@ -14,7 +14,6 @@ export interface QuickstartGuideMap {
 export interface QuickstartGuide {
   title: string
   body: string
-  videoLink: string
   learnMoreLink: string
   buttonLink: string
   buttonText: string
@@ -31,7 +30,6 @@ export interface QuickstartGuideSettings {
 const connectFirstIntegrationGuide: QuickstartGuide = {
   title: 'Connect your first integration',
   body: 'Connect with our built-in integrations to start syncing data from your digital channels.',
-  videoLink: 'https://www.loom.com/share/578ea6ef431c48e0b338cf975d3b80bc',
   learnMoreLink: 'https://docs.crowd.dev/docs/getting-started/integrations',
   buttonLink: '/integrations',
   buttonText: 'Connect integrations',
@@ -41,7 +39,6 @@ const connectFirstIntegrationGuide: QuickstartGuide = {
 const viewReportGuide: QuickstartGuide = {
   title: 'Look into reports',
   body: 'Check our specially crafted default reports and dig into the inner workings of your community.',
-  videoLink: 'https://www.loom.com/share/545e7dfc692540d09115ee32653640ca',
   learnMoreLink: 'https://docs.crowd.dev/docs/guides/reports',
   buttonLink: '/reports',
   buttonText: 'Explore reports',
@@ -51,7 +48,6 @@ const viewReportGuide: QuickstartGuide = {
 const inviteColleaguesGuide: QuickstartGuide = {
   title: 'Invite your colleagues',
   body: 'Invite colleagues to your crowd.dev workspace by giving full access or read-only permissions.',
-  videoLink: 'https://www.loom.com/share/f12d87814e7447edab4282eab6bb3ccf',
   learnMoreLink: 'https://docs.crowd.dev/docs/getting-started/getting-set-up',
   buttonLink: '/settings',
   buttonText: 'Invite colleagues',
@@ -61,7 +57,6 @@ const inviteColleaguesGuide: QuickstartGuide = {
 const exploreOrganizations: QuickstartGuide = {
   title: 'Explore organizations',
   body: 'Discover organizations with developers engaging with your ecosystem or technology and see which companies fit your ICP.',
-  videoLink: 'https://www.loom.com/share/f12d87814e7447edab4282eab6bb3ccf',
   learnMoreLink: 'https://docs.crowd.dev/docs/guides/organizations',
   buttonLink: '/organizations',
   buttonText: 'Explore organizations',
@@ -71,7 +66,6 @@ const exploreOrganizations: QuickstartGuide = {
 const exploreContacts: QuickstartGuide = {
   title: 'Explore contacts',
   body: 'Get an overview of all contacts that engaged with your product or community across the platforms you have connected.',
-  videoLink: 'https://www.loom.com/share/f12d87814e7447edab4282eab6bb3ccf',
   learnMoreLink: 'https://docs.crowd.dev/docs/guides/contacts',
   buttonLink: '/contacts',
   buttonText: 'Explore contacts',
@@ -81,7 +75,6 @@ const exploreContacts: QuickstartGuide = {
 const createAutomations: QuickstartGuide = {
   title: 'Create automations',
   body: 'Stop focusing on repetitive tasks and concentrate on building deeper relationships with your community by automating your workflows via HubSpot syncs, Slack notifications, or Webhooks.',
-  videoLink: 'https://www.loom.com/share/f12d87814e7447edab4282eab6bb3ccf',
   learnMoreLink: 'https://docs.crowd.dev/docs/guides/automations',
   buttonLink: '/automations',
   buttonText: 'Create automations',
