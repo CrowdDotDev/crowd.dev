7dec360c255f057dc00b2798176b8f1542d564b3
Loïc Saint-Roch <loic@nunchi.studio>
2023-11-27 09:55:52 +0100

    Add TenantId as Temporal search attribute

--- services/apps/emails_worker/src/workflows/weekly-analytics/getAndSendNextEmails.ts
+++ services/apps/emails_worker/src/workflows/weekly-analytics/getAndSendNextEmails.ts
@@ -55,6 +55,9 @@ export async function weeklyGetAndSendNextEmails(): Promise<void> {
             dateTimeStartPreviousWeek: calculatedTimes.dateTimeStartPreviousWeek,
           },
         ],
+        searchAttributes: {
+          TenantId: [tenant.tenantId],
+        },
       })
     }),
   )
