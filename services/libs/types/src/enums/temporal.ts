5e3628ce38b4c1c5932a8487d449647c24330086
Loïc Saint-Roch <loic@nunchi.studio>
2023-11-27 10:07:41 +0100

    Fix typo in Temporal workflows name

--- services/libs/types/src/enums/temporal.ts
+++ services/libs/types/src/enums/temporal.ts
@@ -2,6 +2,6 @@ export enum TemporalWorkflowId {
   NEW_ACTIVITY_AUTOMATION = 'new-activity-automation',
   NEW_MEMBER_AUTOMATION = 'new-member-automation',
 
-  EMAIL_WEEKLY_ANALYTICS = 'email-eagleeye-digest',
-  EMAIL_EAGLEEYE_DIGEST = 'email-weekly-analytics',
+  EMAIL_WEEKLY_ANALYTICS = 'email-weekly-analytics',
+  EMAIL_EAGLEEYE_DIGEST = 'email-eagleeye-digest',
 }
