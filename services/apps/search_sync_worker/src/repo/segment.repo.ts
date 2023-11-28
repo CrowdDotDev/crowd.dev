ee42a6b5c8d0a6cc02388377a3aff1c7dd909268
anil <epipav@gmail.com>
2023-11-28 17:38:14 +0100

    segment repo cleaned

--- services/apps/search_sync_worker/src/repo/segment.repo.ts
+++ services/apps/search_sync_worker/src/repo/segment.repo.ts
@@ -8,27 +8,21 @@ export class SegmentRepository extends RepositoryBase<SegmentRepository> {
   }
 
   public async getParentSegmentIds(childSegmentIds: string[]): Promise<IDbSegmentInfo[]> {
-    console.log('Getting parent segmentIds!')
-    console.log(childSegmentIds)
-    try {
-      const query = `select s.id, pd.id as "parentId", gpd.id as "grandParentId"
+    const results = await this.db().any(
+      `
+      select s.id, pd.id as "parentId", gpd.id as "grandParentId"
       from segments s
               inner join segments pd
                           on pd."tenantId" = s."tenantId" and pd.slug = s."parentSlug" and pd."grandparentSlug" is null and
                             pd."parentSlug" is not null
               inner join segments gpd on gpd."tenantId" = s."tenantId" and gpd.slug = s."grandparentSlug" and
                                           gpd."grandparentSlug" is null and gpd."parentSlug" is null
-      where s.id in ($(childSegmentIds:csv));`
-      console.log(query)
-      const results = await this.db().any(query, {
+      where s.id in ($(childSegmentIds:csv));
+      `,
+      {
         childSegmentIds,
-      })
-      console.log('Done getting results!')
-      console.log(results)
-      return results
-    } catch (e) {
-      console.log('Error while getting parent segment ids! ')
-      console.log(e)
-    }
+      },
+    )
+    return results
   }
 }
