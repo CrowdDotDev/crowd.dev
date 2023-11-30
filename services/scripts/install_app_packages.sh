3819c0d55badf5b7fff01ff558fe5a09c785348a
Uroš Marolt <uros@crowd.dev>
2023-11-30 16:32:30 +0100

    wip

--- services/scripts/install_app_packages.sh
+++ services/scripts/install_app_packages.sh
@@ -5,14 +5,11 @@ CLI_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $CLI_HOME/utils.sh
 
-FLAGS=$1
-
-
 for app_dir in $CLI_HOME/../apps/*/; do
   if [ -f "${app_dir}package.json" ]; then
     app=$(basename $app_dir)
-    yell "Installing packages for app: $app! $FLAGS"
-    (cd $app_dir && npm ci) &
+    yell "Installing packages for app: $app!"
+    (cd $app_dir && pnpm i --frozen-lockfile) &
   fi
 done
 
--- services/scripts/install_lib_packages.sh
+++ services/scripts/install_lib_packages.sh
@@ -16,22 +16,22 @@ fi
 set -eo pipefail
 CLI_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-FLAGS=$1
+export _FLAGS=$1
 N=3  # change this to control the concurrency level
 
 printf '%s\0' $CLI_HOME/../libs/*/ | xargs -0 -n1 -P$N -I{} bash -c '
     if [ -f "$0/package.json" ]; then
         lib=$(basename $0)
-        printf "${YELLOW}Installing packages for library: $lib! $FLAGS${RESET}\n"
-        (cd $0 && npm ci $FLAGS)
+        printf "${YELLOW}Installing packages for library: $lib! Flags: $_FLAGS ${RESET}\n"
+        (cd $0 && pnpm i --frozen-lockfile $_FLAGS)
     fi
 ' {}
 
 printf '%s\0' $CLI_HOME/../archetypes/*/ | xargs -0 -n1 -P$N -I{} bash -c '
     if [ -f "$0/package.json" ]; then
         archetype=$(basename $0)
-        printf "${YELLOW}Installing packages for archetype: $archetype! $FLAGS${RESET}\n"
-        (cd $0 && npm ci $FLAGS)
+        printf "${YELLOW}Installing packages for archetype: $archetype! $_FLAGS${RESET}\n"
+        (cd $0 && pnpm i --frozen-lockfile $_FLAGS)
     fi
 ' {}
 
