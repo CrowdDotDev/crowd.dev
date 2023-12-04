f8464c0d688f8f5e37fcdcecaa5fc9c1c19e6aab
Gasper Grom <gasper.grom@gmail.com>
2023-12-04 22:59:52 +0700

    Revert "Fix member find handling"
    This reverts commit 5e9db26afef92f6bb67b47d577e84a52aabfc271.

--- frontend/src/modules/member/member-service.js
+++ frontend/src/modules/member/member-service.js
@@ -92,6 +92,8 @@ export class MemberService {
     const sampleTenant = AuthCurrentTenant.getSampleTenantData();
     const tenantId = sampleTenant?.id || AuthCurrentTenant.get();
 
+    const [segmentId] = segments;
+
     const response = await authAxios.get(
       `/tenant/${tenantId}/member/${id}`,
       {
@@ -99,7 +101,7 @@ export class MemberService {
           Authorization: sampleTenant?.token,
         },
         params: {
-          segmentId: segments.length > 0 ? segments[0] : undefined,
+          segmentId,
         },
       },
     );
