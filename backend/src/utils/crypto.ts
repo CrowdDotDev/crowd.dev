1693a7662debc9a8642dd0e1cee16f74c601028a
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2023-12-05 16:59:52 +0300

    Add segmentId parameter to updateParentIds method (#1929)

--- backend/src/utils/crypto.ts
+++ backend/src/utils/crypto.ts
@@ -2,7 +2,6 @@ import * as crypto from 'crypto'
 import * as buffer from 'buffer'
 import { ENCRYPTION_CONFIG } from '../conf'
 
-
 export function generateWebhookSecret(length: number = 32): string {
   return crypto.randomBytes(length).toString('hex')
 }
@@ -23,26 +22,25 @@ export function verifyWebhookSignature(
 }
 
 export function encryptData(password: string): string {
-  const algo = "aes-256-cbc";
+  const algo = 'aes-256-cbc'
   const secretKey = ENCRYPTION_CONFIG.secretKey
   const inVec = ENCRYPTION_CONFIG.initVector
   const cipherText = crypto.createCipheriv(algo, secretKey, inVec)
 
-  let encryptedData = cipherText.update(password, "utf-8", "hex")
-  encryptedData += cipherText.final("hex")
+  let encryptedData = cipherText.update(password, 'utf-8', 'hex')
+  encryptedData += cipherText.final('hex')
 
   return encryptedData
 }
 
-
 export function decryptData(password: string): string {
-  const algo = "aes-256-cbc";
+  const algo = 'aes-256-cbc'
   const secretKey = ENCRYPTION_CONFIG.secretKey
   const inVec = ENCRYPTION_CONFIG.initVector
   const decipherText = crypto.createDecipheriv(algo, secretKey, inVec)
 
-  let decryptedData = decipherText.update(password, "hex", "utf-8")
-  decryptedData += decipherText.final("utf8")
+  let decryptedData = decipherText.update(password, 'hex', 'utf-8')
+  decryptedData += decipherText.final('utf8')
 
   return decryptedData
 }
