f87162c5297624dfed34bdfb61041146b00e3dc5
Gasper Grom <gasper.grom@gmail.com>
2023-12-07 21:27:36 +0700

    Fix error

--- frontend/src/modules/member/components/member-enrichment.vue
+++ frontend/src/modules/member/components/member-enrichment.vue
@@ -3,19 +3,19 @@
     <div class="flex justify-between items-center">
       <div class="flex gap-2">
         <app-svg
-          name="enrichment"
-          class="w-5 h-5 enrichment-icon"
-          color="#111827"
+            name="enrichment"
+            class="w-5 h-5 enrichment-icon"
+            color="#111827"
         />
         <span class="text-gray-900 font-semibold text-sm">Contributor enrichment</span>
       </div>
       <el-tooltip placement="top" content="Learn more">
         <a
-          aria-label="Learn more"
-          class="btn btn-link btn-link--primary !h-8 !w-8 !text-gray-400 hover:!text-gray-600 hover:no-underline"
-          href="https://docs.crowd.dev/docs/member-enrichment"
-          target="_blank"
-          rel="noopener noreferrer"
+            aria-label="Learn more"
+            class="btn btn-link btn-link--primary !h-8 !w-8 !text-gray-400 hover:!text-gray-600 hover:no-underline"
+            href="https://docs.crowd.dev/docs/member-enrichment"
+            target="_blank"
+            rel="noopener noreferrer"
         ><i class="ri-question-line text-lg" /></a>
       </el-tooltip>
     </div>
@@ -26,35 +26,35 @@
     </div>
 
     <el-tooltip
-      placement="top"
-      content="Contributor enrichment requires an associated GitHub profile or Email"
-      :disabled="!isEnrichmentDisabled"
-      popper-class="max-w-[260px]"
+        placement="top"
+        content="Contributor enrichment requires an associated GitHub profile or Email"
+        :disabled="!isEnrichmentDisabled"
+        popper-class="max-w-[260px]"
     >
       <span v-if="isFindGitHubFeatureEnabled">
         <el-button
-          v-if="!isEnrichmentDisabled"
-          class="btn btn--primary btn--full !h-8"
-          :disabled="isEditLockedForSampleData"
-          @click="onEnrichmentClick"
+            v-if="!isEnrichmentDisabled"
+            class="btn btn--primary btn--full !h-8"
+            :disabled="isEditLockedForSampleData"
+            @click="onEnrichmentClick"
         >Enrich member</el-button>
         <el-button
-          v-else
-          class="btn btn--primary btn--full !h-8"
-          :disabled="isEditLockedForSampleData"
-          @click="onFindGithubClick"
+            v-else
+            class="btn btn--primary btn--full !h-8"
+            :disabled="isEditLockedForSampleData"
+            @click="onFindGithubClick"
         >
           <i class="ri-github-fill pr-2" /> Find GitHub
         </el-button>
       </span>
       <span v-else>
         <el-button
-          class="btn btn--primary btn--full !h-8"
-          :disabled="
+            class="btn btn--primary btn--full !h-8"
+            :disabled="
             isEnrichmentDisabled
               || isEditLockedForSampleData
           "
-          @click="onEnrichmentClick"
+            @click="onEnrichmentClick"
         >Enrich contributor</el-button>
       </span>
     </el-tooltip>
@@ -63,16 +63,16 @@
       * requires a GitHub profile or Email
     </div>
     <app-member-find-github-drawer
-      v-if="openFindGitHubDrawer"
-      v-model="openFindGitHubDrawer"
-      :member="member"
+        v-if="openFindGitHubDrawer"
+        v-model="openFindGitHubDrawer"
+        :member="member"
     />
   </div>
 </template>
 
 <script setup>
 import {
-  computed, defineProps, onMounted, ref,
+  computed, defineProps, ref,
 } from 'vue';
 import { mapActions, mapGetters } from '@/shared/vuex/vuex.helpers';
 import AppSvg from '@/shared/svg/svg.vue';
@@ -90,18 +90,18 @@ const { doEnrich } = mapActions('member');
 const { currentTenant, currentUser } = mapGetters('auth');
 
 const isFindGitHubFeatureEnabled = FeatureFlag.isFlagEnabled(
-  FEATURE_FLAGS.findGitHub,
+    FEATURE_FLAGS.findGitHub,
 );
 
 const isEnrichmentDisabled = computed(
-  () => !props.member.username?.github?.length && !props.member.emails?.length,
+    () => !props.member.username?.github?.length && !props.member.emails?.length,
 );
 
 const openFindGitHubDrawer = ref(false);
 
 const isEditLockedForSampleData = computed(
-  () => new MemberPermissions(currentTenant.value, currentUser.value)
-    .editLockedForSampleData,
+    () => new MemberPermissions(currentTenant.value, currentUser.value)
+        .editLockedForSampleData,
 );
 
 const onEnrichmentClick = async () => {
