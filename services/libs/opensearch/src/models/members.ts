0d9dc01aeff7b0bdcac73b37fc091283ec120e96
Yeganathan S <hilariousengineer@protonmail.com>
2023-12-18 19:40:46 +0000

    Serve contact profiles from OpenSearch (#1954)

--- services/libs/opensearch/src/models/members.ts
+++ services/libs/opensearch/src/models/members.ts
@@ -96,5 +96,19 @@ export class MembersOpensearch extends OpensearchModelBase {
         fromOpensearch: 'nested_organizations',
       },
     },
+    notes: {
+      type: OpensearchFieldType.NESTED,
+      customTranslation: {
+        toOpensearch: 'nested_notes.uuid_id',
+        fromOpensearch: 'nested_notes',
+      },
+    },
+    tasks: {
+      type: OpensearchFieldType.NESTED,
+      customTranslation: {
+        toOpensearch: 'nested_tasks.uuid_id',
+        fromOpensearch: 'nested_tasks',
+      },
+    },
   }
 }
