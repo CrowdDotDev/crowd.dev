575e5ecf18a7a380bba81fc3a2884ee1e04dda1c
Yeganathan S <hilariousengineer@protonmail.com>
2024-01-02 21:24:31 +0530

    make linter happy

--- backend/src/services/__tests__/tenantService.test.ts
+++ backend/src/services/__tests__/tenantService.test.ts
@@ -171,7 +171,7 @@ describe('TenantService tests', () => {
         id: tenantCreatedPlain.id,
         name: 'testName',
         url: 'testUrl',
-        plan: "Essential",
+        plan: 'Essential',
         isTrialPlan: false,
         trialEndsAt: null,
         onboardedAt: null,
