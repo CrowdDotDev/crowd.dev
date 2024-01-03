74f011e89b7394102a9277a72460c69e435a4212
Mish Savelyev <1564970+sausage-todd@users.noreply.github.com>
2024-01-03 12:35:49 +0100

    Async merging (#2006)

---
+++ services/apps/entity_merging_worker/src/activities.ts
@@ -0,0 +1,8 @@
+export { deleteMember, moveActivitiesBetweenMembers } from './activities/members'
+
+export {
+  deleteOrganization,
+  moveActivitiesBetweenOrgs,
+  markMergeActionDone,
+  notifyFrontend,
+} from './activities/organizations'
