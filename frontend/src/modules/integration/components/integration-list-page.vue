210223d97d0d60a47b57915e535ceb552942757f
Efren Lim <elim@linuxfoundation.org>
2024-01-04 17:06:26 +0800

    fix lint errors
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/src/modules/integration/components/integration-list-page.vue
+++ frontend/src/modules/integration/components/integration-list-page.vue
@@ -94,7 +94,6 @@ import ConfirmDialog from '@/shared/dialog/confirm-dialog';
 import Message from '@/shared/message/message';
 import AppIntegrationList from './integration-list.vue';
 
-
 export default {
   name: 'AppIntegrationListPage',
 
