bdc4f0bfac473abc963a3c5b129cd07ee477c582
Uroš Marolt <uros@crowd.dev>
2024-01-04 08:29:52 +0100

    wip

--- services/apps/premium/organizations_enrichment_worker/src/workflows/entrichTenantOrganizations.ts
+++ services/apps/premium/organizations_enrichment_worker/src/workflows/entrichTenantOrganizations.ts
@@ -11,9 +11,18 @@ export async function enrichTenantOrganizations(tenant: IPremiumTenantInfo): Pro
   // this will be our limit (1 credit = 1 enriched organization)
   const credits = await getTenantCredits(tenant)
 
+  if (credits === 0) {
+    // we have no credits left on this tenant
+    return
+  }
+
   if (credits === -1) {
     // we have unlimited credits
   } else if (credits > 0) {
     // we have a finite amount of credits left
   }
+
+  // get organizationCache records connected to this tenant that can be enriched
+  // we get all that weren't enriched yet or were enriched more that 3 months ago
+  // we are gonna enrich organizationCache records than update organizations rows
 }
