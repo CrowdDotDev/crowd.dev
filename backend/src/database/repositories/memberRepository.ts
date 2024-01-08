815bd13c7bdcbea9af37c9a96a33fb93d74dbd3b
Misha Savelyev <mikhail@crowd.dev>
2024-01-08 14:16:29 +0000

    Fetch member's segments after by-id query to opensearch

--- backend/src/database/repositories/memberRepository.ts
+++ backend/src/database/repositories/memberRepository.ts
@@ -1216,6 +1216,25 @@ class MemberRepository {
       })
     }
 
+    const seq = SequelizeRepository.getSequelize(options)
+    result.segments = await seq.query(
+      `
+      SELECT
+          s.id,
+          s.name
+      FROM mv_activities_cube a
+      JOIN segments s ON s.id = a."segmentId"
+      WHERE a."memberId" = :id
+      GROUP BY s.id
+      `,
+      {
+        replacements: {
+          id,
+        },
+        type: QueryTypes.SELECT,
+      },
+    )
+
     return result
   }
 
