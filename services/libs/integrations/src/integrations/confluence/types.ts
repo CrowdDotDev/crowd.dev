be63af86c65eb1319e3beaa8b7ae6a2f818afa9d
Ayman <enkhalifapro@gmail.com>
2024-01-16 06:03:51 +0200

    code clean up
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- services/libs/integrations/src/integrations/confluence/types.ts
+++ services/libs/integrations/src/integrations/confluence/types.ts
@@ -1,13 +1,13 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export enum ConfluenceActivityType {
-    PAGE_CREATED = 'page-created',
-    PAGE_UPDATED = 'page-updated',
+  PAGE_CREATED = 'page-created',
+  PAGE_UPDATED = 'page-updated',
 
-    COMMENT_CREATED = 'comment-created',
+  COMMENT_CREATED = 'comment-created',
 
-    ATTACHMENT_CREATED = 'attachment-created',
+  ATTACHMENT_CREATED = 'attachment-created',
 
-    BLOG_POST = 'blogpost',
-    ATTACHMENT = 'attachment',
-    COMMENT = 'comment',
+  BLOG_POST = 'blogpost',
+  ATTACHMENT = 'attachment',
+  COMMENT = 'comment',
 }
