4277ac302285ed0a1e68808990b724366e4f1414
Ayman <enkhalifapro@gmail.com>
2024-01-16 05:48:32 +0200

    added the missing confluence types
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- services/libs/integrations/src/integrations/confluence/types.ts
+++ services/libs/integrations/src/integrations/confluence/types.ts
@@ -1,7 +1,13 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export enum ConfluenceActivityType {
-  PAGE = 'page',
-  BLOG_POST = 'blogpost',
-  ATTACHMENT = 'attachment',
-  COMMENT = 'comment',
+    PAGE_CREATED = 'page-created',
+    PAGE_UPDATED = 'page-updated',
+
+    COMMENT_CREATED = 'comment-created',
+
+    ATTACHMENT_CREATED = 'attachment-created',
+
+    BLOG_POST = 'blogpost',
+    ATTACHMENT = 'attachment',
+    COMMENT = 'comment',
 }
