41814313b5605690370a0c9e34775236e188e030
anil <epipav@gmail.com>
2024-01-17 20:49:11 +0100

    tweaking configs

--- services/apps/cache_worker/src/main.ts
+++ services/apps/cache_worker/src/main.ts
@@ -15,7 +15,7 @@ const config: Config = {
 }
 
 const options: Options = {
-  maxConcurrentActivityTaskExecutions: 100,
+  maxConcurrentActivityTaskExecutions: 1000,
   maxTaskQueueActivitiesPerSecond: 100,
   postgres: {
     enabled: true,
