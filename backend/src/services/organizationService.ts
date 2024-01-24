c86e36f3b33e92226df8d2f50f121b1f594031c5
Joana Maia <joana@crowd.dev>
2024-01-24 10:49:48 +0000

    Fix lint

--- backend/src/services/organizationService.ts
+++ backend/src/services/organizationService.ts
@@ -190,7 +190,7 @@ export default class OrganizationService extends LoggerBase {
           original.displayName,
           toMerge.displayName,
           this.options.currentTenant.id,
-          this.options.currentUser.id
+          this.options.currentUser.id,
         ],
         searchAttributes: {
           TenantId: [this.options.currentTenant.id],
