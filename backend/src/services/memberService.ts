e657f34c4b57cfe6cd7b27e88252c988a129a4bb
Yeganathan S <hilariousengineer@protonmail.com>
2024-02-02 16:55:20 +0530

    Remove console logs and make prettier happy

--- backend/src/services/memberService.ts
+++ backend/src/services/memberService.ts
@@ -1674,9 +1674,16 @@ export default class MemberService extends LoggerBase {
   }
 
   async findAllAutocomplete(data) {
-    this.log.info('search and limit', JSON.stringify(data))
-    // return MemberRepository.findAllAutocomplete(search, limit, this.options)
-    return MemberRepository.findAndCountAllOpensearch({ filter: data.filter, offset: data.offset, orderBy: data.orderBy, limit: data.limit, segments: data.segments }, this.options)
+    return MemberRepository.findAndCountAllOpensearch(
+      {
+        filter: data.filter,
+        offset: data.offset,
+        orderBy: data.orderBy,
+        limit: data.limit,
+        segments: data.segments,
+      },
+      this.options,
+    )
   }
 
   async findAndCountActive(
@@ -1725,8 +1732,6 @@ export default class MemberService extends LoggerBase {
     } else {
       data.segments = [(await new SegmentRepository(this.options).getDefaultSegment()).id]
     }
-
-    this.log.info('memberQueryV2', JSON.stringify(data))
 
     const memberAttributeSettings = (
       await MemberAttributeSettingsRepository.findAndCountAll({}, this.options)
