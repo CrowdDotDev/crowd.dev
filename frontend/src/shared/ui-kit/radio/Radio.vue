8db80cff777a8c9509d4984ae0bf757fe6d3a377
Gašper Grom <gasper.grom@gmail.com>
2024-02-06 11:58:32 +0100

    Ui kit radio (#2180)

---
+++ frontend/src/shared/ui-kit/radio/Radio.vue
@@ -0,0 +1,46 @@
+<template>
+  <label
+    class="c-radio"
+    :class="[
+      `c-radio--${props.size}`,
+    ]"
+  >
+    <input v-model="checked" type="radio" :value="props.value" :disabled="props.disabled">
+    <span class="flex flex-col">
+      <slot />
+    </span>
+  </label>
+</template>
+
+<script setup lang="ts">
+import { RadioSize } from '@/shared/ui-kit/radio/types/RadioSize';
+import { computed, withDefaults } from 'vue';
+
+const props = withDefaults(defineProps<{
+  size?: RadioSize,
+  modelValue: string,
+  value?: string,
+  disabled?: boolean,
+}>(), {
+  size: 'medium',
+  value: '',
+  disabled: false,
+});
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string | boolean): any}>();
+
+const checked = computed<string | boolean>({
+  get() {
+    return props.modelValue;
+  },
+  set(val: string | boolean) {
+    emit('update:modelValue', val);
+  },
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'CrRadio',
+};
+</script>
