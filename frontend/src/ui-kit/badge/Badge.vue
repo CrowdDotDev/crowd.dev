f8de7a818de45d951661e3e56ced71feab814914
Gasper Grom <gasper.grom@gmail.com>
2024-02-09 09:31:20 +0100

    Badge ui kit

---
+++ frontend/src/ui-kit/badge/Badge.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="c-badge"
+    :class="[
+      `c-badge--${props.size}`,
+      `c-badge--${props.type}`,
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BadgeSize } from '@/ui-kit/badge/types/BadgeSize';
+import { BadgeType } from '@/ui-kit/badge/types/BadgeType';
+
+const props = withDefaults(defineProps<{
+  size?: BadgeSize,
+  type?: BadgeType,
+}>(), {
+  size: 'medium',
+  type: 'default',
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'CrBadge',
+};
+</script>
