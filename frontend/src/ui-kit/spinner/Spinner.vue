840fc33a951ca230f58aa2f2cc30e9e3af2cbd67
Gašper Grom <gasper.grom@gmail.com>
2024-02-12 11:35:16 +0100

    Ui kit spinner (#2190)

---
+++ frontend/src/ui-kit/spinner/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="c-spinner" :style="{ '--cr-spinner-size': props.size }" />
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  size?: string,
+}>(), {
+  size: '2rem',
+});
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'CrSpinner',
+};
+</script>
