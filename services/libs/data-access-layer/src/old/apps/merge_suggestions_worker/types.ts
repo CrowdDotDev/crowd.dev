56ab78189e2bb5f228e006f4ccd1966fa9bdc48c
Misha Savelyev <mikhail@crowd.dev>
2024-02-13 19:00:55 +0000

    Move merge_suggestions_worker sql code

---
+++ services/libs/data-access-layer/src/old/apps/merge_suggestions_worker/types.ts
@@ -0,0 +1,17 @@
+export interface IMemberNoMerge {
+  memberId: string
+  noMergeId: string
+}
+
+export interface IMemberMergeSuggestionsLatestGeneratedAt {
+  memberMergeSuggestionsLastGeneratedAt: string
+}
+
+export interface IMemberId {
+  id: string
+}
+
+export interface ITenant {
+  tenantId: string
+  plan: string
+}
