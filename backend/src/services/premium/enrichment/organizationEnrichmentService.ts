e2611a92e6e38f990c3e1f843c76822d14866dbf
Yeganathan S <hilariousengineer@protonmail.com>
2024-02-22 19:05:00 +0530

    Update envs for contact and organization enrichment

--- backend/src/services/premium/enrichment/organizationEnrichmentService.ts
+++ backend/src/services/premium/enrichment/organizationEnrichmentService.ts
@@ -89,6 +89,12 @@ export default class OrganizationEnrichmentService extends LoggerBase {
     includeOrgsActiveLastYear: boolean = false,
     verbose: boolean = false,
   ): Promise<IOrganization[]> {
+
+    // Check for organization enrichment API key
+    if (!this.apiKey) {
+      throw new Error('Organization enrichment API Key is not set!')
+    }
+
     const enrichmentPlatformPriority = [
       PlatformType.GITHUB,
       PlatformType.LINKEDIN,
