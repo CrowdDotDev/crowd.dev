3bc50af211fdb9fdd0b69bb3678b5e9d591b45d6
anilb <epipav@gmail.com>
2024-02-27 18:29:21 +0100

    removed ping logs

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -217,26 +217,14 @@ setImmediate(async () => {
     try {
       const [osPingRes, redisPingRes, dbPingRes, temporalPingRes] = await Promise.all([
         // ping opensearch
-        opensearch.ping().then((res) => {
-          console.log('Pinged opensearch!')
-          return res.body
-        }),
+        opensearch.ping().then((res) => res.body),
         // ping redis,
-        redis.ping().then((res) => {
-          console.log('Pinged redis!')
-          return res === 'PONG'
-        }),
+        redis.ping().then((res) => res === 'PONG'),
         // ping database
-        seq.query('select 1', { type: QueryTypes.SELECT }).then((rows) => {
-          console.log('Pinged database!')
-          return rows.length === 1
-        }),
+        seq.query('select 1', { type: QueryTypes.SELECT }).then((rows) => rows.length === 1),
         // ping temporal
         req.temporal
-          ? (req.temporal as TemporalClient).workflowService.getSystemInfo({}).then(() => {
-              console.log('Pinged temporal!')
-              return true
-            })
+          ? (req.temporal as TemporalClient).workflowService.getSystemInfo({}).then(() => true)
           : Promise.resolve(true),
       ])
 
