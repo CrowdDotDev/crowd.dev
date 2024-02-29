fe7ca1e00087f10dcf6692f98ee8c2ee6ffbb071
Uroš Marolt <uros@crowd.dev>
2024-02-29 17:53:08 +0000

    fix

--- backend/src/database/migrations/V1709228609__indexed-entities.sql
+++ backend/src/database/migrations/V1709228609__indexed-entities.sql
@@ -4,7 +4,7 @@ create table if not exists indexed_entities (
     tenant_id  uuid         not null,
     indexed_at timestamptz  not null default now(),
 
-    primary key (entity_id)
+    primary key (type, entity_id)
 );
 
 create index if not exists ix_indexed_entities_tenant on indexed_entities (tenant_id);
