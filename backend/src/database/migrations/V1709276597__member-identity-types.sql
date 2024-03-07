317d6b287750d2f854de176254546b3d181fedfa
Uroš Marolt <uros@crowd.dev>
2024-03-07 09:00:18 +0000

    migration script fixes

--- backend/src/database/migrations/V1709276597__member-identity-types.sql
+++ backend/src/database/migrations/V1709276597__member-identity-types.sql
@@ -11,7 +11,6 @@ set type = 'username';
 alter table "memberIdentities"
     alter column type set not null;
 
-
 alter table members
     rename column emails to "oldEmails";
 
@@ -25,23 +24,28 @@ $$
             loop
                 foreach email in array member_row."oldEmails"
                     loop
-                        if member_row."lastEnriched" is not null then
-                            if member_row."lastEnriched" > '2024-01-01 00:01:00 +00:00'::timestamptz then
-                                -- if it was enriched after January 1st emails that were set are reliable
-                                raise notice 'inserting enriched member (%) identity email "%" that is verified', member_row.id, email;
-                                insert into "memberIdentities"("memberId", platform, value, type, "sourceId", "tenantId", "isVerified")
-                                values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", true);
+                        if email is not null then
+                            if member_row."lastEnriched" is not null then
+                                if member_row."lastEnriched" > '2024-01-01 00:01:00 +00:00'::timestamptz then
+                                    -- if it was enriched after January 1st emails that were set are reliable
+                                    raise notice 'inserting enriched member (%) identity email "%" that is verified', member_row.id, email;
+                                    insert into "memberIdentities"("memberId", platform, value, type, "sourceId", "tenantId", "isVerified")
+                                    values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", true)
+                                    on conflict do nothing;
+                                else
+                                    -- if member was enriched before January 1st emails that were set are not reliable
+                                    raise notice 'inserting enriched member (%) identity email "%" that is not verified', member_row.id, email;
+                                    insert into "memberIdentities"("memberId", platform, value, type, "sourceId", "tenantId", "isVerified")
+                                    values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", false)
+                                    on conflict do nothing;
+                                end if;
                             else
-                                -- if member was enriched before January 1st emails that were set are not reliable
-                                raise notice 'inserting enriched member (%) identity email "%" that is not verified', member_row.id, email;
+                                -- member is not enriched -> emails came from integrations and are verified
+                                raise notice 'inserting member (%) identity email "%" that is verified', member_row.id, email;
                                 insert into "memberIdentities"("memberId", platform, value, type, "sourceId", "tenantId", "isVerified")
-                                values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", false);
+                                values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", true)
+                                on conflict do nothing;
                             end if;
-                        else
-                            -- member is not enriched -> emails came from integrations and are verified
-                            raise notice 'inserting member (%) identity email "%" that is verified', member_row.id, email;
-                            insert into "memberIdentities"("memberId", platform, value, type, "sourceId", "tenantId", "isVerified")
-                            values (member_row.id, 'unknown', email, 'email', null, member_row."tenantId", true);
                         end if;
                     end loop;
             end loop;
