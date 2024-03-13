4ab632036108ab9c206190ddf4a2c8f2608cf7a5
anilb <epipav@gmail.com>
2024-03-13 14:04:32 +0100

    fix workflow import

--- services/apps/profiles_worker/src/workflows.ts
+++ services/apps/profiles_worker/src/workflows.ts
@@ -1,5 +1,11 @@
 import { memberUpdate } from './workflows/member/memberUpdate'
 import { recalculateAffiliationsForNewRoles } from './workflows/member/recalculateAffiliationsForNewRoles'
+import { triggerRecalculateAffiliationsForEachTenant } from './workflows/member/triggerRecalculateAffiliationsForEachTenant'
 import { organizationUpdate } from './workflows/organization/organizationUpdate'
 
-export { memberUpdate, organizationUpdate, recalculateAffiliationsForNewRoles }
+export {
+  memberUpdate,
+  organizationUpdate,
+  recalculateAffiliationsForNewRoles,
+  triggerRecalculateAffiliationsForEachTenant,
+}
