dc9983fa5cbc59825d0a8f791a0bbbb20dae1e7d
anilb <epipav@gmail.com>
2024-03-13 13:56:04 +0100

    using continueAsNew for affiliation recalculation workflow

--- services/apps/profiles_worker/src/workflows/member/recalculateAffiliationsForNewRoles.ts
+++ services/apps/profiles_worker/src/workflows/member/recalculateAffiliationsForNewRoles.ts
@@ -3,13 +3,14 @@ import {
   ParentClosePolicy,
   executeChild,
   proxyActivities,
+  continueAsNew,
 } from '@temporalio/workflow'
 
 import { memberUpdate } from './memberUpdate'
 
 import * as activities from '../../activities'
 import { TemporalWorkflowId } from '@crowd/types'
-import { ITenantWithIdOnly } from '../../types/member'
+import { IRecalculateAffiliationsForNewRolesInput } from '../../types/member'
 
 const {
   getAffiliationsLastCheckedAtOfTenant,
@@ -24,55 +25,57 @@ const {
   Role changes are checked against the affiliationsLastCheckedAt date of the tenant.
   If this date is null, affiliations for all members activities are recalculated.
 */
-export async function recalculateAffiliationsForNewRoles(input: ITenantWithIdOnly): Promise<void> {
+export async function recalculateAffiliationsForNewRoles(
+  input: IRecalculateAffiliationsForNewRolesInput,
+): Promise<void> {
   try {
     const affiliationsLastChecked = await getAffiliationsLastCheckedAtOfTenant(input.tenant.id)
 
     const MEMBER_PAGE_SIZE = 100
-    let offset = 0
+    const offset = input.offset || 0
 
-    let memberIds = await getMemberIdsForAffiliationUpdates(
+    const memberIds = await getMemberIdsForAffiliationUpdates(
       affiliationsLastChecked,
       MEMBER_PAGE_SIZE,
       offset,
     )
 
-    while (memberIds.length > 0) {
-      // for MEMBER_PAGE_SIZE memberIds, wait execution of memberUpdate workflow for these, and get new memberIds
-      await Promise.all(
-        memberIds.map((id) => {
-          return executeChild(memberUpdate, {
-            workflowId: `${TemporalWorkflowId.MEMBER_UPDATE}/${input.tenant.id}/${id}`,
-            cancellationType: ChildWorkflowCancellationType.ABANDON,
-            parentClosePolicy: ParentClosePolicy.PARENT_CLOSE_POLICY_ABANDON,
-            retry: {
-              backoffCoefficient: 2,
-              initialInterval: 2 * 1000,
-              maximumInterval: 30 * 1000,
-            },
-            args: [
-              {
-                member: {
-                  id,
-                },
-              },
-            ],
-            searchAttributes: {
-              TenantId: [input.tenant.id],
-            },
-          })
-        }),
-      )
-
-      offset += MEMBER_PAGE_SIZE
-      memberIds = await getMemberIdsForAffiliationUpdates(
-        affiliationsLastChecked,
-        MEMBER_PAGE_SIZE,
-        offset,
-      )
+    if (memberIds.length === 0) {
+      await updateAffiliationsLastCheckedAtOfTenant(input.tenant.id)
+      return
     }
 
-    await updateAffiliationsLastCheckedAtOfTenant(input.tenant.id)
+    // for MEMBER_PAGE_SIZE memberIds, wait execution of memberUpdate workflow for these
+    // then trigger another workflow for the next MEMBER_PAGE_SIZE memberIds, using continueAsNew
+    await Promise.all(
+      memberIds.map((id) => {
+        return executeChild(memberUpdate, {
+          workflowId: `${TemporalWorkflowId.MEMBER_UPDATE}/${input.tenant.id}/${id}`,
+          cancellationType: ChildWorkflowCancellationType.ABANDON,
+          parentClosePolicy: ParentClosePolicy.PARENT_CLOSE_POLICY_ABANDON,
+          retry: {
+            backoffCoefficient: 2,
+            initialInterval: 2 * 1000,
+            maximumInterval: 30 * 1000,
+          },
+          args: [
+            {
+              member: {
+                id,
+              },
+            },
+          ],
+          searchAttributes: {
+            TenantId: [input.tenant.id],
+          },
+        })
+      }),
+    )
+
+    await continueAsNew<typeof recalculateAffiliationsForNewRoles>({
+      tenant: input.tenant,
+      offset: offset + MEMBER_PAGE_SIZE,
+    })
   } catch (err) {
     throw new Error(err)
   }
