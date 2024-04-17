75a41075dd947d4c6230d06a566a48f698fa9603
anilb <epipav@gmail.com>
2024-04-17 11:23:51 +0200

    reduce page size drastically

--- services/apps/merge_suggestions_worker/src/workflows/generateMemberMergeSuggestions.ts
+++ services/apps/merge_suggestions_worker/src/workflows/generateMemberMergeSuggestions.ts
@@ -10,8 +10,8 @@ const activity = proxyActivities<typeof activities>({ startToCloseTimeout: '1 mi
 export async function generateMemberMergeSuggestions(
   args: IProcessGenerateMemberMergeSuggestionsArgs,
 ): Promise<void> {
-  const PAGE_SIZE = 500
-  const PARALLEL_SUGGESTION_PROCESSING = 50
+  const PAGE_SIZE = 50
+  const PARALLEL_SUGGESTION_PROCESSING = 10
 
   let lastUuid: string = args.lastUuid || null
 
