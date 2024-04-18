f17662029dde59b1d9695f368e9b28f7c28c5170
Yeganathan S <hilariousengineer@protonmail.com>
2024-04-18 22:07:45 +0530

    make formatter happy

--- backend/src/services/memberService.ts
+++ backend/src/services/memberService.ts
@@ -1697,7 +1697,7 @@ export default class MemberService extends LoggerBase {
               await searchSyncService.triggerOrganizationSync(this.options.currentTenant.id, org.id)
             }
           }
-          
+
           // return updated record from OpenSearch instead of db
           // quick hack to make sure tests don't fail if OpenSearch is disabled
           return await MemberRepository.findByIdOpensearch(record.id, this.options)
