854080c7c90656adbe2794c38ecd8d694c94ee5a
anilb <epipav@gmail.com>
2024-04-25 12:18:46 +0200

    revert similarity filter bypass

--- backend/src/database/repositories/memberRepository.ts
+++ backend/src/database/repositories/memberRepository.ts
@@ -269,8 +269,7 @@ class MemberRepository {
       ? 'mtm."activityEstimate" desc, mtm.similarity desc, mtm."memberId", mtm."toMergeId"'
       : 'mtm.similarity desc, mtm."activityEstimate" desc, mtm."memberId", mtm."toMergeId"'
 
-    // const similarityFilter = isSegmentsEnabled ? ' and mtm.similarity > 0.95 ' : ''
-    const similarityFilter = ''
+    const similarityFilter = isSegmentsEnabled ? ' and mtm.similarity > 0.95 ' : ''
 
     const memberFilter = memberId
       ? ` and (mtm."memberId" = :memberId OR mtm."toMergeId" = :memberId)`
