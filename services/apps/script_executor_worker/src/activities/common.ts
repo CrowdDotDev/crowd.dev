b4ad8d7f4de53854afae0c2f2fccede89ddfce07
anilb <epipav@gmail.com>
2024-05-06 09:29:48 +0200

    testing fix for fetch error `Cannot construct a Request with a Request object that has already been used`

--- services/apps/script_executor_worker/src/activities/common.ts
+++ services/apps/script_executor_worker/src/activities/common.ts
@@ -3,19 +3,18 @@ export async function mergeMembers(
   secondaryMemberId: string,
   tenantId: string,
 ): Promise<void> {
-  const res = await fetch(
-    `${process.env['CROWD_API_SERVICE_URL']}/tenant/${tenantId}/member/${primaryMemberId}/merge`,
-    {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${process.env['CROWD_API_SERVICE_USER_TOKEN']}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        memberToMerge: secondaryMemberId,
-      }),
+  const url = `${process.env['CROWD_API_SERVICE_URL']}/tenant/${tenantId}/member/${primaryMemberId}/merge`
+  const requestOptions = {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${process.env['CROWD_API_SERVICE_USER_TOKEN']}`,
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify({
+      memberToMerge: secondaryMemberId,
+    }),
+  }
+  const res = await fetch(url, requestOptions)
 
   if (res.status !== 200) {
     throw new Error(`Failed to merge member ${primaryMemberId} with ${secondaryMemberId}!`)
