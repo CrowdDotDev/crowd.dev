d659b0dbf4bcf940fd9af3084195d352096cb840
anilb <epipav@gmail.com>
2024-05-10 17:45:24 +0200

    more debugging

--- services/apps/merge_suggestions_worker/src/workflows/llm.ts
+++ services/apps/merge_suggestions_worker/src/workflows/llm.ts
@@ -13,7 +13,7 @@ export async function llm(args: IProcessCheckMemberSimilarityWithLLM): Promise<v
     const res = await activity.getLLMResult(members)
     const result: ILLMResult = JSON.parse(res)
     console.log(`Raw res: `)
-    console.log(result.generation.replace(/`/g, '').trim())
+    console.log(result)
     const textResponse =
       result.generation.replace(/`/g, '').trim() === 'true' ? 'similar' : 'not similar'
     console.log(`LLM thinks that ${memberCouple[0]} and ${memberCouple[1]} are ${textResponse}`)
