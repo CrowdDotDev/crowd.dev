442b1c3115bd1260d554cc81320bd14d9f5d49ac
Yeganathan S <hilariousengineer@protonmail.com>
2024-05-23 13:26:10 +0530

    fix linter and tsc

--- backend/src/database/repositories/sequelizeRepository.ts
+++ backend/src/database/repositories/sequelizeRepository.ts
@@ -11,7 +11,8 @@ import {
   SequelizeQueryExecutor,
   TransactionalSequelizeQueryExecutor,
 } from '@crowd/data-access-layer/src/queryExecutor'
-import { API_CONFIG, IS_TEST_ENV, REDIS_CONFIG, TEMPORAL_CONFIG, UNLEASH_CONFIG } from '../../conf'
+import { getDbConnection } from '@crowd/data-access-layer/src/database'
+import { API_CONFIG, IS_TEST_ENV, PRODUCT_DB_CONFIG, REDIS_CONFIG, TEMPORAL_CONFIG, UNLEASH_CONFIG } from '../../conf'
 import { databaseInit } from '../databaseConnection'
 import { IRepositoryOptions } from './IRepositoryOptions'
 import { IServiceOptions } from '../../services/IServiceOptions'
@@ -63,6 +64,7 @@ export default class SequelizeRepository {
       redis: await getRedisClient(REDIS_CONFIG, true),
       unleash,
       temporal,
+      productDb: await getDbConnection(PRODUCT_DB_CONFIG),
     }
   }
 
--- backend/src/database/utils/sequelizeTestUtils.ts
+++ backend/src/database/utils/sequelizeTestUtils.ts
@@ -6,7 +6,8 @@ import { SegmentStatus, TenantPlans } from '@crowd/types'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import moment from 'moment'
-import { API_CONFIG, REDIS_CONFIG, TEMPORAL_CONFIG } from '../../conf'
+import { getDbConnection } from '@crowd/data-access-layer/src/database'
+import { API_CONFIG, PRODUCT_DB_CONFIG, REDIS_CONFIG, TEMPORAL_CONFIG } from '../../conf'
 import Roles from '../../security/roles'
 import { IServiceOptions } from '../../services/IServiceOptions'
 import { databaseInit } from '../databaseConnection'
@@ -176,6 +177,7 @@ export default class SequelizeTestUtils {
       log,
       redis,
       temporal: await getTemporalClient(TEMPORAL_CONFIG),
+      productDb: await getDbConnection(PRODUCT_DB_CONFIG),
     } as IServiceOptions
   }
 
@@ -236,6 +238,7 @@ export default class SequelizeTestUtils {
       log,
       redis,
       temporal: await getTemporalClient(TEMPORAL_CONFIG),
+      productDb: await getDbConnection(PRODUCT_DB_CONFIG),
     } as IRepositoryOptions
   }
 
