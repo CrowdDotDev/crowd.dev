821204635447be8cae3f47553ab6aa4b863405eb
Yeganathan S <hilariousengineer@protonmail.com>
2024-05-24 16:46:01 +0530

    wip

--- backend/src/api/product/productSessionCreate.ts
+++ backend/src/api/product/productSessionCreate.ts
@@ -1,12 +1,20 @@
 import { Error403 } from '@crowd/common'
 import ProductAnalyticsService from '@/services/productAnalyticsService'
+import { lookup } from 'fast-geoip'
 
 export default async (req, res) => {
   if (!req.currentUser || !req.currentUser.id) {
     throw new Error403(req.language)
   }
 
-  await new ProductAnalyticsService(req).createSession(req.body)
+  // req.log.info('ProductSessionCreate', req.ip)
+  // req.log.info('location', await lookup('10.90.0.1'))
+
+  const ip = req.headers['x-forwarded-for']
+  req.log.info('ip', ip)
+
+  // await new ProductAnalyticsService(req).createSession(req.body)
+
 
   const payload = true
 
