412eff72a937a71fdf60272324612ae47b28258e
Yeganathan S <hilariousengineer@protonmail.com>
2024-05-27 16:18:55 +0530

    Optimize `sync-all-*` scripts with read and write host (#2311)
    Co-authored-by: Joana Maia <joana@crowd.dev>

--- services/libs/opensearch/src/service/organization.sync.service.ts
+++ services/libs/opensearch/src/service/organization.sync.service.ts
@@ -21,17 +21,19 @@ export class OrganizationSyncService {
   private readonly indexingRepo: IndexingRepository
 
   constructor(
-    store: DbStore,
+    writeStore: DbStore,
     private readonly openSearchService: OpenSearchService,
     parentLog: Logger,
     serviceConfig: IServiceConfig,
+    readStore?: DbStore,
   ) {
     this.log = getChildLogger('organization-sync-service', parentLog)
     this.serviceConfig = serviceConfig
 
+    const store = readStore || writeStore
     this.orgRepo = new OrganizationRepository(store, this.log)
     this.segmentRepo = new SegmentRepository(store, this.log)
-    this.indexingRepo = new IndexingRepository(store, this.log)
+    this.indexingRepo = new IndexingRepository(writeStore, this.log)
   }
 
   public async getAllIndexedTenantIds(
@@ -208,14 +210,20 @@ export class OrganizationSyncService {
     }
   }
 
-  public async syncTenantOrganizations(tenantId: string, batchSize = 100): Promise<void> {
+  public async syncTenantOrganizations(tenantId: string, batchSize = 200): Promise<void> {
     this.log.warn({ tenantId }, 'Syncing all tenant organizations!')
     let docCount = 0
     let organizationCount = 0
+    let previousBatchIds: string[] = []
+    const now = new Date()
 
     await logExecutionTime(
       async () => {
-        let organizationIds = await this.orgRepo.getTenantOrganizationsForSync(tenantId, batchSize)
+        let organizationIds = await this.orgRepo.getTenantOrganizationsForSync(
+          tenantId,
+          batchSize,
+          previousBatchIds,
+        )
 
         while (organizationIds.length > 0) {
           const { organizationsSynced, documentsIndexed } = await this.syncOrganizations(
@@ -225,9 +233,12 @@ export class OrganizationSyncService {
           organizationCount += organizationsSynced
           docCount += documentsIndexed
 
+          const diffInMinutes = (new Date().getTime() - now.getTime()) / 1000 / 60
           this.log.info(
             { tenantId },
-            `Synced ${organizationCount} organizations with ${docCount} documents!`,
+            `Synced ${organizationCount} organizations! Speed: ${Math.round(
+              organizationCount / diffInMinutes,
+            )} organizations/minute!`,
           )
 
           await this.indexingRepo.markEntitiesIndexed(
@@ -239,7 +250,13 @@ export class OrganizationSyncService {
               }
             }),
           )
-          organizationIds = await this.orgRepo.getTenantOrganizationsForSync(tenantId, batchSize)
+
+          previousBatchIds = organizationIds
+          organizationIds = await this.orgRepo.getTenantOrganizationsForSync(
+            tenantId,
+            batchSize,
+            previousBatchIds,
+          )
         }
       },
       this.log,
