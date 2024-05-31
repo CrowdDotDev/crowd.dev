4e71f7f20297aee6f8f7c2253825c762ba05c3e0
anilb <epipav@gmail.com>
2024-05-31 12:44:38 +0200

    fix typo

--- backend/src/services/memberService.ts
+++ backend/src/services/memberService.ts
@@ -716,10 +716,8 @@ export default class MemberService extends LoggerBase {
 
       // move memberOrganizations
       if (payload.secondary.memberOrganizations.length > 0) {
-        console.log(`nonExistingIds:`, nonExistingIds)
-        for (const role of payload.secondary.memberOrganizations.filter((r) => !nonExistingIds.includes(r.organizationId))) {
-          console.log(`creating role:`)
-          console.log(role)
+        const nonExistingOrganizationIds = await OrganizationRepository.findNonExistingIds(payload.secondary.memberOrganizations.map((o) => o.organizationId), repoOptions)
+        for (const role of payload.secondary.memberOrganizations.filter((r) => !nonExistingOrganizationIds.includes(r.organizationId))) {
           await MemberOrganizationRepository.addMemberRole(
             { ...role, memberId: secondaryMember.id },
             repoOptions,
