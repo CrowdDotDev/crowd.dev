8a4eb62ffa0774ce8489d00e7d4bb10fe36f58e6
Gašper Grom <gasper.grom@gmail.com>
2024-06-03 11:18:15 +0200

    Avatar theme component (#2446)

---
+++ frontend/src/ui-kit/avatar/Avatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="c-avatar"
+    :style="{ '--lf-avatar-size': `${props.size / 16}rem` }"
+    :data-initials="initials"
+  >
+    <img v-if="props.src" :alt="props.name" :src="props.src" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<{
+  size?: number,
+  name?: string,
+  src?: string,
+}>(), {
+  size: 48,
+  name: '',
+  src: '',
+});
+
+const initials = computed(() => props.name.match(/\b\w/g)?.join('').substring(0, 2) || '');
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfAvatar',
+};
+</script>
