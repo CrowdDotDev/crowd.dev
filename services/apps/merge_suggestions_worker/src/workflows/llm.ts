532db3e76313b1869e8b123df68bc384a59eed2d
anilb <epipav@gmail.com>
2024-06-04 14:25:50 +0200

    some fixes and better logging for test results

--- services/apps/merge_suggestions_worker/src/workflows/llm.ts
+++ services/apps/merge_suggestions_worker/src/workflows/llm.ts
@@ -21,33 +21,42 @@ const commonActivitiesProxy = proxyActivities<typeof commonActivities>({
 export async function llm(args: IProcessCheckSimilarityWithLLM): Promise<void> {
   console.log('llm workflow')
 
-  for (const memberCouple of args.memberCouples) {
-    const members = await memberActivitiesProxy.getMembersForLLMConsumption(memberCouple)
-    const res = await commonActivitiesProxy.getLLMResult(
-      members,
-      args.modelId,
-      args.prompt,
-      args.region,
-      args.modelSpecificArgs,
-    )
-    const result: ILLMResult = JSON.parse(res)
-    console.log(`Raw res: `)
-    console.log(result)
+  if (args.memberCouples && args.memberCouples.length > 0) {
+    for (const memberCouple of args.memberCouples) {
+      console.log(`Checking similarity between: ${memberCouple[0]} and ${memberCouple[1]}`)
+
+      const members = await memberActivitiesProxy.getMembersForLLMConsumption(memberCouple)
+      const res = await commonActivitiesProxy.getLLMResult(
+        members,
+        args.modelId,
+        args.prompt,
+        args.region,
+        args.modelSpecificArgs,
+      )
+      const result: ILLMResult = JSON.parse(res)
+      console.log(`Raw res: `)
+      console.log(result)
+    }
   }
 
-  for (const organizationCouple of args.organizationCouples) {
-    const organizations = await organizationActivitiesProxy.getOrganizationsForLLMConsumption(
-      organizationCouple,
-    )
-    const res = await commonActivitiesProxy.getLLMResult(
-      organizations,
-      args.modelId,
-      args.prompt,
-      args.region,
-      args.modelSpecificArgs,
-    )
-    const result: ILLMResult = JSON.parse(res)
-    console.log(`Raw res: `)
-    console.log(result)
+  if (args.organizationCouples && args.organizationCouples.length > 0) {
+    for (const organizationCouple of args.organizationCouples) {
+      console.log(
+        `Checking similarity between: ${organizationCouple[0]} and ${organizationCouple[1]}`,
+      )
+      const organizations = await organizationActivitiesProxy.getOrganizationsForLLMConsumption(
+        organizationCouple,
+      )
+      const res = await commonActivitiesProxy.getLLMResult(
+        organizations,
+        args.modelId,
+        args.prompt,
+        args.region,
+        args.modelSpecificArgs,
+      )
+      const result: ILLMResult = JSON.parse(res)
+      console.log(`Raw res: `)
+      console.log(result)
+    }
   }
 }
