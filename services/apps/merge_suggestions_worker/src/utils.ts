afc4525a7b22e8a4548c94beda6f5093f312c0a0
anilb <epipav@gmail.com>
2024-06-12 16:00:47 +0200

    email obfuscation before sending prompt to llm

--- services/apps/merge_suggestions_worker/src/utils.ts
+++ services/apps/merge_suggestions_worker/src/utils.ts
@@ -1,3 +1,5 @@
+import { ILLMConsumableMember } from '@crowd/types'
+
 export const prefixLength = (string: string) => {
   if (string.length > 5 && string.length < 8) {
     return 6
@@ -12,4 +14,42 @@ export function chunkArray<T>(array: T[], chunkSize: number): T[][] {
     chunks.push(array.slice(i, i + chunkSize))
   }
   return chunks
+}
+
+export const obfuscateEmail = (email: string): string => {
+  // make email all lowercase
+  email = email.toLowerCase()
+  const obfuscatedArray: string[] = []
+
+  for (let i = 0; i < email.length; i++) {
+    const char = email[i]
+
+    // Check if the character is a '.' or '@'
+    if (char === '.' || char === '@') {
+      obfuscatedArray.push(char)
+    } else {
+      // Get the character code of the current character
+      const charCode = char.charCodeAt(0)
+
+      // Calculate the new character code by adding 1 and using modulus 256 for overflow
+      // 256 is chosen because char codes range from 0 to 255 in the ASCII table
+      const newCharCode = (charCode + 1) % 256
+
+      // Convert the new character code back to a string and add it to the array
+      obfuscatedArray.push(String.fromCharCode(newCharCode))
+    }
+  }
+
+  // Join the array into a single string and return it
+  return obfuscatedArray.join('')
+}
+
+export const obfuscateEmailIdentitiesOfMember = (
+  member: ILLMConsumableMember,
+): ILLMConsumableMember => {
+  for (const emailIdentity of member.identities.filter((identity) => identity.type === 'email')) {
+    emailIdentity.value = obfuscateEmail(emailIdentity.value)
+  }
+
+  return member
 }
--- services/apps/merge_suggestions_worker/src/workflows/mergeMembersWithLLM.ts
+++ services/apps/merge_suggestions_worker/src/workflows/mergeMembersWithLLM.ts
@@ -8,6 +8,7 @@ import {
   IProcessMergeOrganizationSuggestionsWithLLM,
 } from '.pnpm/node_modules/@crowd/merge-suggestions-worker/src/types'
 import { LLMSuggestionVerdictType } from '@crowd/types'
+import { obfuscateEmailIdentitiesOfMember } from '../utils'
 
 const memberActivitiesProxy = proxyActivities<typeof memberActivities>({
   startToCloseTimeout: '1 minute',
@@ -66,7 +67,7 @@ export async function mergeMembersWithLLM(
     const members = await memberActivitiesProxy.getMembersForLLMConsumption(suggestion)
 
     const llmResult: ILLMResult = await commonActivitiesProxy.getLLMResult(
-      members,
+      members.map((member) => obfuscateEmailIdentitiesOfMember(member)),
       MODEL_ID,
       PROMPT,
       REGION,
