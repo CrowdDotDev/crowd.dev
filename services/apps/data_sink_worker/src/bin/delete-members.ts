911a668e4a3226d1d66e9a2e6f66678da2beb396
Yeganathan S <hilariousengineer@protonmail.com>
2024-06-19 15:17:31 +0530

    improve the script

--- services/apps/data_sink_worker/src/bin/delete-members.ts
+++ services/apps/data_sink_worker/src/bin/delete-members.ts
@@ -1,6 +1,6 @@
-import { DbStore, getDbConnection } from '@crowd/data-access-layer/src/database'
+import { getDbConnection } from '@crowd/data-access-layer/src/database'
 import { getServiceLogger } from '@crowd/logging'
-import { DB_CONFIG, REDIS_CONFIG, SQS_CONFIG, UNLEASH_CONFIG } from '../conf'
+import { DB_CONFIG } from '../conf'
 
 import {
   checkIfMemberExists,
@@ -8,19 +8,9 @@ import {
   deleteMember,
   deleteMemberActivities,
 } from '@crowd/data-access-layer/src/old/apps/data_sink_worker/scripts/delete-members'
-import { getSqsClient } from '@crowd/sqs'
-import {
-  PriorityLevelContextRepository,
-  QueuePriorityContextLoader,
-  SearchSyncWorkerEmitter,
-} from '@crowd/common_services'
-import { getUnleashClient } from '@crowd/feature-flags'
-import { getRedisClient } from '@crowd/redis'
-import { getServiceTracer } from '@crowd/tracing'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-const tracer = getServiceTracer()
 const log = getServiceLogger()
 
 const processArguments = process.argv.slice(2)
@@ -34,44 +24,22 @@ const memberIds = processArguments[0].split(',')
 
 setImmediate(async () => {
   const dbClient = await getDbConnection(DB_CONFIG())
-  const sqsClient = getSqsClient(SQS_CONFIG())
-  const unleash = await getUnleashClient(UNLEASH_CONFIG())
-  const redis = await getRedisClient(REDIS_CONFIG())
-
-  const store = new DbStore(log, dbClient)
-
-  const priorityLevelRepo = new PriorityLevelContextRepository(store, log)
-  const loader: QueuePriorityContextLoader = (tenantId: string) =>
-    priorityLevelRepo.loadPriorityLevelContext(tenantId)
 
   log.info(`Started deleting members with ids: ${memberIds.join(', ')}`)
 
   const totalMemberIds = memberIds.length
   let processedMembers = 0
 
-  const searchSyncWorkerEmitter = new SearchSyncWorkerEmitter(
-    sqsClient,
-    redis,
-    tracer,
-    unleash,
-    loader,
-    log,
-  )
+  for (const memberId of memberIds) {
+    const memberExists = await checkIfMemberExists(dbClient, memberId)
 
-  await searchSyncWorkerEmitter.init()
-
-  while (processedMembers < totalMemberIds) {
-    const memberId = memberIds.shift()
-
-    if (!checkIfMemberExists(dbClient, memberId)) {
+    if (!memberExists) {
       log.info(`Member not found: ${memberId}`)
       continue
     }
 
     await deleteMemberActivities(dbClient, memberId)
-
     await deleteMemberSegments(dbClient, memberId)
-
     await deleteMember(dbClient, memberId)
 
     processedMembers += 1
