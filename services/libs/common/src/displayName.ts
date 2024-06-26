50a401b15cf1a56873707e274fc0b36b8f2ea2c6
garrrikkotua <kotuaigor@gmail.com>
2024-06-26 17:53:05 +0300

    improve logic

--- services/libs/common/src/displayName.ts
+++ services/libs/common/src/displayName.ts
@@ -1,4 +1,4 @@
-import { isEmail } from './validations'
+import { isEmail, isPartialEmail } from './validations'
 
 /**
  * Get a proper display name from a given name string.
@@ -19,23 +19,32 @@ import { isEmail } from './validations'
  * getProperDisplayName('John john.doe@example.com Doe') // returns 'John Doe'
  * getProperDisplayName('john.doe@example.com jane.doe@example.com') // returns 'john.doe'
  * getProperDisplayName('john@gmail.com') // returns 'john'
+ * getProperDisplayName('john@gmail') // returns 'john'
+ * getProperDisplayName('john@gmail.') // returns 'john'
+ * getProperDisplayName('john@g') // returns 'john'
+ * getProperDisplayName('@johndoe') // returns '@johndoe'
+ * getProperDisplayName('johndoe@') // returns 'johndoe@'
  */
 export function getProperDisplayName(name: string): string {
-  const nameParts = name.trim().split(' ')
+  const nameParts = name.trim().split(/\s+/)
+
+  if (nameParts.length === 1 && !nameParts[0]) {
+    throw new Error('Display name cannot be empty')
+  }
 
   if (nameParts.length === 0) {
     throw new Error('Display name cannot be empty')
   }
 
   if (nameParts.length === 1) {
-    if (isEmail(nameParts[0]) || nameParts[0].includes('@')) {
+    if (isEmail(nameParts[0]) || isPartialEmail(nameParts[0])) {
       return nameParts[0].split('@')[0]
     }
     return nameParts[0]
   }
 
   // parts that are not emails
-  const filteredNameParts = nameParts.filter((part) => !isEmail(part) && !part.includes('@'))
+  const filteredNameParts = nameParts.filter((part) => !isEmail(part) && !isPartialEmail(part))
 
   if (filteredNameParts.length > 0) {
     return filteredNameParts.join(' ')
