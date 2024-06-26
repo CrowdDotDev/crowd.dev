9872aa813f74d7725b47379c307162191459d0f3
Misha Savelyev <mikhail@crowd.dev>
2024-06-26 09:39:39 +0000

    Don't allow null values for attributes

--- backend/src/database/migrations/V1718289198__org-attributes.sql
+++ backend/src/database/migrations/V1718289198__org-attributes.sql
@@ -7,7 +7,7 @@ CREATE TABLE "orgAttributes" (
     "name" VARCHAR(255) NOT NULL,
     "source" VARCHAR(255) NOT NULL,
     "default" BOOLEAN NOT NULL DEFAULT FALSE,
-    "value" TEXT
+    "value" TEXT NOT NULL
 );
 
 -- make sure there is only one default attribute
@@ -16,6 +16,25 @@ CREATE UNIQUE INDEX "orgAttributes_organizationId_name_default" ON "orgAttribute
 -- make sure there are no duplicate values for the same attribute and the same source
 -- MD5 is needed because descriptions and *ByMonth fields can be too long for an index
 CREATE UNIQUE INDEX "orgAttributes_organizationId_name_source_value" ON "orgAttributes" ("organizationId", "name", "source", MD5("value"));
+
+CREATE OR REPLACE FUNCTION add_org_attribute(
+   _org_id UUID,
+   _type TEXT,
+   _name TEXT,
+   _source TEXT,
+   _default BOOLEAN,
+   _value anyelement
+)
+RETURNS VOID AS $$
+BEGIN
+    IF _value IS NOT NULL THEN
+        INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
+        VALUES (_org_id, _type, _name, _source, _default, _value::TEXT)
+        ON CONFLICT DO NOTHING;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 
 DO $$
 DECLARE
@@ -32,88 +51,72 @@ BEGIN
              ELSE 'unknown'
          END;
 
-        INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-        VALUES
-            (_org.id, 'string', 'description', _source, TRUE, _org.description),
-            (_org.id, 'string', 'logo', _source, TRUE, _org.logo),
-            (_org.id, 'string', 'location', _source, TRUE, _org.location),
-            (_org.id, 'string', 'type', _source, TRUE, _org.type),
-            (_org.id, 'string', 'geoLocation', _source, TRUE, _org."geoLocation"),
-            (_org.id, 'string', 'size', _source, TRUE, _org.size),
-            (_org.id, 'string', 'ticker', _source, TRUE, _org.ticker),
-            (_org.id, 'string', 'headline', _source, TRUE, _org.headline),
-            (_org.id, 'string', 'industry', _source, TRUE, _org.industry),
-            (_org.id, 'string', 'gicsSector', _source, TRUE, _org."gicsSector"),
-            (_org.id, 'string', 'ultimateParent', _source, TRUE, _org."ultimateParent"),
-            (_org.id, 'string', 'immediateParent', _source, TRUE, _org."immediateParent"),
+        PERFORM add_org_attribute(_org.id, 'string', 'description', _source, TRUE, _org.description);
 
-            (_org.id, 'integer', 'employees', _source, TRUE, _org.employees),
-            (_org.id, 'integer', 'founded', _source, TRUE, _org.founded),
+        PERFORM add_org_attribute(_org.id, 'string', 'logo', _source, TRUE, _org.logo);
+        PERFORM add_org_attribute(_org.id, 'string', 'location', _source, TRUE, _org.location);
+        PERFORM add_org_attribute(_org.id, 'string', 'type', _source, TRUE, _org.type);
+        PERFORM add_org_attribute(_org.id, 'string', 'geoLocation', _source, TRUE, _org."geoLocation");
+        PERFORM add_org_attribute(_org.id, 'string', 'size', _source, TRUE, _org.size);
+        PERFORM add_org_attribute(_org.id, 'string', 'ticker', _source, TRUE, _org.ticker);
+        PERFORM add_org_attribute(_org.id, 'string', 'headline', _source, TRUE, _org.headline);
+        PERFORM add_org_attribute(_org.id, 'string', 'industry', _source, TRUE, _org.industry);
+        PERFORM add_org_attribute(_org.id, 'string', 'gicsSector', _source, TRUE, _org."gicsSector");
+        PERFORM add_org_attribute(_org.id, 'string', 'ultimateParent', _source, TRUE, _org."ultimateParent");
+        PERFORM add_org_attribute(_org.id, 'string', 'immediateParent', _source, TRUE, _org."immediateParent");
 
-            (_org.id, 'decimal', 'averageEmployeeTenure', _source, TRUE, _org."averageEmployeeTenure"),
+        PERFORM add_org_attribute(_org.id, 'integer', 'employees', _source, TRUE, _org.employees);
+        PERFORM add_org_attribute(_org.id, 'integer', 'founded', _source, TRUE, _org.founded);
 
-            (_org.id, 'object', 'revenueRange', _source, TRUE, _org."revenueRange"),
-            (_org.id, 'object', 'employeeCountByCountry', _source, TRUE, _org."employeeCountByCountry"),
-            (_org.id, 'object', 'address', _source, TRUE, _org.address),
-            (_org.id, 'object', 'averageTenureByLevel', _source, TRUE, _org."averageTenureByLevel"),
-            (_org.id, 'object', 'averageTenureByRole', _source, TRUE, _org."averageTenureByRole"),
-            (_org.id, 'object', 'employeeChurnRate', _source, TRUE, _org."employeeChurnRate"),
-            (_org.id, 'object', 'employeeCountByMonth', _source, TRUE, _org."employeeCountByMonth"),
-            (_org.id, 'object', 'employeeGrowthRate', _source, TRUE, _org."employeeGrowthRate"),
-            (_org.id, 'object', 'employeeCountByMonthByLevel', _source, TRUE, _org."employeeCountByMonthByLevel"),
-            (_org.id, 'object', 'employeeCountByMonthByRole', _source, TRUE, _org."employeeCountByMonthByRole"),
-            (_org.id, 'object', 'grossAdditionsByMonth', _source, TRUE, _org."grossAdditionsByMonth"),
-            (_org.id, 'object', 'grossDeparturesByMonth', _source, TRUE, _org."grossDeparturesByMonth"),
-            (_org.id, 'object', 'naics', _source, TRUE, _org.naics)
-        ON CONFLICT DO NOTHING;
+        PERFORM add_org_attribute(_org.id, 'decimal', 'averageEmployeeTenure', _source, TRUE, _org."averageEmployeeTenure");
+
+        PERFORM add_org_attribute(_org.id, 'object', 'revenueRange', _source, TRUE, _org."revenueRange");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeCountByCountry', _source, TRUE, _org."employeeCountByCountry");
+        PERFORM add_org_attribute(_org.id, 'object', 'address', _source, TRUE, _org.address);
+        PERFORM add_org_attribute(_org.id, 'object', 'averageTenureByLevel', _source, TRUE, _org."averageTenureByLevel");
+        PERFORM add_org_attribute(_org.id, 'object', 'averageTenureByRole', _source, TRUE, _org."averageTenureByRole");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeChurnRate', _source, TRUE, _org."employeeChurnRate");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeCountByMonth', _source, TRUE, _org."employeeCountByMonth");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeGrowthRate', _source, TRUE, _org."employeeGrowthRate");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeCountByMonthByLevel', _source, TRUE, _org."employeeCountByMonthByLevel");
+        PERFORM add_org_attribute(_org.id, 'object', 'employeeCountByMonthByRole', _source, TRUE, _org."employeeCountByMonthByRole");
+        PERFORM add_org_attribute(_org.id, 'object', 'grossAdditionsByMonth', _source, TRUE, _org."grossAdditionsByMonth");
+        PERFORM add_org_attribute(_org.id, 'object', 'grossDeparturesByMonth', _source, TRUE, _org."grossDeparturesByMonth");
+        PERFORM add_org_attribute(_org.id, 'object', 'naics', _source, TRUE, _org.naics);
 
         FOR _value IN SELECT UNNEST(_org."emails") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'email', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'email', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."names") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'name', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'name', _source, FALSE, _value);
         END LOOP;
-        INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-        VALUES (_org.id, 'string', 'name', _source, TRUE, _org."displayName")
-        ON CONFLICT DO NOTHING;
+        PERFORM add_org_attribute(_org.id, 'string', 'name', _source, TRUE, _org."displayName");
         FOR _value IN SELECT UNNEST(_org."phoneNumbers") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'phoneNumber', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'phoneNumber', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."tags") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'tag', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'tag', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."profiles") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'profile', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'profile', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."allSubsidiaries") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'subsidiary', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'subsidiary', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."alternativeNames") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'alternativeName', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'alternativeName', _source, FALSE, _value);
         END LOOP;
         FOR _value IN SELECT UNNEST(_org."directSubsidiaries") AS value LOOP
-            INSERT INTO "orgAttributes" ("organizationId", "type", "name", "source", "default", "value")
-            VALUES (_org.id, 'string', 'directSubsidiary', _source, FALSE, _value)
-            ON CONFLICT DO NOTHING;
+            PERFORM add_org_attribute(_org.id, 'string', 'directSubsidiary', _source, FALSE, _value);
         END LOOP;
+--         EXCEPTION WHEN OTHERS THEN
+--             RAISE EXCEPTION '_org: %', _org.id;
+--         END;
     END LOOP;
 END;
 $$;
 
+DROP FUNCTION add_org_attribute(UUID, TEXT, TEXT, TEXT, BOOLEAN, TEXT);
 
 ALTER TABLE organizations RENAME COLUMN "emails" TO "old_emails";
 ALTER TABLE organizations RENAME COLUMN "phoneNumbers" TO "old_phoneNumbers";
