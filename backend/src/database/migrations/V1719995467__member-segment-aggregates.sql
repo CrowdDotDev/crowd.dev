18c7f117402fb7ad4bc4ac70dbab8f9dcbbf4fb5
anil <epipav@gmail.com>
2024-07-03 12:56:49 +0300

    migs start

---
+++ backend/src/database/migrations/V1719995467__member-segment-aggregates.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "memberSegmentsAgg" (
+    "id" UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "memberId" UUID NOT NULL,
+    "segmentId" UUID NOT NULL,
+    "tenantId" UUID NOT NULL,
+    "activityCount" BIGINT NOT NULL,
+    "lastActive" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "activityTypes" TEXT[] NOT NULL,
+    "activeOn" TEXT[] NOT NULL,
+    "activeDaysCount" BIGINT NOT NULL,
+    "averageSentiment" FLOAT NOT NULL,
+    UNIQUE ("memberId", "segmentId")
+);
