cc7db9b50dd076986fa2928dd20f82a0f61587ca
Yeganathan S <hilariousengineer@protonmail.com>
2024-07-09 14:22:01 +0530

    check script

--- services/apps/data_sink_worker/src/bin/fix-org-identities-website.ts
+++ services/apps/data_sink_worker/src/bin/fix-org-identities-website.ts
@@ -8,34 +8,49 @@ import { websiteNormalizer } from '@crowd/common'
 
 const log = getServiceLogger()
 
-async function getOrgsWithWrongWebsite(db: DbConnection, options: { countOnly?: boolean }) {
+async function getOrgsWithWrongWebsite(
+  db: DbConnection,
+  tenantId: string,
+  options: { countOnly?: boolean },
+) {
   if (options.countOnly) {
-    const result = await db.one(`
+    const result = await db.one(
+      `
       SELECT COUNT(*)
       FROM "organizationIdentities"
-      WHERE value LIKE '%www%' AND (type = 'alternative-domain' OR type = 'primary-domain');
-    `)
+      WHERE value LIKE '%www%' AND (type = 'alternative-domain' OR type = 'primary-domain') AND "tenantId" = $(tenantId);
+    `,
+      {
+        tenantId,
+      },
+    )
 
     return result.count
   }
 
-  const result = await db.any(`
+  const result = await db.any(
+    `
    SELECT *
     FROM "organizationIdentities"
     WHERE value LIKE '%www%' AND (type = 'alternative-domain' OR type = 'primary-domain')
-    LIMIT 100;
-  `)
+    AND "tenantId" = $(tenantId) LIMIT 100;
+  `,
+    {
+      tenantId,
+    },
+  )
 
   return result
 }
 
-async function updateOrgWebsite(
+async function updateOrgIdentity(
   db: DbConnection,
   orgId: string,
   website: string,
   platform: string,
   type: string,
   verified: boolean,
+  tenantId: string,
 ) {
   await db.none(
     `
@@ -44,18 +59,20 @@ async function updateOrgWebsite(
         WHERE "organizationId" = $(orgId)
         AND platform = $(platform)
         AND type = $(type)
-        AND verified = $(verified);
+        AND verified = $(verified)
+        AND "tenantId" = $(tenantId);
     `,
-    { orgId, website, platform, type, verified },
+    { orgId, website, platform, type, verified, tenantId },
   )
 }
 
-async function findOrgByIdentityAndPlatform(
+async function findExistingIdentity(
   db: DbConnection,
   value: string,
   platform: string,
   type: string,
   verified: boolean,
+  tenantId: string,
 ) {
   const result = await db.any(
     `
@@ -64,13 +81,63 @@ async function findOrgByIdentityAndPlatform(
         WHERE value = $(value)
         AND platform = $(platform)
         AND type = $(type)
-        AND verified = $(verified);
+        AND verified = $(verified)
+        AND "tenantId" = $(tenantId);
       `,
-    { value, platform, type, verified },
+    { value, platform, type, verified, tenantId },
   )
 
   return result
 }
+
+async function updateActivitiesOrgId(
+  db: DbConnection,
+  originalId: string,
+  toUpdateId: string,
+  tenantId: string,
+) {
+  await db.none(
+    `
+        UPDATE "activities"
+        SET "organizationId" = $(toUpdateId)
+        WHERE "organizationId" = $(originalId)
+        AND "tenantId" = $(tenantId);
+    `,
+    { originalId, toUpdateId, tenantId },
+  )
+}
+
+async function deleteOrgIdentity(
+  db: DbConnection,
+  orgId: string,
+  platform: string,
+  type: string,
+  value: string,
+  verified: boolean,
+  tenantId: string,
+) {
+  await db.none(
+    `
+        DELETE FROM "organizationIdentities"
+        WHERE "organizationId" = $(orgId)
+        AND platform = $(platform)
+        AND type = $(type)
+        AND value = $(value)
+        AND verified = $(verified)
+        AND "tenantId" = $(tenantId);
+    `,
+    { orgId, platform, type, value, verified, tenantId },
+  )
+}
+
+const processArguments = process.argv.slice(2)
+
+if (processArguments.length !== 1) {
+  log.error('Expected 1 argument: tenantId')
+  process.exit(1)
+}
+
+const tenantId = processArguments[0]
 
 setImmediate(async () => {
   const dbClient = await getDbConnection(DB_CONFIG())
@@ -79,7 +146,7 @@ setImmediate(async () => {
 
   let processedOrgs = 0
 
-  const totalWrongOrgs = await getOrgsWithWrongWebsite(dbClient, { countOnly: true })
+  const totalWrongOrgs = await getOrgsWithWrongWebsite(dbClient, tenantId, { countOnly: true })
 
   if (totalWrongOrgs == 0) {
     log.info('No orgs with incorrect identities found!')
@@ -89,34 +156,54 @@ setImmediate(async () => {
   log.info(`Found ${totalWrongOrgs} orgs!`)
 
   while (processedOrgs < totalWrongOrgs) {
-    const orgs = await getOrgsWithWrongWebsite(dbClient, {})
+    const orgs = await getOrgsWithWrongWebsite(dbClient, tenantId, {})
     for (const org of orgs) {
       const website = websiteNormalizer(org.value)
-      const existingOrg = await findOrgByIdentityAndPlatform(
+      const existingOrg = await findExistingIdentity(
         dbClient,
         website,
         org.platform,
         org.type,
         org.verified,
+        tenantId,
       )
+
+      // check if org with website already exists
+      // if (existingOrg.length > 0) {
+      //   log.info(`Organization with website ${website} already exists!`)
+      //   const toUpdateId = existingOrg[0].organizationId
+
+      //   log.info(`Moving activities from ${org.organizationId} to ${toUpdateId}`)
+
+      //   await updateActivitiesOrgId(dbClient, org.organizationId, toUpdateId, tenantId)
+      //   await deleteOrgIdentity(
+      //     dbClient,
+      //     org.organizationId,
+      //     org.platform,
+      //     org.type,
+      //     org.value,
+      //     org.verified,
+      //     tenantId,
+      //   )
+      // } else {
+      //   await updateOrgIdentity(
+      //     dbClient,
+      //     org.organizationId,
+      //     website,
+      //     org.platform,
+      //     org.type,
+      //     org.verified,
+      //     tenantId,
+      //   )
+      // }
 
       if (existingOrg.length > 0) {
-        log.info(`Organization with website ${website} already exists!`)
-        continue
+        console.log('found existing org identity', JSON.stringify(existingOrg))
       }
-
-      await updateOrgWebsite(
-        dbClient,
-        org.organizationId,
-        website,
-        org.platform,
-        org.type,
-        org.verified,
-      )
 
       processedOrgs++
 
-      log.info(`processed ${processedOrgs}/${totalWrongOrgs} orgs`)
+      log.info(`Processed ${processedOrgs}/${totalWrongOrgs} orgs`)
     }
   }
 
