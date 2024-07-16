107f7b1f1e1d49795c1e874a8e8dc770eafd69bb
Loïc Saint-Roch <loic@nunchi.studio>
2024-07-16 10:11:09 +0200

    Do not execute ilp.flush if no activities

--- services/libs/data-access-layer/src/activities/ilp.ts
+++ services/libs/data-access-layer/src/activities/ilp.ts
@@ -11,153 +11,151 @@ export async function insertActivities(activities: IDbActivityCreateData[]): Pro
   const ids: string[] = []
   const now = Date.now()
 
-  for (const activity of activities) {
-    const id = activity.id || generateUUIDv4()
-    ids.push(id)
+  if (activities.length > 0) {
+    for (const activity of activities) {
+      const id = activity.id || generateUUIDv4()
+      ids.push(id)
 
-    const row = ilp
-      .table('activities')
-      .symbol('tenantId', activity.tenantId)
-      .symbol('segmentId', activity.segmentId)
-      .symbol('platform', activity.platform)
-      .stringColumn('id', id)
-      .timestampColumn('createdAt', now, 'ms')
-      .timestampColumn('updatedAt', now, 'ms')
-      .stringColumn('attributes', objectToBytes(activity.attributes))
-      .booleanColumn('member_isTeamMember', activity.isTeamMemberActivity || false)
-      .booleanColumn('member_isBot', activity.isBotActivity || false)
+      const row = ilp
+        .table('activities')
+        .symbol('tenantId', activity.tenantId)
+        .symbol('segmentId', activity.segmentId)
+        .symbol('platform', activity.platform)
+        .stringColumn('id', id)
+        .timestampColumn('createdAt', now, 'ms')
+        .timestampColumn('updatedAt', now, 'ms')
+        .stringColumn('attributes', objectToBytes(activity.attributes))
+        .booleanColumn('member_isTeamMember', activity.isTeamMemberActivity || false)
+        .booleanColumn('member_isBot', activity.isBotActivity || false)
 
-    if (activity.platform === 'git' || activity.platform === 'github') {
-      if (activity.attributes['isMainBranch']) {
-        row.booleanColumn('gitIsMainBranch', activity.attributes['isMainBranch'] as boolean)
+      if (activity.platform === 'git' || activity.platform === 'github') {
+        if (activity.attributes['isMainBranch']) {
+          row.booleanColumn('gitIsMainBranch', activity.attributes['isMainBranch'] as boolean)
+        }
+
+        if (activity.attributes['isIndirectFork']) {
+          row.booleanColumn('gitIsIndirectFork', activity.attributes['isIndirectFork'] as boolean)
+        }
+
+        if (activity.attributes['additions']) {
+          row.intColumn('gitInsertions', activity.attributes['additions'] as number)
+        }
+
+        if (activity.attributes['deletions']) {
+          row.intColumn('gitDeletions', activity.attributes['deletions'] as number)
+        }
       }
 
-      if (activity.attributes['isIndirectFork']) {
-        row.booleanColumn('gitIsIndirectFork', activity.attributes['isIndirectFork'] as boolean)
+      if (activity.type) {
+        row.stringColumn('type', activity.type)
       }
 
-      if (activity.attributes['additions']) {
-        row.intColumn('gitInsertions', activity.attributes['additions'] as number)
+      if (typeof activity.isContribution === "boolean") {
+        row.booleanColumn('isContribution', activity.isContribution)
       }
 
-      if (activity.attributes['deletions']) {
-        row.intColumn('gitDeletions', activity.attributes['deletions'] as number)
-      }
-    }
-
-    if (activity.type) {
-      row.stringColumn('type', activity.type)
-    }
-
-    if (typeof activity.isContribution === "boolean") {
-      row.booleanColumn('isContribution', activity.isContribution)
-    }
-
-    if (activity.sourceId) {
-      row.stringColumn('sourceId', activity.sourceId)
-    }
-
-    if (activity.username) {
-      row.stringColumn('username', activity.username)
-    }
-
-    if (activity.score) {
-      row.intColumn('score', activity.score)
-    }
-
-    if (activity.sourceParentId) {
-      row.stringColumn('sourceParentId', activity.sourceParentId)
-    }
-
-    if (activity.organizationId) {
-      row.stringColumn('organizationId', activity.organizationId)
-    }
-
-    if (activity.conversationId) {
-      row.stringColumn('conversationId', activity.conversationId)
-    }
-
-    if (activity.channel) {
-      row.stringColumn('channel', activity.channel)
-    }
-
-    if (activity.importHash) {
-      row.stringColumn('importHash', activity.importHash)
-    }
-
-    if (activity.body) {
-      row.stringColumn('body', activity.body)
-    }
-
-    if (activity.title) {
-      row.stringColumn('title', activity.title)
-    }
-
-    if (activity.url) {
-      row.stringColumn('url', activity.url)
-    }
-
-    if (activity.parentId) {
-      row.stringColumn('parentId', activity.parentId)
-    }
-
-    if (activity.memberId) {
-      row.stringColumn('memberId', activity.memberId)
-    }
-
-    if (activity.username) {
-      row.stringColumn('username', activity.username)
-    }
-
-    if (activity.objectMemberId) {
-      row.stringColumn('objectMemberId', activity.objectMemberId)
-    }
-
-    if (activity.objectMemberUsername) {
-      row.stringColumn('objectMemberUsername', activity.objectMemberUsername)
-    }
-
-    if (activity.sentiment) {
-      if (activity.sentiment.label) {
-        row.stringColumn('sentimentLabel', activity.sentiment.label)
+      if (activity.sourceId) {
+        row.stringColumn('sourceId', activity.sourceId)
       }
 
-      if (activity.sentiment.sentiment) {
-        row.intColumn('sentimentScore', activity.sentiment.sentiment)
+      if (activity.username) {
+        row.stringColumn('username', activity.username)
       }
 
-      if (activity.sentiment.negative) {
-        row.floatColumn('sentimentScoreNegative', activity.sentiment.negative)
+      if (activity.score) {
+        row.intColumn('score', activity.score)
       }
 
-      if (activity.sentiment.mixed) {
-        row.floatColumn('sentimentScoreMixed', activity.sentiment.mixed)
+      if (activity.sourceParentId) {
+        row.stringColumn('sourceParentId', activity.sourceParentId)
       }
 
-      if (activity.sentiment.positive) {
-        row.floatColumn('sentimentScorePositive', activity.sentiment.positive)
+      if (activity.organizationId) {
+        row.stringColumn('organizationId', activity.organizationId)
       }
 
-      if (activity.sentiment.neutral) {
-        row.floatColumn('sentimentScoreNeutral', activity.sentiment.neutral)
+      if (activity.conversationId) {
+        row.stringColumn('conversationId', activity.conversationId)
       }
+
+      if (activity.channel) {
+        row.stringColumn('channel', activity.channel)
+      }
+
+      if (activity.importHash) {
+        row.stringColumn('importHash', activity.importHash)
+      }
+
+      if (activity.body) {
+        row.stringColumn('body', activity.body)
+      }
+
+      if (activity.title) {
+        row.stringColumn('title', activity.title)
+      }
+
+      if (activity.url) {
+        row.stringColumn('url', activity.url)
+      }
+
+      if (activity.parentId) {
+        row.stringColumn('parentId', activity.parentId)
+      }
+
+      if (activity.memberId) {
+        row.stringColumn('memberId', activity.memberId)
+      }
+
+      if (activity.username) {
+        row.stringColumn('username', activity.username)
+      }
+
+      if (activity.objectMemberId) {
+        row.stringColumn('objectMemberId', activity.objectMemberId)
+      }
+
+      if (activity.objectMemberUsername) {
+        row.stringColumn('objectMemberUsername', activity.objectMemberUsername)
+      }
+
+      if (activity.sentiment) {
+        if (activity.sentiment.label) {
+          row.stringColumn('sentimentLabel', activity.sentiment.label)
+        }
+
+        if (activity.sentiment.sentiment) {
+          row.intColumn('sentimentScore', activity.sentiment.sentiment)
+        }
+
+        if (activity.sentiment.negative) {
+          row.floatColumn('sentimentScoreNegative', activity.sentiment.negative)
+        }
+
+        if (activity.sentiment.mixed) {
+          row.floatColumn('sentimentScoreMixed', activity.sentiment.mixed)
+        }
+
+        if (activity.sentiment.positive) {
+          row.floatColumn('sentimentScorePositive', activity.sentiment.positive)
+        }
+
+        if (activity.sentiment.neutral) {
+          row.floatColumn('sentimentScoreNeutral', activity.sentiment.neutral)
+        }
+      }
+
+      if (activity.createdById) {
+        row.stringColumn('createdById', activity.createdById)
+      }
+
+      if (activity.updatedById) {
+        row.stringColumn('updatedById', activity.updatedById)
+      }
+
+      await row.at(activity.timestamp ? new Date(activity.timestamp).getTime() : now, 'ms')
     }
 
-    if (activity.createdById) {
-      row.stringColumn('createdById', activity.createdById)
-    }
-
-    if (activity.updatedById) {
-      row.stringColumn('updatedById', activity.updatedById)
-    }
-
-    await row.at(activity.timestamp ? new Date(activity.timestamp).getTime() : now, 'ms')
-  }
-
-  try {
     await ilp.flush()
-  } catch (err) {
-    throw err
   }
 
   return ids
