7f9e914672ffb2ab1a388cddcdb1c1936724a721
Yeganathan S <hilariousengineer@protonmail.com>
2024-07-17 15:56:20 +0530

    fix linter

--- backend/src/database/repositories/memberRepository.ts
+++ backend/src/database/repositories/memberRepository.ts
@@ -59,6 +59,7 @@ import {
 import { findTags } from '@crowd/data-access-layer/src/others'
 import { fetchAbsoluteMemberAggregates } from '@crowd/data-access-layer/src/members/segments'
 import { OrganizationField, queryOrgs } from '@crowd/data-access-layer/src/orgs'
+import { fetchManySegments } from '@crowd/data-access-layer/src/segments'
 import { KUBE_MODE, SERVICE } from '@/conf'
 import { ServiceType } from '../../conf/configTypes'
 import isFeatureEnabled from '../../feature-flags/isFeatureEnabled'
@@ -84,7 +85,6 @@ import {
   mapUsernameToIdentities,
 } from './types/memberTypes'
 import { IFetchMemberMergeSuggestionArgs, SimilarityScoreRange } from '@/types/mergeSuggestionTypes'
-import { fetchManySegments } from '@crowd/data-access-layer/src/segments'
 
 const { Op } = Sequelize
 
