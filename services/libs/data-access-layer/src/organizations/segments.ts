412c27a7283a86f9fb152deaeccf249e26bb74fb
Misha Savelyev <mikhail@crowd.dev>
2024-07-17 08:52:10 +0000

    Optimize how we retrieve many segments for orgs by id

--- services/libs/data-access-layer/src/organizations/segments.ts
+++ services/libs/data-access-layer/src/organizations/segments.ts
@@ -70,6 +70,7 @@ export async function fetchManyOrgSegments(
             ARRAY_AGG("segmentId") AS segments
           FROM "organizationSegmentsAgg"
           WHERE "organizationId" = $(organizationId)
+          GROUP BY "organizationId"
         `,
         {
           organizationId,
