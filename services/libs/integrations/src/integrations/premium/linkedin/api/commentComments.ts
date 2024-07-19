86154397ebed182f9c971453b3be0774649b5b6b
Igor Kotua <36304232+garrrikkotua@users.noreply.github.com>
2024-07-19 19:31:05 +0800

    Change Linkedin APi version to June 2023 version (#2534)

--- services/libs/integrations/src/integrations/premium/linkedin/api/commentComments.ts
+++ services/libs/integrations/src/integrations/premium/linkedin/api/commentComments.ts
@@ -22,7 +22,7 @@ export const getCommentComments = async (
       start,
     },
     headers: {
-      'LinkedIn-Version': '202406',
+      'LinkedIn-Version': '202306',
       Authorization: `Bearer ${accessToken}`,
     },
   }
