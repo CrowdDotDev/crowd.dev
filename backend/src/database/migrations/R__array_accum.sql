431fd00fcf55d8e4697e22fe368fb970b90378d2
Uroš Marolt <uros@crowd.dev>
2024-07-31 06:46:32 +0000

    fix for oracle cloud db

--- backend/src/database/migrations/R__array_accum.sql
+++ backend/src/database/migrations/R__array_accum.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE AGGREGATE array_accum (ANYARRAY) (
-    SFUNC = array_append,
-    STYPE = ANYARRAY,
+CREATE OR REPLACE AGGREGATE array_accum (anycompatiblearray) (
+    SFUNC = array_cat,
+    STYPE = anycompatiblearray,
     INITCOND = '{}'
 );
