58bfa10597646999361003babdda6f7322c10cde
garrrikkotua <kotuaigor@gmail.com>
2024-08-02 14:11:02 +0800

    scaffold

---
+++ services/libs/integrations/src/integrations/gitlab/grid.ts
@@ -0,0 +1,7 @@
+// grid.ts content
+import { IActivityScoringGrid } from '@crowd/types'
+import { GitlabActivityType } from './types'
+
+export const Gitlab_GRID: Record<GitlabActivityType, IActivityScoringGrid> = {
+    // your code goes here
+}
---
+++ services/libs/integrations/src/integrations/gitlab/processStream.ts
@@ -0,0 +1,8 @@
+// processStream.ts content
+import { ProcessStreamHandler } from '../../types'
+
+const handler: ProcessStreamHandler = async (ctx) => {
+  const streamIdentifier = ctx.stream.identifier
+}
+
+export default handler
