5ef501a36b861ce6cf053150517e0b322dab8fa3
Mish Savelyev <1564970+sausage-todd@users.noreply.github.com>
2024-08-09 14:00:23 +0300

    Kafka Oracle SASL auth (#2563)

--- services/libs/queue/src/vendors/kafka/config.ts
+++ services/libs/queue/src/vendors/kafka/config.ts
@@ -3,27 +3,27 @@ import { IKafkaConfig } from './types'
 
 export const INTEGRATION_RUN_WORKER_QUEUE_SETTINGS: IKafkaConfig = {
   name: CrowdQueue.INTEGRATION_RUN_WORKER,
-  partitionCount: 10,
+  partitionCount: 2,
 }
 
 export const INTEGRATION_STREAM_WORKER_QUEUE_SETTINGS: IKafkaConfig = {
   name: CrowdQueue.INTEGRATION_STREAM_WORKER,
-  partitionCount: 10,
+  partitionCount: 2,
 }
 
 export const DATA_SINK_WORKER_QUEUE_SETTINGS: IKafkaConfig = {
   name: CrowdQueue.DATA_SINK_WORKER,
-  partitionCount: 10,
+  partitionCount: 2,
 }
 
 export const SEARCH_SYNC_WORKER_QUEUE_SETTINGS: IKafkaConfig = {
   name: CrowdQueue.SEARCH_SYNC_WORKER,
-  partitionCount: 10,
+  partitionCount: 2,
 }
 
 export const INTEGRATION_SYNC_WORKER_QUEUE_SETTINGS: IKafkaConfig = {
   name: CrowdQueue.INTEGRATION_SYNC_WORKER,
-  partitionCount: 10,
+  partitionCount: 2,
 }
 
 export const configMap = {
