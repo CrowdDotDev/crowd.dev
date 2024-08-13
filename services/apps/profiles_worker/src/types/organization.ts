5b7ecea0e079871a7918861d9923a7873e4f095f
anilb <epipav@gmail.com>
2024-08-13 10:56:12 +0200

    more transparent and resilient org affiliation update workflow

--- services/apps/profiles_worker/src/types/organization.ts
+++ services/apps/profiles_worker/src/types/organization.ts
@@ -4,4 +4,5 @@ export interface IOrganizationAffiliationUpdateInput {
     id: string
   }
   syncToOpensearch?: boolean
+  afterMemberId?: string
 }
--- services/apps/profiles_worker/src/workflows/organization/organizationUpdate.ts
+++ services/apps/profiles_worker/src/workflows/organization/organizationUpdate.ts
@@ -1,10 +1,12 @@
-import { proxyActivities } from '@temporalio/workflow'
+import { continueAsNew, proxyActivities } from '@temporalio/workflow'
 
 import * as activities from '../../activities'
 import { IOrganizationAffiliationUpdateInput } from '../../types/organization'
 
 // Configure timeouts and retry policies to update a member in the database.
-const { updateOrganizationAffiliations, syncOrganization } = proxyActivities<typeof activities>({
+const { updateMemberAffiliations, syncOrganization, findMembersInOrganization } = proxyActivities<
+  typeof activities
+>({
   startToCloseTimeout: '5 minutes',
 })
 
@@ -17,11 +19,28 @@ export async function organizationUpdate(
   input: IOrganizationAffiliationUpdateInput,
 ): Promise<void> {
   try {
-    await updateOrganizationAffiliations(input)
-    if (input.syncToOpensearch) {
+    const ORGANIZATION_MEMBER_AFFILIATIONS_UPDATED_PER_RUN = 500
+
+    const memberIds = await findMembersInOrganization(
+      input.organization.id,
+      ORGANIZATION_MEMBER_AFFILIATIONS_UPDATED_PER_RUN,
+      input.afterMemberId,
+    )
+
+    if (memberIds.length === 0 && input.syncToOpensearch) {
       // sync organization
       await syncOrganization(input.organization.id)
+      return
     }
+
+    for (const memberId of memberIds) {
+      await updateMemberAffiliations({ member: { id: memberId } })
+    }
+
+    await continueAsNew<typeof organizationUpdate>({
+      ...input,
+      afterMemberId: memberIds[memberIds.length - 1],
+    })
   } catch (err) {
     throw new Error(err)
   }
