c17eaf1f8825cec816e0b8faae4b3b7bf1961a3a
anilb <epipav@gmail.com>
2024-08-22 14:53:58 +0300

    Cache worker adapted for LFX only (#2586)

--- backend/src/bin/jobs/refreshMaterializedViewsForCube.ts
+++ backend/src/bin/jobs/refreshMaterializedViewsForCube.ts
@@ -1,11 +1,7 @@
-import { Edition } from '@crowd/types'
-import { EDITION } from '@crowd/common'
 import { Logger, getServiceChildLogger } from '@crowd/logging'
-import { getTemporalClient } from '@crowd/temporal'
 import { CrowdJob } from '../../types/jobTypes'
 import { databaseInit } from '../../database/databaseConnection'
 import { refreshMaterializedView } from './refreshMaterializedViews'
-import { TEMPORAL_CONFIG } from '@/conf'
 
 const job: CrowdJob = {
   name: 'Refresh Materialized View For Cube',
@@ -33,23 +29,6 @@ const job: CrowdJob = {
       }
     } catch (e) {
       log.error({ error: e }, `Error while refreshing materialized views!`)
-    }
-
-    // For LFX we use temporal schedules to trigger this
-    // it'll only be triggered once a day
-    if (EDITION !== Edition.LFX && refreshed.length > 0) {
-      log.info(`Refreshed [${refreshed.join(', ')}] materialized views! Triggering cube refresh!`)
-      const temporal = await getTemporalClient(TEMPORAL_CONFIG)
-
-      await temporal.workflow.start('spawnDashboardCacheRefreshForAllTenants', {
-        taskQueue: 'cache',
-        workflowId: `refreshAllTenants`,
-        retry: {
-          maximumAttempts: 10,
-        },
-        args: [],
-        searchAttributes: {},
-      })
     }
   },
 }
