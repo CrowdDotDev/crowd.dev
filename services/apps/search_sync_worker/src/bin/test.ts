1b765cb13c5f7f47a602ccce3365ba46f2dec035
Uroš Marolt <uros@crowd.dev>
2024-08-22 10:43:26 +0000

    test

--- services/apps/search_sync_worker/src/bin/test.ts
+++ services/apps/search_sync_worker/src/bin/test.ts
@@ -1,18 +1,24 @@
 import { getRedisClient } from '@crowd/redis'
 import { REDIS_CONFIG } from '../conf'
+import { timeout } from '@crowd/common'
 
 setImmediate(async () => {
   const redis = await getRedisClient(REDIS_CONFIG())
+
+  let i = 0
 
   const id = 'test_redis_connection'
   while (true) {
     const key = await redis.get(id)
 
     if (!key) {
-      console.log('setting the key with 10 second expiration')
+      console.log(i, 'setting the key with 10 second expiration')
       await redis.setEx(id, 10, '1')
     } else {
-      console.log('key is there already')
+      console.log(i, 'key is there already')
     }
+
+    await timeout(500)
+    i++
   }
 })
