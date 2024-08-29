bf06be838b553550895c751a82fab819c5e927fa
Loïc Saint-Roch <loic@nunchi.studio>
2024-08-29 11:36:29 +0200

    Use ILP token auth instead of password

--- services/libs/questdb/src/ilp.ts
+++ services/libs/questdb/src/ilp.ts
@@ -14,15 +14,14 @@ export const getClientILP = (): Sender => {
 
   const host = process.env['CROWD_QUESTDB_ILP_HOST']
   const port = process.env['CROWD_QUESTDB_ILP_PORT']
-  const username = process.env['CROWD_QUESTDB_ILP_USERNAME']
-  const password = process.env['CROWD_QUESTDB_ILP_PASSWORD']
+  const token = process.env['CROWD_QUESTDB_ILP_TOKEN']
   const autoflush = 'auto_flush=on;auto_flush_rows=10000;auto_flush_interval=10000;'
 
   log.trace({ host, port }, 'Creating QuestDB client (ILP) instance!')
 
   let conn = `http::addr=${host}:${port};${autoflush}`
   if (getEnv() !== 'local') {
-    conn = `https::addr=${host}:${port};username=${username};password=${password};${autoflush}`
+    conn = `https::addr=${host}:${port};token=${token};${autoflush}`
   }
 
   client = Sender.fromConfig(conn)
