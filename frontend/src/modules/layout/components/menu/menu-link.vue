3193a830a76731fd83146753acdd2a53a6979e34
Gašper Grom <gasper.grom@gmail.com>
2024-09-20 09:34:12 +0200

    Align main menu for lf design (#2615)

---
+++ frontend/src/modules/layout/components/menu/menu-link.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-tooltip
+    :hide-after="50"
+    effect="dark"
+    placement="right"
+    raw-content
+    :content="props.title"
+    :disabled="!props.isCollapsed"
+  >
+    <component
+      :is="props.disabled ? 'span' : 'router-link'"
+      :to="props.to"
+      v-bind="$attrs"
+      class="l-menu__link"
+      active-class="is-active"
+      :class="{ 'is-disabled': props.disabled }"
+    >
+      <lf-icon :name="props.icon" :size="16" />
+      <span class="transition" :class="!props.isCollapsed ? 'opacity-100' : 'opacity-0'">
+        {{ props.title }}
+      </span>
+    </component>
+  </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import LfIcon from '@/ui-kit/icon/Icon.vue';
+
+const props = defineProps<{
+  isCollapsed: boolean;
+  to: any,
+  icon: string,
+  title: string,
+  disabled?: boolean,
+}>();
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfMenuLink',
+};
+</script>
