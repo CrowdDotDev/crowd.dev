352e4be9cc2c69aef7e56fca84a67ee71a07a90d
Gašper Grom <gasper.grom@gmail.com>
2024-09-24 13:25:59 +0200

    Add maintainers data layout

---
+++ frontend/src/modules/contributor/components/details/overview/contributor-details-maintainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-wrap gap-x-2 gap-y-1">
+    <lf-badge v-if="isMaintainer" type="secondary">
+      Maintainer
+    </lf-badge>
+    <lf-badge type="secondary">
+      Contributor
+    </lf-badge>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Contributor } from '@/modules/contributor/types/Contributor';
+import LfBadge from '@/ui-kit/badge/Badge.vue';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  contributor: Contributor,
+}>();
+
+const isMaintainer = computed(() => props.contributor.isMaintainer);
+</script>
+<script lang="ts">
+export default {
+  name: 'LfContributorDetailsMaintainer',
+};
+</script>
---
+++ frontend/src/modules/contributor/components/details/overview/project/contributor-details-projects-sorting.vue
@@ -0,0 +1,59 @@
+<template>
+  <lf-dropdown width="10rem" placement="bottom-end">
+    <template #trigger>
+      <div class="flex py-1">
+        <p class="text-small pr-1">
+          <span class="font-semibold">Sort:</span> {{ label }}
+        </p>
+        <lf-icon name="arrow-down-s-line" :size="16" />
+      </div>
+    </template>
+    <lf-dropdown-item
+      v-for="option of options"
+      :key="option.value"
+      @click="sortBy = option.value"
+    >
+      {{ option.label }}
+    </lf-dropdown-item>
+  </lf-dropdown>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import LfDropdown from '@/ui-kit/dropdown/Dropdown.vue';
+import LfDropdownItem from '@/ui-kit/dropdown/DropdownItem.vue';
+import LfIcon from '@/ui-kit/icon/Icon.vue';
+
+const props = defineProps<{
+  sorting: string,
+}>();
+
+const emit = defineEmits<{(e: 'update:sorting', value: string)}>();
+
+const sortBy = computed({
+  get: () => props.sorting,
+  set: (value: string) => emit('update:sorting', value),
+});
+
+const label = computed(() => {
+  const option = options.find((o) => o.value === sortBy.value);
+  return option ? option.label : 'All';
+});
+
+interface SortOption {
+  value: string;
+  label: string;
+}
+
+const options: SortOption[] = [
+  { value: 'name_ASC', label: 'Alphabetically' },
+  { value: 'activityCount_DESC', label: 'Most activities' },
+  { value: 'createdAt_DESC', label: 'Most recent' },
+];
+</script>
+
+<script lang="ts">
+export default {
+  name: 'LfContributorDetailsProjectsSorting',
+};
+</script>
