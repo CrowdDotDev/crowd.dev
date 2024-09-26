b005851f4c19877e366cc2928019e8727e66b6a5
anilb <epipav@gmail.com>
2024-09-26 11:19:44 +0200

    get profileUrl from request body

--- backend/src/services/dataIssueService.ts
+++ backend/src/services/dataIssueService.ts
@@ -10,7 +10,7 @@ import { DataIssueEntity } from '@crowd/types'
 import { InstallationAccessTokenData } from '@octokit/auth-app/dist-types/types'
 import { IServiceOptions } from './IServiceOptions'
 import SequelizeRepository from '@/database/repositories/sequelizeRepository'
-import { API_CONFIG, GITHUB_ISSUE_REPORTER_CONFIG } from '@/conf'
+import { GITHUB_ISSUE_REPORTER_CONFIG } from '@/conf'
 
 export interface IDataIssueCreatePayload {
   entity: DataIssueEntity
@@ -41,7 +41,6 @@ export default class DataIssueService extends LoggerBase {
     const user = SequelizeRepository.getCurrentUser(this.options)
 
     let entityName: string
-    let entityUrl: string
     let reportedBy: string
 
     if (data.entity === DataIssueEntity.ORGANIZATION) {
@@ -50,11 +49,9 @@ export default class DataIssueService extends LoggerBase {
         OrganizationField.DISPLAY_NAME,
       ])
       entityName = organization.displayName
-      entityUrl = `${API_CONFIG.frontendUrl}/organizations/${organization.id}`
     } else if (data.entity === DataIssueEntity.PERSON) {
       const member = await findMemberById(qx, entityId, [MemberField.ID, MemberField.DISPLAY_NAME])
       entityName = member.displayName
-      entityUrl = `${API_CONFIG.frontendUrl}/members/${member.id}`
     } else {
       throw new Error(`Unsupported data issue entity ${data.entity}!1`)
     }
@@ -81,7 +78,7 @@ export default class DataIssueService extends LoggerBase {
           title: `[Data Issue] ${entityName} (${data.entity[0].toUpperCase()}${data.entity
             .slice(1)
             .toLowerCase()})`,
-          body: `**Entity**\n${entityName}\n\n**Profile**\n[${entityUrl}](${entityUrl})\n\n**Data Issue**\n${data.dataIssue}\n\n**Description**\n${data.description}\n\n**Reported by**\n${reportedBy}`,
+          body: `**Entity**\n${entityName}\n\n**Profile**\n[${data.profileUrl}](${data.profileUrl})\n\n**Data Issue**\n${data.dataIssue}\n\n**Description**\n${data.description}\n\n**Reported by**\n${reportedBy}`,
           labels: ['Data issue'],
         },
       )
@@ -89,7 +86,6 @@ export default class DataIssueService extends LoggerBase {
         ...data,
         githubIssueUrl: result.data.html_url,
         createdById: user.id,
-        profileUrl: entityUrl,
       })
 
       return res
