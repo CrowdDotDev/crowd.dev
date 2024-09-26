82171de558874b0ec62db24ce746b71feb1c077f
Gašper Grom <gasper.grom@gmail.com>
2024-09-26 12:46:52 +0200

    Report modal display

---
+++ frontend/src/shared/modules/report-issue/config/type/type-domain.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="text-medium">
+    {{ props.attribute.value }}
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ContributorIdentity } from '@/modules/contributor/types/Contributor';
+import { OrganizationIdentity } from '@/modules/organization/types/Organization';
+
+const props = defineProps<{
+  attribute: ContributorIdentity | OrganizationIdentity
+}>();
+</script>
