cd69d253fdf1a38bc1821d45509332abf771c5d1
Loïc Saint-Roch <loic@nunchi.studio>
2024-09-30 17:51:27 +0200

    Try wihout connection error handler

--- services/libs/questdb/src/sql.ts
+++ services/libs/questdb/src/sql.ts
@@ -15,21 +15,6 @@ export const getClientSQL = async (): Promise<pgpromise.IDatabase<unknown>> => {
   log.info('Creating QuestDB client (SQL) instance!')
 
   client = pgpromise({
-    // tslint:disable-next-line:max-line-length
-    // see https://stackoverflow.com/questions/36120435/verify-database-connection-with-pg-promise-when-starting-an-app
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async error(err: any, e: pgpromise.IEventContext): Promise<void> {
-      if (e.cn) {
-        log.fatal(err, { cn: e.cn }, 'QuestDB connection error. Stopping process')
-        // logs don't have flush:
-        await new Promise((resolve) => setTimeout(resolve, 100))
-        process.nextTick(() => process.exit())
-      }
-
-      if (e.query) {
-        log.error(err, { query: e.query, params: e.params }, 'Error executing a QuestDB query!')
-      }
-    },
     query(e) {
       log.debug({ query: e.query, params: e.params }, 'Executing QuestDB query')
     },
