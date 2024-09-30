6fec83515bd9b940d424fed2c29c9e354672a36e
Loïc Saint-Roch <loic@nunchi.studio>
2024-09-30 18:15:54 +0200

    Reduce QuestDB client idle timeout

--- services/libs/questdb/src/sql.ts
+++ services/libs/questdb/src/sql.ts
@@ -15,6 +15,21 @@ export const getClientSQL = async (): Promise<pgpromise.IDatabase<unknown>> => {
   log.info('Creating QuestDB client (SQL) instance!')
 
   client = pgpromise({
+    // tslint:disable-next-line:max-line-length
+    // see https://stackoverflow.com/questions/36120435/verify-database-connection-with-pg-promise-when-starting-an-app
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async error(err: any, e: pgpromise.IEventContext): Promise<void> {
+      if (e.cn) {
+        log.fatal(err, { cn: e.cn }, 'QuestDB connection error. Stopping process')
+        // logs don't have flush:
+        await new Promise((resolve) => setTimeout(resolve, 100))
+        process.nextTick(() => process.exit())
+      }
+
+      if (e.query) {
+        log.error(err, { query: e.query, params: e.params }, 'Error executing a QuestDB query!')
+      }
+    },
     query(e) {
       log.debug({ query: e.query, params: e.params }, 'Executing QuestDB query')
     },
@@ -26,7 +41,7 @@ export const getClientSQL = async (): Promise<pgpromise.IDatabase<unknown>> => {
     database: process.env['CROWD_QUESTDB_SQL_DATABASE'],
     application_name: process.env.SERVICE || 'unknown-app',
     ssl: getEnv() !== 'local' ? true : false,
-    idleTimeoutMillis: 280000,
+    idleTimeoutMillis: 120000,
   })
 
   return client
