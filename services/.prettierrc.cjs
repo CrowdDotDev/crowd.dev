1e9a2bb27619e14e94177e2de826ba4a76c2d9f6
Misha Savelyev <mikhail@crowd.dev>
2024-10-21 13:06:41 +0000

    Configure prettier import sorting plugin

--- services/.prettierrc.cjs
+++ services/.prettierrc.cjs
@@ -4,4 +4,15 @@ module.exports = {
   printWidth: 100,
   trailingComma: 'all',
   semi: false,
+  importOrder: [
+    '^(?!(\\.|@crowd/|@/|\\.\\./))(.*)$', // 3rd-party imports
+    '^@crowd/(.*)$', // crowd packages
+    '^@/(.*)$', // local package absolute imports that start with @/
+    '^\\.\\./', // relative imports that start with ../
+    '^\\./', // same directory imports that start with ./
+    '^\\.$', // current directory
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
 }
