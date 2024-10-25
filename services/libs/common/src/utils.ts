4c95990d9f85b433b08178eb930b2ea55126c515
anilb <epipav@gmail.com>
2024-10-25 20:53:57 +0200

    improve redactNullByte

--- services/libs/common/src/utils.ts
+++ services/libs/common/src/utils.ts
@@ -70,7 +70,7 @@ export const escapeNullByte = (str: string | null | undefined): string =>
   str ? str.replace(/\0/g, 'u0000') : str
 
 export const redactNullByte = (str: string | null | undefined): string =>
-  str ? str.replace(/\0/g, '[NULL]') : ''
+  str ? str.replace(/\\u0000|\0/g, '[NULL]') : ''
 
 export const dateEqualityChecker = (a, b) => {
   if (a instanceof Date) {
