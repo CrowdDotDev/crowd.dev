e874c92ebd92a26be9a26be0facb89a1afe8c351
Gašper Grom <gasper.grom@gmail.com>
2024-10-29 18:08:32 +0100

    Fix queries

--- services/libs/data-access-layer/src/data-quality/members.ts
+++ services/libs/data-access-layer/src/data-quality/members.ts
@@ -312,17 +312,16 @@ export async function fetchMembersWithConflictingWorkExperiences(
             msa."activityCount",
             COUNT(DISTINCT mo1.id) AS "organizationsCount"
         FROM "members" m
-                 JOIN "memberOrganizations" mo1 ON m.id = mo1."memberId" AND mo1."deletedAt" IS NULL
-                 INNER JOIN "memberSegmentsAgg" msa ON m.id = msa."memberId" AND msa."segmentId" = '${segmentId}'
-                 LEFT JOIN "memberOrganizations" mo2 ON mo1."memberId" = mo2."memberId"
-            AND mo1."deletedAt" IS NULL
-            AND mo1.id != mo2.id
-            AND (
-                (mo1."dateStart" < COALESCE(mo2."dateEnd", 'infinity'::timestamp)
-                    AND COALESCE(mo1."dateEnd", 'infinity'::timestamp) > mo2."dateStart") OR
-                (mo2."dateStart" < COALESCE(mo1."dateEnd", 'infinity'::timestamp)
-                    AND COALESCE(mo2."dateEnd", 'infinity'::timestamp) > mo1."dateStart")
-            )
+                JOIN "memberOrganizations" mo1 ON m.id = mo1."memberId" AND mo1."deletedAt" IS NULL
+                INNER JOIN "memberSegmentsAgg" msa ON m.id = msa."memberId" AND msa."segmentId" = '${segmentId}'
+                LEFT JOIN "memberOrganizations" mo2 ON mo1."memberId" = mo2."memberId" AND mo1."deletedAt" IS NULL
+                AND mo1.id != mo2.id
+                AND (
+                   (mo1."dateStart" < COALESCE(mo2."dateEnd", 'infinity'::timestamp)
+                       AND COALESCE(mo1."dateEnd", 'infinity'::timestamp) > mo2."dateStart") OR
+                   (mo2."dateStart" < COALESCE(mo1."dateEnd", 'infinity'::timestamp)
+                       AND COALESCE(mo2."dateEnd", 'infinity'::timestamp) > mo1."dateStart")
+               )
         WHERE m."tenantId" = '${tenantId}'
         GROUP BY m.id, msa."activityCount"
         ORDER BY msa."activityCount" DESC
