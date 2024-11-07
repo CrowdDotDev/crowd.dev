66ab4bf680d9d3ddf927bf615c822788587876a0
anilb <epipav@gmail.com>
2024-11-07 15:59:08 +0100

    using chunks and big enrichable members per run

--- services/apps/premium/members_enrichment_worker/src/workflows/getMembersToEnrich.ts
+++ services/apps/premium/members_enrichment_worker/src/workflows/getMembersToEnrich.ts
@@ -6,7 +6,8 @@ import {
   proxyActivities,
 } from '@temporalio/workflow'
 
-import { MemberEnrichmentSource } from '@crowd/types'
+import { chunkArray } from '@crowd/common'
+import { IEnrichableMember, MemberEnrichmentSource } from '@crowd/types'
 
 import * as activities from '../activities/getMembers'
 import { IGetMembersForEnrichmentArgs } from '../types'
@@ -18,7 +19,8 @@ const { getEnrichableMembers } = proxyActivities<typeof activities>({
 })
 
 export async function getMembersToEnrich(args: IGetMembersForEnrichmentArgs): Promise<void> {
-  const MEMBER_ENRICHMENT_PER_RUN = 50
+  const QUERY_FOR_ENRICHABLE_MEMBERS_PER_RUN = 1000
+  const PARALLEL_ENRICHMENT_WORKFLOWS = 10
   const afterCursor = args?.afterCursor || null
   const sources = [
     MemberEnrichmentSource.PROGAI,
@@ -28,37 +30,45 @@ export async function getMembersToEnrich(args: IGetMembersForEnrichmentArgs): Pr
     MemberEnrichmentSource.CRUSTDATA,
   ]
 
-  const members = await getEnrichableMembers(MEMBER_ENRICHMENT_PER_RUN, sources, afterCursor)
+  const members = await getEnrichableMembers(
+    QUERY_FOR_ENRICHABLE_MEMBERS_PER_RUN,
+    sources,
+    afterCursor,
+  )
 
   if (members.length === 0) {
     return
   }
 
-  await Promise.all(
-    members.map((member) => {
-      return executeChild(enrichMember, {
-        workflowId: 'member-enrichment/' + member.tenantId + '/' + member.id,
-        cancellationType: ChildWorkflowCancellationType.WAIT_CANCELLATION_COMPLETED,
-        parentClosePolicy: ParentClosePolicy.PARENT_CLOSE_POLICY_REQUEST_CANCEL,
-        workflowExecutionTimeout: '15 minutes',
-        retry: {
-          backoffCoefficient: 2,
-          maximumAttempts: 10,
-          initialInterval: 2 * 1000,
-          maximumInterval: 30 * 1000,
-        },
-        args: [member, sources],
-        searchAttributes: {
-          TenantId: [member.tenantId],
-        },
-      })
-    }),
-  )
+  const chunks = chunkArray<IEnrichableMember>(members, PARALLEL_ENRICHMENT_WORKFLOWS)
+
+  for (const chunk of chunks) {
+    await Promise.all(
+      chunk.map((member) => {
+        return executeChild(enrichMember, {
+          workflowId: 'member-enrichment/' + member.tenantId + '/' + member.id,
+          cancellationType: ChildWorkflowCancellationType.WAIT_CANCELLATION_COMPLETED,
+          parentClosePolicy: ParentClosePolicy.PARENT_CLOSE_POLICY_REQUEST_CANCEL,
+          workflowExecutionTimeout: '15 minutes',
+          retry: {
+            backoffCoefficient: 2,
+            maximumAttempts: 10,
+            initialInterval: 2 * 1000,
+            maximumInterval: 30 * 1000,
+          },
+          args: [member, sources],
+          searchAttributes: {
+            TenantId: [member.tenantId],
+          },
+        })
+      }),
+    )
+  }
 
   await continueAsNew<typeof getMembersToEnrich>({
     afterCursor: {
-      memberId: members[members.length - 1].id,
-      activityCount: members[members.length - 1].activityCount,
+      memberId: chunks[chunks.length - 1][chunks[chunks.length - 1].length - 1].id,
+      activityCount: chunks[chunks.length - 1][chunks[chunks.length - 1].length - 1].activityCount,
     },
   })
 }
