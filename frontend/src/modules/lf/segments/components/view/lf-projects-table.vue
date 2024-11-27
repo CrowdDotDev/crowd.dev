282d6c69fa12ba05391bf81b0230b96f50ef96f3
Efren Lim <elim@linuxfoundation.org>
2024-11-27 11:51:51 +0800

    refactored integration column into separate component
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/src/modules/lf/segments/components/view/lf-projects-table.vue
+++ frontend/src/modules/lf/segments/components/view/lf-projects-table.vue
@@ -15,7 +15,6 @@
     >
       <template #header>
         <div>
-          <span class="pt-1.5 pb-3 block">Project</span>
           <div
             class="text-base font-semibold text-gray-900 normal-case flex items-center h-8 mb-4"
           >
@@ -32,97 +31,18 @@
     <el-table-column width="250" class-name="table-columns !py-0 !bg-white">
       <template #header>
         <div>
-          <span class="pt-1.5 pb-3 block">Connected Integrations</span>
           <div class="flex items-center h-8 mb-4">
             <span class="text-gray-400 text-sm normal-case font-normal">-</span>
           </div>
         </div>
       </template>
       <template #default="{ row }">
-        <div
-          v-if="row.integrations?.length"
-          class="flex gap-1 items-center py-2 overflow-x-auto"
-          style="max-width: 100%; scrollbar-width: thin"
-        >
-          <div v-for="integration in row.integrations" :key="integration.id">
-            <el-popover
-              :disabled="
-                integration.status !== 'in-progress'
-                  && integration.type !== 'mapped'
-                  && !getProgress(row.id, integration.platform)
-                  && !progressError
-              "
-              :width="290"
-              placement="top"
-            >
-              <template #reference>
-                <div
-                  class="relative w-6 h-6 flex-shrink-0 flex items-center justify-center"
-                >
-                  <app-platform-svg
-                    :platform="integration.platform"
-                    :color="integration.platform === 'github' && integration?.type === 'mapped' ? 'gray' : 'black'"
-                  />
-
-                  <lf-icon
-                    v-if="integration.status === 'no-data'"
-                    name="triangle-exclamation"
-                    type="solid"
-                    :size="12"
-                    class="absolute right-0 top-0 leading-3 text-yellow-500"
-                  />
-
-                  <lf-icon
-                    v-else-if="integration.status === 'error'"
-                    name="circle-exclamation"
-                    type="solid"
-                    :size="12"
-                    class="absolute right-0 top-0 leading-3 text-red-600"
-                  />
-                  <div
-                    v-else-if="integration.status === 'in-progress'"
-                    class="w-4 h-4 bg-white rounded-full -ml-2 flex items-center justify-center -mt-5"
-                  >
-                    <lf-spinner size="0.75rem" class="!border-black" />
-                  </div>
-                </div>
-              </template>
-              <div class="px-1">
-                <template v-if="integration.type === 'mapped'">
-                  <p class="text-xs text-black leading-5">
-                    Syncing
-                    {{
-                      CrowdIntegrations.getConfig(integration.platform)?.name
-                    }}
-                    data since this sub-project is mapped with
-                    <b>{{ integration.mappedWith }}</b> sub-project
-                  </p>
-                </template>
-                <template v-else>
-                  <app-integration-progress
-                    v-if="!progressError"
-                    :progress="getProgress(row.id, integration.platform)"
-                    :show-bar="true"
-                    :show-parts="true"
-                  >
-                    <h6 class="text-xs text-black leading-5 pb-3">
-                      Connecting
-                      {{
-                        CrowdIntegrations.getConfig(integration.platform)?.name
-                      }}
-                      integration
-                    </h6>
-                  </app-integration-progress>
-                  <div v-if="progressError" class="text-xs text-gray-500 flex items-center">
-                    <lf-icon name="triangle-exclamation" type="light" :size="13" class="text-yellow-600" />
-                    Error loading progress
-                  </div>
-                </template>
-              </div>
-            </el-popover>
-          </div>
-        </div>
-        <span v-else class="text-gray-400 text-sm">No integrations</span>
+        <app-lf-project-integration-column
+          :segment-id="row.id"
+          :integrations="row.integrations"
+          :progress="props.progress"
+          :progress-error="progressError"
+        />
       </template>
     </el-table-column>
 
@@ -130,7 +50,6 @@
     <el-table-column width="150" class-name="table-columns !bg-white">
       <template #header>
         <div>
-          <span class="pt-1.5 pb-3 block">Status</span>
           <div class="flex items-center gap-3 h-8 mb-4">
             <span
               class="w-1.5 h-1.5 rounded-full"
@@ -244,13 +163,9 @@ import statusOptions from '@/modules/lf/config/status';
 import AppLfProjectsDropdown from '@/modules/lf/segments/components/lf-projects-dropdown.vue';
 import AppLfSubProjectsDropdown from '@/modules/lf/segments/components/lf-sub-projects-dropdown.vue';
 import { useRoute } from 'vue-router';
-import AppPlatformSvg from '@/shared/modules/platform/components/platform-svg.vue';
-import LfSpinner from '@/ui-kit/spinner/Spinner.vue';
-import AppIntegrationProgress from '@/modules/integration/components/integration-progress.vue';
 import usePermissions from '@/shared/modules/permissions/helpers/usePermissions';
 import { LfPermission } from '@/shared/modules/permissions/types/Permissions';
-import { CrowdIntegrations } from '@/integrations/integrations-config';
-import LfIcon from '@/ui-kit/icon/Icon.vue';
+import AppLfProjectIntegrationColumn from '../fragments/lf-project-integration-column.vue';
 
 const route = useRoute();
 
@@ -279,9 +194,6 @@ const { hasPermission, hasAccessToSegmentId } = usePermissions();
 
 const statusDisplay = (status) => statusOptions.find((s) => s.value === status);
 
-const getProgress = (segmentId, platform) => (props.progress || []).find(
-  (p) => p.segmentId === segmentId && p.platform === platform,
-);
 </script>
 
 <script>
