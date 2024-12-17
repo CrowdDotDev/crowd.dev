b66d2c17d93016d3b00a85e9d1d3a7d5b6a2e270
Sameh Mohamed <sameh_mohamed16@Hotmail.com>
2024-12-17 14:21:16 +0200

    Handle Roles not being displayed properly in users list (#2719)
    Signed-off-by: Sameh16 <sameh_mohamed16@hotmail.com>

--- frontend/src/modules/admin/modules/users/models/User.model.ts
+++ frontend/src/modules/admin/modules/users/models/User.model.ts
@@ -1,6 +1,12 @@
+export enum UserRole {
+  admin = 'admin',
+  projectAdmin = 'projectAdmin',
+  readonly = 'readonly',
+}
 export interface UserModel {
   id: string;
   email: string;
   fullName: string;
-  roles: string[];
+  roles: UserRole[];
+  adminSegments: string[];
 }
