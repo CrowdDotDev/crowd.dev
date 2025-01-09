7ba0382ce45a1b1b938ca056a25d8a90e1e6e500
anil <epipav@gmail.com>
2025-01-09 12:02:07 +0300

    fix conflicsts

--- services/apps/cache_worker/src/activities/getSegmentInfo.ts
+++ services/apps/cache_worker/src/activities/getSegmentInfo.ts
@@ -1,7 +1,5 @@
 import SegmentRepository from '@crowd/data-access-layer/src/old/apps/cache_worker/segment.repo'
 import { ISegment } from '@crowd/data-access-layer/src/old/apps/cache_worker/types'
-import { isFeatureEnabled } from '@crowd/feature-flags'
-import { FeatureFlag } from '@crowd/types'
 
 import { svc } from '../main'
 
@@ -18,8 +16,4 @@ export async function getProjectLeafSegments(parentSlug: string): Promise<ISegme
 export async function getProjectGroupLeafSegments(grandparentSlug: string): Promise<ISegment[]> {
   const segmentRepository = new SegmentRepository(svc.postgres.writer.connection(), svc.log)
   return segmentRepository.getProjectGroupLeafSegments(grandparentSlug)
-}
-
-export async function isSegmentsEnabled(): Promise<boolean> {
-  return isFeatureEnabled(FeatureFlag.SEGMENTS, null)
 }
