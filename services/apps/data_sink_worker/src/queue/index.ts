a9a3ee062b4503171ec123d784b6b44162e6f702
Uroš Marolt <uros@marolt.me>
2025-01-11 09:24:54 +0100

    concurrent processing

--- services/apps/data_sink_worker/src/queue/index.ts
+++ services/apps/data_sink_worker/src/queue/index.ts
@@ -30,7 +30,7 @@ export class WorkerQueueReceiver extends PrioritizedQueueReciever {
       level,
       client,
       client.getQueueChannelConfig(CrowdQueue.DATA_SINK_WORKER),
-      20,
+      5,
       parentLog,
       undefined,
       undefined,
