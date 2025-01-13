d014be4f88d86aa60e693f1949f7aae580e5a0ea
Efren Lim <elim@linuxfoundation.org>
2025-01-13 10:18:37 +0800

    remove unused components and functions from shared
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/src/shared/shared-module.js
+++ frontend/src/shared/shared-module.js
@@ -1,23 +1,21 @@
-import StringArrayInput from '@/shared/form/string-array-input.vue';
-import AutocompleteOneInput from '@/shared/form/autocomplete-one-input.vue';
-import AutocompleteManyInput from '@/shared/form/autocomplete-many-input.vue';
-import KeywordsInput from '@/shared/form/keywords-input.vue';
-import Avatar from '@/shared/avatar/avatar.vue';
-import SquaredAvatar from '@/shared/avatar/squared-avatar.vue';
-import Alert from '@/shared/alert/alert.vue';
-import Teleport from '@/shared/teleport/teleport.vue';
-import Pagination from '@/shared/pagination/pagination.vue';
-import InfinitePagination from '@/shared/pagination/infinite-pagination.vue';
-import PaginationSorter from '@/shared/pagination/pagination-sorter.vue';
-import FilterTypeSelectMulti from '@/shared/filter/components/type/filter-type-select-multi.vue';
-import InlineSelectInput from '@/shared/form/inline-select-input.vue';
-import Dialog from '@/shared/dialog/dialog.vue';
-import EmptyStateCta from '@/shared/empty-state/empty-state-cta.vue';
-import EmptyState from '@/shared/empty-state/empty-state.vue';
-import Drawer from '@/shared/drawer/drawer.vue';
-import AppLoader from '@/shared/loading/loader.vue';
-import AppPageWrapper from '@/shared/layout/page-wrapper.vue';
-import Image from '@/shared/image/image.vue';
+import StringArrayInput from '@/shared/form/string-array-input.vue'
+import AutocompleteOneInput from '@/shared/form/autocomplete-one-input.vue'
+import AutocompleteManyInput from '@/shared/form/autocomplete-many-input.vue'
+import KeywordsInput from '@/shared/form/keywords-input.vue'
+import Avatar from '@/shared/avatar/avatar.vue'
+import SquaredAvatar from '@/shared/avatar/squared-avatar.vue'
+import Teleport from '@/shared/teleport/teleport.vue'
+import Pagination from '@/shared/pagination/pagination.vue'
+import InfinitePagination from '@/shared/pagination/infinite-pagination.vue'
+import PaginationSorter from '@/shared/pagination/pagination-sorter.vue'
+import InlineSelectInput from '@/shared/form/inline-select-input.vue'
+import Dialog from '@/shared/dialog/dialog.vue'
+import EmptyStateCta from '@/shared/empty-state/empty-state-cta.vue'
+import EmptyState from '@/shared/empty-state/empty-state.vue'
+import Drawer from '@/shared/drawer/drawer.vue'
+import AppLoader from '@/shared/loading/loader.vue'
+import AppPageWrapper from '@/shared/layout/page-wrapper.vue'
+import Image from '@/shared/image/image.vue'
 
 /**
  * All shared components are globally registered, so there's no need to import them from other components
@@ -27,10 +25,8 @@ export default {
     'app-autocomplete-one-input': AutocompleteOneInput,
     'app-autocomplete-many-input': AutocompleteManyInput,
     'app-string-array-input': StringArrayInput,
-    'app-filter-type-select-multi': FilterTypeSelectMulti,
     'app-avatar': Avatar,
     'app-squared-avatar': SquaredAvatar,
-    'app-alert': Alert,
     'app-keywords-input': KeywordsInput,
     'app-teleport': Teleport,
     'app-pagination': Pagination,
@@ -45,4 +41,4 @@ export default {
     'app-page-wrapper': AppPageWrapper,
     'app-image': Image,
   },
-};
+}
