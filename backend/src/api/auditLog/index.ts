ea84f050c7262d3d89bca51d7adafb790ae48db7
anil <epipav@gmail.com>
2025-01-14 10:52:21 +0100

    remove tenants from auditlogs endpoints

--- backend/src/api/auditLog/index.ts
+++ backend/src/api/auditLog/index.ts
@@ -1,7 +1,7 @@
 import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
-  app.get(`/tenant/:tenantId/audit-log`, safeWrap(require('./auditLogList').default))
+  app.get(`/tenant/audit-log`, safeWrap(require('./auditLogList').default))
 
-  app.post(`/tenant/:tenantId/audit-logs/query`, safeWrap(require('./auditLogsQuery').default))
+  app.post(`/tenant/audit-logs/query`, safeWrap(require('./auditLogsQuery').default))
 }
