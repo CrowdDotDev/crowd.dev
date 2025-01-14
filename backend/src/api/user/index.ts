8846cc1807293492138336f773f33b13d2fd9e21
anilb <epipav@gmail.com>
2025-01-14 15:56:26 +0100

    remove tenants from user endpoints

--- backend/src/api/user/index.ts
+++ backend/src/api/user/index.ts
@@ -1,7 +1,7 @@
 import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
-  app.get(`/tenant/:tenantId/user`, safeWrap(require('./userList').default))
-  app.get(`/tenant/:tenantId/user/autocomplete`, safeWrap(require('./userAutocomplete').default))
-  app.get(`/tenant/:tenantId/user/:id`, safeWrap(require('./userFind').default))
+  app.get(`/user`, safeWrap(require('./userList').default))
+  app.get(`/user/autocomplete`, safeWrap(require('./userAutocomplete').default))
+  app.get(`/user/:id`, safeWrap(require('./userFind').default))
 }
