991f33f2c54f588bd8f142e700d90c1263ef4cda
anil <epipav@gmail.com>
2025-01-14 11:05:38 +0100

    simplify auditLog endpoints even more

--- backend/src/api/auditLog/index.ts
+++ backend/src/api/auditLog/index.ts
@@ -1,7 +1,7 @@
 import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
-  app.get(`/tenant/audit-log`, safeWrap(require('./auditLogList').default))
+  app.get(`/audit-log`, safeWrap(require('./auditLogList').default))
 
-  app.post(`/tenant/audit-logs/query`, safeWrap(require('./auditLogsQuery').default))
+  app.post(`/audit-logs/query`, safeWrap(require('./auditLogsQuery').default))
 }
