0575f98c87844382c9f9e33b4413698ac8b0acfd
anilb <epipav@gmail.com>
2025-01-14 15:47:41 +0100

    remove tenants from segment endpoints

--- backend/src/api/segment/index.ts
+++ backend/src/api/segment/index.ts
@@ -2,40 +2,37 @@ import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
   app.post(
-    `/tenant/:tenantId/segment/projectGroup`,
+    `/segment/projectGroup`,
     safeWrap(require('./segmentCreateProjectGroup').default),
   )
-  app.post(`/tenant/:tenantId/segment/project`, safeWrap(require('./segmentCreateProject').default))
+  app.post(`/segment/project`, safeWrap(require('./segmentCreateProject').default))
   app.post(
-    `/tenant/:tenantId/segment/subproject`,
+    `/segment/subproject`,
     safeWrap(require('./segmentCreateSubproject').default),
   )
 
   // query all project groups
   app.post(
-    `/tenant/:tenantId/segment/projectGroup/query`,
+    `/segment/projectGroup/query`,
     safeWrap(require('./segmentProjectGroupQuery').default),
   )
 
   // query all projects
   app.post(
-    `/tenant/:tenantId/segment/project/query`,
+    `/segment/project/query`,
     safeWrap(require('./segmentProjectQuery').default),
   )
 
   // query all subprojects
   app.post(
-    `/tenant/:tenantId/segment/subproject/query`,
+    `/segment/subproject/query`,
     safeWrap(require('./segmentSubprojectQuery').default),
   )
 
   // get segment by id
-  app.get(`/tenant/:tenantId/segment/:segmentId`, safeWrap(require('./segmentFind').default))
-  app.put(`/tenant/:tenantId/segment/:segmentId`, safeWrap(require('./segmentUpdate').default))
+  app.get(`/segment/:segmentId`, safeWrap(require('./segmentFind').default))
+  app.put(`/segment/:segmentId`, safeWrap(require('./segmentUpdate').default))
   // Multiple ids
-  app.post(`/tenant/:tenantId/segment/id`, safeWrap(require('./segmentByIds').default))
+  app.post(`/segment/id`, safeWrap(require('./segmentByIds').default))
 
-  // app.get(`/tenant/:tenantId/segment/projectGroup/:id`, safeWrap(require('./segmentFind').default))
-  // app.get(`/tenant/:tenantId/segment/project/:id`, safeWrap(require('./segmentFind').default))
-  // app.get(`/tenant/:tenantId/segment/subproject/:id`, safeWrap(require('./segmentFind').default))
 }
