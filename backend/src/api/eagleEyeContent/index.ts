ccb3853f1f5b6c89d85f4a2b93763233b4bacb16
anil <epipav@gmail.com>
2025-01-14 11:38:47 +0100

    remove tenants from eagle eye endpoints

--- backend/src/api/eagleEyeContent/index.ts
+++ backend/src/api/eagleEyeContent/index.ts
@@ -2,47 +2,47 @@ import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
   app.post(
-    `/tenant/:tenantId/eagleEyeContent/query`,
+    `/eagleEyeContent/query`,
     safeWrap(require('./eagleEyeContentQuery').default),
   )
 
   app.post(
-    `/tenant/:tenantId/eagleEyeContent`,
+    `/eagleEyeContent`,
     safeWrap(require('./eagleEyeContentUpsert').default),
   )
 
   app.post(
-    `/tenant/:tenantId/eagleEyeContent/track`,
+    `/eagleEyeContent/track`,
     safeWrap(require('./eagleEyeContentTrack').default),
   )
 
   app.get(
-    `/tenant/:tenantId/eagleEyeContent/reply`,
+    `/eagleEyeContent/reply`,
     safeWrap(require('./eagleEyeContentReply').default),
   )
 
   app.get(
-    `/tenant/:tenantId/eagleEyeContent/search`,
+    `/eagleEyeContent/search`,
     safeWrap(require('./eagleEyeContentSearch').default),
   )
 
   app.get(
-    `/tenant/:tenantId/eagleEyeContent/:id`,
+    `/eagleEyeContent/:id`,
     safeWrap(require('./eagleEyeContentFind').default),
   )
 
   app.post(
-    `/tenant/:tenantId/eagleEyeContent/:contentId/action`,
+    `/eagleEyeContent/:contentId/action`,
     safeWrap(require('./eagleEyeActionCreate').default),
   )
 
   app.put(
-    `/tenant/:tenantId/eagleEyeContent/settings`,
+    `/eagleEyeContent/settings`,
     safeWrap(require('./eagleEyeSettingsUpdate').default),
   )
 
   app.delete(
-    `/tenant/:tenantId/eagleEyeContent/:contentId/action/:actionId`,
+    `/eagleEyeContent/:contentId/action/:actionId`,
     safeWrap(require('./eagleEyeActionDestroy').default),
   )
 }
