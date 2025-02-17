1151b8d839ee6a6a68c1efa751793615c23987da
Yeganathan S <hilariousengineer@protonmail.com>
2025-02-17 21:05:06 +0530

    Add index

--- backend/src/database/migrations/V1738589807__rmTenantIdAndAddIndexInIndexedEntities.sql
+++ backend/src/database/migrations/V1738589807__rmTenantIdAndAddIndexInIndexedEntities.sql
@@ -1,3 +1,5 @@
+create index if not exists ix_indexed_entities_type_entity_id on indexed_entities (type, entity_id);
+
 drop index if exists ix_indexed_entities_tenant;
 
 alter table indexed_entities
