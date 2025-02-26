f52d3620f6555c0cd57721af8acdfff1b88f4eed
Uroš Marolt <uros@marolt.me>
2025-02-26 21:50:51 +0100

    disable cron job for deleting stale nango connections on production

--- services/apps/cron_service/src/jobs/nangoConnectionCleanup.job.ts
+++ services/apps/cron_service/src/jobs/nangoConnectionCleanup.job.ts
@@ -1,6 +1,6 @@
 import CronTime from 'cron-time-generator'
 
-import { IS_DEV_ENV, singleOrDefault } from '@crowd/common'
+import { IS_DEV_ENV, IS_STAGING_ENV, singleOrDefault } from '@crowd/common'
 import { READ_DB_CONFIG, getDbConnection } from '@crowd/data-access-layer/src/database'
 import { fetchNangoIntegrationData } from '@crowd/data-access-layer/src/integrations'
 import { pgpQx } from '@crowd/data-access-layer/src/queryExecutor'
@@ -17,6 +17,7 @@ const job: IJobDefinition = {
   name: 'nango-connection-cleanup',
   cronTime: IS_DEV_ENV ? CronTime.every(10).minutes() : CronTime.everyDay(),
   timeout: 15 * 60,
+  enabled: async () => IS_DEV_ENV || IS_STAGING_ENV,
   process: async (ctx) => {
     ctx.log.info('Cleaning up stale/old/unconnected nango integration connections!')
 
