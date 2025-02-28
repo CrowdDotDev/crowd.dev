4a7629e5d5295d82c46ed2c8141ffbd9b4dbf31b
Sameh16 <sameh_mohamed16@hotmail.com>
2025-02-28 16:36:32 +0200

    Update image loader to use simplified asset path
    Modify the `getImageUrl` utility function to use a more direct path to frontend assets, removing the 'images' subdirectory from the URL generation.
    
    Signed-off-by: Sameh16 <sameh_mohamed16@hotmail.com>

--- frontend/src/utils/image-loader.ts
+++ frontend/src/utils/image-loader.ts
@@ -1,5 +1,5 @@
 export function getImageUrl(name: string, ext: string) {
-  const url = new URL(`../assets/images/${name}.${ext}`, import.meta.url);
+  const url = new URL(`../assets/${name}.${ext}`, import.meta.url);
   console.log(url);
   return url;
 }
