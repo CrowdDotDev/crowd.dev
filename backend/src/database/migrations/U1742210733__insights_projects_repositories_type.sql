25c382829504cdb47d036ad40eb12e777bcfc012
Mikhail Savelyev <msavelyev@contractor.linuxfoundation.org>
2025-03-17 17:05:53 +0000

    fixup

--- backend/src/database/migrations/U1742210733__insights_projects_repositories_type.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE
-    "insightsProjects"
-ALTER COLUMN
-    "repositories" TYPE JSONB USING (to_jsonb("repositories"));
--- backend/src/database/migrations/V1742210733__insights_projects_repositories_type.sql
+++ backend/src/database/migrations/V1742210733__insights_projects_repositories_type.sql
@@ -1,9 +1,9 @@
-ALTER TABLE
-    "insightsProjects"
-ALTER COLUMN
-    "repositories" TYPE TEXT [] USING (
-        ARRAY(
-            SELECT
-                jsonb_array_elements_text("repositories")
-        )
-    );
+CREATE OR REPLACE FUNCTION jsonb_to_text_array(jsonb) RETURNS text[] AS $$
+SELECT array_agg(value) FROM jsonb_array_elements_text($1)
+$$ LANGUAGE SQL;
+
+ALTER TABLE "insightsProjects"
+    ALTER COLUMN "repositories" TYPE TEXT[]
+        USING jsonb_to_text_array("repositories");
+
+DROP FUNCTION IF EXISTS jsonb_to_text_array(jsonb);
