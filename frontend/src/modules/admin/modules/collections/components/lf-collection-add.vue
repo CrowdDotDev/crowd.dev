353b178131c8759933a4fa39297b900cbfe3acde
Gašper Grom <gasper.grom@gmail.com>
2025-04-03 12:36:26 +0200

    Frontend enable back project validation
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- frontend/src/modules/admin/modules/collections/components/lf-collection-add.vue
+++ frontend/src/modules/admin/modules/collections/components/lf-collection-add.vue
@@ -186,7 +186,7 @@ const rules = {
     maxLength,
   },
   description: { required: (value: string) => value.trim().length },
-  // projects: { required: (value: any) => value.length > 0 },
+  projects: { required: (value: any) => value.length > 0 },
   categoryId: {
     required,
   },
