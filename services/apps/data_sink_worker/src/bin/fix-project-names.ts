9cad24ee1fa5ac71210129522828b578c61f4d96
Gašper Grom <gasper.grom@gmail.com>
2025-04-07 11:59:47 +0200

    Move to openai
    Signed-off-by: Gašper Grom <gasper.grom@gmail.com>

--- services/apps/data_sink_worker/src/bin/fix-project-names.ts
+++ services/apps/data_sink_worker/src/bin/fix-project-names.ts
@@ -1,4 +1,5 @@
-import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime'
+import OpenAI from 'openai'
+import pLimit from 'p-limit'
 
 import { getDbConnection } from '@crowd/data-access-layer/src/database'
 
@@ -6,56 +7,98 @@ import { DB_CONFIG } from '../conf'
 
 setImmediate(async () => {
   const dbClient = await getDbConnection(DB_CONFIG())
-  const client = new BedrockRuntimeClient({
-    credentials: {
-      accessKeyId: process.env['CROWD_AWS_BEDROCK_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['CROWD_AWS_BEDROCK_SECRET_ACCESS_KEY'],
-    },
-    region: 'us-east-1',
+  const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY || '',
   })
 
-  const projects = await dbClient.any('SELECT * FROM "insightsProjects" where name ilike $1', ['%docs%'])
+  await dbClient.any(
+    'ALTER TABLE "insightsProjects" ADD COLUMN IF NOT EXISTS "tempname" VARCHAR(255) DEFAULT name',
+  )
 
-  for (const project of projects.slice(0, 100)) {
-    const name = project.name
-    const description = project.description
+  const projects = await dbClient.any(
+    'SELECT * FROM "insightsProjects" WHERE "segmentId" IS NULL AND tempname IS NULL',
+  )
 
-    const prompt = `
-      give me name of the project from this current name and description include also organization name if needed
+  console.log(`Total Projects to Process: ${projects.length}`)
+
+  const limit = pLimit(3)
+
+  await Promise.all(
+    projects.map((project) =>
+      limit(async () => {
+        const { name, description } = project
+
+        const prompt = `
+give me name of the project from this current name and description include also organization name if needed
 name: ${name}
 description: ${description}
 
-Return just project name no other text`
+Return simply just project name no other text and no formatting`
 
-    const command = new InvokeModelCommand({
-      body: JSON.stringify({
-        messages: [
-          {
-            role: 'user',
-            content: prompt,
-          },
-        ],
-        max_tokens: 512,
-        anthropic_version: 'bedrock-2023-05-31',
+        const chatCompletion = await client.chat.completions.create({
+          messages: [{ role: 'user', content: prompt }],
+          model: 'gpt-4o',
+        })
+        const newName = chatCompletion.choices[0].message.content.trim()
+
+        await dbClient.one(
+          'UPDATE "insightsProjects" SET "tempname" = $1 WHERE "id" = $2 RETURNING *',
+          [newName, project.id],
+        )
+
+        console.log(`${name} -> ${newName}`)
       }),
-      modelId: 'anthropic.claude-3-5-sonnet-20240620-v1:0',
-      accept: 'application/json',
-      contentType: 'application/json',
-    })
+    ),
+  )
 
-    const res = await client.send(command)
+  const duplicates = await dbClient.any(
+    'SELECT tempname, COUNT(*) AS duplicate_count FROM "insightsProjects" GROUP BY tempname HAVING COUNT(*) > 1',
+  )
 
-    const responseBody = JSON.parse(new TextDecoder('utf-8').decode(res.body))
-    const [answer] = responseBody.content
-    const newName = answer.text
+  console.log(`Number of Duplicates: ${duplicates.length}`)
 
-    await dbClient.one(
-      'UPDATE "insightsProjects" SET "tempname" = $1 WHERE "id" = $2 RETURNING *',
-      [newName, project.id],
-    )
+  await Promise.all(
+    duplicates.map((duplicate) =>
+      limit(async () => {
+        const duplicateName = duplicate.tempname
+        const sameResults = await dbClient.any(
+          'SELECT id, name, description FROM "insightsProjects" WHERE tempname = $1',
+          [duplicateName],
+        )
 
-    console.log(name, '->', newName)
-  }
+        const projectDetails = sameResults.map((result) => ({
+          name: result.name,
+          description: result.description,
+        }))
+
+        const prompt = `
+The following projects have the same name. Generate unique names for each by incorporating distinguishing details such as organization name or additional project context. Ensure the names remain relevant and unique.
+Projects: ${JSON.stringify(projectDetails, null, 2)}
+
+Return an array of strings of unique project names in JSON format, maintaining the same order as given.`
+
+        const chatCompletion = await client.chat.completions.create({
+          messages: [{ role: 'user', content: prompt }],
+          model: 'gpt-4o',
+        })
+
+        const responseContent = chatCompletion.choices[0].message.content.trim()
+        const uniqueNames = JSON.parse(responseContent.replace(/```json|```/g, '').trim())
+
+        console.log(duplicateName)
+        console.log(uniqueNames)
+
+        await Promise.all(
+          sameResults.map((result, index) =>
+            dbClient.one(
+              'UPDATE "insightsProjects" SET "tempname" = $1 WHERE "id" = $2 RETURNING *',
+              [uniqueNames[index], result.id],
+            ),
+          ),
+        )
+      }),
+    ),
+  )
 
   process.exit(0)
 })
