a190f72a283ff3bb5dc89d6b3f206243a13190bd
Yeganathan S <hilariousengineer@protonmail.com>
2025-04-09 17:00:14 +0530

    Support `segmentId` in sync workflows (#2972)

--- services/apps/script_executor_worker/src/activities/sync/entity-index.ts
+++ services/apps/script_executor_worker/src/activities/sync/entity-index.ts
@@ -3,10 +3,13 @@ import { IndexingRepository } from '@crowd/opensearch/src/repo/indexing.repo'
 
 import { svc } from '../../main'
 
-export async function deleteIndexedEntities(entityType: IndexedEntityType): Promise<void> {
+export async function deleteIndexedEntities(
+  entityType: IndexedEntityType,
+  segmentIds?: string[],
+): Promise<void> {
   try {
     const indexingRepo = new IndexingRepository(svc.postgres.writer, svc.log)
-    await indexingRepo.deleteIndexedEntities(entityType)
+    await indexingRepo.deleteIndexedEntities(entityType, segmentIds)
   } catch (error) {
     svc.log.error(error, 'Error deleting indexed entities')
     throw error
