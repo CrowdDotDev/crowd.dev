a477e62234d80edcff6cd3a65762148ca8339e49
Yeganathan S <hilariousengineer@protonmail.com>
2025-04-11 21:43:06 +0530

    handle verified type properly when sent in req.query

--- backend/src/api/organization/organizationCanRevertMerge.ts
+++ backend/src/api/organization/organizationCanRevertMerge.ts
@@ -5,9 +5,14 @@ import PermissionChecker from '../../services/user/permissionChecker'
 export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.organizationEdit)
 
+  const identity = {
+    ...req.query.identity,
+    verified: req.query?.identity?.verified === 'true',
+  }
+
   const payload = await new OrganizationService(req).canRevertMerge(
     req.params.organizationId,
-    req.query.identity,
+    identity,
   )
 
   await req.responseHandler.success(req, res, payload, 200)
