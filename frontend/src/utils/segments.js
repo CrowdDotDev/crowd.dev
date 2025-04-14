c7fc2a99626a9aea094205d4829c04b0ad12257d
Sameh16 <sameh_mohamed16@hotmail.com>
2025-04-14 12:04:19 +0200

    Refactor formatting in getSegmentsFromProjectGroup and getProjectGroupsThroughSegments functions for improved readability
    Signed-off-by: Sameh16 <sameh_mohamed16@hotmail.com>

--- frontend/src/utils/segments.js
+++ frontend/src/utils/segments.js
@@ -1,12 +1,21 @@
-import { storeToRefs } from 'pinia';
-import { useLfSegmentsStore } from '@/modules/lf/segments/store';
+import { storeToRefs } from "pinia";
+import { useLfSegmentsStore } from "@/modules/lf/segments/store";
 
-export const getSegmentsFromProjectGroup = (projectGroup, options, isGetMethod) => {
+export const getSegmentsFromProjectGroup = (
+  projectGroup,
+  options,
+  isGetMethod
+) => {
   if (!projectGroup) {
     return [];
   }
 
-  if (options?.url?.includes('/member/query') || options?.url?.includes('/member/active') || options?.url?.includes('/organization/query') || isGetMethod) {
+  if (
+    options?.url?.includes("/member/query") ||
+    options?.url?.includes("/member/active") ||
+    options?.url?.includes("/organization/query") ||
+    isGetMethod
+  ) {
     return [projectGroup.id];
   }
 
@@ -23,15 +32,17 @@ export const getProjectGroupsThroughSegments = (segments) => {
   const lsSegmentsStore = useLfSegmentsStore();
   const { projectGroups } = storeToRefs(lsSegmentsStore);
 
-  return projectGroups.value.list.filter((p) => segments?.includes(p.id)).map((p) => ({
-    id: p.id,
-    name: p.name,
-  }));
+  return projectGroups.value.list
+    .filter((p) => segments?.includes(p.id))
+    .map((p) => ({
+      id: p.id,
+      name: p.name,
+    }));
 };
 
 export const getSegmentName = (segmentId) => {
   const lsSegmentsStore = useLfSegmentsStore();
   const { projectGroups } = storeToRefs(lsSegmentsStore);
 
-  return projectGroups.value.list.find((p) => p.id === segmentId)?.name ?? '';
+  return projectGroups.value.list.find((p) => p.id === segmentId)?.name ?? "";
 };
