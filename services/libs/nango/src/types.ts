18d97aee5818c1a49393c6fc39d24a4013dbd241
Mouad BANI <mouad-mb@outlook.com>
2025-04-18 14:39:08 +0100

    add support jira integrations in nango config

--- services/libs/nango/src/types.ts
+++ services/libs/nango/src/types.ts
@@ -3,10 +3,10 @@ import { PlatformType } from '@crowd/types'
 
 export enum NangoIntegration {
   GERRIT = 'gerrit',
+  GITHUB = 'github',
   JIRA_CLOUD_BASIC = 'jira-basic',
   JIRA_DATA_CENTER_API_KEY = 'jira-data-center-api-key',
-  JIRA_DATA_CENTER_BASIC = 'jira-data-center-basic',
-  GITHUB = 'github',
+  JIRA_DATA_CENTER_BASIC = 'jira-data-center-basic'
 }
 
 export const ALL_NANGO_INTEGRATIONS = Object.values(NangoIntegration)
@@ -72,38 +72,38 @@ export const NANGO_INTEGRATION_CONFIG = {
   },
   [NangoIntegration.JIRA_CLOUD_BASIC]: {
     models: {
-      ISSUES: 'Issue',
-      ISSUE_COMMENT: 'IssueComment',
-      ISSUE_ATTACHMENTS: 'IssueAttachment',
+      ISSUES: "Issue",
+      ISSUE_COMMENT: "IssueComment",
+      ISSUE_ATTACHMENTS: "IssueAttachment"
     },
     syncs: {
-      ISSUES: 'issues',
-      ISSUE_COMMENT: 'issue-comments',
-      ISSUE_ATTACHMENTS: 'issue-attachments',
+      ISSUES: "issues",
+      ISSUE_COMMENT: "issue-comments",
+      ISSUE_ATTACHMENTS: "issue-attachments"
     },
   },
   [NangoIntegration.JIRA_DATA_CENTER_BASIC]: {
     models: {
-      ISSUES: 'Issue',
-      ISSUE_COMMENT: 'IssueComment',
-      ISSUE_ATTACHMENTS: 'IssueAttachment',
+      ISSUES: "Issue",
+      ISSUE_COMMENT: "IssueComment",
+      ISSUE_ATTACHMENTS: "IssueAttachment"
     },
     syncs: {
-      ISSUES: 'issues',
-      ISSUE_COMMENT: 'issue-comments',
-      ISSUE_ATTACHMENTS: 'issue-attachments',
+      ISSUES: "issues",
+      ISSUE_COMMENT: "issue-comments",
+      ISSUE_ATTACHMENTS: "issue-attachments"
     },
   },
   [NangoIntegration.JIRA_DATA_CENTER_API_KEY]: {
     models: {
-      ISSUES: 'Issue',
-      ISSUE_COMMENT: 'IssueComment',
-      ISSUE_ATTACHMENTS: 'IssueAttachment',
+      ISSUES: "Issue",
+      ISSUE_COMMENT: "IssueComment",
+      ISSUE_ATTACHMENTS: "IssueAttachment"
     },
     syncs: {
-      ISSUES: 'issues',
-      ISSUE_COMMENT: 'issue-comments',
-      ISSUE_ATTACHMENTS: 'issue-attachments',
+      ISSUES: "issues",
+      ISSUE_COMMENT: "issue-comments",
+      ISSUE_ATTACHMENTS: "issue-attachments"
     },
   },
 } as const satisfies IntegrationConfig
