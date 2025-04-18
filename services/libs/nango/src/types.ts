c5242c9d4639483c5bebace3c276a6f764cee854
Mouad BANI <mouad-mb@outlook.com>
2025-04-18 15:49:08 +0100

    libs lint

--- services/libs/nango/src/types.ts
+++ services/libs/nango/src/types.ts
@@ -72,14 +72,14 @@ export const NANGO_INTEGRATION_CONFIG = {
   },
   [NangoIntegration.JIRA_CLOUD_BASIC]: {
     models: {
-      ISSUES: "Issue",
-      ISSUE_COMMENT: "IssueComment",
-      ISSUE_ATTACHMENTS: "IssueAttachment"
+      ISSUES: 'Issue',
+      ISSUE_COMMENT: 'IssueComment',
+      ISSUE_ATTACHMENTS: 'IssueAttachment',
     },
     syncs: {
-      ISSUES: "issues",
-      ISSUE_COMMENT: "issue-comments",
-      ISSUE_ATTACHMENTS: "issue-attachments"
+      ISSUES: 'issues',
+      ISSUE_COMMENT: 'issue-comments',
+      ISSUE_ATTACHMENTS: 'issue-attachments',
     },
   },
   [NangoIntegration.JIRA_DATA_CENTER_BASIC]: {
