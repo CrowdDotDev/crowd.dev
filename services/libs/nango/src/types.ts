3f64a815f9b6a8801fbe927c5860c62ba154623d
Mouad BANI <mouad-mb@outlook.com>
2025-04-28 15:39:49 +0100

    fix: lint

--- services/libs/nango/src/types.ts
+++ services/libs/nango/src/types.ts
@@ -94,26 +94,26 @@ export const NANGO_INTEGRATION_CONFIG = {
   },
   [NangoIntegration.JIRA_DATA_CENTER_BASIC]: {
     models: {
-      ISSUES: "Issue",
-      ISSUE_COMMENT: "IssueComment",
-      ISSUE_ATTACHMENTS: "IssueAttachment"
+      ISSUES: 'Issue',
+      ISSUE_COMMENT: 'IssueComment',
+      ISSUE_ATTACHMENTS: 'IssueAttachment',
     },
     syncs: {
-      ISSUES: "issues",
-      ISSUE_COMMENT: "issue-comments",
-      ISSUE_ATTACHMENTS: "issue-attachments"
+      ISSUES: 'issues',
+      ISSUE_COMMENT: 'issue-comments',
+      ISSUE_ATTACHMENTS: 'issue-attachments',
     },
   },
   [NangoIntegration.JIRA_DATA_CENTER_API_KEY]: {
     models: {
-      ISSUES: "Issue",
-      ISSUE_COMMENT: "IssueComment",
-      ISSUE_ATTACHMENTS: "IssueAttachment"
+      ISSUES: 'Issue',
+      ISSUE_COMMENT: 'IssueComment',
+      ISSUE_ATTACHMENTS: 'IssueAttachment',
     },
     syncs: {
-      ISSUES: "issues",
-      ISSUE_COMMENT: "issue-comments",
-      ISSUE_ATTACHMENTS: "issue-attachments"
+      ISSUES: 'issues',
+      ISSUE_COMMENT: 'issue-comments',
+      ISSUE_ATTACHMENTS: 'issue-attachments',
     },
   },
 } as const satisfies IntegrationConfig
