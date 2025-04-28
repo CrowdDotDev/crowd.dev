9fe6742f1a61c012e7e8e1e581b0bf000ab3520b
Yeganathan S <hilariousengineer@protonmail.com>
2025-04-28 17:15:26 +0530

    chore: add timestamp column in db and auto fill

--- backend/src/database/migrations/V1745401102__memberUserValidations.sql
+++ backend/src/database/migrations/V1745401102__memberUserValidations.sql
@@ -3,11 +3,6 @@ create table "memberUserValidations"(
     "memberId" uuid not null references members(id),
     action varchar(255) not null,
     type varchar(255) not null,
-    details jsonb not null
-    -- todo: ask joana if we need timestamp or can reuse createdAt/updatedAt
-    -- createdAt timestamp with time zone default now(),
-    -- updatedAt timestamp with time zone default now(),
-    -- deletedAt timestamp with time zone
+    details jsonb not null,
+    timestamp timestamp with time zone default now()
 );
-
-
