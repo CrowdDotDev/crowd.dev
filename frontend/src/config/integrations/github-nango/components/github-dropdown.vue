21285672b5d9eaf634bbe19a6ec716f518582b5a
Sameh16 <sameh_mohamed16@hotmail.com>
2025-04-29 16:58:36 +0300

    fix: update GitHub dropdown by adding settings component to integration config
    Signed-off-by: Sameh16 <sameh_mohamed16@hotmail.com>

--- frontend/src/config/integrations/github-nango/components/github-dropdown.vue
+++ frontend/src/config/integrations/github-nango/components/github-dropdown.vue
@@ -1,30 +1,23 @@
 <template>
-  <lf-dropdown-item @click="isGitHubSettingsDrawerOpen = true">
+  <lf-dropdown-item @click="emit('open-setting')">
     <lf-icon name="sliders-simple" type="regular" />
     Settings
   </lf-dropdown-item>
-  <lf-github-settings-drawer
-    v-if="isGitHubSettingsDrawerOpen"
-    v-model="isGitHubSettingsDrawerOpen"
-    :integration="props.integration"
-    :segment-id="props.segmentId"
-    :grandparent-id="props.grandparentId"
-  />
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref } from 'vue';
+import { defineProps } from 'vue';
 import LfDropdownItem from '@/ui-kit/dropdown/DropdownItem.vue';
 import LfIcon from '@/ui-kit/icon/Icon.vue';
-import LfGithubSettingsDrawer from '@/config/integrations/github-nango/components/settings/github-settings-drawer.vue';
 
-const props = defineProps<{
+defineProps<{
   integration: any;
   segmentId: string | null;
   grandparentId: string | null;
 }>();
 
-const isGitHubSettingsDrawerOpen = ref(false);
+const emit = defineEmits<{(e: 'open-setting'): void;
+}>();
 </script>
 
 <script lang="ts">
