302ac1d388bfdfbe8037a91f6564ba39e66fe8c5
Uroš Marolt <uros@marolt.me>
2025-04-30 21:37:01 +0200

    Added a manual nango cleanup script
    Signed-off-by: Uroš Marolt <uros@marolt.me>

---
+++ services/apps/nango_worker/src/bin/check-disconnected-connections.ts
@@ -0,0 +1,50 @@
+import { READ_DB_CONFIG, getDbConnection } from '@crowd/data-access-layer/src/database'
+import { fetchNangoIntegrationData } from '@crowd/data-access-layer/src/integrations'
+import { pgpQx } from '@crowd/data-access-layer/src/queryExecutor'
+import { getServiceLogger } from '@crowd/logging'
+import {
+  ALL_NANGO_INTEGRATIONS,
+  getNangoConnections,
+  initNangoCloudClient,
+  nangoIntegrationToPlatform,
+} from '@crowd/nango'
+import { PlatformType } from '@crowd/types'
+
+const log = getServiceLogger()
+
+const processArguments = process.argv.slice(2)
+
+let deleteDisconnectedConnections = false
+if (processArguments.includes('--delete')) {
+  deleteDisconnectedConnections = true
+}
+
+setImmediate(async () => {
+  const db = await getDbConnection(READ_DB_CONFIG())
+
+  await initNangoCloudClient()
+
+  const nangoIntegrations = await fetchNangoIntegrationData(
+    pgpQx(db),
+    ALL_NANGO_INTEGRATIONS.map(nangoIntegrationToPlatform),
+  )
+
+  const nangoConnections = await getNangoConnections()
+
+  const connectionIds: string[] = []
+  for (const int of nangoIntegrations) {
+    if (int.platform === PlatformType.GITHUB_NANGO) {
+      connectionIds.push(...Object.keys(int.settings.nangoMapping))
+    } else {
+      connectionIds.push(int.id)
+    }
+  }
+
+  for (const nangoConnection of nangoConnections.filter(
+    (c) => !c.connection_id.startsWith('github-token-'),
+  )) {
+    if (!connectionIds.includes(nangoConnection.connection_id)) {
+      log.warn(`Connection ${nangoConnection.connection_id} is not connected to integration!`)
+    }
+  }
+})
