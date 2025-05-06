1c9b61b15cbb3055359ecacee3dbccffafe7ea9e
anilb <epipav@gmail.com>
2025-05-06 16:57:32 +0200

    fix(migration): ordering improved

--- backend/src/database/migrations/V1746542390__securityInsightsTableRename.sql
+++ backend/src/database/migrations/V1746542390__securityInsightsTableRename.sql
@@ -1,9 +1,10 @@
+ALTER PUBLICATION sequin_pub DROP TABLE "securityInsightsEvaluationSuiteControlEvaluations";
+ALTER PUBLICATION sequin_pub DROP TABLE "securityInsightsEvaluationSuiteControlEvaluationAssessments";
+
 ALTER TABLE "securityInsightsEvaluationSuiteControlEvaluations" RENAME TO "securityInsightsEvaluations";
 ALTER TABLE "securityInsightsEvaluationSuiteControlEvaluationAssessments" RENAME TO "securityInsightsEvaluationAssessments";
 ALTER TABLE "securityInsightsEvaluationAssessments" RENAME COLUMN "securityInsightsEvaluationSuiteControlEvaluationId" TO "securityInsightsEvaluationId";
 
-ALTER PUBLICATION sequin_pub DROP TABLE "securityInsightsEvaluationSuiteControlEvaluations";
-ALTER PUBLICATION sequin_pub DROP TABLE "securityInsightsEvaluationSuiteControlEvaluationAssessments";
 ALTER PUBLICATION sequin_pub ADD TABLE "securityInsightsEvaluations";
 ALTER PUBLICATION sequin_pub ADD TABLE "securityInsightsEvaluationAssessments";
 ALTER TABLE public."securityInsightsEvaluations" REPLICA IDENTITY FULL;
