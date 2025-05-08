a90562e8ca23a19b3c935ee6de4ffa8316fd3a0d
Sameh16 <sameh_mohamed16@hotmail.com>
2025-05-08 12:59:31 +0300

    feat: implement infinite scrolling for collections with query support
    Signed-off-by: Sameh16 <sameh_mohamed16@hotmail.com>

--- frontend/src/modules/admin/modules/collections/services/collections.service.ts
+++ frontend/src/modules/admin/modules/collections/services/collections.service.ts
@@ -1,4 +1,7 @@
 import authAxios from '@/shared/axios/auth-axios';
+import { Pagination } from '@/shared/types/Pagination';
+import { QueryFunction } from '@tanstack/vue-query';
+import { CollectionModel } from '../models/collection.model';
 
 export class CollectionsService {
   static async list(query: any) {
@@ -8,6 +11,17 @@ export class CollectionsService {
     );
 
     return response.data;
+  }
+
+  query(
+    query: () => Record<string, string | number | object>,
+  ): QueryFunction<Pagination<CollectionModel>> {
+    return ({ pageParam = 0 }) => authAxios
+      .post<Pagination<CollectionModel>>('/collections/query', {
+        ...query(),
+        offset: pageParam,
+      })
+      .then((res) => res.data);
   }
 
   static async create(collection: any) {
@@ -34,3 +48,5 @@ export class CollectionsService {
     return response.data;
   }
 }
+
+export const COLLECTIONS_SERVICE = new CollectionsService();
