7a6b8b3c46c363591c43eb6ebbf31f11567f6787
Uroš Marolt <uros@marolt.me>
2025-06-06 10:40:17 +0200

    fix(system): fixed the ignore queue part
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- services/apps/cron_service/src/jobs/queueMonitoring.job.ts
+++ services/apps/cron_service/src/jobs/queueMonitoring.job.ts
@@ -1,7 +1,4 @@
 import CronTime from 'cron-time-generator'
-import fs from 'fs'
-import os from 'os'
-import path from 'path'
 
 import { IS_PROD_ENV, distinct } from '@crowd/common'
 import { Logger } from '@crowd/logging'
@@ -17,30 +14,11 @@ const job: IJobDefinition = {
   timeout: 30 * 60,
   enabled: async () => IS_PROD_ENV,
   process: async (ctx) => {
-    try {
-      ensureOciConfig()
-    } catch (err) {
-      ctx.log.error(err, 'Failed to ensure OCI config!')
-      throw err
-    }
-
-    const allStreams = await getAllStreams()
-
     const toIgnore = (
       process.env.CROWD_KAFKA_STREAMS_TO_IGNORE_ON_MONITORING?.split(',') ?? []
     ).map((s) => s.trim())
 
     ctx.log.info(`To ignore: ${toIgnore.join(', ')}`)
-
-    const streams: IStream[] = []
-    for (const stream of allStreams) {
-      ctx.log.debug(`Found stream ${stream.name}!`)
-      if (!toIgnore.includes(stream.name.trim())) {
-        streams.push(stream)
-      } else {
-        ctx.log.info(`Ignoring stream ${stream.name}!`)
-      }
-    }
 
     const kafkaClient = getKafkaClient(QUEUE_CONFIG())
     const admin = kafkaClient.admin()
@@ -51,6 +29,11 @@ const job: IJobDefinition = {
     let msg = ``
 
     for (const [topic, groups] of map) {
+      if (toIgnore.includes(topic.trim())) {
+        ctx.log.info(`Ignoring topic ${topic}!`)
+        continue
+      }
+
       const totalMessages = await getTopicMessageCount(ctx.log, admin, topic)
       telemetry.gauge(`kafka.${topic}.total`, totalMessages)
 
@@ -201,111 +184,6 @@ async function isConsumerListeningToTopic(
   }
 }
 
-function ensureOciConfig() {
-  const homeDir = os.homedir()
-  const ociDir = path.join(homeDir, '.oci')
-  const configPath = path.join(ociDir, 'config')
-
-  if (fs.existsSync(configPath)) {
-    return
-  }
-
-  const keyPath = path.join(ociDir, 'private.pem')
-
-  const user = process.env.CROWD_KAFKA_USER_OCID
-  const keyFingerprint = process.env.CROWD_KAFKA_KEY_FINGERPRINT
-  const tenant = process.env.CROWD_KAFKA_TENANCY_OCID
-  const region = process.env.CROWD_KAFKA_REGION
-  const keyBase64 = process.env.CROWD_KAFKA_KEY
-  const compartmentId = process.env.CROWD_KAFKA_COMPARTMENT_OCID
-
-  if (!user) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_USER_OCID')
-  }
-
-  if (!keyFingerprint) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_KEY_FINGERPRINT')
-  }
-
-  if (!tenant) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_TENANCY_OCID')
-  }
-
-  if (!region) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_REGION')
-  }
-
-  if (!keyBase64) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_KEY')
-  }
-
-  if (!compartmentId) {
-    throw new Error('Missing required environment variables - CROWD_KAFKA_COMPARTMENT_OCID')
-  }
-
-  const privateKey = Buffer.from(keyBase64, 'base64').toString('ascii')
-
-  // prepare oracle config
-  const config = `
-      [DEFAULT]
-      user=${user}
-      fingerprint=${keyFingerprint}
-      key_file=${keyPath}
-      tenancy=${tenant}
-      region=${region}
-      `
-
-  // create the ~/.oci folder if it doesn't exists
-  fs.mkdirSync(ociDir, { recursive: true })
-
-  // write config to ~/.oci/config
-  fs.writeFileSync(configPath, config, 'utf8')
-
-  // write private key to ~/.oci/oci_api_key.pem
-  fs.writeFileSync(keyPath, privateKey, 'utf8')
-
-  // chmod 600 to key and config
-  fs.chmodSync(configPath, 0o600)
-  fs.chmodSync(keyPath, 0o600)
-}
-
-async function getAllStreams() {
-  const commonModule = await import('oci-common')
-  const stModule = await import('oci-streaming')
-
-  const common = commonModule.default
-  const st = stModule.default
-
-  const provider = new common.ConfigFileAuthenticationDetailsProvider()
-
-  const streamAdminClient = new st.StreamAdminClient({
-    authenticationDetailsProvider: provider,
-  })
-
-  const streams: IStream[] = []
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const listStreamsRequest: any = {
-    compartmentId: process.env.CROWD_KAFKA_COMPARTMENT_OCID,
-    lifecycleState: st.models.StreamSummary.LifecycleState.Active,
-    limit: 50,
-  }
-
-  const records = await common.paginatedRecordsWithLimit(listStreamsRequest, (req) =>
-    streamAdminClient.listStreams(req),
-  )
-
-  for (let i = 0; i < records.length; i++) {
-    const stream = records[i].value
-    streams.push({
-      name: stream.name,
-      id: stream.id,
-    })
-  }
-
-  return streams
-}
-
 async function getMessageCounts(
   log: Logger,
   admin: KafkaAdmin,
@@ -370,11 +248,6 @@ async function getTopicMessageCount(
     log.error(err, `Failed to get message count for topic ${topic}!`)
     throw err
   }
-}
-
-interface IStream {
-  name: string
-  id: string
 }
 
 export default job
