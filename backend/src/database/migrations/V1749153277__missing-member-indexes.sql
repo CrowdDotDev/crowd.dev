1e2215bcb9d032dfde85a4eb000e815d8d56cbe8
Uroš Marolt <uros@marolt.me>
2025-06-10 09:00:41 +0200

    fix(system): fixed the migration
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- backend/src/database/migrations/V1749153277__missing-member-indexes.sql
+++ backend/src/database/migrations/V1749153277__missing-member-indexes.sql
@@ -1,4 +1,4 @@
-create index concurrently "ix_activityRelations_objectMemberId"
+create index concurrently if not exists "ix_activityRelations_objectMemberId"
     on "activityRelations" ("objectMemberId") where "objectMemberId" is not null;
 
 create index concurrently if not exists "ix_memberOrganizations_memberId"
