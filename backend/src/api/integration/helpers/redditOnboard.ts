2a5deb3de08d812fb17ff9f071bde718f37c82d1
Mouad BANI <mbani@contractor.linuxfoundation.org>
2025-06-18 16:02:19 +0100

    fix: use segmentId instead of tenantId in connectionId for self-hosted-nango integrations [CM-335] (#3173)

--- backend/src/api/integration/helpers/redditOnboard.ts
+++ backend/src/api/integration/helpers/redditOnboard.ts
@@ -4,6 +4,7 @@ import PermissionChecker from '../../../services/user/permissionChecker'
 
 export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.tenantEdit)
-  const payload = await new IntegrationService(req).redditOnboard(req.body.subreddits)
+  const segmentId = req.body.segments[0]
+  const payload = await new IntegrationService(req).redditOnboard(req.body.subreddits, segmentId)
   await req.responseHandler.success(req, res, payload)
 }
