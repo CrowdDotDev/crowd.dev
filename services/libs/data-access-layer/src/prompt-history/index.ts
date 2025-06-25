47b96b51ee6f8996951a45169401324e537dc196
ulemons <ulemons92@gmail.com>
2025-06-25 15:24:46 +0200

    fix: lint

--- services/libs/data-access-layer/src/prompt-history/index.ts
+++ services/libs/data-access-layer/src/prompt-history/index.ts
@@ -1,26 +1,26 @@
-import { ILlmResponse, LlmModelType, LlmQueryType } from "@crowd/types";
-import { QueryExecutor } from "../queryExecutor";
+import { ILlmResponse, LlmModelType, LlmQueryType } from '@crowd/types'
+
+import { QueryExecutor } from '../queryExecutor'
 
 export async function insertPromptHistoryEntry(
-    qx: QueryExecutor,
-    type: LlmQueryType,
-    model: LlmModelType,
-    result: ILlmResponse,
-    entityId?: string,
-    metadata?: Record<string, unknown>,
+  qx: QueryExecutor,
+  type: LlmQueryType,
+  model: LlmModelType,
+  result: ILlmResponse,
+  entityId?: string,
+  metadata?: Record<string, unknown>,
 ): Promise<void> {
-    await qx.result(
-        `
+  await qx.result(
+    `
         insert into "llmPromptHistory"(type, model, "entityId", metadata, prompt, answer, "inputTokenCount", "outputTokenCount", "responseTimeSeconds")
         values($(type), $(model), $(entityId), $(metadata), $(prompt), $(answer), $(inputTokenCount), $(outputTokenCount), $(responseTimeSeconds));
       `,
-        {
-            type,
-            model,
-            entityId,
-            metadata: metadata ? JSON.stringify(metadata) : null,
-            ...result,
-        },
-    )
+    {
+      type,
+      model,
+      entityId,
+      metadata: metadata ? JSON.stringify(metadata) : null,
+      ...result,
+    },
+  )
 }
-
