c2e4f773a551dc862051522597d870b150f1e435
Yeganathan S <hilariousengineer@protonmail.com>
2025-06-30 16:13:49 +0530

    chore: redis key cache clear script

---
+++ services/apps/data_sink_worker/src/bin/clear-redis-cache.ts
@@ -0,0 +1,39 @@
+import { getServiceLogger } from '@crowd/logging'
+import { RedisCache, getRedisClient } from '@crowd/redis'
+
+import { REDIS_CONFIG } from '../conf'
+
+const log = getServiceLogger()
+
+const processArguments = process.argv.slice(2)
+
+if (processArguments.length !== 1) {
+  log.error('Expected 1 argument: redis-key')
+  process.exit(1)
+}
+
+const redisKey = processArguments[0]
+
+setImmediate(async () => {
+  const redis = await getRedisClient(REDIS_CONFIG())
+  const cache = new RedisCache(redisKey, redis, log)
+
+  try {
+    // check if redis key exists
+    const exists = await cache.exists(redisKey)
+    if (!exists) {
+      log.error({ redisKey }, 'Redis key not found!')
+      process.exit(1)
+    }
+
+    // delete redis key
+    await cache.deleteAll()
+
+    log.info({ redisKey }, 'Redis key deleted!')
+
+    process.exit(0)
+  } catch (err) {
+    log.error('Failed to delete redis key!', err)
+    process.exit(1)
+  }
+})
