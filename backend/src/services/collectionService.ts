22eeb6902414f0188f946214491fe41f70d46f8a
ulemons <ulemons92@gmail.com>
2025-07-07 14:09:59 +0200

    fix: remove useless clean function

--- backend/src/services/collectionService.ts
+++ backend/src/services/collectionService.ts
@@ -609,14 +609,4 @@ export class CollectionService extends LoggerBase {
 
     return result
   }
-
-  async cleanInsightsProjectsById(id: string) {
-    const qx = SequelizeRepository.getQueryExecutor(this.options)
-    const result = await updateInsightsProject(qx, id, {
-      widgets: [],
-      repositories: [],
-    })
-
-    return result
-  }
 }
