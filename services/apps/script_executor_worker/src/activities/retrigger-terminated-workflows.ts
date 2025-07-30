c5c46f685555505a6d872f91f188381a311e364d
Yeganathan S <63534555+skwowet@users.noreply.github.com>
2025-07-30 17:33:57 +0530

    fix: simplify logic

--- services/apps/script_executor_worker/src/activities/retrigger-terminated-workflows.ts
+++ services/apps/script_executor_worker/src/activities/retrigger-terminated-workflows.ts
@@ -1,20 +1,13 @@
-import { IMergeAction, MergeActionState, MergeActionStep, MergeActionType } from '@crowd/types'
+import { MergeActionStep, MergeActionType } from '@crowd/types'
 import { svc } from '../main'
 
 import { getIncompleteMergeActions, updateMergeAction } from '@crowd/data-access-layer/src/mergeActions/repo'
 import { pgpQx } from '@crowd/data-access-layer'
 
 export async function getCancelledMergeAndUnmergeWorkflows(type: MergeActionType, step: MergeActionStep) {
-    let mergeActions: IMergeAction[] = []
     const qx = pgpQx(svc.postgres.reader.connection())
 
-    if (type === MergeActionType.MEMBER) {
-        mergeActions = await getIncompleteMergeActions(qx, MergeActionType.MEMBER, step)
-    } else if (type === MergeActionType.ORG) {
-        mergeActions = await getIncompleteMergeActions(qx, MergeActionType.ORG, step)
-    }
-
-    return mergeActions
+    return getIncompleteMergeActions(qx, type, step)
 }
 
 export async function resetMergeActionState(primaryId: string, secondaryId: string) {
