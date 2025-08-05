fbc20148bda26ef00de382bc493c15c193039b03
ulemons <ulemons92@gmail.com>
2025-08-05 15:02:46 +0200

    refactor: remove conversationSettings

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -91,13 +91,13 @@ async function models(queryTimeoutMilliseconds: number, databaseHostnameOverride
       },
       logging: DB_CONFIG.logging
         ? (dbLog) =>
-            log.info(
-              highlight(dbLog, {
-                language: 'sql',
-                ignoreIllegals: true,
-              }),
-              'DB LOG',
-            )
+          log.info(
+            highlight(dbLog, {
+              language: 'sql',
+              ignoreIllegals: true,
+            }),
+            'DB LOG',
+          )
         : false,
     },
   )
@@ -127,7 +127,6 @@ async function models(queryTimeoutMilliseconds: number, databaseHostnameOverride
     require('./tenant').default,
     require('./tenantUser').default,
     require('./user').default,
-    require('./conversationSettings').default,
     require('./eagleEyeContent').default,
     require('./eagleEyeAction').default,
     require('./organization').default,
