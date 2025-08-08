8da6d23616074a2064b9dd9918f53dcf97f2f7d8
Umberto Sgueglia <ulemons92@gmail.com>
2025-08-08 10:00:36 +0200

    refactor: deprecate conversations on frontend [CM-2461] (#3306)

--- frontend/src/config/identities/discord/config.ts
+++ frontend/src/config/identities/discord/config.ts
@@ -14,13 +14,6 @@ const discord: IdentityConfig = {
   activity: {
     showLink: true,
   },
-  conversation: {
-    replyContent: (conversation) => ({
-      icon: 'reply',
-      copy: 'reply',
-      number: conversation.activityCount - 1,
-    }),
-  },
 };
 
 export default discord;
--- frontend/src/config/identities/slack/config.ts
+++ frontend/src/config/identities/slack/config.ts
@@ -14,13 +14,6 @@ const slack: IdentityConfig = {
   activity: {
     showLink: true,
   },
-  conversation: {
-    replyContent: (conversation) => ({
-      icon: 'reply',
-      copy: 'reply',
-      number: conversation.activityCount - 1,
-    }),
-  },
 };
 
 export default slack;
