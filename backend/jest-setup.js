220b434900b3b6969b1d58948f293efe96d8c9c4
ulemons <ulemons92@gmail.com>
2025-08-12 16:55:21 +0200

    fix: lint

--- backend/jest-setup.js
+++ backend/jest-setup.js
@@ -1,36 +1,36 @@
 /* eslint-env jest, node */
 /* eslint-disable class-methods-use-this */
 jest.mock('config', () => {
-    const DEFAULTS = {
-        queue: { host: 'localhost', port: 1234, ssl: false },
-        encryption: { secretKey: 'default', initVector: 'default' },
-        redis: { username: 'default', password: 'default', host: 'localhost', port: 6379 },
-    }
-    const getPath = (obj, path) =>
-        String(path)
-            .split('.')
-            .filter(Boolean)
-            .reduce((cur, k) => (cur && k in cur ? cur[k] : undefined), obj)
-    const api = {
-        get: (key) => getPath(DEFAULTS, key) ?? {},
-        has: (key) => getPath(DEFAULTS, key) !== undefined,
-        __set: (obj) => Object.assign(DEFAULTS, obj),
-    }
-    return api
+  const DEFAULTS = {
+    queue: { host: 'localhost', port: 1234, ssl: false },
+    encryption: { secretKey: 'default', initVector: 'default' },
+    redis: { username: 'default', password: 'default', host: 'localhost', port: 6379 },
+  }
+  const getPath = (obj, path) =>
+    String(path)
+      .split('.')
+      .filter(Boolean)
+      .reduce((cur, k) => (cur && k in cur ? cur[k] : undefined), obj)
+  const api = {
+    get: (key) => getPath(DEFAULTS, key) ?? {},
+    has: (key) => getPath(DEFAULTS, key) !== undefined,
+    __set: (obj) => Object.assign(DEFAULTS, obj),
+  }
+  return api
 })
 
 jest.mock('@octokit/rest', () => ({
-    Octokit: class {
-        request() {
-            return Promise.resolve({})
-        }
-    },
+  Octokit: class {
+    request() {
+      return Promise.resolve({})
+    }
+  },
 }))
 jest.mock('@octokit/request', () => ({ request: () => Promise.resolve({}) }))
 jest.mock('@octokit/graphql', () => {
-    const base = () => Promise.resolve({})
-    base.defaults = () => base
-    return { graphql: base }
+  const base = () => Promise.resolve({})
+  base.defaults = () => base
+  return { graphql: base }
 })
 
 const lodashIsArray = require('lodash/isArray')
