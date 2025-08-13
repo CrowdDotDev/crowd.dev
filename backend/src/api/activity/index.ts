f8507bbdad9c8b2c74264eaaf80f40d0472ba410
ulemons <ulemons92@gmail.com>
2025-08-13 13:10:09 +0200

    fix: remove activity update endpoint

--- backend/src/api/activity/index.ts
+++ backend/src/api/activity/index.ts
@@ -3,7 +3,6 @@ import { safeWrap } from '../../middlewares/errorMiddleware'
 export default (app) => {
   app.post(`/activity`, safeWrap(require('./activityCreate').default))
   app.post(`/activity/query`, safeWrap(require('./activityQuery').default))
-  app.put(`/activity/:id`, safeWrap(require('./activityUpdate').default))
   app.delete(`/activity`, safeWrap(require('./activityDestroy').default))
   app.get(`/activity/type`, safeWrap(require('./activityTypes').default))
   app.get(`/activity/channel`, safeWrap(require('./activityChannels').default))
