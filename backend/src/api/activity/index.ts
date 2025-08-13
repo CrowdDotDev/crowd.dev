737de15ad1114581f7262bcc51491cf27a28f699
ulemons <ulemons92@gmail.com>
2025-08-13 14:39:49 +0200

    refactor: remove create activity api

--- backend/src/api/activity/index.ts
+++ backend/src/api/activity/index.ts
@@ -1,7 +1,6 @@
 import { safeWrap } from '../../middlewares/errorMiddleware'
 
 export default (app) => {
-  app.post(`/activity`, safeWrap(require('./activityCreate').default))
   app.post(`/activity/query`, safeWrap(require('./activityQuery').default))
   app.delete(`/activity`, safeWrap(require('./activityDestroy').default))
   app.get(`/activity/type`, safeWrap(require('./activityTypes').default))
