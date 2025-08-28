f974fde237ea7d9a969f201f816c587775f89788
Yeganathan S <63534555+skwowet@users.noreply.github.com>
2025-08-28 22:52:03 +0530

    fix: prettier and linter

--- services/apps/profiles_worker/src/workflows.ts
+++ services/apps/profiles_worker/src/workflows.ts
@@ -1,5 +1,5 @@
-import { memberUpdate } from './workflows/member/memberUpdate'
 import { analyzeMemberBotCharacteristicsWithLLM } from './workflows/member/analyzeMemberBotCharacteristicsWithLLM'
+import { memberUpdate } from './workflows/member/memberUpdate'
 import { recalculateAffiliationsForNewRoles } from './workflows/member/recalculateAffiliationsForNewRoles'
 import { refreshMemberDisplayAggregates } from './workflows/member/refreshMemberDisplayAggregates'
 import { triggerRecalculateAffiliations } from './workflows/member/triggerRecalculateAffiliations'
