ea7c9633156b1729c861883729f92129e73513b6
Yeganathan S <63534555+skwowet@users.noreply.github.com>
2025-09-04 14:40:39 +0530

    fix: prettier and linter

--- services/libs/data-access-layer/src/members/bot.ts
+++ services/libs/data-access-layer/src/members/bot.ts
@@ -1,9 +1,7 @@
+/* eslint-disable no-useless-escape */
 import { QueryExecutor } from '../queryExecutor'
 
-export async function fetchBotCandidateMembers(
-  qx: QueryExecutor,
-  limit: number = 100,
-): Promise<string[]> {
+export async function fetchBotCandidateMembers(qx: QueryExecutor, limit = 100): Promise<string[]> {
   const rows = await qx.select(
     `
     SELECT DISTINCT m.id AS "memberId"
@@ -12,12 +10,12 @@ export async function fetchBotCandidateMembers(
     WHERE m."deletedAt" IS NULL
         AND (
             mi.value ~* '(^|[-_/\s])(bot|robot)($|[-_/\s])'
-            OR mi.value ~* '(^|[-_/])(bot|automation|ci|cd|deploy|build|release)[-_]?\\d+$'
+            OR mi.value ~* '(^|[-_/])(bot|automation|ci|cd|deploy|build|release)[-_]?\d+$'
             OR mi.value ~* '(^|[-_/])(github|gitlab|bitbucket|azure|aws|gcp|k8s|openshift)[-_](bot|ci|actions?|pipeline|runner|automation)($|[-_/])'
             OR mi.value ~* '^(bot[-_/\s\\d]*|.*[-_/\s\\d]bot)$'
 
             OR m."displayName" ~* '(^|[-_/\s])(bot|robot)($|[-_/\s])'
-            OR m."displayName" ~* '(^|[-_/])(bot|automation|ci|cd|deploy|build|release)[-_]?\\d+$'
+            OR m."displayName" ~* '(^|[-_/])(bot|automation|ci|cd|deploy|build|release)[-_]?\d+$'
             OR m."displayName" ~* '(^|[-_/])(github|gitlab|bitbucket|azure|aws|gcp|k8s|openshift)[-_](bot|ci|actions?|pipeline|runner|automation)($|[-_/])'
             OR m."displayName" ~* '^(bot[-_/\s\\d]*|.*[-_/\s\\d]bot)$'
         )
