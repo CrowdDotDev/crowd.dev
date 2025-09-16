087cb6cb9bfbd1e79b1867198ef2e1b73ad946b5
Uroš Marolt <uros@marolt.me>
2025-09-16 08:47:25 +0200

    chore: script for deprecating corrupted github token connections on nango
    Signed-off-by: Uroš Marolt <uros@marolt.me>

---
+++ services/apps/nango_worker/src/bin/remove-deprecated-tokens.ts
@@ -0,0 +1,62 @@
+import { getServiceLogger } from '@crowd/logging'
+import {
+  NangoIntegration,
+  deleteNangoConnection,
+  getNangoConnectionData,
+  getNangoConnections,
+  initNangoCloudClient,
+  setNangoMetadata,
+} from '@crowd/nango'
+
+const log = getServiceLogger()
+
+const tokenConnectionsToRemove = (process.env.TOKEN_CONNECTIONS_TO_REMOVE ?? '').split(',')
+
+if (tokenConnectionsToRemove.length === 0) {
+  log.error('No token connections to remove')
+  process.exit(1)
+}
+
+setImmediate(async () => {
+  await initNangoCloudClient()
+
+  for (const tokenConnection of tokenConnectionsToRemove) {
+    log.info(`Removing token connection ${tokenConnection}`)
+    await deleteNangoConnection(NangoIntegration.GITHUB, tokenConnection)
+  }
+
+  const allConnections = await getNangoConnections()
+  const githubConnections = allConnections.filter(
+    (c) => c.provider_config_key === NangoIntegration.GITHUB,
+  )
+
+  for (const githubConnection of githubConnections) {
+    log.info(`Modifying metadata for connection ${githubConnection.connection_id}!`)
+
+    const data = await getNangoConnectionData(
+      NangoIntegration.GITHUB,
+      githubConnection.connection_id,
+    )
+
+    const metadata = data.metadata
+
+    const newMetadata = {
+      ...metadata,
+      connection_ids: (metadata.connection_ids as string[]).filter(
+        (c) => !tokenConnectionsToRemove.includes(c),
+      ),
+    }
+
+    log.info(
+      {
+        oldCount: (metadata.connection_ids as string[]).length,
+        newCount: newMetadata.connection_ids.length,
+      },
+      'Changed',
+    )
+
+    // await setNangoMetadata(NangoIntegration.GITHUB, githubConnection.connection_id, newMetadata)
+  }
+
+  process.exit(0)
+})
