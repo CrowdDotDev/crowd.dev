37082908172fe724b9d0c39fd45b4f3eee52ded4
Umberto Sgueglia <ulemons@Mac.lan>
2025-09-18 11:33:36 +0200

    fix: adding logs for formatter

--- services/libs/integrations/src/integrations/activityDisplayService.ts
+++ services/libs/integrations/src/integrations/activityDisplayService.ts
@@ -59,6 +59,9 @@ export class ActivityDisplayService {
           }
 
           if (displayOptions.formatter && displayOptions.formatter[dv]) {
+            log.info(
+              `dv=${dv}, replacement=${JSON.stringify(replacement)}, hasFormatter=${!!displayOptions.formatter?.[dv]}`,
+            )
             replacement = displayOptions.formatter[dv](replacement)
           }
           displayOptions[key] = displayOptions[key].replace(`{${dv}}`, replacement)
