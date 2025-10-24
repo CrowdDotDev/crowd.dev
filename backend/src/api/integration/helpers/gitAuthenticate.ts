e662dc204ac79a8f5dc87787c86b8869b1d85444
Mouad BANI <mbani@contractor.linuxfoundation.org>
2025-10-24 12:11:23 +0100

    fix: connect git integration payload (CM-756) (#3544)

--- backend/src/api/integration/helpers/gitAuthenticate.ts
+++ backend/src/api/integration/helpers/gitAuthenticate.ts
@@ -4,6 +4,11 @@ import PermissionChecker from '../../../services/user/permissionChecker'
 
 export default async (req, res) => {
   new PermissionChecker(req).validateHas(Permissions.values.tenantEdit)
-  const payload = await new IntegrationService(req).gitConnectOrUpdate(req.body)
+  const integrationData = {
+    ...req.body,
+    remotes: req.body.remotes?.map((remote) => ({ url: remote, forkedFrom: null })) || [],
+  }
+
+  const payload = await new IntegrationService(req).gitConnectOrUpdate(integrationData)
   await req.responseHandler.success(req, res, payload)
 }
