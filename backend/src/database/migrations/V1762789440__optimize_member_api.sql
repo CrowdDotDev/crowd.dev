d17a4e0d75ac0cac32385a6b7e89a9e1f4d580f1
Umberto Sgueglia <ulemons92@gmail.com>
2025-11-10 16:50:09 +0100

    feat: create index cuncurently

--- backend/src/database/migrations/V1762789440__optimize_member_api.sql
+++ backend/src/database/migrations/V1762789440__optimize_member_api.sql
@@ -1,11 +1,11 @@
-CREATE INDEX IF NOT EXISTS idx_msa_segment_activitycount_desc_member
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_msa_segment_activitycount_desc_member
 ON public."memberSegmentsAgg" ("segmentId", "activityCount" DESC, "memberId");
 
-CREATE INDEX IF NOT EXISTS idx_members_displayname_trgm
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_members_displayname_trgm
 ON public."members"
 USING gin (LOWER("displayName") gin_trgm_ops);
 
-CREATE INDEX IF NOT EXISTS idx_memberidentities_email_verified_trgm
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_memberidentities_email_verified_trgm
 ON public."memberIdentities"
 USING gin (LOWER("value") gin_trgm_ops)
 WHERE verified = true
