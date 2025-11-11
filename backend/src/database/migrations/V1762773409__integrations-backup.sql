7105c102aced881afd49503d2ef58ed26f4c647e
Uroš Marolt <uros@marolt.me>
2025-11-11 08:55:38 +0100

    chore: rename
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- backend/src/database/migrations/V1762773409__integrations-backup.sql
+++ backend/src/database/migrations/V1762773409__integrations-backup.sql
@@ -1,10 +1,10 @@
-create table "integrationsHistory" as
+create table "integrationsBackup" as
 select *
 from "integrations"
 where 1 = 2;
 
-alter table "integrationsHistory"
-    add column "historyCreatedAt" timestamptz not null default now();
+alter table "integrationsBackup"
+    add column "backupCreatedAt" timestamptz not null default now();
 
-create index if not exists ix_integration_history_integration_id on "integrationsHistory" ("id");
-create index if not exists ix_integration_history_history_created_at on "integrationsHistory" ("historyCreatedAt");
+create index if not exists ix_integration_history_integration_id on "integrationsBackup" ("id");
+create index if not exists ix_integration_history_history_created_at on "integrationsBackup" ("backupCreatedAt");
