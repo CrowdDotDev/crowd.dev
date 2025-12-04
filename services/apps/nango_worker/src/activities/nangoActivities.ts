90b63fd3269254a896cd45f335935e65de8f2155
Joana Maia <jmaia@contractor.linuxfoundation.org>
2025-12-04 17:01:35 +0000

    fix: nango repos to delete

--- services/apps/nango_worker/src/activities/nangoActivities.ts
+++ services/apps/nango_worker/src/activities/nangoActivities.ts
@@ -4,6 +4,7 @@ import {
   addGitHubRepoMapping,
   addGithubNangoConnection,
   addRepoToGitIntegration,
+  fetchDeletedIntegrationById,
   fetchIntegrationById,
   findIntegrationDataForNangoWebhookProcessing,
   removeGitHubRepoMapping,
@@ -221,6 +222,10 @@ export async function analyzeGithubIntegration(
   const reposToSync: IGithubRepoData[] = []
 
   const integration = await fetchIntegrationById(dbStoreQx(svc.postgres.writer), integrationId)
+  const deletedIntegration = await fetchDeletedIntegrationById(
+    dbStoreQx(svc.postgres.writer),
+    integrationId,
+  )
 
   if (integration) {
     if (integration.platform === PlatformType.GITHUB_NANGO) {
@@ -340,6 +345,23 @@ export async function analyzeGithubIntegration(
     } else {
       svc.log.warn(`Integration ${integrationId} is not a Github Nango integration!`)
     }
+  } else if (deletedIntegration && deletedIntegration.platform === PlatformType.GITHUB_NANGO) {
+    const settings = deletedIntegration.settings
+
+    if (settings.nangoMapping) {
+      const nangoMapping = settings.nangoMapping as Record<string, IGithubRepoData>
+      const connectionIds = Object.keys(nangoMapping)
+
+      for (const connectionId of connectionIds) {
+        reposToDelete.push({
+          repo: nangoMapping[connectionId],
+          connectionId,
+        })
+      }
+    }
+    svc.log.info(
+      `For deleted integration ${integrationId} found ${reposToDelete.length} connections to delete!`,
+    )
   } else {
     svc.log.warn(`Integration ${integrationId} not found!`)
   }
