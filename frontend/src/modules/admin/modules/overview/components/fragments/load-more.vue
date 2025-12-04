dcd5758d3640e7092cef2477d02623738220f406
Efren Lim <elim@linuxfoundation.org>
2025-12-04 13:53:55 +0800

    chore: added project filter
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

---
+++ frontend/src/modules/admin/modules/overview/components/fragments/load-more.vue
@@ -0,0 +1,87 @@
+<!--
+Copyright (c) 2025 The Linux Foundation and each contributor.
+SPDX-License-Identifier: MIT
+-->
+<template>
+  <div
+    ref="loadMore"
+    class="flex flex-row gap-2 items-center justify-center w-full"
+  >
+    <span>
+      <lf-spinner
+        size="16px"
+        class="text-brand-300"
+      />
+    </span>
+    <span class="text-sm text-brand-300 text-sm font-semibold">
+      {{ text }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, watch } from 'vue';
+import LfSpinner from '@/ui-kit/spinner/Spinner.vue';
+
+const emit = defineEmits<{ (e: 'loadMore'): void }>();
+
+const props = defineProps<{
+  text: string;
+  isFetchingNextPage: boolean;
+}>();
+
+const loadMore = ref(null);
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0,
+};
+
+const handleIntersectCallback = (entries: IntersectionObserverEntry[]) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      emit('loadMore');
+    }
+  });
+};
+
+const isLoadMoreVisible = () => {
+  if (!loadMore.value) {
+    return false;
+  }
+
+  return isElementVisible(loadMore.value as HTMLElement);
+};
+
+const isElementVisible = (element: HTMLElement) => {
+  const rect = element.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  return rect.top >= 0 && rect.bottom <= windowHeight;
+};
+
+onMounted(() => {
+  if (loadMore.value) {
+    const observer = new IntersectionObserver(handleIntersectCallback, options);
+    observer.observe(loadMore.value);
+  }
+});
+
+watch(
+  () => props.isFetchingNextPage,
+  (newVal: boolean) => {
+    if (!newVal) {
+      // check if the load more is visible
+      if (isLoadMoreVisible()) {
+        emit('loadMore');
+      }
+    }
+  },
+);
+</script>
+
+<script lang="ts">
+export default {
+  name: 'AppLfLoadMore',
+};
+</script>
---
+++ frontend/src/modules/admin/modules/overview/components/sections/overview-filter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center">
+    <app-lf-project-group-filter />
+    <app-lf-project-filter :projects="projects" v-if="selectedProjectGroup && projects.length > 0" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import AppLfProjectGroupFilter from '../fragments/project-group-filter.vue';
+import AppLfProjectFilter from '../fragments/project-filter.vue';
+import { useOverviewStore } from '../../store/overview.store';
+import { storeToRefs } from 'pinia';
+
+const overviewStore = useOverviewStore();
+const { selectedProjectGroup } = storeToRefs(overviewStore);
+
+const projects = computed(() => {
+  return selectedProjectGroup.value?.projects || [];
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: 'AppLfOverviewFilter',
+};
+</script>
