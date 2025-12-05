df239fb8be245617f9e95346e13e281d14b56dfb
Efren Lim <elim@linuxfoundation.org>
2025-12-05 15:31:46 +0800

    chore: changed integration row display to match integration page
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- frontend/src/modules/admin/modules/integration/pages/integration-status.page.vue
+++ frontend/src/modules/admin/modules/integration/pages/integration-status.page.vue
@@ -213,6 +213,7 @@ const fetchGlobalIntegrations = () => {
     status: lfIntegrationStatusesTabs[status.value].statuses,
   })
     .then((res) => {
+      console.log(res);
       if (res.offset > 0) {
         integrations.value = [...integrations.value, ...res.rows];
       } else {
