b5e021df7a6136e6e3255dc43d13cbd0b1c6f915
Mouad BANI <mouad-mb@outlook.com>
2025-12-10 13:14:41 +0100

    fix: path

--- frontend/src/modules/auth/auth.socket.ts
+++ frontend/src/modules/auth/auth.socket.ts
@@ -30,8 +30,16 @@ export const connectSocket = (token) => {
     socketIoClient.disconnect();
   }
 
+  const getSocketPath = () => {
+    const { env } = config;
+    const path = env === 'production' || env === 'staging' ? '/api/socket.io' : '/socket.io';
+    console.info(`Socket.IO connecting to: ${path}`);
+
+    return path;
+  };
+
   socketIoClient = io(`${config.websocketsUrl}/user`, {
-    path: `${config.env === 'production' || config.env === 'staging' ? '/api' : ''}/socket.io`,
+    path: getSocketPath(),
     query: {
       token,
     },
