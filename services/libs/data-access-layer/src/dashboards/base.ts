6730a32bfb757f026e99096a1fa54a2c3fcee67f
Umberto Sgueglia <ulemons92@gmail.com>
2025-12-12 15:01:27 +0100

    fix: remove mock

--- services/libs/data-access-layer/src/dashboards/base.ts
+++ services/libs/data-access-layer/src/dashboards/base.ts
@@ -7,46 +7,20 @@ export async function getMetrics(
   qx: QueryExecutor,
   segmentId?: string,
 ): Promise<IDashboardMetrics> {
-  try {
-    const [snapshotData, projectsData] = await Promise.all([
-      getSnapshotMetrics(qx, segmentId),
-      getSubProjectsCount(qx, segmentId),
-    ])
+  const [snapshotData, projectsData] = await Promise.all([
+    getSnapshotMetrics(qx, segmentId),
+    getSubProjectsCount(qx, segmentId),
+  ])
 
-    if (!snapshotData) {
-      // TODO: remove this mock once Tinybird sinks are available
-      const mockMetrics = getMockMetrics()
-      return {
-        ...mockMetrics,
-        projectsTotal: projectsData.projectsTotal,
-        projectsLast30Days: projectsData.projectsLast30Days,
-      }
-    }
-
-    return {
-      ...snapshotData,
-      projectsTotal: projectsData.projectsTotal,
-      projectsLast30Days: projectsData.projectsLast30Days,
-    }
-  } catch (error: unknown) {
-    const msg = error instanceof Error ? error.message : ''
-    const code = error && typeof error === 'object' && 'code' in error ? error.code : null
-
-    // Detect missing table
-    const isMissingTable = code === '42P01' || /does not exist/i.test(msg)
-
-    if (isMissingTable) {
-      // TODO: remove this mock once Tinybird sinks are available
-      const mockMetrics = getMockMetrics()
-      const projectsData = await getSubProjectsCount(qx, segmentId)
-      return {
-        ...mockMetrics,
-        projectsTotal: projectsData.projectsTotal,
-        projectsLast30Days: projectsData.projectsLast30Days,
-      }
-    }
-
-    throw error
+  return {
+    activitiesLast30Days: snapshotData?.activitiesLast30Days || 0,
+    activitiesTotal: snapshotData?.activitiesTotal || 0,
+    membersLast30Days: snapshotData?.membersLast30Days || 0,
+    membersTotal: snapshotData?.membersTotal || 0,
+    organizationsLast30Days: snapshotData?.organizationsLast30Days || 0,
+    organizationsTotal: snapshotData?.organizationsTotal || 0,
+    projectsLast30Days: projectsData.projectsLast30Days,
+    projectsTotal: projectsData.projectsTotal,
   }
 }
 
@@ -82,17 +56,4 @@ async function getSnapshotMetrics(
   const [row] = await qx.select(query, params)
 
   return row || null
-}
-
-function getMockMetrics(): IDashboardMetrics {
-  return {
-    activitiesTotal: 9926553,
-    activitiesLast30Days: 64329,
-    organizationsTotal: 104300,
-    organizationsLast30Days: 36,
-    membersTotal: 798730,
-    membersLast30Days: 2694,
-    projectsTotal: 123,
-    projectsLast30Days: 12312,
-  }
 }
