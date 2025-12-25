80f810287989cd80595977605812af42f40e5254
Mouad BANI <mouad-mb@outlook.com>
2025-12-25 14:20:26 +0100

    feat: db migration for new repositories table

---
+++ backend/src/database/migrations/V1766661879__createRepositoriesTable.sql
@@ -0,0 +1,27 @@
+CREATE TABLE public.repositories(
+	id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+	"url" VARCHAR(1024) NOT NULL UNIQUE,
+	"segmentId" UUID NOT NULL REFERENCES "segments"(id) ON DELETE CASCADE,
+	"gitIntegrationId" UUID NOT NULL REFERENCES public."integrations" (id) ON DELETE CASCADE,
+	"sourceIntegrationId" UUID NOT NULL REFERENCES public."integrations" (id) ON DELETE CASCADE,
+	"insightsProjectId" UUID REFERENCES "insightsProjects"(id) ON DELETE CASCADE,
+    "archived" BOOLEAN NOT NULL DEFAULT FALSE,
+	"forkedFrom" VARCHAR(1024) DEFAULT NULL,
+	"excluded" BOOLEAN NOT NULL DEFAULT FALSE,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "deletedAt" TIMESTAMP WITH TIME ZONE,
+	"lastArchivedCheckAt" TIMESTAMP WITH TIME ZONE
+);
+
+CREATE INDEX ix_repositories_segmentId 
+		ON repositories ("segmentId") 
+		WHERE "deletedAt" IS NULL;
+	
+CREATE INDEX ix_repositories_sourceIntegrationId 
+	ON repositories ("sourceIntegrationId") 
+	WHERE "deletedAt" IS NULL;
+
+CREATE INDEX ix_repositories_insightsProjectId 
+	ON repositories ("insightsProjectId") 
+	WHERE "deletedAt" IS NULL;
