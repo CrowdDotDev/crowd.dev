8bb4fcc4b369d49c18bcacc1be26b53433bfebb1
Yeganathan S <63534555+skwowet@users.noreply.github.com>
2026-01-06 16:01:22 +0530

    chore: add script to invalidate member query cache

---
+++ backend/src/bin/scripts/invalidate-member-query-cache.ts
@@ -0,0 +1,15 @@
+import { MemberQueryCache } from '@crowd/data-access-layer/src/members/queryCache'
+import { getServiceLogger } from '@crowd/logging'
+import { getRedisClient } from '@crowd/redis'
+
+import { REDIS_CONFIG } from '@/conf'
+
+const log = getServiceLogger()
+
+setImmediate(async () => {
+  const redis = await getRedisClient(REDIS_CONFIG, true)
+  const cache = new MemberQueryCache(redis)
+  await cache.invalidateAll()
+  log.info('Invalidated member query cache')
+  process.exit(0)
+})
