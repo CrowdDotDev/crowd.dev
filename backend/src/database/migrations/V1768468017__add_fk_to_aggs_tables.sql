ebcc02f6c857472e7a528d0a8294c86c2e54d260
Umberto Sgueglia <ulemons92@gmail.com>
2026-01-15 16:51:11 +0100

    fix: remove not valid from fks

--- backend/src/database/migrations/V1768468017__add_fk_to_aggs_tables.sql
+++ backend/src/database/migrations/V1768468017__add_fk_to_aggs_tables.sql
@@ -2,19 +2,10 @@ ALTER TABLE public."memberSegmentsAgg"
 ADD CONSTRAINT "memberSegmentsAgg_memberId_fkey"
 FOREIGN KEY ("memberId")
 REFERENCES public."members"(id)
-ON DELETE CASCADE
-NOT VALID;
+ON DELETE CASCADE;
 
 ALTER TABLE public."organizationSegmentsAgg"
 ADD CONSTRAINT "organizationSegmentsAgg_organizationId_fkey"
 FOREIGN KEY ("organizationId")
 REFERENCES public."organizations"(id)
-ON DELETE CASCADE
-NOT VALID;
-
--- Validate the foreign key constraints after adding them when the data is consistent
--- ALTER TABLE public."memberSegmentsAgg"
--- VALIDATE CONSTRAINT "memberSegmentsAgg_memberId_fkey";
-
--- ALTER TABLE public."organizationSegmentsAgg"
--- VALIDATE CONSTRAINT "organizationSegmentsAgg_organizationId_fkey";
+ON DELETE CASCADE;
