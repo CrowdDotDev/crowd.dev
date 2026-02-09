8b401d212b9d4553e1e63c1e80f9537d581d1233
Uros Marolt <uros@marolt.me>
2026-02-09 14:58:08 +0100

    chore: refactor some activities into a separate child workflow (CM-922) (#3831)
    Signed-off-by: Uroš Marolt <uros@marolt.me>

--- services/apps/nango_worker/src/workflows.ts
+++ services/apps/nango_worker/src/workflows.ts
@@ -1,4 +1,13 @@
+import { deleteDuplicateGithubConnection } from './workflows/deleteDuplicateGithubConnection'
+import { deleteGithubRepoConnection } from './workflows/deleteGithubRepoConnection'
 import { processNangoWebhook } from './workflows/processNangoWebhook'
 import { syncGithubIntegration } from './workflows/syncGithubIntegration'
+import { syncGithubRepo } from './workflows/syncGithubRepo'
 
-export { processNangoWebhook, syncGithubIntegration }
+export {
+  deleteDuplicateGithubConnection,
+  deleteGithubRepoConnection,
+  processNangoWebhook,
+  syncGithubIntegration,
+  syncGithubRepo,
+}
