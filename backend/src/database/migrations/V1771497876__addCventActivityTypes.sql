732f8bd143471db6ad94a7f4094da5a42fa9858b
Mouad BANI <mouad-mb@outlook.com>
2026-02-23 14:50:20 +0000

    fix: update activity description

--- backend/src/database/migrations/V1771497876__addCventActivityTypes.sql
+++ backend/src/database/migrations/V1771497876__addCventActivityTypes.sql
@@ -1,3 +1,3 @@
 INSERT INTO "activityTypes" ("activityType", platform, "isCodeContribution", "isCollaboration", description, "label") VALUES
-('registered-event', 'cvent', false, false, 'Registered for a Cvent event', 'Registered for an event'),
-('attended-event', 'cvent', false, false, 'Attended a Cvent event', 'Attended an event');
+('registered-event', 'cvent', false, false, 'User registers to an event', 'Registered for an event'),
+('attended-event', 'cvent', false, false, 'User attends an event', 'Attended an event');
