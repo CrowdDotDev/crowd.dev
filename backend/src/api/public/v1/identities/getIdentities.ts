49d0dabcbb1fe84163b54b86a85f72ce50789ae9
Yeganathan S <63534555+skwowet@users.noreply.github.com>
2026-02-23 17:41:53 +0530

    fix: include actor scopes in test endpoint
    Signed-off-by: Yeganathan S <63534555+skwowet@users.noreply.github.com>

--- backend/src/api/public/v1/identities/getIdentities.ts
+++ backend/src/api/public/v1/identities/getIdentities.ts
@@ -5,9 +5,6 @@ import type { ApiRequest } from '@/types/api'
 export async function getIdentities(req: ApiRequest, res: Response): Promise<void> {
   res.status(200).json({
     status: 'ok',
-    actor: {
-      id: req.actor.id,
-      type: req.actor.type,
-    },
+    actor: req.actor,
   })
 }
